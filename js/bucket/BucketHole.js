cd2708851322bde868505b5a65cc192446c53f1b
jbphet <john.blanco@colorado.edu>
2013-04-10 14:55:17 -0600

    Added ability to use a model-view transform.

--- js/bucket/BucketHole.js
+++ js/bucket/BucketHole.js
@@ -3,35 +3,35 @@
 /*
  * The hole of a bucket
  */
-define( function( require ) {
-  
+define( function ( require ) {
+
   var Inheritance = require( 'PHETCOMMON/util/Inheritance' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Path = require( 'SCENERY/nodes/Path' );
   var LinearGradient = require( 'SCENERY/util/LinearGradient' );
   var Shape = require( 'KITE/Shape' );
-  var Matrix3 = require( 'DOT/Matrix3' );
-  
-  var BucketHole = function( bucket ) {
+
+  var BucketHole = function ( bucket, mvt ) {
     Node.call( this );
-    
-    var gradientPaint = new LinearGradient( -bucket.size.width / 2, 0, bucket.size.width / 2, 0 );
+
+    var transformedShape = mvt.modelToViewShape( bucket.holeShape );
+    var gradientPaint = new LinearGradient( transformedShape.bounds.getMinX(), 0, transformedShape.bounds.getMaxX(), 0 );
     gradientPaint.addColorStop( 0, 'black' );
     gradientPaint.addColorStop( 1, '#c0c0c0' );
-    
+
     this.addChild( new Path( {
-      shape: bucket.holeShape,
-      // shape: bucket.holeShape.transformed( Matrix3.Y_REFLECTION ),
-      fill: gradientPaint,
-      stroke: '#777',
-      lineWidth: 1
-    } ) );
-    
-    this.x = bucket.position.x;
-    this.y = bucket.position.y;
+                               shape: transformedShape,
+                               fill: gradientPaint,
+                               stroke: '#777',
+                               lineWidth: 1
+                             } ) );
+
+    // Set initial position.
+    this.x = mvt.modelToViewPosition( bucket.position ).x;
+    this.y = mvt.modelToViewPosition( bucket.position ).y;
   };
-  
+
   Inheritance.inheritPrototype( BucketHole, Node );
-  
+
   return BucketHole;
 } );
