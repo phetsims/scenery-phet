d2eb597432f64adbb719b3b55445d9cbe2f80b5d
Sam Reid <reids@colorado.edu>
2013-04-17 21:09:37 -0600

    Improvements to the home screen

--- js/HomeScreen.js
+++ js/HomeScreen.js
@@ -7,40 +7,79 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var Layout = require( 'SCENERY_PHET/Layout' );
 
-  var HEIGHT = 120;
+  var HEIGHT = 110;
 
   function HomeScreen( title, tabs, model ) {
+    var homeScreen = this;
     Node.call( this );
 
     //iPad doesn't support Century Gothic, so fall back to Futura, see http://wordpress.org/support/topic/font-not-working-on-ipad-browser
-    this.textLabel = new Text( title, {fontSize: 42, fontFamily: 'Century Gothic, Futura', fill: 'white', y: 100, centerX: 981 / 2} );
-    this.phetLabel = new Text( "PhET", {fontSize: 24, fill: 'yellow', top: 5, left: 5} );
+    this.textLabel = new Text( title, {fontSize: 52, fontFamily: 'Century Gothic, Futura', fill: 'white', y: 140, centerX: 981 / 2} );
+    this.phetLabel = new Text( "PhET", {fontSize: 36, fill: 'yellow', top: 5, left: 5} );
     this.addChild( this.textLabel );
     this.addChild( this.phetLabel );
-
-    var textLabels = _.map( tabs, function( tab ) {
-      return new Text( tab.name, {fontSize: 24, fill: 'white'} ).mutate( {centerY: HEIGHT / 2} );
-    } );
 
     var index = 0;
     var tabChildren = _.map( tabs, function( tab ) {
       tab.index = index++;
       var child = new Node( {children: [tab.icon]} );
+      child.smallTextLabel = new Text( tab.name, {fontSize: 36, fill: 'gray'} );
+      child.largeTextLabel = new Text( tab.name, {fontSize: 52, fill: 'yellow'} );
+      homeScreen.addChild( child.smallTextLabel );
+      homeScreen.addChild( child.largeTextLabel );
       child.scale( HEIGHT / tab.icon.height );
       child.cursor = 'pointer';
-      model.link( 'tab', function( t ) {
-        child.invalidateBounds();
-        child.opacity = t === tab.index ? 1 : 0.5;
-      } );
+      child.tab = tab;
+
+      //Tap once to select, a second time to start that tab
       child.addInputListener( { down: function() {
-        model.tab = tab.index;
-        model.home = false;
+        if ( model.tab === tab.index ) {
+          model.home = false;
+        }
+        else {
+          model.tab = tab.index;
+        }
       }} );
       return child;
     } );
 
-    this.tabsNode = new HBox( {children: tabChildren, spacing: 3} ).mutate( {centerX: Layout.width / 2, centerY: Layout.height / 2} );
-    this.addChild( this.tabsNode );
+    for ( var i = 0; i < tabChildren.length; i++ ) {
+      this.addChild( tabChildren[i] );
+    }
+
+    model.link( 'tab', function( tab ) {
+      for ( var i = 0; i < tabChildren.length; i++ ) {
+        var child = tabChildren[i];
+        child.invalidateBounds();
+        var selected = tab === child.tab.index;
+        child.selected = selected;
+        child.opacity = selected ? 1 : 0.5;
+        child.resetTransform();
+        child.scale( selected ? HEIGHT / child.tab.icon.height * 2 : HEIGHT / child.tab.icon.height );
+      }
+
+      var width = 0;
+      for ( var i = 0; i < tabChildren.length; i++ ) {
+        var child = tabChildren[i];
+        width = width + child.width;
+      }
+      var spacing = 41 / 1.25;
+      width = width + spacing * (tabChildren.length - 1);
+
+      var x = Layout.width / 2 - width / 2;
+
+      for ( var i = 0; i < tabChildren.length; i++ ) {
+        var child = tabChildren[i];
+        child.x = x;
+        child.y = Layout.height / 2 - 111 / 1.25;
+        x += child.width + spacing;
+        child.largeTextLabel.visible = child.selected;
+        child.smallTextLabel.visible = !child.selected;
+        var label = child.selected ? child.largeTextLabel : child.smallTextLabel;
+        label.top = child.bottom + 20;
+        label.x = child.x;
+      }
+    } );
   }
 
   inherit( HomeScreen, Node );
