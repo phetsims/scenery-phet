48c8909329316224186663d2ce5f538bb89cf08c
Sam Reid <reids@colorado.edu>
2013-07-08 20:58:14 -0600

    Refactored ArrowNode to make createArrowShape static and put the shape in the default options

--- js/ArrowNode.js
+++ js/ArrowNode.js
@@ -23,17 +23,19 @@ define( function( require ) {
    * @param {number} headHeight
    * @param {number} headWidth
    * @param {number} tailWidth
+   * @param {Object} options
    * @constructor
    */
   function ArrowNode( tailX, tailY, tipX, tipY, headHeight, headWidth, tailWidth, options ) {
 
     // default options
     options = _.extend(
-        {
-          fill: 'black',
-          stroke: 'black',
-          lineWidth: 1
-        }, options );
+      {
+        fill: 'black',
+        stroke: 'black',
+        lineWidth: 1,
+        shape: ArrowNode.createArrowShape( tailX, tailY, tipX, tipY, tailWidth, headWidth, headHeight )
+      }, options );
 
     // things you're likely to mess up, add more as needed
     assert && assert( headWidth >= tailWidth );
@@ -41,53 +43,56 @@ define( function( require ) {
     // Call super constructor.
     Node.call( this, options );
 
-    // Function for creating arrow shape.  All parameters are of type number.
-    var createArrowShape = function( tailX, tailY, tipX, tipY, tailWidth, headWidth, headHeight ) {
-      var arrowShape = new Shape();
-      if ( tipX === tailX && tipY === tailY ) {
-        return arrowShape;
-      }
-      var vector = new Vector2( tipX - tailX, tipY - tailY );
-      var xHatUnit = vector.normalized();
-      var yHatUnit = xHatUnit.rotated( Math.PI / 2 );
-      var length = vector.magnitude();
-
-      //Set up a coordinate frame that goes from the tail of the arrow to the tip.
-      function getPoint( xHat, yHat ) {
-        var x = xHatUnit.x * xHat + yHatUnit.x * yHat + tailX;
-        var y = xHatUnit.y * xHat + yHatUnit.y * yHat + tailY;
-        return new Vector2( x, y );
-      }
-
-      if ( headHeight > length / 2 ) {
-        headHeight = length / 2;
-      }
-
-      var tailLength = length - headHeight;
-      var points = [
-        getPoint( 0, tailWidth / 2 ),
-        getPoint( tailLength, tailWidth / 2 ),
-        getPoint( tailLength, headWidth / 2 ),
-        getPoint( length, 0 ),
-        getPoint( tailLength, -headWidth / 2 ),
-        getPoint( tailLength, -tailWidth / 2 ),
-        getPoint( 0, -tailWidth / 2 )
-      ];
-
-      arrowShape.moveTo( points[0].x, points[0].y );
-      var tail = _.tail( points );
-      _.each( tail, function( element ) { arrowShape.lineTo( element.x, element.y ); } );
-      arrowShape.close();
-
-      return arrowShape;
-    };
-
     this.path = new Path( options );
-    this.path.shape = createArrowShape( tailX, tailY, tipX, tipY, tailWidth, headWidth, headHeight );
     this.addChild( this.path );
   }
 
-  inherit( Node, ArrowNode );
+  return inherit( Node, ArrowNode,
 
-  return ArrowNode;
+    //Instance methods & fields
+    {},
+
+    //Static methods & fields
+    {
+      // Function for creating arrow shape.  All parameters are of type number.
+      createArrowShape: function( tailX, tailY, tipX, tipY, tailWidth, headWidth, headHeight ) {
+        var arrowShape = new Shape();
+        if ( tipX === tailX && tipY === tailY ) {
+          return arrowShape;
+        }
+        var vector = new Vector2( tipX - tailX, tipY - tailY );
+        var xHatUnit = vector.normalized();
+        var yHatUnit = xHatUnit.rotated( Math.PI / 2 );
+        var length = vector.magnitude();
+
+        //Set up a coordinate frame that goes from the tail of the arrow to the tip.
+        function getPoint( xHat, yHat ) {
+          var x = xHatUnit.x * xHat + yHatUnit.x * yHat + tailX;
+          var y = xHatUnit.y * xHat + yHatUnit.y * yHat + tailY;
+          return new Vector2( x, y );
+        }
+
+        if ( headHeight > length / 2 ) {
+          headHeight = length / 2;
+        }
+
+        var tailLength = length - headHeight;
+        var points = [
+          getPoint( 0, tailWidth / 2 ),
+          getPoint( tailLength, tailWidth / 2 ),
+          getPoint( tailLength, headWidth / 2 ),
+          getPoint( length, 0 ),
+          getPoint( tailLength, -headWidth / 2 ),
+          getPoint( tailLength, -tailWidth / 2 ),
+          getPoint( 0, -tailWidth / 2 )
+        ];
+
+        arrowShape.moveTo( points[0].x, points[0].y );
+        var tail = _.tail( points );
+        _.each( tail, function( element ) { arrowShape.lineTo( element.x, element.y ); } );
+        arrowShape.close();
+
+        return arrowShape;
+      }
+    } );
 } );
