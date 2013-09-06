a48fe2b0c6e04d729402b99e3c02e95e3e34785d
Chris Malley <cmalley@pixelzoom.com>
2013-09-05 22:05:46 -0600

    rewrite ShadedSphereNode so that interface and look matches the Java implementation, to simplify porting

--- js/ShadedSphereNode.js
+++ js/ShadedSphereNode.js
@@ -9,7 +9,6 @@ define( function( require ) {
   'use strict';
 
   // imports
-  var assert = require( 'ASSERT/assert' )( 'scenery-phet' );
   var Circle = require( 'SCENERY/nodes/Circle' );
   var RadialGradient = require( 'SCENERY/util/RadialGradient' );
   var inherit = require( 'PHET_CORE/inherit' );
@@ -17,23 +16,20 @@ define( function( require ) {
   function ShadedSphereNode( diameter, options ) {
 
     options = _.extend( {
-      outerColor: 'gray',
+      mainColor: 'gray',
       highlightColor: 'white',
-      highlightXOffset: -0.4, // x-offset of the highlight from the center of the sphere, percentage of radius, [-1,1]
-      highlightYOffset: -0.4  // y-offset of the highlight from the center of the sphere, percentage of radius, [-1,1]
+      shadowColor: 'black'
     }, options );
-
-    // highlight must be inside the sphere
-    assert && assert( Math.abs( options.highlightXOffset ) <= 1 && Math.abs( options.highlightYOffset ) <= 1 );
 
     var radius = diameter / 2;
     options.fill = new RadialGradient(
-      radius * options.highlightXOffset, radius * options.highlightYOffset, 0,
-      radius * options.highlightXOffset, radius * options.highlightYOffset, 2 * radius )
-      .addColorStop( 0, options.highlightColor )
-      .addColorStop( 1, options.outerColor );
+          radius * -0.4, radius * -0.4, 0,
+          radius * -0.4, radius * -0.4, diameter )
+          .addColorStop( 0, options.highlightColor )
+          .addColorStop( 0.5, options.mainColor )
+          .addColorStop( 1, options.shadowColor );
 
-    Circle.call( this, radius, options ); //TODO remove subtype-specific options before passing to supertype
+    Circle.call( this, radius, options );
   }
 
   return inherit( Circle, ShadedSphereNode );
