c3032ea778051385329655dc1ccda0886ec83af3
Jonathan Olson <olsonsjc@gmail.com>
2013-09-10 14:46:56 -0600

    Adding NextPreviousNavigationNode

---
+++ js/NextPreviousNavigationNode.js
@@ -0,0 +1,135 @@
+// Copyright 2002-2013, University of Colorado Boulder
+
+/**
+ * Shows a central node surrounded with next/previous arrows. Need to implement next(),previous(),
+ * and when availability changes modify hasNext and hasPrevious
+ *
+ * @author Jonathan Olson <olsonsjc@gmail.com>
+ */
+define( function( require ) {
+  'use strict';
+
+  // imports
+  var assert = require( 'ASSERT/assert' )( 'scenery-phet' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var extend = require( 'PHET_CORE/extend' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Path = require( 'SCENERY/nodes/Path' );
+  var Color = require( 'SCENERY/util/Color' );
+  var Shape = require( 'KITE/Shape' );
+  var PropertySet = require( 'AXON/PropertySet' );
+  var ButtonListener = require( 'SCENERY/input/ButtonListener' );
+  
+  var arrowPadding = 8;
+  
+  /*
+   * @param {Node} centerNode
+   * @param {Object} selfOptions  Valid options are:
+   *                                arrowColor      - color for the arrow's fill
+   *                                arrowStrokColor - color for the arrow's stroke
+   *                                arroWidth       - the width of the arrow, from its point to its side
+   *                                arrowHeight     - the height of the arrow, from tip to tip
+   *                                next            - a function to be called when the "next" arrow is pressed
+   *                                previous        - a function to be called when the "previous" arrow is pressed
+   * @param {Object} nodeOptions  Passed to the Node constructor
+   */
+  function NextPreviousNavigationNode( centerNode, selfOptions, nodeOptions ) {
+    var self = this;
+    
+    PropertySet.call( this, {
+      hasNext: false,
+      hasPrevious: false
+    } );
+    Node.call( this, {} );
+    
+    selfOptions = _.extend( {
+      arrowColor: Color.YELLOW,
+      arrowStrokeColor: Color.BLACK,
+      arrowWidth: 14,
+      arrowHeight: 18,
+      next: null, // function() { ... }
+      previous: null // function() { ... }
+    }, selfOptions );
+    
+    var arrowWidth = selfOptions.arrowWidth;
+    var arrowHeight = selfOptions.arrowHeight;
+
+    /*---------------------------------------------------------------------------*
+    * previous
+    *----------------------------------------------------------------------------*/
+    
+    // triangle pointing to the left
+    var previousShape = new Shape().moveTo( 0, arrowHeight / 2 )
+                                   .lineTo( arrowWidth, 0 )
+                                   .lineTo( arrowWidth, arrowHeight )
+                                   .close();
+    
+    var previousKitNode = new Path( previousShape, {
+      fill: selfOptions.arrowColor,
+      stroke: selfOptions.arrowStrokeColor,
+      cursor: 'pointer' // TODO: buttonListener adds this maybe?
+    } );
+    previousKitNode.addInputListener( new ButtonListener( {
+      fire: function( evt ) {
+        if ( self.hasPrevious ) {
+          selfOptions.previous && selfOptions.previous();
+        }
+      }
+    } ) );
+    this.hasPreviousProperty.link( function( available ) {
+      previousKitNode.visible = available;
+    } );
+    
+    this.addChild( previousKitNode );
+
+    /*---------------------------------------------------------------------------*
+    * center
+    *----------------------------------------------------------------------------*/
+
+    this.addChild( centerNode );
+
+    /*---------------------------------------------------------------------------*
+    * next
+    *----------------------------------------------------------------------------*/
+    
+    // triangle pointing to the right
+    var nextShape = new Shape().moveTo( arrowWidth, arrowHeight / 2 )
+                               .lineTo( 0, 0 )
+                               .lineTo( 0, arrowHeight )
+                               .close();
+                               
+    var nextKitNode = new Path( nextShape, {
+      fill: selfOptions.arrowColor,
+      stroke: selfOptions.arrowStrokeColor,
+      cursor: 'pointer' // TODO: buttonListener adds this maybe?
+    } );
+    nextKitNode.addInputListener( new ButtonListener( {
+      fire: function( evt ) {
+        if ( self.hasNext ) {
+          selfOptions.next && selfOptions.next();
+        }
+      }
+    } ) );
+    this.hasNextProperty.link( function( available ) {
+      nextKitNode.visible = available;
+    } );
+    
+    this.addChild( nextKitNode );
+
+    /*---------------------------------------------------------------------------*
+    * positioning
+    *----------------------------------------------------------------------------*/
+
+    var maxHeight = Math.max( arrowHeight, centerNode.height );
+    
+    previousKitNode.setTranslation( 0, ( maxHeight - arrowHeight ) / 2 );
+    centerNode.setTranslation( arrowWidth + arrowPadding, ( maxHeight - centerNode.height ) / 2 );
+    nextKitNode.setTranslation( centerNode.right + arrowPadding, ( maxHeight - arrowHeight ) / 2 );
+    
+    Node.prototype.mutate.call( this, nodeOptions );
+  }
+
+  inherit( PropertySet, NextPreviousNavigationNode, extend( {}, Node.prototype ) );
+
+  return NextPreviousNavigationNode;
+} );
