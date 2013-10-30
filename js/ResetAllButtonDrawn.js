757d28508aae62fd5e3f2a8e0f11ac2be8b3e7b6
jbphet <john.blanco@colorado.edu>
2013-10-29 17:08:31 -0700

    Added the pushbutton gradient, adjusted other gradients.

--- js/ResetAllButtonDrawn.js
+++ js/ResetAllButtonDrawn.js
@@ -2,6 +2,8 @@
 
 /**
  * Reset All button.
+ *
+ * @author John Blanco
  */
 define( function( require ) {
   'use strict';
@@ -57,28 +59,42 @@ define( function( require ) {
     curvedArrowShape.arc( 0, 0, innerRadius, neckAngle, startAngle ); // Inner curve.
     curvedArrowShape.close();
 
-    // Local convenience function for creating gradients to use on buttons.
+    // Local convenience functions for creating gradients to use on buttons.
     var createButtonFillGradient = function( baseColor ) {
-      var buttonGradient = new RadialGradient( -options.radius * 0.4, -options.radius * 0.4, 0, -options.radius * 0.4, -options.radius * 0.4, options.radius * 1.6 );
-      buttonGradient.addColorStop( 0, baseColor.colorUtilsBrighter( 0.4 ) );
-      buttonGradient.addColorStop( 1, baseColor );
+      var buttonGradient = new RadialGradient( options.radius * 0.1, options.radius * 0.1, options.radius * 0.8, 0, 0, options.radius );
+      buttonGradient.addColorStop( 0, baseColor );
+      buttonGradient.addColorStop( 0.8, baseColor.colorUtilsBrighter( 0.7 ) );
+      buttonGradient.addColorStop( 1, 'white' );
       return buttonGradient;
-    }
+    };
+    var createPushedButtonGradient = function( baseColor ) {
+      var buttonGradient = new RadialGradient( 0, 0, options.radius * 0.5, 0, 0, options.radius );
+      buttonGradient.addColorStop( 0, baseColor );
+      buttonGradient.addColorStop( 0.6, baseColor.colorUtilsDarker( 0.1 ) );
+      buttonGradient.addColorStop( 0.8, baseColor );
+      return buttonGradient;
+    };
 
     var outlineGradient = new RadialGradient( -options.radius * 0.4, -options.radius * 0.4, 0, -options.radius * 0.4, -options.radius * 0.4, options.radius * 1.6 );
     outlineGradient.addColorStop( 0, 'rgb( 230, 230, 230 )' );
     outlineGradient.addColorStop( 1, 'black' );
 
+    // Create the nodes for each of the button states.
     var upNode = new Circle( options.radius, { fill: createButtonFillGradient( new Color( 247, 151, 34 ) ), stroke: outlineGradient, lineWidth: 2 } );
     upNode.addChild( new Path( curvedArrowShape, { fill: 'white' } ) );
     var overNode = new Circle( options.radius, { fill: createButtonFillGradient( new Color( 251, 171, 39 ) ), stroke: outlineGradient, lineWidth: 2 } );
     overNode.addChild( new Path( curvedArrowShape, { fill: 'white' } ) );
-    var downNode = new Circle( options.radius, { fill: 'rgb( 235, 141, 24 )', stroke: outlineGradient, lineWidth: 2 } );
-    downNode.addChild( new Path( curvedArrowShape, { fill: 'white' } ) );
+    var downNode = new Circle( options.radius, { fill: createPushedButtonGradient( new Color( 235, 141, 24 ) ), stroke: outlineGradient, lineWidth: 2 } );
+    var downNodeArrow = new Path( curvedArrowShape, { fill: 'white' } );
+    downNodeArrow.translate( -options.radius * 0.03, -options.radius * 0.03 );
+    downNode.addChild( downNodeArrow );
     var disabledNode = new Circle( options.radius, { fill: createButtonFillGradient( new Color( 220, 220, 220 ) ), stroke: outlineGradient, lineWidth: 2 } );
     disabledNode.addChild( new Path( curvedArrowShape, { fill: 'rgb( 240, 240, 240 )' } ) );
 
+    // Create the actual button by invoking the parent type.
     PushButton.call( this, upNode, overNode, downNode, disabledNode, callback, options );
+
+    // Expend the touch area so that the button works better on touch devices.
     this.touchArea = Shape.circle( 0, 0, options.touchAreaRadius );
   }
 
