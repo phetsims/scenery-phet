5dbde8b9011da1d84f0a9cfb8d8ca7c716f53b58
jbphet <john.blanco@colorado.edu>
2013-11-12 15:27:44 -0800

    Added an 'x' to indicate when the sound is off, see #20.

--- js/SoundToggleButton.js
+++ js/SoundToggleButton.js
@@ -5,18 +5,35 @@
  *
  * @author Chris Malley (PixelZoom, Inc.)
  * @author Sam Reid
+ * @author John Blanco
  */
 define( function( require ) {
   'use strict';
 
   var FontAwesomeNode = require( 'SUN/FontAwesomeNode' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Path = require( 'SCENERY/nodes/Path' );
+  var Shape = require( 'KITE/Shape' );
   var ToggleButton = require( 'SUN/ToggleButton' );
 
+  var X_WIDTH = 12; // Empirically determined.
+
   function SoundToggleButton( property, options ) {
+    var soundOffNode = new Node();
+    soundOffNode.addChild( new FontAwesomeNode( 'volume_off' ) );
+    var soundOffX = new Path( new Shape().moveTo( 0, 0 ).lineTo( X_WIDTH, X_WIDTH ).moveTo( 0, X_WIDTH ).lineTo( X_WIDTH, 0 ),
+      {
+        stroke: 'black',
+        lineWidth: 3,
+        left: soundOffNode.width + 5,
+        centerY: soundOffNode.centerY
+      } );
+    soundOffNode.addChild( soundOffX );
+
     ToggleButton.call( this,
       new FontAwesomeNode( 'volume_up' ),
-      new FontAwesomeNode( 'volume_off' ),
+      soundOffNode,
       property,
       _.extend( { addRectangle: true, label: 'Sound' }, options ) );
   }
