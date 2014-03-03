229f3c39d2eee64cbd5ff9792b8c9557e05beddc
Chris Malley <cmalley@pixelzoom.com>
2014-03-03 10:06:07 -0700

    generalize ph-scale.ZoomButton and migrate to scenery-phet

---
+++ js/ZoomButton.js
@@ -0,0 +1,54 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ * Zoom button, has an icon with a magnifying glass, with either a plus or minus sign in the center of the glass.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define( function( require ) {
+  'use strict';
+
+  // imports
+  var Dimension2 = require( 'DOT/Dimension2' );
+  var Circle = require( 'SCENERY/nodes/Circle' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var MinusNode = require( 'SCENERY_PHET/MinusNode' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var PlusNode = require( 'SCENERY_PHET/PlusNode' );
+  var Line = require( 'SCENERY/nodes/Line' );
+  var RectanglePushButton = require( 'SUN/RectanglePushButton' );
+
+  function ZoomButton( options ) {
+
+    options = _.extend( {
+      in: true, // true: zoom-in button, false: zoom-out button
+      radius: 15,
+      magnifyingGlassFill: 'white', // center of the glass
+      magnifyingGlassStroke: 'black', // rim and handle
+      magnifyingGlassStrokeDisabled: 'rgb(128,128,128)' // rim and handle when disabled
+    }, options );
+
+    // the magnifying glass
+    var glassNode = new Circle( options.radius, { fill: options.magnifyingGlassFill, stroke: options.magnifyingGlassStroke, lineWidth: options.radius / 4 } );
+
+    // handle at lower-left of glass, at a 45-degree angle
+    var handleNode = new Line( options.radius * Math.cos( Math.PI / 4 ), options.radius * Math.sin( Math.PI / 4 ),
+      options.radius * Math.cos( Math.PI / 4 ) + ( 0.65 * options.radius ), options.radius * Math.sin( Math.PI / 4 ) + ( 0.65 * options.radius ),
+      { stroke: options.magnifyingGlassStroke, lineWidth: 0.4 * options.radius, lineCap: 'round' } );
+
+    // plus or minus sign in middle of magnifying glass
+    var signOptions = { size: new Dimension2( 1.3 * options.radius, options.radius / 3 ), centerX: glassNode.centerX, centerY: glassNode.centerY };
+    var signNode = options.in ? new PlusNode( signOptions ) : new MinusNode( signOptions );
+
+    RectanglePushButton.call( this, new Node( { children: [ handleNode, glassNode, signNode ] } ) );
+
+    this.getEnabledProperty().link( function( enabled ) {
+      glassNode.stroke = handleNode.stroke = signNode.fill = enabled ? options.magnifyingGlassStroke : options.magnifyingGlassStrokeDisabled;
+      glassNode.fill = enabled ? options.magnifyingGlassFill : null;
+    } );
+
+    this.mutate( options );
+  }
+
+  return inherit( RectanglePushButton, ZoomButton );
+} );
