aa3d1248a9ee306a5d2e1c10633300fed93afd13
jbphet <john.blanco@colorado.edu>
2014-03-14 11:58:03 -0700

    Moved KitControlNode and KitSelectionNode from the Balancing Act simulation to the scenery-phet repo.

---
+++ js/KitControlNode.js
@@ -0,0 +1,87 @@
+// Copyright 2002-2013, University of Colorado Boulder
+
+/**
+ * A scenery node that is used to control which kit is selected within a kit
+ * selection carousel node.  It contains the back and forward buttons and,
+ * optionally, a title node.
+ *
+ * @author John Blanco
+ * @author Sam Reid
+ */
+
+define( function( require ) {
+  'use strict';
+
+  // Imports
+  var Color = require( 'SCENERY/util/Color' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var RoundShinyButton = require( 'SCENERY_PHET/RoundShinyButton' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Path = require( 'SCENERY/nodes/Path' );
+  var Shape = require( 'KITE/Shape' );
+
+  /**
+   * @param {number} numKits
+   * @param {Property} selectedKit - A property that tracks the selected kit as an integer
+   * @param {Object} options
+   * @constructor
+   */
+  function KitControlNode( numKits, selectedKit, options ) {
+    Node.call( this );
+    options = _.extend(
+      {
+        // Default values
+        titleNode: null,
+        inset: 5,
+        buttonColor: 'rgb( 255, 184, 77 )',
+        minButtonXSpace: 30
+      }, options );
+
+    var baseColor = new Color( 255, 204, 0 );
+    var buttonOptions = {
+      radius: 12,
+      upFill: baseColor,
+      overFill: baseColor.colorUtilsBrighter( 0.2 ),
+      disabledFill: new Color( 180, 180, 180 ),
+      downFill: baseColor.colorUtilsDarker( 0.2 ),
+      xMargin: 5,
+      yMargin: 3
+    };
+
+    var iconOptions = { stroke: 'black', lineWidth: 3, lineCap: 'round' };
+    var nextIcon = new Path( new Shape().moveTo( 0, 0 ).lineTo( 5, 5 ).lineTo( 0, 10 ), iconOptions );
+    var previousIcon = new Path( new Shape().moveTo( 0, 0 ).lineTo( -5, 5 ).lineTo( 0, 10 ), iconOptions );
+
+    var nextKitButton = new RoundShinyButton( function() {
+      selectedKit.value = selectedKit.value + 1;
+    }, nextIcon, _.extend( { iconOffsetX: 1 }, buttonOptions ) );
+    this.addChild( nextKitButton );
+
+    var previousKitButton = new RoundShinyButton( function() {
+      selectedKit.value = selectedKit.value - 1;
+    }, previousIcon, _.extend( { iconOffsetX: -1 }, buttonOptions ) );
+    this.addChild( previousKitButton );
+
+    // Control button enabled state
+    selectedKit.link( function( kitNum ) {
+      nextKitButton.enabled = ( kitNum < numKits - 1 );
+      previousKitButton.enabled = ( kitNum !== 0 );
+    } );
+
+    // Layout
+    var interButtonXSpace = Math.max( options.minButtonXSpace, 2 * options.inset + ( options.titleNode === null ? 0 : options.titleNode.width ) );
+    nextKitButton.left = previousKitButton.right + interButtonXSpace;
+    if ( options.titleNode !== null ) {
+      this.addChild( options.titleNode, { centerX: ( previousKitButton.centerX + nextKitButton.centerX ) / 2 } );
+    }
+
+    // If there is only one kit, show the title but not the control buttons.
+    // Leave the buttons in the scene graph for keeping layout consistent.
+    previousKitButton.visible = numKits > 1;
+    nextKitButton.visible = numKits > 1;
+
+    this.mutate( options );
+  }
+
+  return inherit( Node, KitControlNode );
+} );
---
+++ js/KitSelectionNode.js
@@ -0,0 +1,122 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ * Node for showing and scrolling between kits.
+ *
+ * @author Sam Reid
+ * @author John Blanco
+ */
+
+define( function( require ) {
+  'use strict';
+
+  // Imports
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Dimension2 = require( 'DOT/Dimension2' );
+  var KitControlNode = require( 'SCENERY_PHET/KitControlNode' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  var Shape = require( 'KITE/Shape' );
+  var Timer = require( 'JOIST/Timer' );
+
+  // Constants
+  var SLOT_CHANGE_TIME = 0.35; // In seconds
+
+  /**
+   * @param {Property} selectedKit
+   * @param {Array} kits
+   * @param {Object} options
+   * @constructor
+   */
+  function KitSelectionNode( selectedKit, kits, options ) {
+    Node.call( this );
+    var thisNode = this;
+
+    options = _.extend(
+      {
+        // Defaults
+        titleNode: null
+      }, options
+    );
+
+    // Make the selected kit property visible externally.
+    thisNode.selectedKit = selectedKit;
+
+    // Determine the max size of all the kit contents for layout purposes.
+    var maxKitContentSize = new Dimension2( 0, 0 );
+    var maxKitTitleSize = new Dimension2( 0, 0 );
+    kits.forEach( function( kit ) {
+      maxKitContentSize.width = Math.max( maxKitContentSize.width, kit.content.width );
+      maxKitContentSize.height = Math.max( maxKitContentSize.height, kit.content.height );
+      maxKitTitleSize.width = Math.max( maxKitTitleSize.width, kit.title.width );
+      maxKitTitleSize.height = Math.max( maxKitTitleSize.height, kit.title.height );
+    } );
+
+    var controlNode = new KitControlNode( kits.length, selectedKit, { titleNode: options.titleNode, minButtonXSpace: 70 } );
+
+    // Construct and add the background.  Make it big enough to hold the largest kit.
+    thisNode.selectorSize = new Dimension2( Math.max( Math.max( maxKitContentSize.width, maxKitTitleSize.width ), controlNode.width ),
+      controlNode.height + maxKitContentSize.height + maxKitTitleSize.height );
+
+    // Create the layer that contains all the kits, and add the kits side by
+    // side spaced by the distance of the background so only 1 kit will be
+    // visible at a time.
+    thisNode.kitLayer = new Node();
+
+    // Add the kits to the kit layer, spacing them out so they don't overlap.
+    var x = 0;
+    kits.forEach( function( kit ) {
+      // Put the title centered at the top and the content node centered in the
+      // available space beneath.
+      if ( kit.title ) {
+        kit.title.centerX = x + thisNode.selectorSize.width / 2;
+        kit.title.top = 0;
+        thisNode.kitLayer.addChild( kit.title );
+      }
+
+      kit.content.centerX = x + thisNode.selectorSize.width / 2;
+      kit.content.centerY = kit.title.bottom + maxKitContentSize.height / 2;
+      thisNode.kitLayer.addChild( kit.content );
+
+      // Move over to the next kit
+      x += thisNode.selectorSize.width;
+    } );
+
+    // Clip the kits so that the unselected ones are invisible.
+    thisNode.clipArea = new Shape.rect( 0, 0, thisNode.selectorSize.width, thisNode.selectorSize.height );
+
+    // Add the remaining nodes.
+    thisNode.kitLayer.top = controlNode.height;
+    thisNode.addChild( thisNode.kitLayer );
+    thisNode.addChild( controlNode.mutate( { top: 0, centerX: thisNode.selectorSize.width / 2 } ) );
+
+    // Set up an observer to set visibility of the selected kit.
+    selectedKit.link( function( kit ) {
+      thisNode.scrollTo( kit );
+    } );
+
+    // Set up the timer and function that will animate the carousel position.
+    var motionVelocity = thisNode.selectorSize.width / SLOT_CHANGE_TIME;
+    thisNode.kitLayerTargetX = thisNode.kitLayer.x;
+    Timer.addStepListener( function( dt ) {
+      if ( thisNode.kitLayer.x !== thisNode.kitLayerTargetX ) {
+        var dx = dt * motionVelocity * ( thisNode.kitLayerTargetX < thisNode.kitLayer.x ? -1 : 1 );
+        if ( Math.abs( thisNode.kitLayer.x - thisNode.kitLayerTargetX ) <= Math.abs( dx ) ) {
+          thisNode.kitLayer.x = thisNode.kitLayerTargetX;
+        }
+        else {
+          thisNode.kitLayer.x += dx;
+        }
+      }
+    } );
+
+    // Pass through any options intended for Node.
+    thisNode.mutate( options );
+  }
+
+  return inherit( Node, KitSelectionNode, {
+    scrollTo: function( kitNumber ) {
+      this.kitLayerTargetX = -kitNumber * this.selectorSize.width;
+    }
+  } );
+} );
