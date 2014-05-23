45cebfcab8cde45f0453dea682141f9a2b5012ea
Sam Reid <reids@colorado.edu>
2014-05-22 23:32:30 -0600

    refactored star geometry

--- js/StarNode.js
+++ js/StarNode.js
@@ -13,6 +13,7 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var Path = require( 'SCENERY/nodes/Path' );
   var Shape = require( 'KITE/Shape' );
+  var Vector2 = require( 'DOT/Vector2' );
 
   /**
    * @param options
@@ -33,11 +34,11 @@ define( function( require ) {
 
       fill: '#fcff03', // a bold yellow indeed,
       stroke: 'black',
-      lineWidth: 3
+      lineWidth: 3,
+      lineJoin: 'round'
     }, options );
 
-    var shape = new Shape().moveTo( 0, 0 );
-    var moved = false;
+    var points = [];
     for ( var i = 0; i < 10; i++ ) {
 
       //Start at the top
@@ -45,15 +46,15 @@ define( function( require ) {
       var radius = i % 2 === 0 ? options.outerRadius : options.innerRadius;
       var x = radius * Math.cos( angle );
       var y = radius * Math.sin( angle );
-      if ( !moved ) {
-        shape.moveTo( x, y );
-        moved = true;
-      }
-      else {
-        shape.lineTo( x, y );
-      }
+      points.push( new Vector2( x, y ) );
+    }
+
+    var shape = new Shape().moveTo( points[0].x, points[0].y );
+    for ( i = 1; i < points.length; i++ ) {
+      shape.lineTo( points[i].x, points[i].y );
     }
     shape.close();
+
     Path.call( this, shape, options );
   }
 
