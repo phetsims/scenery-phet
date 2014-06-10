33ce7a07c8d8c2c3bf72375e1a9ac6844291240c
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-06-10 17:22:08 -0600

    add curves to wavelength slider thumb

--- js/WavelengthSlider.js
+++ js/WavelengthSlider.js
@@ -61,13 +61,40 @@ define( function( require ) {
    * @constructor
    */
   function Thumb( width, height ) {
+    // set the radius of the arcs based on the height or width, whichever is smaller
+    var radiusScale = 0.15;
+    var radius = ( width < height ) ? radiusScale * width : radiusScale * height;
+
+    // calculate some parameters of the upper triangles of the thumb for getting arc offsets
+    var hypotenuse = Math.sqrt( Math.pow( 0.5 * width, 2 ) + Math.pow( 0.3 * height, 2 ) );
+    var angle = Math.acos( width * 0.5 / hypotenuse );
+    var topAngle = Math.PI / 2 - angle;
+    var heightOffset = radius * Math.sin( angle );
+
+    // draw the thumb shape starting at the right upper corner of the pentagon below the arc,
+    // this way we can get the arc coordinates for the arc in this corner from the other side, which
+    // will be easier to calculate arcing from bottom to top
     var shape = new Shape()
-      .moveTo( 0, 0 )
-      .lineTo( 0.5 * width, 0.3 * height )
-      .lineTo( 0.5 * width, 1 * height )
-      .lineTo( -0.5 * width, 1 * height )
-      .lineTo( -0.5 * width, 0.3 * height )
+      .moveTo( 0.5 * width, 0.3 * height + heightOffset)
+      .lineTo( 0.5 * width, 1 * height - radius )
+      .arc( 0.5 * width - radius, 1 * height - radius, radius, 0, Math.PI / 2 )
+      .lineTo( -0.5 * width + radius, 1 * height )
+      .arc( -0.5 * width + radius, 1 * height - radius, radius, Math.PI / 2, Math.PI )
+      .lineTo( -0.5 * width, 0.3 * height + heightOffset)
+      .arc( -0.5 * width + radius, 0.3 * height + heightOffset, radius, Math.PI, Math.PI + angle );
+
+    // save the coordinates for the point above the left side arc, for use on the other side
+    var sideArcPoint = shape.getLastPoint();
+
+    // get the coordinates for where to start and end the top arc
+    var centerCurveOffset = new Shape().moveTo(0, 0).arc( 0, radius, radius, -Math.PI / 2, -topAngle ).getLastPoint();
+
+    shape.lineTo( -centerCurveOffset.x, centerCurveOffset.y )
+      .arc( 0, radius, radius, -2 * topAngle, -topAngle )
+      .lineTo( -sideArcPoint.x, sideArcPoint.y )
+      .arc( 0.5 * width - radius, 0.3 * height + heightOffset, radius, -angle, 0 )
       .close();
+
     Path.call( this, shape, { stroke: 'black', lineWidth: 1, fill: 'black' } );
 
     // touch area, don't extend above so that we don't encroach on slider track
