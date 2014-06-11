ca95f1b746844fc8eada887eedf33370c1c35df6
Chris Malley <cmalley@pixelzoom.com>
2014-06-11 12:59:19 -0600

    phetsims/scenery-phet#52 change 'score' to 'points' throughout the interface and internal implementation, with the exception of the type name (that will be changed last)

--- js/FaceWithScoreNode.js
+++ js/FaceWithScoreNode.js
@@ -6,6 +6,7 @@
  *
  * @author John Blanco
  * @author Sam Reid
+ * @author Chris Malley
  */
 define( function( require ) {
   'use strict';
@@ -26,76 +27,72 @@ define( function( require ) {
    * @constructor
    */
   function FaceWithScoreNode( options ) {
-    Node.call( this );
 
     this.options = _.extend( {
       faceDiameter: 100, // in screen coords, which are fairly close to pixels
       faceOpacity: 0.6,
-      scoreAlignment: 'rightBottom', // valid values are 'rightBottom' and 'centerBottom'
-      scoreTextSize: 44,
-      scoreFill: 'yellow',
-      scoreStroke: 'black',
-      score: 0
+      pointsAlignment: 'rightBottom', // valid values are 'rightBottom' and 'centerBottom'
+      pointsTextSize: 44,
+      pointsFill: 'yellow',
+      pointsStroke: 'black',
+      points: 0
     }, options );
 
     this.faceNode = new FaceNode( this.options.faceDiameter, { opacity: this.options.faceOpacity } ); // @private
-    this.addChild( this.faceNode );
 
     // @private
-    this.pointDisplay = new Text( '',
+    this.pointsNode = new Text( '',
       {
-        font: new PhetFont( { size: this.options.scoreTextSize, weight: 'bold', lineWidth: 1.5, fill: 'yellow' } ),
-        fill: this.options.scoreFill,
-        stroke: this.options.scoreStroke,
+        font: new PhetFont( { size: this.options.pointsTextSize, weight: 'bold', lineWidth: 1.5, fill: 'yellow' } ),
+        fill: this.options.pointsFill,
+        stroke: this.options.pointsStroke,
         lineWidth: 1
       } );
-    this.addChild( this.pointDisplay );
 
-    this.setScore( this.options.score );
+    this.options.children = [ this.faceNode, this.pointsNode ];
+    Node.call( this, this.options );
 
-    // Pass options through to the parent class.
-    this.mutate( this.options );
+    this.setPoints( this.options.points );
   }
 
   return inherit( Node, FaceWithScoreNode, {
 
     smile: function() {
       this.faceNode.smile();
-      this.pointDisplay.visible = true;
+      this.pointsNode.visible = true;
     },
 
     frown: function() {
       this.faceNode.frown();
-      this.pointDisplay.visible = false;
+      this.pointsNode.visible = false;
     },
 
-    setScore: function( score ) {
-      assert && assert( score >= 0 );
+    setPoints: function( points ) {
+      assert && assert( points >= 0 );
 
-      if ( score === 0 ) {
-        // If the score is zero, then don't show any text, since "0" is just weird.
-        this.pointDisplay.text = '';
+      if ( points === 0 ) {
+        // If zero, then don't show any text, since "0" is just weird.
+        this.pointsNode.text = '';
       }
-      else if ( score < 0 ) {
-        this.pointDisplay.text = score + '';
+      else if ( points < 0 ) {
+        this.pointsNode.text = points + '';
       }
       else {
         // + sign for positive numbers, order localized
-        this.pointDisplay.text = StringUtils.format( pattern_0sign_1number, '+', score );
+        this.pointsNode.text = StringUtils.format( pattern_0sign_1number, '+', points );
       }
-      this.updateScoreLocation();
+      this.updatePointsLocation();
     },
 
     // @private
-    updateScoreLocation: function() {
-
-      if ( this.options.scoreAlignment === 'centerBottom' ) {
-        this.pointDisplay.centerX = this.faceNode.centerX;
-        this.pointDisplay.top = this.faceNode.bottom + 2;
+    updatePointsLocation: function() {
+      if ( this.options.pointsAlignment === 'centerBottom' ) {
+        this.pointsNode.centerX = this.faceNode.centerX;
+        this.pointsNode.top = this.faceNode.bottom + 2;
       }
-      else if ( this.options.scoreAlignment === 'rightBottom' ) {
-        this.pointDisplay.centerX = this.options.faceDiameter * 0.4;
-        this.pointDisplay.centerY = this.options.faceDiameter / 2;
+      else if ( this.options.pointsAlignment === 'rightBottom' ) {
+        this.pointsNode.centerX = this.options.faceDiameter * 0.4;
+        this.pointsNode.centerY = this.options.faceDiameter / 2;
       }
     }
   } );
