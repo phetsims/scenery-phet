c34eae62ae9b64468086a1883cef149e0c615c4c
jbphet <john.blanco@colorado.edu>
2014-10-16 16:22:00 -0600

    Changed the hand positions and hub size for the clock icon, see #86.

--- js/SimpleClockIcon.js
+++ js/SimpleClockIcon.js
@@ -9,7 +9,7 @@
 define( function( require ) {
   'use strict';
 
-  // Imports
+  // modules
   var Circle = require( 'SCENERY/nodes/Circle' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Line = require( 'SCENERY/nodes/Line' );
@@ -25,9 +25,16 @@ define( function( require ) {
     options = _.extend( { fill: 'white', stroke: 'black', lineWidth: 2 }, options );
     var rootNode = new Node();
     rootNode.addChild( new Circle( radius, options ) );
-    rootNode.addChild( new Circle( radius * 0.1, { fill: options.stroke } ) );
-    rootNode.addChild( new Line( 0, 0, 0, -radius * 0.8, { stroke: options.stroke, lineWidth: options.lineWidth } ) );
-    rootNode.addChild( new Line( 0, 0, radius * 0.5, 0, { stroke: options.stroke, lineWidth: options.lineWidth } ) );
+    rootNode.addChild( new Circle( radius * 0.15, { fill: options.stroke } ) );
+    var lineOptionsForClockHands = {
+      stroke: options.stroke,
+      lineWidth: options.lineWidth,
+      lineCap: 'round',
+      lineJoin: 'round'
+    };
+    // Hands at 4 o'clock
+    rootNode.addChild( new Line( 0, 0, 0, -radius * 0.75, lineOptionsForClockHands ) );
+    rootNode.addChild( new Line( 0, 0, radius * 0.45, radius * 0.3, lineOptionsForClockHands ) );
     rootNode.centerX = radius;
     rootNode.centerY = radius;
     this.addChild( rootNode );
