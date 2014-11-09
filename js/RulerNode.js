43efd1941c2a3be2e40f91dc3fd3c1837b7339ed
Chris Malley <cmalley@pixelzoom.com>
2014-11-09 15:44:48 -0700

    #91 reduce number of nodes by using 1 Path for major ticks, 1 Path for minor ticks

--- js/RulerNode.js
+++ js/RulerNode.js
@@ -22,15 +22,15 @@ define( function( require ) {
   var DEFAULT_FONT = new PhetFont( 18 );
 
   /**
-   * @param {number} width  distance between left-most and right-most tick, insets will be added to this
-   * @param {number} height
+   * @param {number} rulerWidth  distance between left-most and right-most tick, insets will be added to this
+   * @param {number} rulerHeight
    * @param {number} majorTickWidth
    * @param {string[]} majorTickLabels
    * @param {string} units
    * @param {Object} [options]
    * @constructor
    */
-  function RulerNode( width, height, majorTickWidth, majorTickLabels, units, options ) {
+  function RulerNode( rulerWidth, rulerHeight, majorTickWidth, majorTickLabels, units, options ) {
 
     // default options
     options = _.extend(
@@ -42,12 +42,12 @@ define( function( require ) {
         insetsWidth: 14, // space between the ends of the ruler and the first and last tick marks
         // major tick options
         majorTickFont: DEFAULT_FONT,
-        majorTickHeight: ( 0.4 * height ) / 2,
+        majorTickHeight: ( 0.4 * rulerHeight ) / 2,
         majorTickStroke: 'black',
         majorTickLineWidth: 1,
         // minor tick options
         minorTickFont: DEFAULT_FONT,
-        minorTickHeight: ( 0.2 * height ) / 2,
+        minorTickHeight: ( 0.2 * rulerHeight ) / 2,
         minorTickStroke: 'black',
         minorTickLineWidth: 1,
         minorTicksPerMajorTick: 0,
@@ -61,30 +61,30 @@ define( function( require ) {
       }, options );
 
     // things you're likely to mess up, add more as needed
-    assert && assert( Math.floor( width / majorTickWidth ) + 1 === majorTickLabels.length ); // do we have enough major tick labels?
+    assert && assert( Math.floor( rulerWidth / majorTickWidth ) + 1 === majorTickLabels.length ); // do we have enough major tick labels?
     assert && assert( options.unitsMajorTickIndex < majorTickLabels.length );
-    assert && assert( options.majorTickHeight < height / 2 );
-    assert && assert( options.minorTickHeight < height / 2 );
+    assert && assert( options.majorTickHeight < rulerHeight / 2 );
+    assert && assert( options.minorTickHeight < rulerHeight / 2 );
 
-    var thisNode = this;
-    Node.call( thisNode, options );
+    Node.call( this );
 
     // background
-    var backgroundNode = new Rectangle( 0, 0, width + ( 2 * options.insetsWidth ), height,
+    var backgroundNode = new Rectangle( 0, 0, rulerWidth + ( 2 * options.insetsWidth ), rulerHeight,
       { fill: options.backgroundFill,
         stroke: options.backgroundStroke,
         lineWidth: options.backgroundLineWidth } );
-    thisNode.addChild( backgroundNode );
+    this.addChild( backgroundNode );
 
     // Lay out tick marks from left to right
     var minorTickWidth = majorTickWidth / ( options.minorTicksPerMajorTick + 1 );
     var x = options.insetsWidth;
     var majorTickIndex = 0;
 
-    var ticksContainerNode = new Node( { pickable: false } );
-    this.addChild( ticksContainerNode );
+    // Minimize number of nodes by using one Path for each type of tick
+    var majorTicksShape = new Shape();
+    var minorTicksShape = new Shape();
 
-    while ( x < ( width + options.insetsWidth + options.insetsWidth ) ) {
+    while ( x < ( rulerWidth + options.insetsWidth + options.insetsWidth ) ) {
 
       if ( ( x - options.insetsWidth ) % majorTickWidth === 0 ) {
 
@@ -97,17 +97,21 @@ define( function( require ) {
 
         // Only add the major tick label if the insetsWidth is nonzero, or if it is not an end label
         if ( options.insetsWidth !== 0 || ( majorTickIndex !== 0 ) ) {
-          ticksContainerNode.addChild( majorTickLabelNode );
+          this.addChild( majorTickLabelNode );
         }
 
         // Major tick mark
-        var majorTickNode = createTickMarkNode( x, height, options.majorTickHeight, options.majorTickStroke, options.majorTickLineWidth, options.tickMarksOnTop, options.tickMarksOnBottom );
-        ticksContainerNode.addChild( majorTickNode );
+        if ( options.tickMarksOnTop ) {
+          majorTicksShape.moveTo( x, 0 ).lineTo( x, options.majorTickHeight );
+        }
+        if ( options.tickMarksOnBottom ) {
+          majorTicksShape.moveTo( x, rulerHeight - options.majorTickHeight ).lineTo( x, rulerHeight );
+        }
 
         // units label
         if ( majorTickIndex === options.unitsMajorTickIndex ) {
           var unitsNode = new Text( units, { font: options.unitsFont } );
-          ticksContainerNode.addChild( unitsNode );
+          this.addChild( unitsNode );
           unitsNode.x = majorTickLabelNode.x + majorTickLabelNode.width + options.unitsSpacing;
           unitsNode.y = majorTickLabelNode.y + majorTickLabelNode.height - unitsNode.height;
         }
@@ -116,42 +120,33 @@ define( function( require ) {
         x += minorTickWidth;
       }
       else {
-        // Minor tick marks
-        for ( var k = 1; ( k <= options.minorTicksPerMajorTick ) && ( x < ( width + options.insetsWidth + options.insetsWidth ) ); k++ ) {
-          var minorTickNode = createTickMarkNode( x, height, options.minorTickHeight, options.minorTickStroke, options.minorTickLineWidth, options.tickMarksOnTop, options.tickMarksOnBottom );
-          ticksContainerNode.addChild( minorTickNode );
+        // Minor tick marks, up to the next major tick
+        for ( var k = 1; ( k <= options.minorTicksPerMajorTick ) && ( x < ( rulerWidth + options.insetsWidth + options.insetsWidth ) ); k++ ) {
+          if ( options.tickMarksOnTop ) {
+            minorTicksShape.moveTo( x, 0 ).lineTo( x, options.minorTickHeight );
+          }
+          if ( options.tickMarksOnBottom ) {
+            minorTicksShape.moveTo( x, rulerHeight - options.minorTickHeight ).lineTo( x, rulerHeight );
+          }
           x += minorTickWidth;
         }
       }
     }
+
+    // major ticks
+    this.addChild( new Path( majorTicksShape, {
+      stroke: options.majorTickStroke,
+      lineWidth: options.majorTickLineWidth
+    } ) );
+
+    // minor ticks
+    this.addChild( new Path( minorTicksShape, {
+      stroke: options.minorTickStroke,
+      lineWidth: options.minorTickLineWidth
+    } ) );
+
+    this.mutate( options );
   }
 
-  inherit( Node, RulerNode );
-
-  /**
-   * Creates a tick mark at a specific x location.
-   * Each tick is marked at the top and bottom of the ruler.
-   * If you desire a different style of tick mark, override this method.
-   *
-   * @param {number} x
-   * @param {number} rulerHeight
-   * @param {number} tickHeight
-   * @param {string} stroke stroke color as a CSS string
-   * @param {number} lineWidth
-   * @param {boolean} drawUpper
-   * @param {boolean} drawLower
-   * @return {Node}
-   */
-  var createTickMarkNode = function( x, rulerHeight, tickHeight, stroke, lineWidth, drawUpper, drawLower ) {
-    var shape = new Shape();
-    if ( drawUpper ) {
-      shape.moveTo( x, 0 ).lineTo( x, tickHeight );
-    }
-    if ( drawLower ) {
-      shape.moveTo( x, rulerHeight - tickHeight ).lineTo( x, rulerHeight );
-    }
-    return new Path( shape, { stroke: stroke, lineWidth: lineWidth } );
-  };
-
-  return RulerNode;
+  return inherit( Node, RulerNode );
 } );
