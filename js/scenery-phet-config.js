bc7b9c0985aa7569d9f031eb84caea16451da2d3
Sam Reid <reids@colorado.edu>
2015-02-03 20:52:42 -0700

    Added cache buster as an option & standardized formatting for config files, see https://github.com/phetsims/joist/issues/196

--- js/scenery-phet-config.js
+++ js/scenery-phet-config.js
@@ -1,34 +1,34 @@
 // Copyright 2002-2014, University of Colorado Boulder
 
 // RequireJS configuration file for scenery-phet
-require.config(
-  {
-    deps: [ 'scenery-phet-main' ],
+require.config( {
+  deps: [ 'scenery-phet-main' ],
 
-    paths: {
+  paths: {
 
-      // third party libs
-      text: '../../sherpa/text',
+    // third party libs
+    text: '../../sherpa/text',
 
-      // plugins
-      image: '../../chipper/requirejs-plugins/image',
-      string: '../../chipper/requirejs-plugins/string',
+    // plugins
+    image: '../../chipper/requirejs-plugins/image',
+    string: '../../chipper/requirejs-plugins/string',
 
-      // PhET libs, uppercase names to identify them in require.js imports
-      AXON: '../../axon/js',
-      BRAND: '../../brand/js',
-      DOT: '../../dot/js',
-      JOIST: '../../joist/js',
-      KITE: '../../kite/js',
-      PHETCOMMON: '../../phetcommon/js',
-      PHET_CORE: '../../phet-core/js',
-      SCENERY: '../../scenery/js',
-      SCENERY_PHET: '../../scenery-phet/js',
-      SHERPA: '../../sherpa',
-      SUN: '../../sun/js',
-      VEGAS: '../../vegas/js'
-    },
+    // PhET libs, uppercase names to identify them in require.js imports
+    AXON: '../../axon/js',
+    BRAND: '../../brand/js',
+    DOT: '../../dot/js',
+    JOIST: '../../joist/js',
+    KITE: '../../kite/js',
+    PHETCOMMON: '../../phetcommon/js',
+    PHET_CORE: '../../phet-core/js',
+    SCENERY: '../../scenery/js',
+    SCENERY_PHET: '../../scenery-phet/js',
+    SHERPA: '../../sherpa',
+    SUN: '../../sun/js',
+    VEGAS: '../../vegas/js'
+  },
 
-    urlArgs: new Date().getTime()  // cache buster to make browser refresh load all included scripts
+  // optional cache buster to make browser refresh load all included scripts, can be disabled with ?cacheBuster=false
+  urlArgs: phet.phetcommon.getCacheBusterArgs()
 
-  } );
+} );
