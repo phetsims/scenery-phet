44f3d70091f1a2f7c5c370be73c50d271d713eda
Chris Malley <cmalley@pixelzoom.com>
2015-03-06 12:15:51 -0700

    #140 use Color instances for all colors, as in scenery.Color

--- js/PhetColorScheme.js
+++ js/PhetColorScheme.js
@@ -3,6 +3,7 @@
 /**
  * Colors that are specific to PhET simulations.
  * Reuse these in sims whenever possible to facilitate uniformity across sims.
+ * These should all be instances of scenery.Color, since {Color} can typically be used anywhere but {string} cannot.
  * <p>
  * This is based on the google doc here:
  * http://spreadsheets.google.com/ccc?key=0Ajw3oS4YmCBqdDZzYUhlMksxZ0lfUHZ3bXUzM0JNU3c&hl=en&pli=1#gid=0
@@ -17,28 +18,28 @@ define( function( require ) {
   var Color = require( 'SCENERY/util/Color' );
 
   // Colors that are used for one or more things in the color scheme.
-  var DARK_GREEN = 'rgb( 0, 200, 0 )';
-  var RED_COLORBLIND = 'rgb( 255, 85, 0 )'; // looks good in colorblind tests, typically used in place of 'red'
-  var TAN_ORANGE ='rgb( 236, 153, 55 )';
+  var DARK_GREEN = new Color( 0, 200, 0 );
+  var RED_COLORBLIND = new Color( 255, 85, 0 ); // looks good in colorblind tests, typically used in place of 'red'
+  var TAN_ORANGE = new Color( 236, 153, 55 );
 
   return {
     ACCELERATION: Color.GREEN,
     APPLIED_FORCE: TAN_ORANGE,
-    ELASTIC_ENERGY: 'rgb( 153, 51, 102  )',
+    ELASTIC_ENERGY: new Color( 153, 51, 102  ),
     FRICTION_FORCE: RED_COLORBLIND,
-    GRAVITATIONAL_FORCE: 'rgb( 50, 130, 215 )',
+    GRAVITATIONAL_FORCE: new Color( 50, 130, 215 ),
     HEAT_THERMAL_ENERGY: RED_COLORBLIND,
-    IMAGINARY_PART: 'rgb( 153, 51, 102  )',
+    IMAGINARY_PART: new Color( 153, 51, 102  ),
     KINETIC_ENERGY: Color.GREEN,
     NET_WORK: DARK_GREEN,
-    NORMAL_FORCE: 'rgb( 255, 235, 0 )',
+    NORMAL_FORCE: new Color( 255, 235, 0 ),
     POSITION: Color.BLUE,
     POTENTIAL_ENERGY: Color.BLUE,
-    REAL_PART: 'rgb( 255, 153, 0 )',
+    REAL_PART: new Color( 255, 153, 0 ),
     RED_COLORBLIND: RED_COLORBLIND,
     TOTAL_ENERGY: TAN_ORANGE,
     TOTAL_FORCE: DARK_GREEN,
     VELOCITY: RED_COLORBLIND,
-    WALL_FORCE: 'rgb( 153, 51, 0 )'
+    WALL_FORCE: new Color( 153, 51, 0 )
   };
 } );
