412a5af809616aa1a5ab536749b946547ba50e16
Chris Malley <cmalley@pixelzoom.com>
2015-04-09 11:16:29 -0600

    remove extra space from copyright comment on line 1 of .js files

--- js/ContextLossFailureDialog.js
+++ js/ContextLossFailureDialog.js
@@ -1,4 +1,4 @@
-//  Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2002-2014, University of Colorado Boulder
 
 /**
  * Failure message displayed when a WebGL context loss is experienced and we can't recover. Offers a button to reload
