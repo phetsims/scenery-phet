efb36c1a2913f98233d70e1fc7c3b16ca275dd10
Jonathan Olson <olsonsjc@gmail.com>
2015-05-04 15:24:44 -0600

    Removing duplicate version.js (and adding REPOSITORY require.js prefix in config). See https://github.com/phetsims/chipper/issues/4

--- js/version.js
+++
@@ -1,9 +0,0 @@
-// Copyright 2002-2014, University of Colorado Boulder
-
-// Returns version identifier.
-define( function() {
-  'use strict';
-  return function() {
-    return '1.0.0-dev.3';
-  };
-} );
