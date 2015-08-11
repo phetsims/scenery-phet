6c8a24230434c604b49a26a855486ddc7c243cb3
Jonathan Olson <olsonsjc@gmail.com>
2015-08-11 14:47:56 -0600

    IE11 warning dialog, see https://github.com/phetsims/molecule-shapes/issues/133

---
+++ js/IE11StencilWarningNode.js
@@ -0,0 +1,62 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ * Warning displayed when we have to fall back to Canvas (not due to normal lack-of-WebGL reasons, but specifically for
+ * IE11 that is too low of a version). See https://github.com/phetsims/molecule-shapes/issues/133 and
+ * https://github.com/phetsims/molecule-shapes/issues/132.
+ *
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var HBox = require( 'SCENERY/nodes/HBox' );
+  var VBox = require( 'SCENERY/nodes/VBox' );
+  var Text = require( 'SCENERY/nodes/Text' );
+  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  var FontAwesomeNode = require( 'SUN/FontAwesomeNode' );
+
+  var titleString = require( 'string!SCENERY_PHET/webglWarning.title' );
+  var bodyString = require( 'string!SCENERY_PHET/webglWarning.ie11StencilBody' );
+
+  function IE11StencilWarningNode() {
+    HBox.call( this, _.extend( {
+      children: [
+        new FontAwesomeNode( 'warning_sign', {
+          fill: '#E87600', // "safety orange", according to Wikipedia
+          scale: 0.6
+        } ),
+        new VBox( {
+          children: [
+            new Text( titleString, {
+              font: new PhetFont( 14 ),
+              fill: '#ddd'
+            } ),
+            new Text( bodyString, {
+              font: new PhetFont( 10 ),
+              fill: '#999'
+            } )
+          ],
+          spacing: 3,
+          align: 'left'
+        } )
+      ],
+      spacing: 12,
+      align: 'center',
+      cursor: 'pointer'
+    } ) );
+
+    this.mouseArea = this.touchArea = this.localBounds;
+
+    this.addInputListener( {
+      up: function() {
+        var phetWindow = window.open( 'http://windowsupdate.microsoft.com/', '_blank' );
+        phetWindow.focus();
+      }
+    } );
+  }
+
+  return inherit( HBox, IE11StencilWarningNode );
+} );
