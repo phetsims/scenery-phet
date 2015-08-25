70815580956c98e9d0c20d550d7dac58fbbcc1a0
Chris Malley <cmalley@pixelzoom.com>
2015-08-25 13:32:43 -0600

    #33 adjust local var names

--- js/ArrowShape.js
+++ js/ArrowShape.js
@@ -46,15 +46,15 @@ define( function( require ) {
       var length = vector.magnitude();
 
       // scale down the head if head is dynamic.
-      var tempHeadHeight = options.headHeight;
-      var tempHeadWidth = options.headWidth;
-      var tempTailWidth = options.tailWidth;
+      var headWidth = options.headWidth;
+      var headHeight = options.headHeight;
+      var tailWidth = options.tailWidth;
       if ( options.isHeadDynamic ) {
         if ( length < options.headHeight / options.fractionalHeadHeight ) {
-          tempHeadHeight = length * options.fractionalHeadHeight;
+          headHeight = length * options.fractionalHeadHeight;
           if ( options.scaleTailToo ) {
-            tempTailWidth = options.tailWidth * tempHeadHeight / options.headHeight;
-            tempHeadWidth = options.headWidth * tempHeadHeight / options.headHeight;
+            tailWidth = options.tailWidth * headHeight / options.headHeight;
+            headWidth = options.headWidth * headHeight / options.headHeight;
           }
         }
         else {
@@ -63,7 +63,7 @@ define( function( require ) {
       }
       // otherwise, just make sure that head height is less than arrow length
       else {
-        tempHeadHeight = Math.min( options.headHeight, options.doubleHead ? 0.35 * length : 0.99 * length );
+        headHeight = Math.min( options.headHeight, options.doubleHead ? 0.35 * length : 0.99 * length );
       }
 
       // Set up a coordinate frame that goes from the tail of the arrow to the tip.
@@ -77,23 +77,23 @@ define( function( require ) {
       var points = [];
       if ( options.doubleHead ) {
         points.push( getPoint( 0, 0 ) );
-        points.push( getPoint( tempHeadHeight, tempHeadWidth / 2 ) );
-        points.push( getPoint( tempHeadHeight, tempTailWidth / 2 ) );
+        points.push( getPoint( headHeight, headWidth / 2 ) );
+        points.push( getPoint( headHeight, tailWidth / 2 ) );
       }
       else {
-        points.push( getPoint( 0, tempTailWidth / 2 ) );
+        points.push( getPoint( 0, tailWidth / 2 ) );
       }
-      points.push( getPoint( length - tempHeadHeight, tempTailWidth / 2 ) );
-      points.push( getPoint( length - tempHeadHeight, tempHeadWidth / 2 ) );
+      points.push( getPoint( length - headHeight, tailWidth / 2 ) );
+      points.push( getPoint( length - headHeight, headWidth / 2 ) );
       points.push( getPoint( length, 0 ) );
-      points.push( getPoint( length - tempHeadHeight, -tempHeadWidth / 2 ) );
-      points.push( getPoint( length - tempHeadHeight, -tempTailWidth / 2 ) );
+      points.push( getPoint( length - headHeight, -headWidth / 2 ) );
+      points.push( getPoint( length - headHeight, -tailWidth / 2 ) );
       if ( options.doubleHead ) {
-        points.push( getPoint( tempHeadHeight, -tempTailWidth / 2 ) );
-        points.push( getPoint( tempHeadHeight, -tempHeadWidth / 2 ) );
+        points.push( getPoint( headHeight, -tailWidth / 2 ) );
+        points.push( getPoint( headHeight, -headWidth / 2 ) );
       }
       else {
-        points.push( getPoint( 0, -tempTailWidth / 2 ) );
+        points.push( getPoint( 0, -tailWidth / 2 ) );
       }
 
       // Describe the shape
