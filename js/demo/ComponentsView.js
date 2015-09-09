6d87be2b578eb9b0d5099663208ca55598228daa
Chris Malley <cmalley@pixelzoom.com>
2015-09-09 17:15:54 -0600

    #177 instantiate demos on demand, set initial selection based on 'component' query parameter

--- js/demo/ComponentsView.js
+++ js/demo/ComponentsView.js
@@ -49,55 +49,49 @@ define( function( require ) {
     var layoutBounds = this.layoutBounds;
 
     // To add a demo, create an entry here.
-    var allDemos = [
-      { label: 'BracketNode', getNode: function() {return demoBracketNode( layoutBounds );} },
-      { label: 'ConductivityTesterNode', getNode: function() {return demoConductivityTesterNode( layoutBounds );} },
-      { label: 'EyeDropperNode', getNode: function() {return demoEyeDropperNode( layoutBounds );} },
-      { label: 'FaucetNode', getNode: function() {return demoFaucetNode( layoutBounds );} },
-      { label: 'MeasuringTape', getNode: function() {return demoMeasuringTape( layoutBounds );} },
-      { label: 'NumberPicker', getNode: function() {return demoNumberPicker( layoutBounds );} },
-      { label: 'ProbeNode', getNode: function() {return demoProbeNode( layoutBounds );} },
-      { label: 'RulerNode', getNode: function() {return demoRulerNode( layoutBounds );} },
-      { label: 'StarNode', getNode: function() {return demoStarNode( layoutBounds );} },
-      { label: 'ThermometerNode', getNode: function() {return demoTemperatureNode( layoutBounds ) } }
+    // Demos are instantiated on demand.
+    // A node field will be added to each of these entries when the demo is instantiated.
+    var demos = [
+      { label: 'BracketNode', getNode: function() { return demoBracketNode( layoutBounds ); } },
+      { label: 'ConductivityTesterNode', getNode: function() { return demoConductivityTesterNode( layoutBounds ); } },
+      { label: 'EyeDropperNode', getNode: function() { return demoEyeDropperNode( layoutBounds ); } },
+      { label: 'FaucetNode', getNode: function() { return demoFaucetNode( layoutBounds ); } },
+      { label: 'MeasuringTape', getNode: function() { return demoMeasuringTape( layoutBounds ); } },
+      { label: 'NumberPicker', getNode: function() { return demoNumberPicker( layoutBounds ); } },
+      { label: 'ProbeNode', getNode: function() { return demoProbeNode( layoutBounds ); } },
+      { label: 'RulerNode', getNode: function() { return demoRulerNode( layoutBounds ); } },
+      { label: 'StarNode', getNode: function() { return demoStarNode( layoutBounds ); } },
+      { label: 'ThermometerNode', getNode: function() { return demoTemperatureNode( layoutBounds ); } }
     ];
-
-    // Choose a particular node based on the ?component query parameter
-    var component = phet.chipper.getQueryParameter( 'component' );
-    var demos = allDemos.filter( function( item ) {
-      return component ? item.label === component : true;
-    } );
-
-    // For any of the selected demos, instantiate its node.
-    demos.forEach( function( demo ) {
-      demo.node = demo.getNode();
-    } );
 
     // Sort the demos by label, so that they appear in the combo box in alphabetical order
     demos = _.sortBy( demos, function( demo ) {
       return demo.label;
     } );
 
+    // All demos will be children of this node, to maintain rendering order with combo box list
+    var demosParent = new Node();
+    this.addChild( demosParent );
+
+    // add each demo to the combo box
     var comboBoxItems = [];
-
     demos.forEach( function( demo ) {
-
-      // add demo to the combo box
-      comboBoxItems.push( ComboBox.createItem( new Text( demo.label, { font: new PhetFont( 20 ) } ), demo.node ) );
-
-      // add demo to the scenegraph
-      thisView.addChild( demo.node );
-
-      // demo is invisible until selected via the combo box
-      demo.node.visible = false;
+      comboBoxItems.push( ComboBox.createItem( new Text( demo.label, { font: new PhetFont( 20 ) } ), demo ) );
     } );
 
     // Parent for the combo box popup list
     var listParent = new Node();
     this.addChild( listParent );
 
+    // Choose a particular node based on the ?component query parameter
+    var component = phet.chipper.getQueryParameter( 'component' );
+    var selectedDemo = demos.find( function( demo ) {
+      return ( demo.label === component );
+    } );
+    selectedDemo = selectedDemo || demos[ 0 ];
+
     // Combo box for selecting which component to view
-    var selectedDemoProperty = new Property( demos[ 0 ].node );
+    var selectedDemoProperty = new Property( selectedDemo );
     var comboBox = new ComboBox( comboBoxItems, selectedDemoProperty, listParent, {
       buttonFill: 'rgb( 218, 236, 255 )',
       top: 20,
@@ -107,8 +101,23 @@ define( function( require ) {
 
     // Make the selected demo visible
     selectedDemoProperty.link( function( demo, oldDemo ) {
-      if ( oldDemo ) { oldDemo.visible = false; }
-      demo.visible = true;
+
+      // make the previous selection invisible
+      if ( oldDemo ) {
+        oldDemo.node.visible = false;
+      }
+
+      if ( demo.node ) {
+
+        // If the selected demo has an associated node, make it visible.
+        demo.node.visible = true;
+      }
+      else {
+
+        // If the selected demo doesn't doesn't have an associated node, create it.
+        demo.node = demo.getNode();
+        demosParent.addChild( demo.node );
+      }
     } );
   }
 
@@ -203,6 +212,7 @@ define( function( require ) {
 
   // Creates a demo for ProbeNode
   var demoProbeNode = function( layoutBounds ) {
+    console.log( 'demoProbeNode' );//XXX
 
     var demoParent = new Node();
 
