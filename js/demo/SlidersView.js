075660ee19f7316e7e5a00a5de7897f709fb7c8e
Chris Malley <cmalley@pixelzoom.com>
2015-09-23 18:15:44 -0600

    move demo of HSlider to sun, phetsims/scenery-phet#188

--- js/demo/SlidersView.js
+++ js/demo/SlidersView.js
@@ -12,61 +12,14 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var CheckBox = require( 'SUN/CheckBox' );
   var DemosView = require( 'SUN/demo/DemosView' );
-  var Dimension2 = require( 'DOT/Dimension2' );
-  var HSlider = require( 'SUN/HSlider' );
   var inherit = require( 'PHET_CORE/inherit' );
   var NumberControl = require( 'SCENERY_PHET/NumberControl' );
-  var Node = require( 'SCENERY/nodes/Node' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var Property = require( 'AXON/Property' );
   var Range = require( 'DOT/Range' );
   var Text = require( 'SCENERY/nodes/Text' );
   var WavelengthSlider = require( 'SCENERY_PHET/WavelengthSlider' );
-
-  // Creates a demo for HSlider
-  var demoHSlider = function( layoutBounds ) {
-
-    var property = new Property( 0 );
-    var range = new Range( 0, 100 );
-    var tickLabelOptions = { font: new PhetFont( 16 ) };
-    var slider = new HSlider( property, range, {
-      trackSize: new Dimension2( 300, 5 ),
-      center: layoutBounds.center
-    } );
-
-    // major ticks
-    slider.addMajorTick( range.min, new Text( range.min, tickLabelOptions ) );
-    slider.addMajorTick( range.getCenter(), new Text( range.getCenter(), tickLabelOptions ) );
-    slider.addMajorTick( range.max, new Text( range.max, tickLabelOptions ) );
-
-    // minor ticks
-    slider.addMinorTick( range.min + 0.25 * range.getLength() );
-    slider.addMinorTick( range.min + 0.75 * range.getLength() );
-
-    // show/hide major ticks
-    var majorTicksVisibleProperty = new Property( true );
-    majorTicksVisibleProperty.link( function( visible ) {
-      slider.majorTicksVisible = visible;
-    } );
-    var majorTicksCheckBox = CheckBox.createTextCheckBox( 'Major ticks visible', { font: new PhetFont( 20 ) }, majorTicksVisibleProperty, {
-      left: slider.left,
-      top: slider.bottom + 40
-    } );
-
-    // show/hide minor ticks
-    var minorTicksVisibleProperty = new Property( true );
-    minorTicksVisibleProperty.link( function( visible ) {
-      slider.minorTicksVisible = visible;
-    } );
-    var minorTicksCheckBox = CheckBox.createTextCheckBox( 'Minor ticks visible', { font: new PhetFont( 20 ) }, minorTicksVisibleProperty, {
-      left: slider.left,
-      top: majorTicksCheckBox.bottom + 40
-    } );
-
-    return new Node( { children: [ slider, majorTicksCheckBox, minorTicksCheckBox ] } );
-  };
 
   // Creates a demo for NumberControl
   var demoNumberControl = function( layoutBounds ) {
@@ -102,7 +55,6 @@ define( function( require ) {
       // To add a demo, create an entry here.
       // label is a {string} that will appear in the combo box.
       // getNode is a {function} that takes a {Bounds2} layoutBounds and returns a {Node}.
-      { label: 'HSlider', getNode: demoHSlider },
       { label: 'NumberControl', getNode: demoNumberControl },
       { label: 'WavelengthSlider', getNode: demoWavelengthSlider }
     ] );
