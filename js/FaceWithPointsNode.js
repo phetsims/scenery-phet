be011cead0a898a01db0dd16650999f5d59730d6
Chris Malley <cmalley@pixelzoom.com>
2015-10-30 13:42:23 -0600

    #200 handle options properly in FaceWithPointsNode

--- js/FaceWithPointsNode.js
+++ js/FaceWithPointsNode.js
@@ -28,13 +28,13 @@ define( function( require ) {
    */
   function FaceWithPointsNode( options ) {
 
-    //TODO antipattern for extending options, see #200
-    // @private
-    this.options = _.extend( {
+    options = _.extend( {
       spacing: 2, // space between face and points
+
       // face options
       faceDiameter: 100,
       faceOpacity: 1, // 0-1, see scenery.Node.opacity
+
       // points options
       pointsAlignment: 'centerBottom', // 'centerBottom', 'rightBottom', 'rightCenter'
       pointsFont: new PhetFont( { size: 44, weight: 'bold' } ),
@@ -45,22 +45,27 @@ define( function( require ) {
       points: 0 // {number} the number of points
     }, options );
 
+    // @private options needed by prototype functions
+    this.pointsAlignment = options.pointsAlignment;
+    this.spacing = options.spacing;
+    this.showZeroPoints = options.showZeroPoints;
+
     // @private
-    this.faceNode = new FaceNode( this.options.faceDiameter, { opacity: this.options.faceOpacity } );
+    this.faceNode = new FaceNode( options.faceDiameter, { opacity: options.faceOpacity } );
 
     // @private
     this.pointsNode = new Text( '', {
-      font: this.options.pointsFont,
-      fill: this.options.pointsFill,
-      opacity: this.options.pointsOpacity,
-      stroke: this.options.pointsStroke,
+      font: options.pointsFont,
+      fill: options.pointsFill,
+      opacity: options.pointsOpacity,
+      stroke: options.pointsStroke,
       lineWidth: 1
     } );
 
-    this.options.children = [ this.faceNode, this.pointsNode ];
-    Node.call( this, this.options );
+    options.children = [ this.faceNode, this.pointsNode ];
+    Node.call( this, options );
 
-    this.setPoints( this.options.points );
+    this.setPoints( options.points );
   }
 
   return inherit( Node, FaceWithPointsNode, {
@@ -85,7 +90,7 @@ define( function( require ) {
 
     // @public sets the number of {number} points
     setPoints: function( points ) {
-      if ( points === 0 && !this.options.showZeroPoints ) {
+      if ( points === 0 && !this.showZeroPoints ) {
         this.pointsNode.text = '';
       }
       else if ( points < 0 ) {
@@ -100,29 +105,29 @@ define( function( require ) {
 
     // @private Adjusts location of the points to match the specified value of options.pointsAlignment.
     updatePointsLocation: function() {
-      switch( this.options.pointsAlignment ) {
+      switch( this.pointsAlignment ) {
 
         case 'centerBottom':
           this.pointsNode.centerX = this.faceNode.centerX;
-          this.pointsNode.top = this.faceNode.bottom + this.options.spacing;
+          this.pointsNode.top = this.faceNode.bottom + this.spacing;
           break;
 
         case 'rightBottom':
-          var position = new Vector2( this.faceNode.right + this.options.spacing, this.faceNode.centerY );
+          var position = new Vector2( this.faceNode.right + this.spacing, this.faceNode.centerY );
           position.rotate( Math.PI / 4 );
           this.pointsNode.left = position.x;
           this.pointsNode.centerY = position.y;
           break;
 
         case 'rightCenter':
-          this.pointsNode.left = this.faceNode.right + this.options.spacing;
+          this.pointsNode.left = this.faceNode.right + this.spacing;
           this.pointsNode.centerY = this.faceNode.centerY;
           break;
 
         // Add other alignments here as needed, please document in options.
 
         default:
-          throw new Error( 'unsupported pointsAlignment: ' + this.options.pointsAlignment );
+          throw new Error( 'unsupported pointsAlignment: ' + this.pointsAlignment );
       }
     }
   } );
