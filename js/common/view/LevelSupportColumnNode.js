6ef2f9ae8260a4e505c0744741161c038ccbdc6b
samreid <reids@colorado.edu>
2015-11-06 20:50:43 -0700

    updated copyright info using grunt update-copyright-dates, see  https://github.com/phetsims/chipper/issues/403

--- js/common/view/LevelSupportColumnNode.js
+++ js/common/view/LevelSupportColumnNode.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2013, University of Colorado Boulder
+// Copyright 2013-2015, University of Colorado Boulder
 
 /**
  * Node that represents a support column with a flat top in the view.
