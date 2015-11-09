e2da53239e5ba7d78c68c859fa2aae9b78e9fae7
jbphet <john.blanco@colorado.edu>
2015-11-09 14:53:04 -0700

    lint fix

--- js/StarNode.js
+++ js/StarNode.js
@@ -10,7 +10,6 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var Bounds2 = require( 'DOT/Bounds2' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Path = require( 'SCENERY/nodes/Path' );
