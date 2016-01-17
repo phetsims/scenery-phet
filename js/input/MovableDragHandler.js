176f89a6d0347eddd1338e46f58ae79ae6565cf4
samreid <reids@colorado.edu>
2016-01-17 14:22:41 -0700

    Added TandemDragHandler, see https://github.com/phetsims/together/issues/254

--- js/input/MovableDragHandler.js
+++ js/input/MovableDragHandler.js
@@ -12,8 +12,7 @@ define( function( require ) {
   var Bounds2 = require( 'DOT/Bounds2' );
   var inherit = require( 'PHET_CORE/inherit' );
   var ModelViewTransform2 = require( 'PHETCOMMON/view/ModelViewTransform2' );
-  var SimpleDragHandler = require( 'SCENERY/input/SimpleDragHandler' );
-  var Events = require( 'AXON/Events' );
+  var TandemDragHandler = require( 'SCENERY_PHET/input/TandemDragHandler' );
   var sceneryPhet = require( 'SCENERY_PHET/sceneryPhet' );
 
   /**
@@ -26,18 +25,16 @@ define( function( require ) {
 
     var self = this;
 
-    // @private Events for signifying when processing callbacks starts/ends
-    this.events = new Events();
-
     options = _.extend( {
       dragBounds: Bounds2.EVERYTHING, // {Bounds2} dragging will be constrained to these bounds, in model coordinate frame
       modelViewTransform: ModelViewTransform2.createIdentity(), // {ModelViewTransform2} defaults to identity
       startDrag: function( event ) {},  // use this to do something at the start of dragging, like moving a node to the foreground
       endDrag: function( event ) {},  // use this to do something at the end of dragging, like 'snapping'
       onDrag: function( event ) {}, // use this to do something every time drag is called, such as notify that a user has modified the position
-      targetNode: null // MovableDragHandler defaults to using event.currentTarget for its reference coordinate frame, but
+      targetNode: null, // MovableDragHandler defaults to using event.currentTarget for its reference coordinate frame, but
       // the target can be overriden here.  This is useful when you need to attach a listener to a sub-
       // component of a node hierarchy
+      tandem: null
     }, options );
 
     this.locationProperty = locationProperty; // @private
@@ -49,16 +46,12 @@ define( function( require ) {
     // @private - note where the drag started
     this.movableDragHandlerStart = function( event ) {
 
-      self.events.trigger1( 'startedCallbacksForDragStarted', locationProperty.get() );
-
       options.startDrag( event );
 
       // Note the options.startDrag can change the locationProperty, so read it again above, see https://github.com/phetsims/scenery-phet/issues/157
       var targetNode = options.targetNode || event.currentTarget;
       var location = self._modelViewTransform.modelToViewPosition( locationProperty.get() );
       startOffset = targetNode.globalToParentPoint( event.pointer.point ).minus( location );
-
-      self.events.trigger0( 'endedCallbacksForDragStarted' );
     };
 
     // @private - change the location, adjust for starting offset, constrain to drag bounds
@@ -68,36 +61,31 @@ define( function( require ) {
       var parentPoint = targetNode.globalToParentPoint( event.pointer.point ).minus( startOffset );
       var location = self._modelViewTransform.viewToModelPosition( parentPoint );
       location = self._dragBounds.closestPointTo( location );
-      self.events.trigger1( 'startedCallbacksForDragged', location );
 
       locationProperty.set( location );
 
       options.onDrag( event );
-
-      self.events.trigger0( 'endedCallbacksForDragged' );
     };
 
     // @private
     this.movableDragHandlerEnd = function( event ) {
-      self.events.trigger1( 'startedCallbacksForDragEnded', locationProperty.get() );
       options.endDrag( event );
-      self.events.trigger0( 'endedCallbacksForDragEnded' );
     };
-    SimpleDragHandler.call( this, {
 
+    TandemDragHandler.call( this, {
+      tandem: options.tandem,
       allowTouchSnag: true,
-
       start: this.movableDragHandlerStart,
-
       drag: this.movableDragHandlerDrag,
-
       end: this.movableDragHandlerEnd
     } );
   }
 
   sceneryPhet.register( 'MovableDragHandler', MovableDragHandler );
 
-  return inherit( SimpleDragHandler, MovableDragHandler, {
+  // MovableDragHandler extends TandemDragHandler to facilitate phet-io instrumentation.  If no tandem is provided,
+  // then no additional work is done.
+  return inherit( TandemDragHandler, MovableDragHandler, {
 
     /**
      * Sets the dragBounds.
---
+++ js/input/TandemDragHandler.js
@@ -0,0 +1,91 @@
+// Copyright 2015, University of Colorado Boulder
+
+/**
+ * SimpleDragHandler subclass that adds tandem registration and together event emission.
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Emitter = require( 'AXON/Emitter' );
+  var SimpleDragHandler = require( 'SCENERY/input/SimpleDragHandler' );
+  var sceneryPhet = require( 'SCENERY_PHET/sceneryPhet' );
+  var Brand = require( 'BRAND/Brand' );
+
+  /**
+   * Even though the tandem is required, it is passed via options for compatibility with SimpleDragHandler
+   * @param options
+   * @constructor
+   */
+  function TandemDragHandler( options ) {
+
+    // Generate all emitters in every case to minimize the number of hidden classes,
+    // see http://www.html5rocks.com/en/tutorials/speed/v8/
+    this.startedCallbacksForDragStartedEmitter = new Emitter(); // @public (together)
+    this.endedCallbacksForDragStartedEmitter = new Emitter(); // @public (together)
+
+    this.startedCallbacksForDraggedEmitter = new Emitter(); // @public (together)
+    this.endedCallbacksForDraggedEmitter = new Emitter(); // @public (together)
+
+    this.startedCallbacksForDragEndedEmitter = new Emitter(); // @public (together)
+    this.endedCallbacksForDragEndedEmitter = new Emitter(); // @public (together)
+
+    // For non-phet-io brands, skip tandem callbacks to save CPU
+    if ( Brand.id === 'phet-io' ) {
+      var tandemDragHandler = this;
+
+      var newOptions = _.extend( {}, options );
+
+      if ( options.start ) {
+
+        newOptions.start = function( event, trail ) {
+          tandemDragHandler.startedCallbacksForDragStartedEmitter.emit2( event.pointer.point.x, event.pointer.point.y );
+          options.start( event, trail );
+          tandemDragHandler.endedCallbacksForDragStartedEmitter.emit();
+        };
+      }
+
+      if ( options.drag ) {
+
+        newOptions.drag = function( event, trail ) {
+          tandemDragHandler.startedCallbacksForDraggedEmitter.emit2( event.pointer.point.x, event.pointer.point.y );
+          options.drag( event, trail );
+          tandemDragHandler.endedCallbacksForDraggedEmitter.emit();
+        };
+      }
+
+      if ( options.end ) {
+
+        newOptions.end = function( event, trail ) {
+          tandemDragHandler.startedCallbacksForDragEndedEmitter.emit2( event.pointer.point.x, event.pointer.point.y );
+          options.end( event, trail );
+          tandemDragHandler.endedCallbacksForDragEndedEmitter.emit();
+        };
+      }
+      SimpleDragHandler.call( this, newOptions );
+
+      options.tandem && options.tandem.addInstance( this );
+
+      // @private
+      this.disposeTandemDragHandler = function() {
+        options.tandem && options.tandem.removeInstance( tandemDragHandler );
+      };
+    }
+    else {
+      SimpleDragHandler.call( this, options );
+      this.disposeTandemDragHandler = function() {}; // nothing to clean up in this case
+    }
+  }
+
+  sceneryPhet.register( 'TandemDragHandler', TandemDragHandler );
+
+  return inherit( SimpleDragHandler, TandemDragHandler, {
+
+    // @public
+    dispose: function() {
+      this.disposeTandemDragHandler();
+    }
+  } );
+} );
