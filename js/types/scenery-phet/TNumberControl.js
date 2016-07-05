ac56b9007d5721e1bc64c6b86451b096abb68613
samreid <reids@colorado.edu>
2016-07-05 10:43:11 -0600

    Added tandems for NumberControl and NumberDisplay, see https://github.com/phetsims/scenery-phet/issues/241

---
+++ js/types/scenery-phet/TNumberControl.js
@@ -0,0 +1,38 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Andrew Adare
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var assertInstanceOf = require( 'PHET_IO/assertions/assertInstanceOf' );
+  var phetioInherit = require( 'PHET_IO/phetioInherit' );
+  var phetioNamespace = require( 'PHET_IO/phetioNamespace' );
+  var TNode = require( 'PHET_IO/types/scenery/nodes/TNode' );
+  var TButton = require( 'PHET_IO/types/sun/buttons/TButton' );
+  var THSlider = require( 'PHET_IO/types/sun/THSlider' );
+  var TTandemText = require( 'PHET_IO/types/tandem/scenery/nodes/TTandemText' );
+  var TNumberDisplay = require( 'PHET_IO/types/scenery-phet/TNumberDisplay' );
+
+  var TNumberControl = phetioInherit( TNode, 'TNumberControl', function( numberControl, phetioID ) {
+    TNode.call( this, numberControl, phetioID );
+    assertInstanceOf( numberControl, phet.sceneryPhet.NumberControl );
+  }, {}, {
+    api: {
+      leftArrowButton: TButton,
+      rightArrowButton: TButton,
+      slider: THSlider,
+      titleNode: TTandemText,
+      numberDisplay: TNumberDisplay
+    },
+    documentation: 'A number control with a title, slider and +/- buttons'
+  } );
+
+  phetioNamespace.register( 'TNumberControl', TNumberControl );
+
+  return TNumberControl;
+} );
---
+++ js/types/scenery-phet/TNumberDisplay.js
@@ -0,0 +1,33 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Andrew Adare
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var assertInstanceOf = require( 'PHET_IO/assertions/assertInstanceOf' );
+  var phetioInherit = require( 'PHET_IO/phetioInherit' );
+  var phetioNamespace = require( 'PHET_IO/phetioNamespace' );
+  var TNode = require( 'PHET_IO/types/scenery/nodes/TNode' );
+  var TButton = require( 'PHET_IO/types/sun/buttons/TButton' );
+  var THSlider = require( 'PHET_IO/types/sun/THSlider' );
+  var TTandemText = require( 'PHET_IO/types/tandem/scenery/nodes/TTandemText' );
+
+  var TNumberDisplay = phetioInherit( TNode, 'TNumberControl', function( numberControl, phetioID ) {
+    TNode.call( this, numberControl, phetioID );
+    assertInstanceOf( numberControl, phet.sceneryPhet.NumberDisplay );
+  }, {}, {
+    api: {
+      valueNode: TTandemText
+    },
+    documentation: 'A numeric readout with a background'
+  } );
+
+  phetioNamespace.register( 'TNumberDisplay', TNumberDisplay );
+
+  return TNumberDisplay;
+} );
