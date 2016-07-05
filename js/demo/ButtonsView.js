7175c75875648e00ff63187e0d11081c048e4f05
Chris Malley <cmalley@pixelzoom.com>
2016-07-05 14:39:42 -0600

    add buttons to scenery-phet demo

--- js/demo/ButtonsView.js
+++ js/demo/ButtonsView.js
@@ -10,17 +10,28 @@ define( function( require ) {
   'use strict';
 
   // modules
+  var ArrowButton = require( 'SCENERY_PHET/buttons/ArrowButton' );
+  var BackButton = require( 'SCENERY_PHET/buttons/BackButton' );
+  var CloseButton = require( 'SCENERY_PHET/buttons/CloseButton' );
+  var EraserButton = require( 'SCENERY_PHET/buttons/EraserButton' );
   var EyeToggleButton = require( 'SCENERY_PHET/buttons/EyeToggleButton' );
+  var HBox = require( 'SCENERY/nodes/HBox' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var PlayPauseButton = require( 'SCENERY_PHET/buttons/PlayPauseButton' );
   var PropertySet = require( 'AXON/PropertySet' );
+  var RecordStopButton = require( 'SCENERY_PHET/buttons/RecordStopButton' );
   var RefreshButton = require( 'SCENERY_PHET/buttons/RefreshButton' );
+  var ResetButton = require( 'SCENERY_PHET/buttons/ResetButton' );
   var ResetAllButton = require( 'SCENERY_PHET/buttons/ResetAllButton' );
+  var RewindButton = require( 'SCENERY_PHET/buttons/RewindButton' );
   var ScreenView = require( 'JOIST/ScreenView' );
   var SoundToggleButton = require( 'SCENERY_PHET/buttons/SoundToggleButton' );
   var StarButton = require( 'SCENERY_PHET/buttons/StarButton' );
+  var StepButton = require( 'SCENERY_PHET/buttons/StepButton' );
   var TimerToggleButton = require( 'SCENERY_PHET/buttons/TimerToggleButton' );
   var sceneryPhet = require( 'SCENERY_PHET/sceneryPhet' );
   var VBox = require( 'SCENERY/nodes/VBox' );
+  var ZoomButton = require( 'SCENERY_PHET/buttons/ZoomButton' );
 
   /**
    * @constructor
@@ -29,51 +40,121 @@ define( function( require ) {
 
     ScreenView.call( this );
 
-    // If you add a button that requires a Property, add it here, so that resetAllButton functions properly.
-    var buttonProperties = new PropertySet( {
-      soundEnabled: true,
-      timerEnabled: true,
-      eyeOpen: true
+    //------------------------------------------------------------------------------------------------------
+    // Push buttons
+
+    var arrowButton = new ArrowButton( 'left', function() { console.log( 'ArrowButton pressed' ); } );
+
+    var backButton = new BackButton( {
+      listener: function() { console.log( 'BackButton pressed' ); }
     } );
 
-    // Refresh button
+    var closeButton = new CloseButton( {
+      listener: function() { console.log( 'CloseButton pressed' ); }
+    } );
+
+    var eraserButton = new EraserButton( {
+      listener: function() { console.log( 'EraserButton pressed' ); }
+    } );
+
     var refreshButton = new RefreshButton( {
       listener: function() { console.log( 'RefreshButton pressed' ); }
     } );
 
-    // Star button
+    var resetButton = new ResetButton( {
+      listener: function() { console.log( 'ResetButton pressed' ); }
+    } );
+
+    var rewindButton = new RewindButton( {
+      listener: function() { console.log( 'RewindButton pressed' ); }
+    } );
+
     var starButton = new StarButton( {
       listener: function() { console.log( 'StarButton pressed' ); }
     } );
 
-    // Sound toggle button
+    var stepButton = new StepButton( {
+      listener: function() { console.log( 'StepButton pressed' ); }
+    } );
+
+    var zoomButton = new ZoomButton( {
+      listener: function() { console.log( 'ZoomButton pressed' ); }
+    } );
+
+    // Push buttons
+    var pushButtons = new VBox( {
+      children: [
+        arrowButton,
+        backButton,
+        closeButton,
+        eraserButton,
+        refreshButton,
+        resetButton,
+        rewindButton,
+        starButton,
+        stepButton,
+        zoomButton
+      ],
+      spacing: 10,
+      align: 'center',
+      center: this.layoutBounds.center
+    } );
+
+    //------------------------------------------------------------------------------------------------------
+    // Toggle buttons
+
+    // Add button properties here, so that resetAllButton functions properly.
+    var buttonProperties = new PropertySet( {
+      eyeOpen: true,
+      playing: true,
+      recording: true,
+      soundEnabled: true,
+      timerEnabled: true
+    } );
+
+    var eyeButton = new EyeToggleButton( buttonProperties.eyeOpenProperty );
+    buttonProperties.eyeOpenProperty.lazyLink( function( eyeOpen ) {
+      console.log( 'eyeOpen=' + eyeOpen );
+    } );
+
+    var playPauseButton = new PlayPauseButton( buttonProperties.playingProperty );
+    buttonProperties.playingProperty.lazyLink( function( playing ) {
+      console.log( 'playing=' + playing );
+    } );
+
+    var recordStopButton = new RecordStopButton( buttonProperties.recordingProperty );
+    buttonProperties.recordingProperty.lazyLink( function( recording ) {
+      console.log( 'recording=' + recording );
+    } );
+
     var soundToggleButton = new SoundToggleButton( buttonProperties.soundEnabledProperty );
     buttonProperties.soundEnabledProperty.lazyLink( function( soundEnabled ) {
       console.log( 'soundEnabled=' + soundEnabled );
     } );
 
-    // Timer toggle button
     var timerToggleButton = new TimerToggleButton( buttonProperties.timerEnabledProperty );
     buttonProperties.timerEnabledProperty.lazyLink( function( timerEnabled ) {
       console.log( 'timerEnabled=' + timerEnabled );
     } );
 
-    // Eye toggle button
-    var eyeToggleButton = new EyeToggleButton( buttonProperties.eyeOpenProperty );
-    buttonProperties.eyeOpenProperty.lazyLink( function( eyeOpen ) {
-      console.log( 'eyeOpen=' + eyeOpen );
-    } );
-
-    this.addChild( new VBox( {
+    // Toggle button
+    var toggleButtons = new VBox( {
       children: [
-        refreshButton,
-        starButton,
+        eyeButton,
+        playPauseButton,
+        recordStopButton,
         soundToggleButton,
-        timerToggleButton,
-        eyeToggleButton
+        timerToggleButton
       ],
       spacing: 10,
       align: 'center',
+      center: this.layoutBounds.center
+    } );
+
+    this.addChild( new HBox( {
+      children: [ pushButtons, toggleButtons ],
+      spacing: 100,
+      align: 'top',
       center: this.layoutBounds.center
     } ) );
 
