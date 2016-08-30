25be8c387ca5c75637947d60a560d80221ec1d92
Chris Malley <cmalley@pixelzoom.com>
2016-08-30 09:21:19 -0600

    add option to change highlight diameter, #267

--- js/ShadedSphereNode.js
+++ js/ShadedSphereNode.js
@@ -25,16 +25,25 @@ define( function( require ) {
       mainColor: 'gray',
       highlightColor: 'white',
       shadowColor: 'black',
+      highlightDiameter: 0.5 * diameter,
       highlightXOffset: -0.4, // x-offset of the highlight from the center of the sphere, percentage of radius, [-1,1]
       highlightYOffset: -0.4  // y-offset of the highlight from the center of the sphere, percentage of radius, [-1,1]
     }, options );
+
+    // validate option values
+    assert && assert( options.highlightDiameter < diameter,
+      'highlightDiameter must be < diameter: ' + options.highlightDiameter );
+    assert && assert( options.highlightXOffset >= -1 && options.highlightXOffset <= 1,
+      'highlightXOffset out of range: ' + options.highlightXOffset );
+    assert && assert( options.highlightYOffset >= -1 && options.highlightYOffset <= 1,
+      'highlightYOffset out of range: ' + options.highlightYOffset );
 
     var radius = diameter / 2;
     var highlightX = radius * options.highlightXOffset;
     var highlightY = radius * options.highlightYOffset;
     options.fill = new RadialGradient( highlightX, highlightY, 0, highlightX, highlightY, diameter )
       .addColorStop( 0, options.highlightColor )
-      .addColorStop( 0.5, options.mainColor )
+      .addColorStop( options.highlightDiameter / diameter, options.mainColor )
       .addColorStop( 1, options.shadowColor );
 
     Circle.call( this, radius, options );
