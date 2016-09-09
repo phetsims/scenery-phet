9d87b6245aef39b5d14da2b9b20a930c2be7ce93
Chris Malley <cmalley@pixelzoom.com>
2016-09-08 23:26:33 -0600

    change clients to use new Screen constructor signature, https://github.com/phetsims/joist/issues/339, https://github.com/phetsims/joist/issues/351

--- js/scenery-phet-main.js
+++ js/scenery-phet-main.js
@@ -21,15 +21,16 @@ define( function( require ) {
   // strings
   var sceneryPhetTitleString = require( 'string!SCENERY_PHET/scenery-phet.title' );
 
+  // constants
+  var BACKGROUND_COLOR = phet.chipper.getQueryParameter( 'backgroundColor' ) || 'white';
+
   var simOptions = {
     credits: {
       leadDesign: 'PhET'
     }
   };
 
-  var backgroundColor = phet.chipper.getQueryParameter( 'backgroundColor' ) || 'white';
-  var screenOptions = { backgroundColor: backgroundColor };
-
+  // Creates a rectangle filled with a specified color
   var createScreenIcon = function( color ) {
     return new Rectangle( 0, 0, Screen.HOME_SCREEN_ICON_SIZE.width, Screen.HOME_SCREEN_ICON_SIZE.height, { fill: color } );
   };
@@ -37,29 +38,45 @@ define( function( require ) {
   // Create and start sim
   SimLauncher.launch( function() {
     new Sim( sceneryPhetTitleString, [
-      new Screen( 'Buttons',
-        createScreenIcon( 'red' ),
+
+      new Screen(
         function() {return {};},
         function( model ) {return new ButtonsView();},
-        screenOptions
+        {
+          name: 'Buttons',
+          backgroundColor: BACKGROUND_COLOR,
+          homeScreenIcon: createScreenIcon( 'red' )
+        }
       ),
-      new Screen( 'Sliders',
-        createScreenIcon( 'yellow' ),
+
+      new Screen(
         function() {return {};},
         function( model ) {return new SlidersView();},
-        screenOptions
+        {
+          name: 'Sliders',
+          backgroundColor: BACKGROUND_COLOR,
+          homeScreenIcon: createScreenIcon( 'yellow' )
+        }
       ),
-      new Screen( 'Components',
-        createScreenIcon( 'orange' ),
+
+      new Screen(
         function() {return {};},
         function( model ) {return new ComponentsView();},
-        screenOptions
+        {
+          name: 'Components',
+          backgroundColor: BACKGROUND_COLOR,
+          homeScreenIcon: createScreenIcon( 'orange' )
+        }
       ),
-      new Screen( 'Spring',
-        createScreenIcon( 'blue' ),
+
+      new Screen(
         function() {return {};},
         function( model ) {return new SpringView();},
-        screenOptions
+        {
+          name: 'Spring',
+          backgroundColor: BACKGROUND_COLOR,
+          homeScreenIcon: createScreenIcon( 'blue' )
+        }
       )
     ], simOptions ).start();
   } );
