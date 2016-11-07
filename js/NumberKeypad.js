4838dcf2d9cc0ff424e448c6bbdffe98edee8749
Chris Malley <cmalley@pixelzoom.com>
2016-11-07 15:55:38 -0700

    lots of cleanup, consistent names, addressed a couple of TODOs

--- js/NumberKeypad.js
+++ js/NumberKeypad.js
@@ -1,8 +1,8 @@
 // Copyright 2015, University of Colorado Boulder
 
 /**
- * A scenery node that looks like a key pad and allows the user to enter digits.  The entered digits are not displayed
- * by this node - it is intended to be used in conjunction with a separate display of some sort.
+ * A scenery node that looks like a key pad and allows the user to enter a number. The entered number is not
+ * displayed by this node - it is intended to be used in conjunction with a separate display of some sort.
  *
  * @author John Blanco
  * @author Andrey Zelenkov (MLearner)
@@ -35,6 +35,8 @@ define( function( require ) {
       xSpacing: 10,
       ySpacing: 10,
       keyColor: 'white',
+
+      //TODO replace with a function that accepts or rejects entry
       maxDigits: 8, // Maximum number of digits that the user may enter
       digitStringProperty: new Property( '' ) // create a digit string property or use the one supplied by the client
     }, options );
@@ -49,9 +51,8 @@ define( function( require ) {
     // @private - flag used when arming the keypad to start over on the next key stroke
     this.armedForNewEntry = false;
 
-    //TODO how about naming this buttonOptions?
-    // bundle the various options that control the button appearance into a "button specification" or buttonKeySpec
-    var buttonKeySpec = {
+    // options for keys
+    var keyOptions = {
       minWidth: options.minButtonWidth,
       minHeight: options.minButtonHeight,
       baseColor: options.keyColor,
@@ -59,15 +60,15 @@ define( function( require ) {
       maxDigits: options.maxDigits
     };
 
-    // create the backspace button
+    // create the backspace key
     var backspaceIcon = new BackspaceIcon();
     backspaceIcon.scale( Math.min( options.minButtonWidth / backspaceIcon.width * 0.7, ( options.minButtonHeight * 0.65 ) / backspaceIcon.height ) );
-    var backspaceButton = new RectangularPushButton( {
+    var backspaceKey = new RectangularPushButton( {
       content: backspaceIcon,
-      minWidth: buttonKeySpec.minWidth,
-      minHeight: buttonKeySpec.minHeight,
+      minWidth: keyOptions.minWidth,
+      minHeight: keyOptions.minHeight,
       xMargin: 1,
-      baseColor: buttonKeySpec.baseColor,
+      baseColor: keyOptions.baseColor,
       listener: function() {
         if ( self.digitStringProperty.value.length > 0 ) {
 
@@ -85,19 +86,21 @@ define( function( require ) {
       }
     } );
 
-    // create the bottom row of buttons, which can vary based on options
-    var bottomButtonRowChildren = [];
+    // create the bottom row of keys, which can vary based on options
+    var bottomRowChildren = [];
     if ( options.decimalPointKey ) {
+
       // add a decimal point key plus a normal width zero key
-      bottomButtonRowChildren.push( createNumberKey( '.', this, buttonKeySpec ) );
-      bottomButtonRowChildren.push( createNumberKey( 0, this, buttonKeySpec ) );
+      bottomRowChildren.push( createKey( '.', this, keyOptions ) );
+      bottomRowChildren.push( createKey( '0', this, keyOptions ) );
     }
     else {
+
       // add a double-width zero key instead of the decimal point key
-      var doubleRowButtonKeySpec = _.extend( {}, buttonKeySpec, { minWidth: buttonKeySpec.minWidth * 2 + options.xSpacing } );
-      bottomButtonRowChildren.push( createNumberKey( 0, this, doubleRowButtonKeySpec ) );
+      var doubleRowButtonKeySpec = _.extend( {}, keyOptions, { minWidth: keyOptions.minWidth * 2 + options.xSpacing } );
+      bottomRowChildren.push( createKey( '0', this, doubleRowButtonKeySpec ) );
     }
-    bottomButtonRowChildren.push( backspaceButton );
+    bottomRowChildren.push( backspaceKey );
 
     // add the rest of the keys
     VBox.call( this, {
@@ -106,28 +109,31 @@ define( function( require ) {
         new HBox( {
           spacing: options.xSpacing,
           children: [
-            createNumberKey( 7, this, buttonKeySpec ),
-            createNumberKey( 8, this, buttonKeySpec ),
-            createNumberKey( 9, this, buttonKeySpec )
+            createKey( '7', this, keyOptions ),
+            createKey( '8', this, keyOptions ),
+            createKey( '9', this, keyOptions )
           ]
         } ),
         new HBox( {
           spacing: options.xSpacing,
           children: [
-            createNumberKey( 4, this, buttonKeySpec ),
-            createNumberKey( 5, this, buttonKeySpec ),
-            createNumberKey( 6, this, buttonKeySpec )
+            createKey( '4', this, keyOptions ),
+            createKey( '5', this, keyOptions ),
+            createKey( '6', this, keyOptions )
           ]
         } ),
         new HBox( {
           spacing: options.xSpacing,
           children: [
-            createNumberKey( 1, this, buttonKeySpec ),
-            createNumberKey( 2, this, buttonKeySpec ),
-            createNumberKey( 3, this, buttonKeySpec )
+            createKey( '1', this, keyOptions ),
+            createKey( '2', this, keyOptions ),
+            createKey( '3', this, keyOptions )
           ]
         } ),
-        new HBox( { spacing: options.xSpacing, children: bottomButtonRowChildren } )
+        new HBox( {
+          spacing: options.xSpacing,
+          children: bottomRowChildren
+        } )
       ]
     } );
 
@@ -137,52 +143,52 @@ define( function( require ) {
 
   sceneryPhet.register( 'NumberKeypad', NumberKeypad );
 
-  //TODO how about some JSdoc here?
-  //TODO in the case of createNumberKey( '.', ...), parameter 'number' is not a number
-  // convenience function for creating the buttons that act as the individual keys
-  function createNumberKey( number, parentKeypad, buttonSpec ) {
+  /**
+   * Creates a key for the keypad.
+   *
+   * @param {string} keyString - string that appears on the key
+   * @param {NumberKeypad} keypad
+   * @param {Object} keyOptions - see RectangularPushButton.options
+   * @returns {*}
+   */
+  function createKey( keyString, keypad, keyOptions ) {
     return new RectangularPushButton( {
 
-      //TODO 'number' is not always a number
-      content: new Text( number.toString(), { font: buttonSpec.font } ),
-      baseColor: buttonSpec.baseColor,
-      minWidth: buttonSpec.minWidth,
-      minHeight: buttonSpec.minHeight,
+      content: new Text( keyString, { font: keyOptions.font } ),
+      baseColor: keyOptions.baseColor,
+      minWidth: keyOptions.minWidth,
+      minHeight: keyOptions.minHeight,
       xMargin: 5,
       yMargin: 5,
 
       //TODO why do we have an instance of this complex function for every button? Buttons should simply provide their value when pressed.
       listener: function() {
 
-        var decimalIndex = parentKeypad.digitStringProperty.value.indexOf( '.' );
+        var decimalIndex = keypad.digitStringProperty.value.indexOf( '.' );
 
         //TODO bug here? Type '5.67' and digitLength is 2
-        var digitLength = ( decimalIndex === -1 ? parentKeypad.digitStringProperty.value.length :
-                                                  parentKeypad.digitStringProperty.value.length -1 );
+        var digitLength = ( decimalIndex === -1 ? keypad.digitStringProperty.value.length :
+                                                  keypad.digitStringProperty.value.length -1 );
 
         // If armed for new entry, clear the existing string.
-        if ( parentKeypad.armedForNewEntry ) {
-          parentKeypad.digitStringProperty.reset();
-          parentKeypad.armedForNewEntry = false;
+        if ( keypad.armedForNewEntry ) {
+          keypad.digitStringProperty.reset();
+          keypad.armedForNewEntry = false;
         }
 
-        //TODO second time you've called number.toString
-        // the new key entry
-        var numberString = number.toString();
-
         // Add the digit to the string, but limit the length and prevent multiple leading zeros.
-        if ( parentKeypad.digitStringProperty.value === '0' ) {
-          if ( numberString !== 0 ) {
+        if ( keypad.digitStringProperty.value === '0' ) {
+          if ( keyString !== '0' ) {
             // Replace the leading 0 with this digit.
-            parentKeypad.digitStringProperty.value = numberString;
+            keypad.digitStringProperty.value = keyString;
           }
           // else ignore the additional zero
         }
-        else if ( digitLength < buttonSpec.maxDigits ) {
+        else if ( digitLength < keyOptions.maxDigits ) {
 
           // only allow a single decimal point
-          if ( numberString !== '.' || parentKeypad.digitStringProperty.value.indexOf( '.' ) === -1 ) {
-            parentKeypad.digitStringProperty.value += numberString;
+          if ( keyString !== '.' || keypad.digitStringProperty.value.indexOf( '.' ) === -1 ) {
+            keypad.digitStringProperty.value += keyString;
           }
         }
       }
