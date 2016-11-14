770215021a11bc7ec5c9b558a9826cc4e3657ac4
Jonathan Olson <olsonsjc@gmail.com>
2016-11-14 14:24:25 -0700

    Initial ColorProfile conversion away from PropertySet to Property, see https://github.com/phetsims/scenery-phet/issues/278

--- js/ColorProfile.js
+++ js/ColorProfile.js
@@ -1,12 +1,10 @@
 // Copyright 2015, University of Colorado Boulder
 
 /**
- * Abstract class for color profiles for simulations. See GravityAndOrbitsColors for an example.
- * This file was modelled after MoleculeShapesColors.
+ * Abstract type for handling color profiles in simulations. Handles multiple color profiles (by string name), and controls color property values
+ * based on the active color profile.
  *
- * ColorProfile gives the option to synchronize the sim running in an iframe with a color picker.
- * To create a file with this capacity, model it after gravity-and-orbits-colors.html, with the colors array
- * changed to match the color profile you are using, and the title and iframe src changed.
+ * It also contains hooks that allow synchronizing the colors over iframe communication, so that colors can be controlled by a color picker.
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  * @author Aaron Davis
@@ -17,73 +15,69 @@ define( function( require ) {
   // modules
   var Color = require( 'SCENERY/util/Color' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var PropertySet = require( 'AXON/PropertySet' );
+  var Property = require( 'AXON/Property' );
   var sceneryPhet = require( 'SCENERY_PHET/sceneryPhet' );
 
   /**
+   * @constructor
+   *
    * @param {Object} colors - object hash, whose property names become Property names.
    *                          Each property is another hash, whose properties are the colors for each scheme.
    *                          (Confusing? You bet. See https://github.com/phetsims/scenery-phet/issues/277)
-   * @constructor
+   * @param {Array.<string>} profileNames - A list of valid profile names that can be taken.
    */
-  function ColorProfile( colors ) {
-
+  function ColorProfile( colors, profileNames ) {
     var self = this;
 
-    this.colors = colors; // @private
+    // @public {Property.<string>} - The current profile name. Change this Property's value to change which profile is currently active.
+    //                               'default' will use all default colors, and 'projector' is a common color profile that is also used.
+    var profileNameProperty = this.profileNameProperty = new Property( 'default', {
+      validValues: profileNames
+    } );
 
-    // initial properties object, to load into the PropertySet (so reset works nicely)
-    // all properties are @public (read-only) except profileNameProperty, which is @public
-    var initialProperties = {
-      profileName: 'default' // {string} @public - name of the color profile, e.g. 'default' or 'projector'
-    };
+    Object.keys( colors ).sort().forEach( function( key ) {
+      if ( colors.hasOwnProperty( key ) ) {
+        // Turn strings/hex to Color objects
+        var colorMap = _.mapValues( colors[ key ], Color.toColor );
 
-    for ( var key in colors ) {
-      assert && assert( colors[ key ].hasOwnProperty( 'default' ), 'missing default color for "' + key + '"' );
+        assert && assert( colorMap.hasOwnProperty( 'default' ), 'missing default color for ColorProfile: ' + key );
+        assert && assert( key !== 'profileName', 'Unlikely, but would have hilarious consequences since we would overwrite profileNameProperty' );
 
-      // convert all color strings to Color objects
-      for ( var color in colors[ key ] ) {
-        colors[ key ][ color ] = Color.toColor( colors[ key ][ color ] );
+        // Set the property on the color profile
+        var property = self[ key + 'Property' ] = new Property( colorMap.default );
+
+        // Update our property on profile name changes
+        profileNameProperty.lazyLink( function( profileName ) {
+          property.value = colorMap[ profileName ] || colorMap.default;
+        } );
+
+        // Communicate color changes to the iframe
+        property.link( function( color ) {
+          self.reportColor( key );
+        } );
       }
-      initialProperties[ key ] = colors[ key ].default;
-    }
-    PropertySet.call( this, initialProperties );
+    } );
 
     // receives iframe communication to set a color
     window.addEventListener( 'message', function( evt ) {
       var data = JSON.parse( evt.data );
       if ( data.type === 'setColor' ) {
-        self[ data.name ] = new Color( data.value );
+        self[ data.name + 'Property' ].value = new Color( data.value );
       }
     } );
-
-    // @public Applies all colors for the specific named color scheme, ignoring colors that aren't specified for it.
-    this.profileNameProperty.link( function( profileName ) {
-      for ( var key in self.colors ) {
-        var oldColor = self[ key ];
-        var colorObject = self.colors[ key ];
-        var newColor = ( profileName in colorObject ) ? colorObject[ profileName ] : colorObject.default;
-        if ( !newColor.equals( oldColor ) ) {
-          self[ key ] = newColor;
-          self.reportColor( key );
-        }
-      }
-      self.trigger( 'profileChanged' );
-    } );
-
-    // initial communication
-    for ( var colorName in colors ) {
-      this.reportColor( colorName );
-    }
   }
 
   sceneryPhet.register( 'ColorProfile', ColorProfile );
 
-  return inherit( PropertySet, ColorProfile, {
-
-    // @public sends iframe communication to report the current color for the key name
+  return inherit( Object, ColorProfile, {
+    /**
+     * Sends color change events as iframe messages, so that a container can be notified (and possibly update color pickers).
+     * @private
+     *
+     * @param {string} key - The color name that was changed
+     */
     reportColor: function( key ) {
-      var hexColor = this[ key ].toNumber().toString( 16 );
+      var hexColor = this[ key + 'Property' ].value.toNumber().toString( 16 );
       while ( hexColor.length < 6 ) {
         hexColor = '0' + hexColor;
       }
