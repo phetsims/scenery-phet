e24f88145177d8d358bdf5d30cb08aa1a6637a48
zepumph <michael.kauzmann@colorado.edu>
2017-10-31 16:10:53 -0800

    add getters/setters to TUtteranceQueue, https://github.com/phetsims/scenery-phet/issues/342

--- js/accessibility/TUtteranceQueue.js
+++ js/accessibility/TUtteranceQueue.js
@@ -57,6 +57,33 @@ define( function( require ) {
       },
       documentation: 'Set whether the utteranceQueue will be muted or not. If muted, utterances still move through the ' +
                      'queue but will not be read by screen readers.'
+    },
+    getMuted: {
+      returnType: TBoolean,
+      parameterTypes: [ TVoid ],
+      implementation: function( ) {
+        return this.instance.muted();
+      },
+      documentation: 'Get whether the utteranceQueue is muted. If muted, utterances still move through the ' +
+                     'queue but will not be read by screen readers.'
+    },
+    setEnabled: {
+      returnType: TVoid,
+      parameterTypes: [ TBoolean ],
+      implementation: function( enabled ) {
+        this.instance.enabled( enabled );
+      },
+      documentation: 'Set whether the utteranceQueue will be enabled or not. When enabled, Utterances cannot be added to ' +
+                     'the queue, and the Queue cannot be cleared. Also nothing will be sent to assistive technology.'
+    },
+    getEnabled: {
+      returnType: TBoolean,
+      parameterTypes: [ TVoid ],
+      implementation: function( ) {
+        return this.instance.enabled();
+      },
+      documentation: 'Get whether the utteranceQueue is enabled. When enabled, Utterances cannot be added to ' +
+                     'the queue, and the Queue cannot be cleared. Also nothing will be sent to assistive technology.'
     }
   }, {
     documentation: 'Manages a queue of Utterances that are read in order by a screen reader.',
