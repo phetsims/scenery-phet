5d1c0916c42846b09230f6680af1c73a809a6dcc
Chris Malley <cmalley@pixelzoom.com>
2018-03-27 12:38:05 -0600

    factor out InfoButton, https://github.com/phetsims/scenery-phet/issues/363

---
+++ js/buttons/InfoButton.js
@@ -0,0 +1,44 @@
+// Copyright 2018, University of Colorado Boulder
+
+/**
+ * Standard PhET button for 'info', uses the international symbol for 'information'.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var FontAwesomeNode = require( 'SUN/FontAwesomeNode' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var RoundPushButton = require( 'SUN/buttons/RoundPushButton' );
+  var sceneryPhet = require( 'SCENERY_PHET/sceneryPhet' );
+
+  /**
+   * @param {Object} [options]
+   * @constructor
+   */
+  function InfoButton( options ) {
+
+    options = _.extend( {
+      minXMargin: 10,
+      minYMargin: 10,
+      touchAreaXDilation: 10,
+      touchAreaYDilation: 5,
+      baseColor: '#eee'
+    }, options );
+
+    assert && assert( !options.content, 'InfoButton sets content' );
+    options.content = new FontAwesomeNode( 'info_circle', {
+      fill: 'hsl(208,60%,40%)'
+      // maxWidth: zoomInButton.width - chromeBounds.width - 2 * iconPadding,
+      // maxHeight: zoomInButton.height - chromeBounds.height - 2 * iconPadding
+    } );
+
+    RoundPushButton.call( this, options );
+  }
+
+  sceneryPhet.register( 'InfoButton', InfoButton );
+
+  return inherit( RoundPushButton, InfoButton );
+} );
