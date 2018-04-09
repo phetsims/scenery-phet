314084b446df59c1c20042458f131b6fc7f59cd6
Chris Malley <cmalley@pixelzoom.com>
2018-04-09 09:38:04 -0600

    fix StatusBar layout, https://github.com/phetsims/vegas/issues/59
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/StatusBar.js
+++ js/StatusBar.js
@@ -17,14 +17,15 @@ define( function( require ) {
   var vegas = require( 'VEGAS/vegas' );
 
   /**
-   * @param {Property} visibleBoundsProperty - for layout
+   * @param {Bounds2} layoutBounds - static 'safe' bounds of the parent ScreenView
+   * @param {Property.<Bounds2>} visibleBoundsProperty - dynamic bounds of the browser window
    * @param {Node} messageNode - to the right of the back button, typically Text
    * @param {Node} scoreDisplay - intended to be one of the ScoreDisplay* nodes but can be any custom Node provided
    * by the client
    * @param {Object} [options]
    * @constructor
    */
-  function StatusBar( visibleBoundsProperty, messageNode, scoreDisplay, options ) {
+  function StatusBar( layoutBounds, visibleBoundsProperty, messageNode, scoreDisplay, options ) {
 
     options = _.extend( {
       backButtonListener: null,
@@ -32,7 +33,10 @@ define( function( require ) {
       yMargin: 10,
       backgroundFill: 'rgb( 49, 117, 202 )',
       spacing: 8,
-      expandToFitBounds: true // expands to fit window width, otherwise stay inside initial layoutBounds
+
+      // true: keeps things on the status bar aligned with left and right edges of window bounds
+      // false: align things on status bar with left and right edges of static layoutBounds
+      dynamicAlignment: true
     }, options );
 
     var backButton = new BackButton( { listener: options.backButtonListener } );
@@ -51,41 +55,44 @@ define( function( require ) {
 
     Node.call( this, options );
 
-    // Update the messageNode layout when its bounds change.
-    var messageNodeUpdateLayout = function() {
+    // Update the layout of things on the status bar.
+    // Some of this may be unnecessary depending on what changed, but it simplifies to do all layout here.
+    var updateLayout = function() {
+
+      var leftEdge = ( options.dynamicAlignment ) ? backgroundNode.left : layoutBounds.minX;
+      var rightEdge = ( options.dynamicAlignment ) ? backgroundNode.right : layoutBounds.maxX;
+
+      // Back button on left end
+      backButton.left = leftEdge + options.xMargin;
+      backButton.centerY = backgroundNode.centerY;
+
+      // Message to the right of back button
       messageNode.left = backButton.right + options.spacing;
       messageNode.centerY = backgroundNode.centerY;
-    };
-    messageNode.on( 'bounds', messageNodeUpdateLayout );
 
-    // Update the scoreDisplay layout when its bounds change.
-    var scoreDisplayUpdateLayout = function() {
-      scoreDisplay.right = backgroundNode.right - options.xMargin;
+      // Score display on the right end
+      scoreDisplay.right = rightEdge - options.xMargin;
       scoreDisplay.centerY = backgroundNode.centerY;
     };
-    scoreDisplay.on( 'bounds', scoreDisplayUpdateLayout );
+    messageNode.on( 'bounds', updateLayout );
+    scoreDisplay.on( 'bounds', updateLayout );
 
     // Adjust the bar width and (optionally) update the layout when the browser window width changes.
     var visibleBoundsListener = function( bounds ) {
       backgroundNode.setRectX( bounds.minX );
       backgroundNode.setRectWidth( bounds.maxX - bounds.minX );
-      if ( options.expandToFitBounds ) {
-        backButton.left = backgroundNode.left + options.xMargin;
-        backButton.centerY = backgroundNode.centerY;
-        messageNodeUpdateLayout();
-        scoreDisplayUpdateLayout();
-      }
+      updateLayout();
     };
     visibleBoundsProperty.link( visibleBoundsListener );
 
     // @private
     this.disposeStatusBar = function() {
       backButton.dispose();
-      if ( messageNode.hasListener( 'bounds', messageNodeUpdateLayout ) ) {
-        messageNode.off( 'bounds', messageNodeUpdateLayout );
+      if ( messageNode.hasListener( 'bounds', updateLayout ) ) {
+        messageNode.off( 'bounds', updateLayout );
       }
-      if ( scoreDisplay.hasListener( 'bounds', scoreDisplayUpdateLayout ) ) {
-        scoreDisplay.off( 'bounds', scoreDisplayUpdateLayout );
+      if ( scoreDisplay.hasListener( 'bounds', updateLayout ) ) {
+        scoreDisplay.off( 'bounds', updateLayout );
       }
       if ( visibleBoundsProperty.hasListener( visibleBoundsListener ) ) {
         visibleBoundsProperty.unlink( visibleBoundsListener );
