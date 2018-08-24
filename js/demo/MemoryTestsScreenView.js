6f5f279a283d8c437a2b470013c3675daac593d3
chrisklus <christopher.klusendorf@colorado.edu>
2018-08-24 15:48:28 -0600

    Implement memoryTestCreationMax query param set, see https://github.com/phetsims/sun/issues/121

--- js/demo/MemoryTestsScreenView.js
+++ js/demo/MemoryTestsScreenView.js
@@ -8,24 +8,28 @@ define( function( require ) {
   'use strict';
 
   // modules
-  // var HStrut = require( 'SCENERY/nodes/HStrut' ); // TODO: for KitSelectionNode
-  // var KitSelectionNode = require( 'SCENERY_PHET/KitSelectionNode' ); // TODO: for KitSelectionNode
-  // var LeftRightSpinner = require( 'SCENERY_PHET/LeftRightSpinner' );
-  // var PhetFont = require( 'SCENERY_PHET/PhetFont' ); // TODO: for KitSelectionNode
-  // var Text = require( 'SCENERY/nodes/Text' ); // TODO: for KitSelectionNode
+  var HStrut = require( 'SCENERY/nodes/HStrut' );
+  // var KitControlNodeSides = require( 'SCENERY_PHET/KitControlNodeSides' );
+  var KitSelectionNode = require( 'SCENERY_PHET/KitSelectionNode' );
+  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  var Text = require( 'SCENERY/nodes/Text' );
+  // var ExpandCollapseButton = require( 'SUN/ExpandCollapseButton' );
+  // var FaucetNode = require( 'SCENERY_PHET/FaucetNode' );
+  // var GaugeNode = require( 'SCENERY_PHET/GaugeNode' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  // var MeasuringTapeNode = require( 'SCENERY_PHET/MeasuringTapeNode' );
+  // var NumberPicker = require( 'SCENERY_PHET/NumberPicker' );
+  var Property = require( 'AXON/Property' );
+  // var RadioButtonGroupMember = require( 'SUN/buttons/RadioButtonGroupMember' );
+  // var Range = require( 'DOT/Range' );
+  var sceneryPhet = require( 'SCENERY_PHET/sceneryPhet' );
+  // var ScientificNotationNode = require( 'SCENERY_PHET/ScientificNotationNode' );
+  var ScreenView = require( 'JOIST/ScreenView' );
+  // var SoundToggleButton = require( 'SCENERY_PHET/buttons/SoundToggleButton' );
+  // var ThermometerNode = require( 'SCENERY_PHET/ThermometerNode' );
   // var UpDownSpinner = require( 'SCENERY_PHET/UpDownSpinner' );
   // var WavelengthSlider = require( 'SCENERY_PHET/WavelengthSlider' );
-  var FaucetNode = require( 'SCENERY_PHET/FaucetNode' );
-  var GaugeNode = require( 'SCENERY_PHET/GaugeNode' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var MeasuringTapeNode = require( 'SCENERY_PHET/MeasuringTapeNode' );
-  var NumberPicker = require( 'SCENERY_PHET/NumberPicker' );
-  var Property = require( 'AXON/Property' );
-  var Range = require( 'DOT/Range' );
-  var sceneryPhet = require( 'SCENERY_PHET/sceneryPhet' );
-  var ScientificNotationNode = require( 'SCENERY_PHET/ScientificNotationNode' );
-  var ScreenView = require( 'JOIST/ScreenView' );
-  var ThermometerNode = require( 'SCENERY_PHET/ThermometerNode' );
+  var sceneryPhetQueryParameters = require( 'SCENERY_PHET/sceneryPhetQueryParameters' );
 
   function ComponentHolder( createFunction ) {
     var self = this;
@@ -37,45 +41,57 @@ define( function( require ) {
     };
   }
 
-  var booleanProperty = new Property( false );
+  // var booleanProperty = new Property( false );
   var numberProperty = new Property( 1 );
   // var waveLengthProperty = new Property( 400 );
 
   var components = [
-    new ComponentHolder( function() {
-      return new MeasuringTapeNode( new Property( {name: 'cm', multiplier: 100 } ), booleanProperty );
-    } ),
-    new ComponentHolder( function() {
-      return new FaucetNode( 1, numberProperty, booleanProperty );
-    } ),
-    new ComponentHolder( function() {
-      return new GaugeNode( numberProperty, 'label', { min: 0, max: 1 } );
-    } ),
-    new ComponentHolder( function() {
-      return new ThermometerNode( 0, 1, numberProperty );
-    } ),
-    new ComponentHolder( function() {
-      return new ScientificNotationNode( numberProperty );
-    } ),
-    new ComponentHolder( function() {
-      return new NumberPicker( numberProperty, new Property( new Range( 0, 2 ) ) );
-    } )/*,
-    new ComponentHolder( function() {
-      return new WavelengthSlider( waveLengthProperty );
-    } )
-    new ComponentHolder( function() {
-      return new LeftRightSpinner( numberProperty, booleanProperty, booleanProperty );
-    } )
-    new ComponentHolder( function() {
-      return new UpDownSpinner( numberProperty, booleanProperty, booleanProperty );
-    } ),
+    // new ComponentHolder( function() {
+    //   return new MeasuringTapeNode( new Property( {name: 'cm', multiplier: 100 } ), booleanProperty );
+    // } ),
+    // new ComponentHolder( function() {
+    //   return new FaucetNode( 1, numberProperty, booleanProperty );
+    // } ),
+    // new ComponentHolder( function() {
+    //   return new GaugeNode( numberProperty, 'label', { min: 0, max: 1 } );
+    // } ),
+    // new ComponentHolder( function() {
+    //   return new ThermometerNode( 0, 1, numberProperty );
+    // } ),
+    // new ComponentHolder( function() {
+    //   return new ScientificNotationNode( numberProperty );
+    // } ),
+    // new ComponentHolder( function() {
+    //   return new NumberPicker( numberProperty, new Property( new Range( 0, 2 ) ) );
+    // } ),
+    // new ComponentHolder( function() {
+    //   return new WavelengthSlider( waveLengthProperty );
+    // } ) //,
+    // new ComponentHolder( function() {
+    //   return new LeftRightSpinner( numberProperty, booleanProperty, booleanProperty );
+    // } )
+    // new ComponentHolder( function() {
+    //   return new UpDownSpinner( numberProperty, booleanProperty, booleanProperty );
+    // } ),
+    // new ComponentHolder( function() {
+    //   return new KitControlNodeSides( 1, new Property( 0 ), 1 );
+    // } )
+    // new ComponentHolder( function() {
+    //   return new SoundToggleButton( booleanProperty );
+    // } )
+    // new ComponentHolder( function() {
+    //   return new ExpandCollapseButton( booleanProperty );
+    // } ),
+    // new ComponentHolder( function() {
+    //   return new RadioButtonGroupMember( booleanProperty, false );
+    // } )
     new ComponentHolder( function() {
       var kits = [
         { title: new Text( 'kit 0', { font: new PhetFont( 24 ) } ), content: new HStrut( 200 ) },
         { title: new Text( 'kit 1', { font: new PhetFont( 24 ) } ), content: new HStrut( 200 ) }
-      ] ;
-      return new KitSelectionNode( numberProperty, kits );
-    } )*/
+      ];
+      return new KitSelectionNode( numberProperty, kits, { selectorPosition: 'top' } );
+    } )
   ];
 
   /**
@@ -83,6 +99,9 @@ define( function( require ) {
    */
   function MemoryTestsScreenView() {
     ScreenView.call( this );
+
+    this.numTests = 0;
+    this.maxNumTests = sceneryPhetQueryParameters.memoryTestCreationMax;
   }
 
   sceneryPhet.register( 'MemoryTestsScreenView', MemoryTestsScreenView );
@@ -90,18 +109,21 @@ define( function( require ) {
   return inherit( ScreenView, MemoryTestsScreenView, {
     step: function() {
 
-      for ( var i = 0; i < components.length; i++ ) {
-        var holder = components[ i ];
+      if ( this.numTests < this.maxNumTests ) {
+        for ( var i = 0; i < components.length; i++ ) {
+          var holder = components[ i ];
 
-        // dispose first, then create and add at the end of the loop so components will be visible on the screen during
-        // animation.
-        holder.instance && this.removeChild( holder.instance );
-        holder.instance && holder.dispose();
+          // dispose first, then create and add at the end of the loop so components will be visible on the screen during
+          // animation.
+          holder.instance && this.removeChild( holder.instance );
+          holder.instance && holder.dispose();
 
-        holder.create();
-        this.addChild( holder.instance );
+          holder.create();
+          this.addChild( holder.instance );
+        }
+        console.log( 'create' );
+        this.numTests++;
       }
-      console.log( 'create' );
     }
   } );
 } );
