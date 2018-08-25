08f7ba71043e1a95b7eabcbd43a1d601f5315340
Jonathan Olson <jonathan.olson@colorado.edu>
2018-08-24 23:11:19 -0600

    Some cleanup of the "graphic" nodes

--- js/intro/view/beaker/BeakerNode.js
+++ js/intro/view/beaker/BeakerNode.js
@@ -1,7 +1,7 @@
 // Copyright 2018, University of Colorado Boulder
 
 /**
- * Draw the beaker
+ * Displays a beaker graphic
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
@@ -9,118 +9,130 @@ define( require => {
   'use strict';
 
   // modules
-  var fractionsCommon = require( 'FRACTIONS_COMMON/fractionsCommon' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var LinearGradient = require( 'SCENERY/util/LinearGradient' );
-  var Node = require( 'SCENERY/nodes/Node' );
-  var Path = require( 'SCENERY/nodes/Path' );
-  var Shape = require( 'KITE/Shape' );
+  const fractionsCommon = require( 'FRACTIONS_COMMON/fractionsCommon' );
+  const FractionsCommonColorProfile = require( 'FRACTIONS_COMMON/common/view/FractionsCommonColorProfile' );
+  const LinearGradient = require( 'SCENERY/util/LinearGradient' );
+  const Node = require( 'SCENERY/nodes/Node' );
+  const Path = require( 'SCENERY/nodes/Path' );
+  const Shape = require( 'KITE/Shape' );
 
   // constants
-  var EMPTY_BEAKER_COLOR = 'rgba(150,150,150,0.15)';
-  var WATER_SIDE_COLOR = 'rgba(30,163,255,0.8)';
-  var WATER_TOP_COLOR = WATER_SIDE_COLOR;
-  var BEAKER_SHINE_COLOR = 'rgba(255,255,255,0.7)';
+  const EMPTY_BEAKER_COLOR = FractionsCommonColorProfile.emptyBeakerProperty;
+  const WATER_SIDE_COLOR = FractionsCommonColorProfile.waterSideProperty;
+  const WATER_TOP_COLOR = FractionsCommonColorProfile.waterTopProperty;
+  const BEAKER_SHINE_COLOR = FractionsCommonColorProfile.beakerShineProperty;
 
-  /**
-   * @constructor
-   * @extends {Node}
-   *
-   * @param {number} numerator
-   * @param {number} denominator
-   * @param {Object} [options]
-   */
-  function BeakerNode( numerator, denominator, options ) {
+  class BeakerNode extends Node {
+    /**
+     * @param {number} numerator
+     * @param {number} denominator
+     * @param {Object} [options]
+     */
+    constructor( numerator, denominator, options ) {
+      assert && assert( typeof numerator === 'number' && numerator >= 0 && numerator % 1 === 0 );
+      assert && assert( typeof denominator === 'number' && denominator >= 1 && denominator % 1 === 0 );
 
-    options = _.extend( {
-      fullHeight: BeakerNode.BEAKER_HEIGHT,
-      xRadius: 40,
-      yRadius: 12
-    }, options );
+      options = _.extend( {
+        // {number}
+        fullHeight: BeakerNode.DEFAULT_BEAKER_HEIGHT,
+        xRadius: 40,
+        yRadius: 12
+      }, options );
 
-    var height = options.fullHeight * numerator / denominator;
-    var xRadius = options.xRadius;
-    var yRadius = options.yRadius;
+      const height = options.fullHeight * numerator / denominator;
+      const xRadius = options.xRadius;
+      const yRadius = options.yRadius;
+      const numTicks = denominator;
 
-    var glassGradient = new LinearGradient( -xRadius, 0, xRadius, 0 ).addColorStop( 0, EMPTY_BEAKER_COLOR )
-      .addColorStop( 0.666, BEAKER_SHINE_COLOR )
-      .addColorStop( 0.782, BEAKER_SHINE_COLOR )
-      .addColorStop( 1, EMPTY_BEAKER_COLOR );
+      const glassGradient = new LinearGradient( -xRadius, 0, xRadius, 0 )
+        .addColorStop( 0, EMPTY_BEAKER_COLOR )
+        .addColorStop( 0.666, BEAKER_SHINE_COLOR )
+        .addColorStop( 0.782, BEAKER_SHINE_COLOR )
+        .addColorStop( 1, EMPTY_BEAKER_COLOR );
 
-    var bucketFrontShape = new Shape().ellipticalArc( 0, 0, xRadius, yRadius, 0, 0, Math.PI, false )
-      .ellipticalArc( 0, -options.fullHeight, xRadius, yRadius, 0, Math.PI, 0, true ).close();
-    var bucketBackShape = new Shape().ellipticalArc( 0, -options.fullHeight, xRadius, yRadius, 0, Math.PI, 0, false )
-      .ellipticalArc( 0, 0, xRadius, yRadius, 0, 0, Math.PI, true ).close();
-    var bucketBottomShape = new Shape().ellipticalArc( 0, 0, xRadius, yRadius, 0, 0, 2 * Math.PI, false );
+      const bucketFrontShape = new Shape()
+        .ellipticalArc( 0, 0, xRadius, yRadius, 0, 0, Math.PI, false )
+        .ellipticalArc( 0, -options.fullHeight, xRadius, yRadius, 0, Math.PI, 0, true )
+        .close();
+      const bucketBackShape = new Shape()
+        .ellipticalArc( 0, -options.fullHeight, xRadius, yRadius, 0, Math.PI, 0, false )
+        .ellipticalArc( 0, 0, xRadius, yRadius, 0, 0, Math.PI, true )
+        .close();
+      const bucketBottomShape = new Shape()
+        .ellipticalArc( 0, 0, xRadius, yRadius, 0, 0, 2 * Math.PI, false );
+      const waterTopShape = new Shape()
+        .ellipticalArc( 0, -height, xRadius, yRadius, 0, 0, Math.PI * 2, false )
+        .close();
+      const waterSideShape = new Shape()
+        .ellipticalArc( 0, -height, xRadius, yRadius, 0, Math.PI, 0, true )
+        .ellipticalArc( 0, 0, xRadius, yRadius, 0, 0, Math.PI, false )
+        .close();
 
-    var numTicks = denominator;
+      const ticksShape = new Shape();
+      let y = 0;
+      for ( let i = 0; i < numTicks; i++ ) {
+        y -= options.fullHeight / numTicks;
+        ticksShape.moveTo( -xRadius, y ).ellipticalArc( 0, y, xRadius, yRadius, 0, Math.PI, Math.PI * ( i % 2 === 0 ? 0.8 : 0.7 ), true );
+      }
 
-    var waterTopShape = new Shape().ellipticalArc( 0, -height, xRadius, yRadius, 0, 0, Math.PI * 2, false ).close();
-    var waterSideShape = new Shape().ellipticalArc( 0, -height, xRadius, yRadius, 0, Math.PI, 0, true )
-      .ellipticalArc( 0, 0, xRadius, yRadius, 0, 0, Math.PI, false ).close();
-    var ticksShape = new Shape();
-    var y = 0;
-    for ( var i = 0; i < numTicks; i++ ) {
-      y -= options.fullHeight / numTicks;
-      ticksShape.moveTo( -xRadius, y ).ellipticalArc( 0, y, xRadius, yRadius, 0, Math.PI, Math.PI * ( i % 2 === 0 ? 0.8 : 0.7 ), true );
+      const bucketFront = new Path( bucketFrontShape, {
+        stroke: 'grey',
+        fill: glassGradient
+      } );
+      const bucketBack = new Path( bucketBackShape, {
+        stroke: 'grey',
+        fill: glassGradient
+      } );
+      bucketBack.setScaleMagnitude( -1, 1 );
+      const bucketBottom = new Path( bucketBottomShape, {
+        stroke: 'grey',
+        fill: EMPTY_BEAKER_COLOR,
+        pickable: false
+      } );
+      const waterSide = new Path( waterSideShape, {
+        stroke: 'black',
+        fill: WATER_SIDE_COLOR,
+        pickable: false
+      } );
+      const waterTop = new Path( waterTopShape, {
+        fill: WATER_TOP_COLOR,
+        pickable: false
+      } );
+      const ticks = new Path( ticksShape, {
+        stroke: 'black',
+        lineWidth: 1.5,
+        pickable: false
+      } );
+
+      super( {
+        children: [
+          bucketBack,
+          bucketBottom,
+          ...( numerator > 0 ? [
+            waterSide,
+            waterTop
+          ] : [] ),
+          bucketFront,
+          ...( numerator > 0 ? [
+            ticks
+          ] : [] )
+        ]
+      } );
+
+      // @public {Vector2}
+      this.midPointOffset = this.center;
+
+      this.mutate( options );
     }
 
-    var bucketFront = new Path( bucketFrontShape, {
-      stroke: 'grey',
-      fill: glassGradient
-    } );
-    var bucketBack = new Path( bucketBackShape, {
-      stroke: 'grey',
-      fill: glassGradient
-    } );
-    bucketBack.setScaleMagnitude( -1, 1 );
-    var bucketBottom = new Path( bucketBottomShape, {
-      stroke: 'grey',
-      fill: EMPTY_BEAKER_COLOR,
-      pickable: false
-    } );
-    var waterSide = new Path( waterSideShape, {
-      stroke: 'black',
-      fill: WATER_SIDE_COLOR,
-      pickable: false
-    } );
-    var waterTop = new Path( waterTopShape, {
-      fill: WATER_TOP_COLOR,
-      pickable: false
-    } );
-    var ticks = new Path( ticksShape, {
-      stroke: 'black',
-      lineWidth: 1.5,
-      pickable: false
-    } );
-
-    Node.call( this, {
-      children: numerator > 0 ? [
-        bucketBack,
-        bucketBottom,
-        waterSide,
-        waterTop,
-        bucketFront,
-        ticks
-      ] : [
-        // no water if numerator is 0
-        bucketBack,
-        bucketBottom,
-        bucketFront,
-        ticks
-      ]
-    } );
-
-    // @public
-    this.midPointOffset = this.center;
-
-    this.mutate( options );
+    /**
+     * The normal height of a beaker.
+     * @public
+     *
+     * @returns {number}
+     */
+    static get DEFAULT_BEAKER_HEIGHT() { return 150; }
   }
 
-  fractionsCommon.register( 'BeakerNode', BeakerNode );
-
-  return inherit( Node, BeakerNode, {}, {
-    // @public {number}
-    BEAKER_HEIGHT: 150
-  } );
+  return fractionsCommon.register( 'BeakerNode', BeakerNode );
 } );
