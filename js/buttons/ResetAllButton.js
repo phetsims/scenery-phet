8c7d9b415c3609b1a246dac6b4693d6783bf86b5
zepumph <michael.kauzmann@colorado.edu>
2018-10-04 11:55:52 -0800

    rename tandem.isSuppliedAndEnabled to PhetioObject.isPhetioInstrumented, https://github.com/phetsims/tandem/issues/62

--- js/buttons/ResetAllButton.js
+++ js/buttons/ResetAllButton.js
@@ -47,7 +47,7 @@ define( function( require ) {
       arrowColor: 'white',
       tandem: Tandem.required,
 
-      phetioInstanceDocumentation: 'The orange, round button that can be used to restore the initial state',
+      phetioDocumentation: 'The orange, round button that can be used to restore the initial state',
 
       // a11y
       innerContent: resetAllButtonNameString
@@ -60,7 +60,7 @@ define( function( require ) {
     // @private - Mirrored property of `buttonModel.isFiringProperty`, but is phet-io instrumented.
     this.isFiringProperty = new DerivedProperty( [ this.buttonModel.isFiringProperty ], function( a ) { return a; }, {
       tandem: options.tandem.createTandem( 'isFiringProperty' ),
-      phetioInstanceDocumentation: 'Temporarily becomes true while the Reset All button is firing.  Commonly used to disable audio effects during reset.',
+      phetioDocumentation: 'Temporarily becomes true while the Reset All button is firing.  Commonly used to disable audio effects during reset.',
       phetioType: DerivedPropertyIO( BooleanIO ),
       phetioState: false // this is a transient property based on user interaction, should not be stored in the state
     } );
