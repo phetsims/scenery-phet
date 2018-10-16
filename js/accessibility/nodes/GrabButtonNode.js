2ea4274598430bcdb3d4a8d44259d915a2f4e347
zepumph <michael.kauzmann@colorado.edu>
2018-10-16 15:34:30 -0800

    add GrabButtonNode, apply to BookNode, https://github.com/phetsims/friction/issues/126

---
+++ js/accessibility/nodes/GrabButtonNode.js
@@ -0,0 +1,173 @@
+// Copyright 2018, University of Colorado Boulder
+
+/**
+ * @author Michael Kauzmann
+ */
+define( require => {
+  'use strict';
+
+  // modules
+  const FocusHighlightFromNode = require( 'SCENERY/accessibility/FocusHighlightFromNode' );
+  const FocusHighlightPath = require( 'SCENERY/accessibility/FocusHighlightPath' );
+  const KeyboardUtil = require( 'SCENERY/accessibility/KeyboardUtil' );
+  const Node = require( 'SCENERY/nodes/Node' );
+  const sceneryPhet = require( 'SCENERY_PHET/sceneryPhet' );
+  const StringUtils = require( 'PHETCOMMON/util/StringUtils' );
+
+  // constants
+  const grabPatternString = 'Grab {{thingToGrab}}';
+  const defaultThingToGrabString = 'Node';
+
+  /**
+   *
+   * NOTE: if passing inthis class assumes
+   * @param {Node} wrappedNode
+   * @param  {Object} options
+   */
+  class GrabButtonNode extends Node {
+    constructor( wrappedNode, options ) {
+
+      options = _.extend( {
+        cursor: 'pointer',
+
+        thingToGrab: defaultThingToGrabString,
+
+        // a11y - this node will act as a container for more accessible content, its children will implement
+        // most of the keyboard navigation
+        tagName: 'div'
+      }, options );
+
+      assert && assert( wrappedNode.accessibleContent );
+      assert && assert( wrappedNode.parents.length === 0, 'wrappedNode should not have a parent, as the grab button is to be the parent.' );
+
+      if ( wrappedNode.focusHighlight ) {
+        assert && assert( wrappedNode.focusHighlight instanceof phet.scenery.Path,
+          'if provided, focusHighlight must be a path' );
+      }
+
+      // super constructor
+      super( options );
+
+      const accessibleButtonLabel = StringUtils.fillIn( grabPatternString, {
+        thingToGrab: options.thingToGrab
+      } );
+
+      const grabButton = new Node( {
+        pickable: false, // TODO:???custom touch areas applied to parent
+
+        // a11y
+        containerTagName: 'div',
+        tagName: 'button',
+        innerContent: accessibleButtonLabel,
+        focusHighlightLayerable: true
+        // descriptionContent: grabBalloonHelpString, // TODO: description support
+        // appendDescription: true
+      } );
+
+      // make sure that the grabButton actually has some width, so add the wrapped node to it
+      this.addChild( grabButton );
+      grabButton.addChild( wrappedNode );
+
+      // The wrapped node starts invisible in the PDOM
+      wrappedNode.accessibleVisible = false;
+
+      // Update the passed in node's focusHighlight to make it "dashed"
+      let wrappedNodeFocusHighlight = wrappedNode.focusHighlight;
+      if ( !wrappedNodeFocusHighlight ) {
+        wrappedNodeFocusHighlight = new FocusHighlightFromNode( wrappedNode );
+      }
+      wrappedNodeFocusHighlight.makeDashed();
+      wrappedNode.focusHighlight = wrappedNodeFocusHighlight;
+
+      // Make the grab button's focusHighlight in the spitting image of the wrappedNode's
+      const grabButtonFocusHighlight = new FocusHighlightPath( wrappedNodeFocusHighlight.shape );
+      grabButtonFocusHighlight.center = wrappedNode.center;
+      grabButton.focusHighlight = grabButtonFocusHighlight;
+
+      // update the grabButton's focusHighlight whenever the wrappedNode moves.
+      const transformListener = () => {
+        grabButtonFocusHighlight.center = wrappedNode.center;
+      };
+      wrappedNode.on( 'transform', transformListener );
+
+      // for focusHighlightLayerable
+      grabButton.addChild( grabButtonFocusHighlight );
+
+      // when the "Grab Balloon" button is pressed, focus the draggable node and set to dragged state
+      grabButton.addAccessibleInputListener( {
+        click( event ) {
+
+          // if the balloon was released on enter, don't pick it up again until the next click event so we don't pick
+          // it up immediately again
+          if ( !guardKeyPress ) {
+            wrappedNode.accessibleVisible = true;
+            wrappedNode.focus();
+          }
+
+          // pick up the balloon on the next click event
+          guardKeyPress = false;
+        }
+      } );
+
+      /**
+       * Release the balloon after an accessible interaction, resetting  model Properties, returning focus
+       * to the "grab" button, and hiding the draggable balloon.
+       */
+      const a11yReleaseWrappedNode = () => {
+
+        // TODO: need a listener here?
+        // endDragListener();
+
+        // focus the grab balloon button
+        grabButton.focus();
+
+        // the draggable node should no longer be discoverable in the parallel DOM
+        wrappedNode.accessibleVisible = false;
+
+        // reset the key state of the drag handler by interrupting the drag
+        wrappedNode.interruptInput();
+      };
+
+      // some keypresses can fire the grabButton's click from the same press that fires the event below, so guard against that.
+      let guardKeyPress = false;
+      wrappedNode.addAccessibleInputListener( {
+
+        // Release the balloon on 'enter' key, tracking that we have released the balloon with this key so that
+        // we don't immediately catch the 'click' event while the enter key is down on the button
+        keydown( event ) {
+          if ( event.keyCode === KeyboardUtil.KEY_ENTER ) {
+            guardKeyPress = true;
+            a11yReleaseWrappedNode();
+          }
+        },
+        keyup( event ) {
+
+          // Release  on keyup of spacebar so that we don't pick up the balloon again when we release the spacebar
+          // and trigger a click event - escape could be added to either keyup or keydown listeners
+          if ( event.keyCode === KeyboardUtil.KEY_SPACE || event.keyCode === KeyboardUtil.KEY_ESCAPE ) {
+            guardKeyPress = event.keyCode === KeyboardUtil.KEY_SPACE;
+            a11yReleaseWrappedNode();
+          }
+        },
+        blur() {
+          // No need to interrupt the KeyboardDragHandler, accessibilityInputListeners are already interrupted on blur
+
+          // the draggable node should no longer be focusable
+          wrappedNode.accessibleVisible = false;
+        }
+      } );
+
+      this.disposeGrabButtonNode = () => {
+        this.off( 'transform', transformListener );
+      };
+    }
+
+    dispose() {
+      this.disposeGrabButtonNode();
+      super.dispose();
+    }
+  }
+
+  return sceneryPhet.register( 'GrabButtonNode', GrabButtonNode );
+
+} );
