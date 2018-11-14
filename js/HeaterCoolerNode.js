5fc0aebc6ae92e83ed101ee275c540d19fedee28
denz1994 <denz1994@gmail.com>
2018-11-14 10:25:17 -0500

    ES6 conversion of HeaterCoolerNode.js https://github.com/phetsims/scenery-phet/issues/423

--- js/HeaterCoolerNode.js
+++ js/HeaterCoolerNode.js
@@ -10,56 +10,56 @@
  * @author Jesse Greenberg
  */
 
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var Color = require( 'SCENERY/util/Color' );
-  var HeaterCoolerBack = require( 'SCENERY_PHET/HeaterCoolerBack' );
-  var HeaterCoolerFront = require( 'SCENERY_PHET/HeaterCoolerFront' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var Node = require( 'SCENERY/nodes/Node' );
-  var Property = require( 'AXON/Property' );
-  var sceneryPhet = require( 'SCENERY_PHET/sceneryPhet' );
-  var Tandem = require( 'TANDEM/Tandem' );
+  const Color = require( 'SCENERY/util/Color' );
+  const HeaterCoolerBack = require( 'SCENERY_PHET/HeaterCoolerBack' );
+  const HeaterCoolerFront = require( 'SCENERY_PHET/HeaterCoolerFront' );
+  const Node = require( 'SCENERY/nodes/Node' );
+  const Property = require( 'AXON/Property' );
+  const sceneryPhet = require( 'SCENERY_PHET/sceneryPhet' );
+  const Tandem = require( 'TANDEM/Tandem' );
 
-  /**
-   * Constructor for a HeaterCoolerNode.
-   *
-   * @param {Object} [options] that can be passed on to the underlying node
-   * @constructor
-   */
-  function HeaterCoolerNode( options ) {
-    Tandem.indicateUninstrumentedCode();
+  class HeaterCoolerNode extends Node {
+    /**
+     * Constructor for a HeaterCoolerNode.
+     *
+     * @param {Object} [options] that can be passed on to the underlying node
+     * @constructor
+     */
+    constructor( options ) {
+      super();
+      Tandem.indicateUninstrumentedCode();
 
-    Node.call( this );
+      options = _.extend( {
+        baseColor: new Color( 159, 182, 205 ), //  Base color used for the stove body.
+        width: 120, // In screen coords, much of the rest of the size of the stove derives from this value.
+        snapToZero: true, // controls whether the slider will snap to the off through end drag.
+        heatCoolAmountProperty: new Property( 0 ), // Property set through interaction with slider.  +1 for max heating, -1 for max cooling.
+        heatEnabled: true, // Can this node heat the environment?
+        coolEnabled: true // Can this node cool the environment?
+      }, options );
 
-    options = _.extend( {
-      baseColor: new Color( 159, 182, 205 ), //  Base color used for the stove body.
-      width: 120, // In screen coords, much of the rest of the size of the stove derives from this value.
-      snapToZero: true, // controls whether the slider will snap to the off through end drag.
-      heatCoolAmountProperty: new Property( 0 ), // Property set through interaction with slider.  +1 for max heating, -1 for max cooling.
-      heatEnabled: true, // Can this node heat the environment?
-      coolEnabled: true // Can this node cool the environment?
-    }, options );
+      // @public
+      this.heatCoolAmountProperty = options.heatCoolAmountProperty;
 
-    // @public
-    this.heatCoolAmountProperty = options.heatCoolAmountProperty;
+      // Add the HeaterCoolerBack which contains the heater opening and the fire/ice images
+      let heaterCoolerBack = new HeaterCoolerBack( options );
+      this.addChild( heaterCoolerBack );
 
-    // Add the HeaterCoolerBack which contains the heater opening and the fire/ice images
-    var heaterCoolerBack = new HeaterCoolerBack( options );
-    this.addChild( heaterCoolerBack );
+      // Add the HeaterCoolerFront which contains the labels, stove body, and control slider.
+      let heaterCoolerFront = new HeaterCoolerFront( options );
+      heaterCoolerFront.leftTop = heaterCoolerBack.getHeaterFrontPosition();
+      this.addChild( heaterCoolerFront );
 
-    // Add the HeaterCoolerFront which contains the labels, stove body, and control slider.
-    var heaterCoolerFront = new HeaterCoolerFront( options );
-    heaterCoolerFront.leftTop = heaterCoolerBack.getHeaterFrontPosition();
-    this.addChild( heaterCoolerFront );
+      this.mutate( options );
+    }
 
-    this.mutate( options );
+
   }
 
-  sceneryPhet.register( 'HeaterCoolerNode', HeaterCoolerNode );
-
-  return inherit( Node, HeaterCoolerNode );
+  return sceneryPhet.register( 'HeaterCoolerNode', HeaterCoolerNode );
 } );
 
