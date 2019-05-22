b00430a174e1a839de2147e7f476aa1f782138bd
Chris Malley <cmalley@pixelzoom.com>
2019-05-22 10:35:57 -0600

    convert SpectrumNode to class, #501

--- js/SpectrumNode.js
+++ js/SpectrumNode.js
@@ -14,49 +14,48 @@ define( function( require ) {
   var Bounds2 = require( 'DOT/Bounds2' );
   var Dimension2 = require( 'DOT/Dimension2' );
   var Image = require( 'SCENERY/nodes/Image' );
-  var inherit = require( 'PHET_CORE/inherit' );
   var sceneryPhet = require( 'SCENERY_PHET/sceneryPhet' );
   var Util = require( 'DOT/Util' );
 
-  /**
-   * Slider track that displays the visible spectrum.
-   *
-   * @param {Object} config
-   * @constructor
-   */
-  function SpectrumNode( config ) {
 
-    config = _.extend( {
-      size: new Dimension2( 150, 30 ),
-      minValue: 0,
-      maxValue: 1,
-      valueToColor: null // {function} - @required, maps value => Color
-    }, config );
+  class SpectrumNode extends Image {
 
-    // validate values
-    assert && assert( config.minValue < config.maxValue, 'min should be less than max' );
-    assert && assert( !!config.valueToColor, 'valueToColor is required' );
+    /**
+     * @param {Object} [options]
+     * @constructor
+     */
+    constructor( options ) {
 
-    // Draw the spectrum directly to a canvas, to improve performance.
-    var canvas = document.createElement( 'canvas' );
-    var context = canvas.getContext( '2d' );
-    canvas.width = config.size.width;
-    canvas.height = config.size.height;
+      options = _.extend( {
+        size: new Dimension2( 150, 30 ),
+        minValue: 0,
+        maxValue: 1,
+        valueToColor: null // {function} - @required, maps value => Color
+      }, options );
 
-    // map position to wavelength
-    for ( var i = 0; i < config.size.width; i++ ) {
-      var value = Util.clamp( Util.linear( 0, config.size.width, config.minValue, config.maxValue, i ), config.minValue, config.maxValue );
-      context.fillStyle = config.valueToColor( value ).toCSS();
-      context.fillRect( i, 0, 1, config.size.height );
+      // validate values
+      assert && assert( options.minValue < options.maxValue, 'min should be less than max' );
+      assert && assert( !!options.valueToColor, 'valueToColor is required' );
+
+      // Draw the spectrum directly to a canvas, to improve performance.
+      var canvas = document.createElement( 'canvas' );
+      var context = canvas.getContext( '2d' );
+      canvas.width = options.size.width;
+      canvas.height = options.size.height;
+
+      // map position to wavelength
+      for ( var i = 0; i < options.size.width; i++ ) {
+        var value = Util.clamp( Util.linear( 0, options.size.width, options.minValue, options.maxValue, i ), options.minValue, options.maxValue );
+        context.fillStyle = options.valueToColor( value ).toCSS();
+        context.fillRect( i, 0, 1, options.size.height );
+      }
+
+      super( canvas.toDataURL(), options );
+
+      // since the Image's bounds aren't immediately computed, we override it here
+      this.setLocalBounds( new Bounds2( 0, 0, options.size.width, options.size.height ) );
     }
-
-    Image.call( this, canvas.toDataURL(), config );
-
-    // since the Image's bounds aren't immediately computed, we override it here
-    this.setLocalBounds( new Bounds2( 0, 0, config.size.width, config.size.height ) );
   }
 
-  sceneryPhet.register( 'SpectrumNode', SpectrumNode );
-
-  return inherit( Image, SpectrumNode );
+  return sceneryPhet.register( 'SpectrumNode', SpectrumNode );
 } );
