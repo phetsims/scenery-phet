4f2774a2cf9892d8f1f5ad84438862d459b9c0cb
Chris Malley <cmalley@pixelzoom.com>
2019-05-22 10:37:01 -0600

    convert SpectrumNode to ES6, #501

--- js/SpectrumNode.js
+++ js/SpectrumNode.js
@@ -11,11 +11,11 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var Bounds2 = require( 'DOT/Bounds2' );
-  var Dimension2 = require( 'DOT/Dimension2' );
-  var Image = require( 'SCENERY/nodes/Image' );
-  var sceneryPhet = require( 'SCENERY_PHET/sceneryPhet' );
-  var Util = require( 'DOT/Util' );
+  const Bounds2 = require( 'DOT/Bounds2' );
+  const Dimension2 = require( 'DOT/Dimension2' );
+  const Image = require( 'SCENERY/nodes/Image' );
+  const sceneryPhet = require( 'SCENERY_PHET/sceneryPhet' );
+  const Util = require( 'DOT/Util' );
 
 
   class SpectrumNode extends Image {
@@ -38,14 +38,14 @@ define( function( require ) {
       assert && assert( !!options.valueToColor, 'valueToColor is required' );
 
       // Draw the spectrum directly to a canvas, to improve performance.
-      var canvas = document.createElement( 'canvas' );
-      var context = canvas.getContext( '2d' );
+      const canvas = document.createElement( 'canvas' );
+      const context = canvas.getContext( '2d' );
       canvas.width = options.size.width;
       canvas.height = options.size.height;
 
       // map position to wavelength
-      for ( var i = 0; i < options.size.width; i++ ) {
-        var value = Util.clamp( Util.linear( 0, options.size.width, options.minValue, options.maxValue, i ), options.minValue, options.maxValue );
+      for ( let i = 0; i < options.size.width; i++ ) {
+        const value = Util.clamp( Util.linear( 0, options.size.width, options.minValue, options.maxValue, i ), options.minValue, options.maxValue );
         context.fillStyle = options.valueToColor( value ).toCSS();
         context.fillRect( i, 0, 1, options.size.height );
       }
