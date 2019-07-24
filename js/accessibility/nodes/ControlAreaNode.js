66c6633a24029c177b080f24f53e01f44d0c76db
zepumph <michael.kauzmann@colorado.edu>
2019-07-23 16:54:45 -0800

    add accessible section documentation, https://github.com/phetsims/scenery-phet/issues/381 https://github.com/phetsims/scenery-phet/issues/469

--- js/accessibility/nodes/ControlAreaNode.js
+++ js/accessibility/nodes/ControlAreaNode.js
@@ -1,8 +1,12 @@
-// Copyright 2018, University of Colorado Boulder
+// Copyright 2018-2019, University of Colorado Boulder
 
 /**
  *
- * A node that creates a "Play Area" accessible section in the PDOM.
+ * A node that creates a "Control Area" accessible section in the PDOM. This organizational Node should have accessible
+ * content to be displayed under it in the PDOM. This content can be added as a child, or added via `accessibleOrder`.
+ * Items in this section are designed to be secondary to that in the PlayAreaNode. See ScreenView for more documentation
+ * and usage explanation.
+ *
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
--- js/accessibility/nodes/PlayAreaNode.js
+++ js/accessibility/nodes/PlayAreaNode.js
@@ -2,7 +2,10 @@
 
 /**
  *
- * A node that creates a "Play Area" accessible section in the PDOM.
+ * A node that creates a "Play Area" accessible section in the PDOM. This organizational Node should have accessible
+ * content to be displayed under it in the PDOM. This content can be added as a child, or added via `accessibleOrder`.
+ * Items in this section are designed to be the "main interaction and pedagogical learning" to be had for the screen.
+ * See ScreenView for more documentation and usage explanation.
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
