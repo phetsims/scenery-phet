199ecaae2ccc90b6ec484bf0056f135ffb51a529
phet-dev <phetadmin@gmail.com>
2019-08-06 03:28:40 -0600

    update copyright dates from daily grunt work

--- js/TemperatureAndColorSensorNode.js
+++ js/TemperatureAndColorSensorNode.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2019, University of Colorado Boulder
+// Copyright 2019, University of Colorado Boulder
 
 /**
  * A Scenery Node that portrays a thermometer and a triangular indicator of the precise location where the temperature
