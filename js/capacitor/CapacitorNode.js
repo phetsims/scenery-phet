d2883b2b46f95fd118bb81a92b7db7d84996acae
samreid <reids@colorado.edu>
2019-10-07 12:05:31 -0600

    Moved from capacitor-lab-basics/js/common/view/CapacitorNode.js to scenery-phet, see https://github.com/phetsims/scenery-phet/issues/537

---
+++ js/capacitor/CapacitorNode.js
@@ -0,0 +1,108 @@
+// Copyright 2014-2019, University of Colorado Boulder
+
+/**
+ * Visual representation of a capacitor.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ * @author Jesse Greenberg (PhET Interactive Simulations)
+ * @author Andrew Adare (PhET Interactive Simulations)
+ *
+ * Moved from capacitor-lab-basics/js/common/view/CapacitorNode.js on Oct 7, 2019
+ */
+define( require => {
+  'use strict';
+
+  // modules
+  const Bounds2 = require( 'DOT/Bounds2' );
+  const EFieldNode = require( 'SCENERY_PHET/capacitor/EFieldNode' );
+  const inherit = require( 'PHET_CORE/inherit' );
+  const Node = require( 'SCENERY/nodes/Node' );
+  const PlateNode = require( 'SCENERY_PHET/capacitor/PlateNode' );
+  const Property = require( 'AXON/Property' );
+  const sceneryPhet = require( 'SCENERY_PHET/sceneryPhet' );
+
+  /**
+   * @constructor
+   *
+   * @param {ParallelCircuit} circuit
+   * @param {CLBModelViewTransform3D} modelViewTransform
+   * @param {Property.<boolean>} plateChargeVisibleProperty
+   * @param {Property.<boolean>} electricFieldVisibleProperty
+   * @param {Tandem} tandem
+   */
+  function CapacitorNode( circuit, modelViewTransform, plateChargeVisibleProperty, electricFieldVisibleProperty, tandem ) {
+
+    Node.call( this, { tandem: tandem } );
+    const self = this; // extend scope for nested callbacks
+
+    // @private
+    this.capacitor = circuit.capacitor;
+    this.modelViewTransform = modelViewTransform;
+
+    // @private {PlateNode}
+    this.topPlateNode = PlateNode.createTopPlateNode( this.capacitor, modelViewTransform, circuit.maxPlateCharge );
+    this.bottomPlateNode = PlateNode.createBottomPlateNode( this.capacitor, modelViewTransform, circuit.maxPlateCharge );
+
+    const eFieldNode = new EFieldNode( this.capacitor, modelViewTransform, circuit.maxEffectiveEField, this.getPlatesBounds() );
+
+    // rendering order
+    this.addChild( this.bottomPlateNode );
+    this.addChild( eFieldNode );
+    this.addChild( this.topPlateNode );
+
+    Property.multilink( [
+      this.capacitor.plateSizeProperty,
+      this.capacitor.plateSeparationProperty
+    ], function() {
+      self.updateGeometry();
+    } );
+
+    plateChargeVisibleProperty.link( function( visible ) {
+      self.topPlateNode.setChargeVisible( visible );
+      self.bottomPlateNode.setChargeVisible( visible );
+    } );
+
+    electricFieldVisibleProperty.link( function( visible ) {
+      eFieldNode.setVisible( visible );
+    } );
+  }
+
+  sceneryPhet.register( 'CapacitorNode', CapacitorNode );
+
+  return inherit( Node, CapacitorNode, {
+
+    /**
+     * Update the geometry of the capacitor plates.
+     * @public
+     */
+    updateGeometry: function() {
+      // geometry
+      this.topPlateNode.setBoxSize( this.capacitor.plateSizeProperty.value );
+      this.bottomPlateNode.setBoxSize( this.capacitor.plateSizeProperty.value );
+
+      // layout nodes
+      const x = 0;
+      let y = -( this.capacitor.plateSeparationProperty.value / 2 ) - this.capacitor.plateSizeProperty.value.height;
+      const z = 0;
+      this.topPlateNode.center = this.modelViewTransform.modelToViewDeltaXYZ( x, y, z );
+
+      y = this.capacitor.plateSeparationProperty.value / 2;
+      this.bottomPlateNode.center = this.modelViewTransform.modelToViewDeltaXYZ( x, y, z );
+    },
+
+    /**
+     * Get the bound of the capacitor from the plates.  Allows for bounds to be passed into the canvas node before the
+     * children are added to the view.
+     * @public
+     *
+     * @returns {Bounds2}
+     */
+    getPlatesBounds: function() {
+      return new Bounds2(
+        this.topPlateNode.left,
+        this.topPlateNode.top,
+        this.bottomPlateNode.right,
+        this.bottomPlateNode.bottom );
+    }
+  } );
+} );
