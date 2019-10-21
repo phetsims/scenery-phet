02e3551dd8fccd934c920dc1be1a150391163739
zepumph <michael.kauzmann@colorado.edu>
2019-10-21 09:09:12 -0800

    GrabReleaseCueNode to be a Panel, https://github.com/phetsims/scenery-phet/issues/543

--- js/accessibility/nodes/GrabReleaseCueNode.js
+++ js/accessibility/nodes/GrabReleaseCueNode.js
@@ -1,7 +1,7 @@
 // Copyright 2018-2019, University of Colorado Boulder
 
 /**
- * A node that displays a visual queue to use space to grab and release a component.
+ * A Node that displays a visual queue to use space to grab and release a component.
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 define( require => {
@@ -10,8 +10,8 @@ define( require => {
   // modules
   const HBox = require( 'SCENERY/nodes/HBox' );
   const merge = require( 'PHET_CORE/merge' );
+  const Panel = require( 'SUN/Panel' );
   const PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  const Rectangle = require( 'SCENERY/nodes/Rectangle' );
   const RichText = require( 'SCENERY/nodes/RichText' );
   const sceneryPhet = require( 'SCENERY_PHET/sceneryPhet' );
   const SpaceKeyNode = require( 'SCENERY_PHET/keyboard/SpaceKeyNode' );
@@ -19,18 +19,20 @@ define( require => {
   // strings
   const keyToGrabOrReleaseString = require( 'string!SCENERY_PHET/key.toGrabOrRelease' );
 
-  /**
-   *
-   * @param {Object} [options]
-   * @constructor
-   */
-  class GrabReleaseCueNode extends Rectangle {
+  class GrabReleaseCueNode extends Panel {
+
+    /**
+     * @param {Object} [options]
+     */
     constructor( options ) {
 
       options = merge( {
 
         fill: 'white',
         stroke: 'black',
+        xMargin: 15,
+        yMargin: 5,
+        cornerRadius: 0,
 
         spaceKeyWidth: 50, // this space key is wider than default SpaceKeyNode
         keyHeight: 24 // height of the space key, larger than default KeyNode height
@@ -46,9 +48,7 @@ define( require => {
       } );
 
       // rectangle containing the content, not visible until focused the first time
-      super( spaceKeyHBox.bounds.dilatedXY( 15, 5 ), options );
-
-      this.addChild( spaceKeyHBox );
+      super( spaceKeyHBox, options );
     }
   }
 
