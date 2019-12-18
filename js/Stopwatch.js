7e93eaf2b2cddeeeebb8d5e95048b8553b90dd1b
phet-dev <phetadmin@gmail.com>
2019-12-18 03:26:57 -0700

    update copyright dates from daily grunt work

--- js/Stopwatch.js
+++ js/Stopwatch.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2019, University of Colorado Boulder
+// Copyright 2019, University of Colorado Boulder
 
 /**
  * Stopwatch is the model for the stopwatch. It is responsible for time, location, and visibility.
