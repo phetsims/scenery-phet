4c8db2610cb83b400a20b68e974b651d5f23b620
samreid <reids@colorado.edu>
2020-01-16 19:04:22 -0700

    Remove completed TODOs, see https://github.com/phetsims/scenery-phet/issues/548

--- js/BannedNode.js
+++ js/BannedNode.js
@@ -17,18 +17,6 @@ define( require => {
   const Node = require( 'SCENERY/nodes/Node' );
   const sceneryPhet = require( 'SCENERY_PHET/sceneryPhet' );
 
-  // TODO: What is the best name for this node?  We discussed BannedNode and BannedNode, but those terms aren't mentioned
-  // TODO: at all in the wikipedia entry, which uses terms like:
-  // * prohibition sign
-  // * no symbol
-  // * no sign
-  // * circle-backslash symbol
-  // * nay
-  // * interdictory circle
-  // * universal no
-  // TODO: The term "prohibited" is featured prominently in both https://en.wikipedia.org/wiki/No_symbol and
-  // TODO: https://emojipedia.org/no-entry-sign/, so I recommend we use a variant of that, such as ProhibitedNode
-  // TODO: Or ProhibitedSymbolNode or ProhibitionSignNode, see https://github.com/phetsims/scenery-phet/issues/548
   class BannedNode extends Node {
 
     /**
