fff91128cb9e673d126066c187925129de13eece
phet-dev <phetadmin@gmail.com>
2020-01-22 03:57:38 -0700

    update copyright dates from daily grunt work

--- js/accessibility/describers/BorderAlertsDescriber.js
+++ js/accessibility/describers/BorderAlertsDescriber.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2019, University of Colorado Boulder
+// Copyright 2018-2020, University of Colorado Boulder
 
 /**
  * BorderAlertsDescriber is "sub-describer" used in MovementDescriber to manage its border alerts. Border alerts will
