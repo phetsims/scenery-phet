5b351bbb3ea2056c4f32e74e2a91f2c1ffe135de
phet-dev <phetadmin@gmail.com>
2020-01-28 03:32:15 -0700

    update copyright dates from daily grunt work

--- js/NextPreviousNavigationNode.js
+++ js/NextPreviousNavigationNode.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2019, University of Colorado Boulder
+// Copyright 2013-2020, University of Colorado Boulder
 
 /**
  * Shows a central node surrounded with next/previous arrows. Need to implement next(),previous(),
