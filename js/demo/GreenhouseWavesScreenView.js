487177508bb5c406be5c0391c3eab3850bd6c0c8
phet-dev <phetadmin@gmail.com>
2020-02-14 04:04:49 -0700

    update copyright dates from daily grunt work

--- js/demo/GreenhouseWavesScreenView.js
+++ js/demo/GreenhouseWavesScreenView.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2019, University of Colorado Boulder
+// Copyright 2020, University of Colorado Boulder
 
 /**
  * Prototype for greenhouse waves.  It's a prototype, enter at your own risk
