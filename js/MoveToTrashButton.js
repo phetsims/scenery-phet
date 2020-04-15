f5562591464f1b36287c914758437ebe96ab2bdc
pixelzoom <cmalley@pixelzoom.com>
2020-04-15 09:54:42 -0600

    convert to ES6 class, https://github.com/phetsims/scenery-phet/issues/598

--- js/MoveToTrashButton.js
+++ js/MoveToTrashButton.js
@@ -9,61 +9,58 @@
  */
 
 import InstanceRegistry from '../../phet-core/js/documentation/InstanceRegistry.js';
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import Node from '../../scenery/js/nodes/Node.js';
 import Path from '../../scenery/js/nodes/Path.js';
-import Color from '../../scenery/js/util/Color.js';
 import RectangularButtonView from '../../sun/js/buttons/RectangularButtonView.js';
 import RectangularPushButton from '../../sun/js/buttons/RectangularPushButton.js';
 import FontAwesomeNode from '../../sun/js/FontAwesomeNode.js';
 import CurvedArrowShape from './CurvedArrowShape.js';
 import sceneryPhet from './sceneryPhet.js';
 
-/**
- * @constructor
- *
- * @param {Object} [options]
- */
-function MoveToTrashButton( options ) {
+class MoveToTrashButton extends RectangularPushButton {
 
-  options = merge( {
-    arrowColor: 'black',
+  /**
+   * @param {Object} [options]
+   */
+  constructor( options ) {
 
-    // RectangularPushButton options
-    baseColor: new Color( 230, 230, 240 ),
-    disabledBaseColor: 'white',
-    buttonAppearanceStrategy: RectangularButtonView.FlatAppearanceStrategy,
-    cornerRadius: 6,
-    xMargin: 7,
-    yMargin: 3
-  }, options );
+    options = merge( {
+      arrowColor: 'black',
 
-  const trashNode = new FontAwesomeNode( 'trash' );
+      // RectangularPushButton options
+      baseColor: 'rgb( 230, 230, 240 )',
+      disabledBaseColor: 'white',
+      buttonAppearanceStrategy: RectangularButtonView.FlatAppearanceStrategy,
+      cornerRadius: 6,
+      xMargin: 7,
+      yMargin: 3
+    }, options );
 
-  const arrowShape = new CurvedArrowShape( 10, -0.9 * Math.PI, -0.2 * Math.PI, {
-    headWidth: 12,
-    tailWidth: 4
-  } );
+    const trashNode = new FontAwesomeNode( 'trash' );
 
-  const arrowPath = new Path( arrowShape, {
-    fill: options.arrowColor,
-    right: trashNode.left + ( 0.75 * trashNode.width ), // a bit to the left of center
-    bottom: trashNode.top
-  } );
+    const arrowShape = new CurvedArrowShape( 10, -0.9 * Math.PI, -0.2 * Math.PI, {
+      headWidth: 12,
+      tailWidth: 4
+    } );
 
-  options.content = new Node( {
-    children: [ trashNode, arrowPath ],
-    scale: 0.4
-  } );
+    const arrowPath = new Path( arrowShape, {
+      fill: options.arrowColor,
+      right: trashNode.left + ( 0.75 * trashNode.width ), // a bit to the left of center
+      bottom: trashNode.top
+    } );
 
-  RectangularPushButton.call( this, options );
+    options.content = new Node( {
+      children: [ trashNode, arrowPath ],
+      scale: 0.4
+    } );
 
-  // support for binder documentation, stripped out in builds and only runs when ?binder is specified
-  assert && phet.chipper.queryParameters.binder && InstanceRegistry.registerDataURL( 'scenery-phet', 'MoveToTrashButton', this );
+    super( options );
+
+    // support for binder documentation, stripped out in builds and only runs when ?binder is specified
+    assert && phet.chipper.queryParameters.binder && InstanceRegistry.registerDataURL( 'scenery-phet', 'MoveToTrashButton', this );
+  }
 }
 
 sceneryPhet.register( 'MoveToTrashButton', MoveToTrashButton );
-
-inherit( RectangularPushButton, MoveToTrashButton );
 export default MoveToTrashButton;
