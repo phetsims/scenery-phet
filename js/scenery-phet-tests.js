d4727973c227018be0d6277741a32ce4a0511d8b
samreid <reids@colorado.edu>
2020-05-04 17:59:15 -0600

    Use qunitStart(), see https://github.com/phetsims/scenery-phet/issues/602

--- js/scenery-phet-tests.js
+++ js/scenery-phet-tests.js
@@ -10,6 +10,7 @@
 
 import './accessibility/GrabDragInteractionTests.js';
 import './MultiLineTextTests.js';
+import qunitStart from '../../chipper/js/sim-tests/qunitStart.js';
 
 // Since our tests are loaded asynchronously, we must direct QUnit to begin the tests
-QUnit.start();
+qunitStart();
