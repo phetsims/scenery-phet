09e5670f8d80a2a8f16f21ea29666efd14716480
samreid <reids@colorado.edu>
2020-07-14 11:03:14 -0600

    Convert NumberDisplay to class, see https://github.com/phetsims/scenery-phet/issues/565

--- js/NumberDisplay.js
+++ js/NumberDisplay.js
@@ -7,7 +7,6 @@
  */
 
 import Utils from '../../dot/js/Utils.js';
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import StringUtils from '../../phetcommon/js/util/StringUtils.js';
 import Node from '../../scenery/js/nodes/Node.js';
@@ -27,160 +26,215 @@ const DEFAULT_FONT = new PhetFont( 20 );
 // valid values for options.align and options.noValueAlign
 const ALIGN_VALUES = [ 'center', 'left', 'right' ];
 
-/**
- * @param {Property.<number|null>} numberProperty
- * @param {Range} displayRange - this range, with options.decimals applied, is used to determine the display width.
- *                               It is unrelated to the range of numberProperty.
- * @param {Object} [options]
- * @constructor
- */
-function NumberDisplay( numberProperty, displayRange, options ) {
+class NumberDisplay extends Node {
 
-  options = merge( {
-    align: 'right', // see ALIGN_VALUES
+  /**
+   * @param {Property.<number|null>} numberProperty
+   * @param {Range} displayRange - this range, with options.decimals applied, is used to determine the display width.
+   *                               It is unrelated to the range of numberProperty.
+   * @param {Object} [options]
+   */
+  constructor( numberProperty, displayRange, options ) {
 
-    // {string} Pattern used to format the value.
-    // Must contain SunConstants.VALUE_NAMED_PLACEHOLDER or SunConstants.VALUE_NUMBERED_PLACEHOLDER.
-    valuePattern: SunConstants.VALUE_NAMED_PLACEHOLDER,
-    useRichText: false,
+    options = merge( {
+      align: 'right', // see ALIGN_VALUES
 
-    // options passed to Text or RichText (depending on the value of options.useRichText) that displays the value
-    textOptions: {
-      font: DEFAULT_FONT,
-      fill: 'black',
-      maxWidth: null, // {number|null} if null, then it will be computed based on displayRange
-      phetioReadOnly: true
-    },
+      // {string} Pattern used to format the value.
+      // Must contain SunConstants.VALUE_NAMED_PLACEHOLDER or SunConstants.VALUE_NUMBERED_PLACEHOLDER.
+      valuePattern: SunConstants.VALUE_NAMED_PLACEHOLDER,
+      useRichText: false,
 
-    // {number|null} the number of decimal places to show. If null, the full value is displayed.
-    // We attempted to change the default to null, but there were too many usages that relied on the 0 default.
-    // See https://github.com/phetsims/scenery-phet/issues/511
-    decimalPlaces: 0,
+      // options passed to Text or RichText (depending on the value of options.useRichText) that displays the value
+      textOptions: {
+        font: DEFAULT_FONT,
+        fill: 'black',
+        maxWidth: null, // {number|null} if null, then it will be computed based on displayRange
+        phetioReadOnly: true
+      },
 
-    xMargin: 8,
-    yMargin: 2,
-    cornerRadius: 0,
-    backgroundFill: 'white',
-    backgroundStroke: 'lightGray',
-    backgroundLineWidth: 1,
-    backgroundLineDash: [],
-    minBackgroundWidth: 0,
+      // {number|null} the number of decimal places to show. If null, the full value is displayed.
+      // We attempted to change the default to null, but there were too many usages that relied on the 0 default.
+      // See https://github.com/phetsims/scenery-phet/issues/511
+      decimalPlaces: 0,
 
-    // options related to display when numberProperty.value === null
-    noValueString: MathSymbols.NO_VALUE, // {string} default is the PhET standard, do no override lightly.
-    noValueAlign: null, // {string|null} see ALIGN_VALUES. If null, defaults to options.align
-    noValuePattern: null, // {string|null} If null, defaults to options.valuePattern
+      xMargin: 8,
+      yMargin: 2,
+      cornerRadius: 0,
+      backgroundFill: 'white',
+      backgroundStroke: 'lightGray',
+      backgroundLineWidth: 1,
+      backgroundLineDash: [],
+      minBackgroundWidth: 0,
 
-    // phet-io
-    tandem: Tandem.OPTIONAL,
-    phetioType: NumberDisplayIO
-  }, options );
+      // options related to display when numberProperty.value === null
+      noValueString: MathSymbols.NO_VALUE, // {string} default is the PhET standard, do no override lightly.
+      noValueAlign: null, // {string|null} see ALIGN_VALUES. If null, defaults to options.align
+      noValuePattern: null, // {string|null} If null, defaults to options.valuePattern
 
-  // Set default alignments and validate
-  assert && assert( _.includes( ALIGN_VALUES, options.align ), 'invalid align: ' + options.align );
-  if ( !options.noValueAlign ) {
-    options.noValueAlign = options.align;
-  }
-  assert && assert( _.includes( ALIGN_VALUES, options.noValueAlign ), 'invalid noValueAlign: ' + options.noValueAlign );
-  assert && assert( options.textOptions, 'did you accidentally set textOptions to null?' );
+      // phet-io
+      tandem: Tandem.OPTIONAL,
+      phetioType: NumberDisplayIO
+    }, options );
 
-  // Support numbered (old-style) placeholder by replacing it with the corresponding named placeholder.
-  // See https://github.com/phetsims/scenery-phet/issues/446
-  if ( options.valuePattern.indexOf( SunConstants.VALUE_NUMBERED_PLACEHOLDER ) !== -1 ) {
-    options.valuePattern = StringUtils.format( options.valuePattern, SunConstants.VALUE_NAMED_PLACEHOLDER );
-  }
-  assert && assert( !!phet.chipper.queryParameters.stringTest ||
-                    options.valuePattern.indexOf( SunConstants.VALUE_NAMED_PLACEHOLDER ) !== -1,
-    'missing value placeholder in options.valuePattern: ' + options.valuePattern );
+    // Set default alignments and validate
+    assert && assert( _.includes( ALIGN_VALUES, options.align ), 'invalid align: ' + options.align );
+    if ( !options.noValueAlign ) {
+      options.noValueAlign = options.align;
+    }
+    assert && assert( _.includes( ALIGN_VALUES, options.noValueAlign ), 'invalid noValueAlign: ' + options.noValueAlign );
+    assert && assert( options.textOptions, 'did you accidentally set textOptions to null?' );
 
-  // Set default and validate
-  if ( !options.noValuePattern ) {
-    options.noValuePattern = options.valuePattern;
-  }
-  assert && assert( !!phet.chipper.queryParameters.stringTest ||
-                    options.noValuePattern.indexOf( SunConstants.VALUE_NAMED_PLACEHOLDER ) !== -1,
-    'missing value placeholder in options.noValuePattern: ' + options.noValuePattern );
+    // Support numbered (old-style) placeholder by replacing it with the corresponding named placeholder.
+    // See https://github.com/phetsims/scenery-phet/issues/446
+    if ( options.valuePattern.indexOf( SunConstants.VALUE_NUMBERED_PLACEHOLDER ) !== -1 ) {
+      options.valuePattern = StringUtils.format( options.valuePattern, SunConstants.VALUE_NAMED_PLACEHOLDER );
+    }
+    assert && assert( !!phet.chipper.queryParameters.stringTest ||
+                      options.valuePattern.indexOf( SunConstants.VALUE_NAMED_PLACEHOLDER ) !== -1,
+      'missing value placeholder in options.valuePattern: ' + options.valuePattern );
 
-  const self = this;
+    // Set default and validate
+    if ( !options.noValuePattern ) {
+      options.noValuePattern = options.valuePattern;
+    }
+    assert && assert( !!phet.chipper.queryParameters.stringTest ||
+                      options.noValuePattern.indexOf( SunConstants.VALUE_NAMED_PLACEHOLDER ) !== -1,
+      'missing value placeholder in options.noValuePattern: ' + options.noValuePattern );
 
-  // determine the widest value
-  const minString = valueToString( displayRange.min, options.decimalPlaces, options.noValueString );
-  const maxString = valueToString( displayRange.max, options.decimalPlaces, options.noValueString );
-  const longestString = StringUtils.fillIn( options.valuePattern, {
-    value: ( ( minString.length > maxString.length ) ? minString : maxString )
-  } );
-
-  // value
-  const Constructor = options.useRichText ? RichText : Text;
-  this.valueText = new Constructor( longestString, merge( {
-    tandem: options.tandem.createTandem( 'valueText' )
-  }, options.textOptions ) );
-
-  // maxWidth for valueText
-  if ( options.textOptions.maxWidth === null ) {
-    this.valueText.maxWidth = this.valueText.width;
-  }
-  else {
-    this.valueText.maxWidth = options.textOptions.maxWidth;
-  }
-
-  const backgroundWidth = Math.max( options.minBackgroundWidth, this.valueText.width + 2 * options.xMargin );
-
-  // @private background
-  this.backgroundNode = new Rectangle( 0, 0, backgroundWidth, this.valueText.height + 2 * options.yMargin, {
-    cornerRadius: options.cornerRadius,
-    fill: options.backgroundFill,
-    stroke: options.backgroundStroke,
-    lineWidth: options.backgroundLineWidth,
-    lineDash: options.backgroundLineDash
-  } );
-
-  options.children = [ this.backgroundNode, this.valueText ];
-
-  // display the value
-  const numberObserver = function( value ) {
-
-    const valuePattern = ( value === null ) ? options.noValuePattern : options.valuePattern;
-    const stringValue = valueToString( value, options.decimalPlaces, options.noValueString );
-    const align = ( value === null ) ? options.noValueAlign : options.align;
-
-    // update the value
-    self.valueText.text = StringUtils.fillIn( valuePattern, {
-      value: stringValue
+    // determine the widest value
+    const minString = valueToString( displayRange.min, options.decimalPlaces, options.noValueString );
+    const maxString = valueToString( displayRange.max, options.decimalPlaces, options.noValueString );
+    const longestString = StringUtils.fillIn( options.valuePattern, {
+      value: ( ( minString.length > maxString.length ) ? minString : maxString )
     } );
 
-    // horizontally align value in background
-    if ( align === 'center' ) {
-      self.valueText.centerX = self.backgroundNode.centerX;
-    }
-    else if ( align === 'left' ) {
-      self.valueText.left = self.backgroundNode.left + options.xMargin;
-    }
-    else { // right
-      self.valueText.right = self.backgroundNode.right - options.xMargin;
-    }
-    self.valueText.centerY = self.backgroundNode.centerY;
-  };
-  numberProperty.link( numberObserver );
+    // value
+    const Constructor = options.useRichText ? RichText : Text;
+    const valueText = new Constructor( longestString, merge( {
+      tandem: options.tandem.createTandem( 'valueText' )
+    }, options.textOptions ) );
 
-  // @private called by dispose
-  this.disposeNumberDisplay = function() {
-    numberProperty.unlink( numberObserver );
-  };
+    // maxWidth for valueText
+    if ( options.textOptions.maxWidth === null ) {
+      valueText.maxWidth = valueText.width;
+    }
+    else {
+      valueText.maxWidth = options.textOptions.maxWidth;
+    }
 
-  Node.call( this, options );
+    const backgroundWidth = Math.max( options.minBackgroundWidth, valueText.width + 2 * options.xMargin );
+
+    // @private background
+    const backgroundNode = new Rectangle( 0, 0, backgroundWidth, valueText.height + 2 * options.yMargin, {
+      cornerRadius: options.cornerRadius,
+      fill: options.backgroundFill,
+      stroke: options.backgroundStroke,
+      lineWidth: options.backgroundLineWidth,
+      lineDash: options.backgroundLineDash
+    } );
+
+    options.children = [ backgroundNode, valueText ];
+
+    // display the value
+    const numberObserver = function( value ) {
+
+      const valuePattern = ( value === null ) ? options.noValuePattern : options.valuePattern;
+      const stringValue = valueToString( value, options.decimalPlaces, options.noValueString );
+      const align = ( value === null ) ? options.noValueAlign : options.align;
+
+      // update the value
+      valueText.text = StringUtils.fillIn( valuePattern, {
+        value: stringValue
+      } );
+
+      // horizontally align value in background
+      if ( align === 'center' ) {
+        valueText.centerX = backgroundNode.centerX;
+      }
+      else if ( align === 'left' ) {
+        valueText.left = backgroundNode.left + options.xMargin;
+      }
+      else { // right
+        valueText.right = backgroundNode.right - options.xMargin;
+      }
+      valueText.centerY = backgroundNode.centerY;
+    };
+    numberProperty.link( numberObserver );
+
+    super( options );
+
+    // @private
+    this.valueText = valueText;
+
+    // @private
+    this.backgroundNode = backgroundNode;
+
+    // @private called by dispose
+    this.disposeNumberDisplay = () => numberProperty.unlink( numberObserver );
+  }
+
+  // @public
+  dispose() {
+    this.disposeNumberDisplay();
+    Node.prototype.dispose.call( this );
+  }
+
+  /**
+   * Sets the number text font.
+   * @param {Font} font
+   * @public
+   */
+  setNumberFont( font ) {
+    this.valueText.font = font;
+  }
+
+  set numberFont( value ) { this.setNumberFont( value ); }
+
+  /**
+   * Sets the number text fill.
+   * @param {Color|string} fill
+   * @public
+   */
+  setNumberFill( fill ) {
+    this.valueText.fill = fill;
+  }
+
+  set numberFill( value ) { this.setNumberFill( value ); }
+
+  /**
+   * Sets the background fill.
+   * @param {Color|string} fill
+   * @public
+   */
+  setBackgroundFill( fill ) {
+    this.backgroundNode.fill = fill;
+  }
+
+  set backgroundFill( value ) { this.setBackgroundFill( value ); }
+
+  /**
+   * Sets the background stroke.
+   * @param {Color|string} stroke
+   * @public
+   */
+  setBackgroundStroke( stroke ) {
+    this.backgroundNode.stroke = stroke;
+  }
+
+  set backgroundStroke( value ) { this.setBackgroundStroke( value ); }
+
 }
 
 sceneryPhet.register( 'NumberDisplay', NumberDisplay );
 
 /**
  * Converts a numeric value to a string.
- * @param {number} value
+ * @param {number|null} value
  * @param {number|null} decimalPlaces - if null, use the full value
  * @param {string} noValueString
  * @returns {*|string}
  */
-function valueToString( value, decimalPlaces, noValueString ) {
+const valueToString = ( value, decimalPlaces, noValueString ) => {
   let stringValue = noValueString;
   if ( value !== null ) {
     if ( decimalPlaces === null ) {
@@ -191,55 +245,6 @@ function valueToString( value, decimalPlaces, noValueString ) {
     }
   }
   return stringValue;
-}
-
-inherit( Node, NumberDisplay, {
-
-  // @public
-  dispose: function() {
-    this.disposeNumberDisplay();
-    Node.prototype.dispose.call( this );
-  },
-
-  /**
-   * Sets the number text font.
-   * @param {Font} font
-   * @public
-   */
-  setNumberFont: function( font ) {
-    this.valueText.font = font;
-  },
-  set numberFont( value ) { this.setNumberFont( value ); },
-
-  /**
-   * Sets the number text fill.
-   * @param {Color|string} fill
-   * @public
-   */
-  setNumberFill: function( fill ) {
-    this.valueText.fill = fill;
-  },
-  set numberFill( value ) { this.setNumberFill( value ); },
-
-  /**
-   * Sets the background fill.
-   * @param {Color|string} fill
-   * @public
-   */
-  setBackgroundFill: function( fill ) {
-    this.backgroundNode.fill = fill;
-  },
-  set backgroundFill( value ) { this.setBackgroundFill( value ); },
-
-  /**
-   * Sets the background stroke.
-   * @param {Color|string} stroke
-   * @public
-   */
-  setBackgroundStroke: function( stroke ) {
-    this.backgroundNode.stroke = stroke;
-  },
-  set backgroundStroke( value ) { this.setBackgroundStroke( value ); }
-} );
+};
 
 export default NumberDisplay;
