82d25408f8c117f48f50606f34fa8951b2581535
pixelzoom <cmalley@pixelzoom.com>
2020-09-10 13:04:46 -0600

    convert FaceNode to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/FaceNode.js
+++ js/FaceNode.js
@@ -11,7 +11,6 @@
 import DerivedProperty from '../../axon/js/DerivedProperty.js';
 import Shape from '../../kite/js/Shape.js';
 import InstanceRegistry from '../../phet-core/js/documentation/InstanceRegistry.js';
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import Circle from '../../scenery/js/nodes/Circle.js';
 import Node from '../../scenery/js/nodes/Node.js';
@@ -19,106 +18,104 @@ import Path from '../../scenery/js/nodes/Path.js';
 import PaintColorProperty from '../../scenery/js/util/PaintColorProperty.js';
 import sceneryPhet from './sceneryPhet.js';
 
-/**
- * @param {number} headDiameter
- * @param {Object} [options]
- * @constructor
- */
-function FaceNode( headDiameter, options ) {
+class FaceNode extends Node {
 
-  // default options
-  options = merge( {
-    headFill: 'yellow',
-    eyeFill: 'black',
-    mouthStroke: 'black',
-    headLineWidth: 1
-  }, options );
+  /**
+   * @param {number} headDiameter
+   * @param {Object} [options]
+   */
+  constructor( headDiameter, options ) {
 
-  // @private {Property.<Color>}
-  this.headFillProperty = new PaintColorProperty( options.headFill );
+    // default options
+    options = merge( {
+      headFill: 'yellow',
+      headStroke: null,
+      eyeFill: 'black',
+      mouthStroke: 'black',
+      headLineWidth: 1
+    }, options );
 
-  // The derived property listens to our headFillProperty which will be disposed. We don't need to keep a reference.
-  options.headStroke = options.headStroke || new DerivedProperty( [ this.headFillProperty ], function( color ) {
-    return color.darkerColor();
-  } );
+    super();
 
-  Node.call( this );
+    // @private {Property.<Color>}
+    this.headFillProperty = new PaintColorProperty( options.headFill );
 
-  // Add head.
-  this.addChild( new Circle( headDiameter / 2, {
-    fill: options.headFill,
-    stroke: options.headStroke,
-    lineWidth: options.headLineWidth
-  } ) );
+    // The derived property listens to our headFillProperty which will be disposed. We don't need to keep a reference.
+    options.headStroke = options.headStroke ||
+                         new DerivedProperty( [ this.headFillProperty ],
+                           color => color.darkerColor()
+                         );
 
-  // Add the eyes.
-  const eyeDiameter = headDiameter * 0.075;
-  this.addChild( new Circle( eyeDiameter, {
-    fill: options.eyeFill,
-    centerX: -headDiameter * 0.2,
-    centerY: -headDiameter * 0.1
-  } ) );
-  this.addChild( new Circle( eyeDiameter, {
-    fill: options.eyeFill,
-    centerX: headDiameter * 0.2,
-    centerY: -headDiameter * 0.1
-  } ) );
+    // Add head.
+    this.addChild( new Circle( headDiameter / 2, {
+      fill: options.headFill,
+      stroke: options.headStroke,
+      lineWidth: options.headLineWidth
+    } ) );
 
-  // Add the mouths.
-  const mouthLineWidth = headDiameter * 0.05;
+    // Add the eyes.
+    const eyeDiameter = headDiameter * 0.075;
+    this.addChild( new Circle( eyeDiameter, {
+      fill: options.eyeFill,
+      centerX: -headDiameter * 0.2,
+      centerY: -headDiameter * 0.1
+    } ) );
+    this.addChild( new Circle( eyeDiameter, {
+      fill: options.eyeFill,
+      centerX: headDiameter * 0.2,
+      centerY: -headDiameter * 0.1
+    } ) );
 
-  // @private
-  this.smileMouth = new Path( new Shape().arc( 0, headDiameter * 0.05, headDiameter * 0.25, Math.PI * 0.2, Math.PI * 0.8 ), {
-    stroke: options.mouthStroke,
-    lineWidth: mouthLineWidth,
-    lineCap: 'round'
-  } );
-  this.addChild( this.smileMouth );
+    // Add the mouths.
+    const mouthLineWidth = headDiameter * 0.05;
 
-  // @private
-  this.frownMouth = new Path( new Shape().arc( 0, headDiameter * 0.4, headDiameter * 0.20, -Math.PI * 0.75, -Math.PI * 0.25 ), {
-    stroke: options.mouthStroke,
-    lineWidth: mouthLineWidth,
-    lineCap: 'round'
-  } );
-  this.addChild( this.frownMouth );
-  this.smile();
+    // @private
+    this.smileMouth = new Path( new Shape().arc( 0, headDiameter * 0.05, headDiameter * 0.25, Math.PI * 0.2, Math.PI * 0.8 ), {
+      stroke: options.mouthStroke,
+      lineWidth: mouthLineWidth,
+      lineCap: 'round'
+    } );
+    this.addChild( this.smileMouth );
 
-  // Pass through any options for positioning and such.
-  this.mutate( options );
+    // @private
+    this.frownMouth = new Path( new Shape().arc( 0, headDiameter * 0.4, headDiameter * 0.20, -Math.PI * 0.75, -Math.PI * 0.25 ), {
+      stroke: options.mouthStroke,
+      lineWidth: mouthLineWidth,
+      lineCap: 'round'
+    } );
+    this.addChild( this.frownMouth );
+    this.smile();
 
-  // support for binder documentation, stripped out in builds and only runs when ?binder is specified
-  assert && phet.chipper.queryParameters.binder && InstanceRegistry.registerDataURL( 'scenery-phet', 'FaceNode', this );
-}
+    // Pass through any options for positioning and such.
+    this.mutate( options );
 
-sceneryPhet.register( 'FaceNode', FaceNode );
-
-inherit( Node, FaceNode, {
+    // support for binder documentation, stripped out in builds and only runs when ?binder is specified
+    assert && phet.chipper.queryParameters.binder && InstanceRegistry.registerDataURL( 'scenery-phet', 'FaceNode', this );
+  }
 
   // @public
-  smile: function() {
+  smile() {
     this.smileMouth.visible = true;
     this.frownMouth.visible = false;
     return this; // allow chaining
-  },
+  }
 
   // @public
-  frown: function() {
+  frown() {
     this.smileMouth.visible = false;
     this.frownMouth.visible = true;
     return this; // allow chaining
-  },
+  }
 
   /**
-   * Releases references.
    * @public
    * @override
    */
-  dispose: function() {
+  dispose() {
     this.headFillProperty.dispose();
-
-    Node.prototype.dispose.call( this );
+    super.dispose();
   }
-} );
+}
 
+sceneryPhet.register( 'FaceNode', FaceNode );
 export default FaceNode;
