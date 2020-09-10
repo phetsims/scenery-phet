912a1e39007d7e4a237ce33f43104f0470ec9872
pixelzoom <cmalley@pixelzoom.com>
2020-09-10 12:54:54 -0600

    convert EyeDropperNode to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/EyeDropperNode.js
+++ js/EyeDropperNode.js
@@ -9,7 +9,6 @@
 import Property from '../../axon/js/Property.js';
 import Shape from '../../kite/js/Shape.js';
 import InstanceRegistry from '../../phet-core/js/documentation/InstanceRegistry.js';
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import Circle from '../../scenery/js/nodes/Circle.js';
 import Image from '../../scenery/js/nodes/Image.js';
@@ -32,131 +31,143 @@ const GLASS_MAX_Y = -18; // relative to bottom center
 const GLASS_MIN_Y = -124; // relative to bottom center
 const BUTTON_CENTER_Y_OFFSET = 32; // y-offset of button's center in dropper image file
 
-/**
- * @param {Object} [options]
- * @constructor
- */
-function EyeDropperNode( options ) {
+class EyeDropperNode extends Node {
 
-  options = merge( {
-    cursor: 'pointer',
-    dispensingProperty: new Property( false ), // is the dropper dispensing?
-    enabledProperty: new Property( true ), // is the button enabled?
-    emptyProperty: new Property( false ), // does the dropper appear to be empty?
-    buttonTouchAreaDilation: 15, // dilation of the button's radius for touchArea
-    fluidColor: 'yellow', // {Color|String} color of the fluid in the glass
+  /**
+   * @param {Object} [options]
+   */
+  constructor( options ) {
 
-    // Note: EyeDropperNode is not draggable and hence only registers its button with tandem.
-    tandem: Tandem.REQUIRED
-  }, options );
+    options = merge( {
 
-  const self = this;
+      dispensingProperty: new Property( false ), // is the dropper dispensing?
+      enabledProperty: new Property( true ), // is the button enabled?
+      emptyProperty: new Property( false ), // does the dropper appear to be empty?
+      buttonTouchAreaDilation: 15, // dilation of the button's radius for touchArea
+      fluidColor: 'yellow', // {Color|String} color of the fluid in the glass
 
-  // @public
-  this.dispensingProperty = options.dispensingProperty;
-  this.enabledProperty = options.enabledProperty;
-  this.emptyProperty = options.emptyProperty;
+      // Node options
+      cursor: 'pointer',
 
-  // @private fluid fills the glass portion of the dropper, shape is specific to the dropper image file
-  this.fluidNode = new Path( new Shape()
-    .moveTo( -TIP_WIDTH / 2, 0 )
-    .lineTo( -TIP_WIDTH / 2, -TIP_HEIGHT )
-    .lineTo( -GLASS_WIDTH / 2, GLASS_MAX_Y )
-    .lineTo( -GLASS_WIDTH / 2, GLASS_MIN_Y )
-    .lineTo( GLASS_WIDTH / 2, GLASS_MIN_Y )
-    .lineTo( GLASS_WIDTH / 2, GLASS_MAX_Y )
-    .lineTo( TIP_WIDTH / 2, -TIP_HEIGHT )
-    .lineTo( TIP_WIDTH / 2, 0 )
-    .close(), {
-    fill: options.fluidColor
-  } );
+      // phet-io
+      tandem: Tandem.REQUIRED
+    }, options );
 
-  // images, origin moved to bottom center
-  const foreground = new Image( foregroundImage );
-  const background = new Image( backgroundImage );
-  foreground.x = -foreground.width / 2;
-  foreground.y = -foreground.height;
-  background.x = -background.width / 2;
-  background.y = -background.height;
+    super();
 
-  // button, centered in the dropper's bulb
-  const button = new RoundMomentaryButton( false, true, this.dispensingProperty, {
-    baseColor: 'red',
-    radius: 18,
-    listenerOptions: {
-      // We want to be able to drag the dropper WHILE dispensing, see https://github.com/phetsims/ph-scale/issues/86
-      attach: false
-    },
-    tandem: options.tandem.createTandem( 'button' )
-  } );
-  const enabledObserver = function( enabled ) { button.enabled = enabled; };
-  this.enabledProperty.link( enabledObserver );
-  button.touchArea = Shape.circle( 0, 0, ( button.width / 2 ) + options.buttonTouchAreaDilation );
-  button.centerX = foreground.centerX;
-  button.centerY = foreground.top + BUTTON_CENTER_Y_OFFSET;
+    // @public
+    this.dispensingProperty = options.dispensingProperty;
+    this.enabledProperty = options.enabledProperty;
+    this.emptyProperty = options.emptyProperty;
 
-  // make the background visible only when the dropper is empty
-  const emptyObserver = function( empty ) {
-    self.fluidNode.visible = !empty;
-    background.visible = empty;
-  };
-  this.emptyProperty.link( emptyObserver );
+    // @private fluid fills the glass portion of the dropper, shape is specific to the dropper image file
+    this.fluidNode = new Path( new Shape()
+      .moveTo( -TIP_WIDTH / 2, 0 )
+      .lineTo( -TIP_WIDTH / 2, -TIP_HEIGHT )
+      .lineTo( -GLASS_WIDTH / 2, GLASS_MAX_Y )
+      .lineTo( -GLASS_WIDTH / 2, GLASS_MIN_Y )
+      .lineTo( GLASS_WIDTH / 2, GLASS_MIN_Y )
+      .lineTo( GLASS_WIDTH / 2, GLASS_MAX_Y )
+      .lineTo( TIP_WIDTH / 2, -TIP_HEIGHT )
+      .lineTo( TIP_WIDTH / 2, 0 )
+      .close(), {
+      fill: options.fluidColor
+    } );
 
-  options.children = [ this.fluidNode, background, foreground, button ];
+    // images, origin moved to bottom center
+    const foreground = new Image( foregroundImage );
+    const background = new Image( backgroundImage );
+    foreground.x = -foreground.width / 2;
+    foreground.y = -foreground.height;
+    background.x = -background.width / 2;
+    background.y = -background.height;
 
-  // add a red dot at the origin
-  if ( DEBUG_ORIGIN ) {
-    options.children.push( new Circle( { radius: 3, fill: 'red' } ) );
+    // button, centered in the dropper's bulb
+    const button = new RoundMomentaryButton( false, true, this.dispensingProperty, {
+      baseColor: 'red',
+      radius: 18,
+      listenerOptions: {
+        // We want to be able to drag the dropper WHILE dispensing, see https://github.com/phetsims/ph-scale/issues/86
+        attach: false
+      },
+      tandem: options.tandem.createTandem( 'button' )
+    } );
+    const enabledObserver = function( enabled ) { button.enabled = enabled; };
+    this.enabledProperty.link( enabledObserver );
+    button.touchArea = Shape.circle( 0, 0, ( button.width / 2 ) + options.buttonTouchAreaDilation );
+    button.centerX = foreground.centerX;
+    button.centerY = foreground.top + BUTTON_CENTER_Y_OFFSET;
+
+    // make the background visible only when the dropper is empty
+    const emptyObserver = empty => {
+      this.fluidNode.visible = !empty;
+      background.visible = empty;
+    };
+    this.emptyProperty.link( emptyObserver );
+
+    assert && assert( !options.children, 'EyeDropperNode sets children' );
+    options.children = [ this.fluidNode, background, foreground, button ];
+
+    // add a red dot at the origin
+    if ( DEBUG_ORIGIN ) {
+      options.children.push( new Circle( { radius: 3, fill: 'red' } ) );
+    }
+
+    this.mutate( options );
+
+    // @private
+    this.disposeEyeDropperNode = () => {
+      button.dispose();
+      this.enabledProperty.unlink( enabledObserver );
+      this.emptyProperty.unlink( emptyObserver );
+    };
+
+    // support for binder documentation, stripped out in builds and only runs when ?binder is specified
+    assert && phet.chipper.queryParameters.binder && InstanceRegistry.registerDataURL( 'scenery-phet', 'EyeDropperNode', this );
   }
 
-  Node.call( this, options );
+  get fluidColor() {
+    return this.getFluidColor();
+  }
 
-  // @private
-  this.disposeEyeDropperNode = function() {
-    button.dispose();
-    self.enabledProperty.unlink( enabledObserver );
-    self.emptyProperty.unlink( emptyObserver );
-  };
+  set fluidColor( value ) {
+    this.setFluidColor( value );
+  }
 
-  // support for binder documentation, stripped out in builds and only runs when ?binder is specified
-  assert && phet.chipper.queryParameters.binder && InstanceRegistry.registerDataURL( 'scenery-phet', 'EyeDropperNode', this );
+  /**
+   * @public
+   * @override
+   */
+  dispose() {
+    this.disposeEyeDropperNode();
+    super.dispose();
+  }
+
+  /**
+   * Sets the color of the fluid in the dropper.
+   * @param color {ColorDef}
+   * @public
+   */
+  setFluidColor( color ) {
+    this.fluidNode.fill = color;
+  }
+
+  /**
+   * Gets the color of the fluid in the dropper.
+   * @returns {ColorDef}
+   * @public
+   */
+  getFluidColor() {
+    return this.fluidNode.fill;
+  }
 }
 
+// @public You'll need these if you want to create fluid coming out of the tip, or put a label on the glass.
+EyeDropperNode.TIP_WIDTH = TIP_WIDTH;
+EyeDropperNode.TIP_HEIGHT = TIP_HEIGHT;
+EyeDropperNode.GLASS_WIDTH = GLASS_WIDTH;
+EyeDropperNode.GLASS_MIN_Y = GLASS_MIN_Y;
+EyeDropperNode.GLASS_MAX_Y = GLASS_MAX_Y;
+
 sceneryPhet.register( 'EyeDropperNode', EyeDropperNode );
-
-inherit( Node, EyeDropperNode, {
-
-  // @public makes this instance eligible for garbage collection
-  dispose: function() {
-    this.disposeEyeDropperNode();
-    Node.prototype.dispose.call( this );
-  },
-
-  set dispensing( value ) { this.dispensingProperty.value = value; },
-
-  get dispensing() { return this.dispensingProperty.value; },
-
-  set enabled( value ) { this.enabledProperty.value = value; },
-
-  get enabled() { return this.enabledProperty.value; },
-
-  set empty( value ) { this.emptyProperty.value = value; },
-
-  get empty() { return this.emptyProperty.value; },
-
-  set fluidColor( value ) { this.fluidNode.fill = value; },
-
-  get fluidColor() { return this.fluidNode.fill; }
-
-}, {
-
-  // @static
-  // You'll need these if you want to create fluid coming out of the tip, or put a label on the glass.
-  TIP_WIDTH: TIP_WIDTH,
-  TIP_HEIGHT: TIP_HEIGHT,
-  GLASS_WIDTH: GLASS_WIDTH,
-  GLASS_MIN_Y: GLASS_MIN_Y,
-  GLASS_MAX_Y: GLASS_MAX_Y
-} );
-
 export default EyeDropperNode;
