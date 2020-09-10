7baf5ee3e5658b8e2f9cc085c70fe8371e97277b
pixelzoom <cmalley@pixelzoom.com>
2020-09-10 11:10:24 -0600

    convert CanvasWarningNode to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/CanvasWarningNode.js
+++ js/CanvasWarningNode.js
@@ -6,59 +6,54 @@
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 
-import inherit from '../../phet-core/js/inherit.js';
 import openPopup from '../../phet-core/js/openPopup.js';
 import HBox from '../../scenery/js/nodes/HBox.js';
 import Text from '../../scenery/js/nodes/Text.js';
 import VBox from '../../scenery/js/nodes/VBox.js';
 import FontAwesomeNode from '../../sun/js/FontAwesomeNode.js';
 import PhetFont from './PhetFont.js';
-import sceneryPhetStrings from './sceneryPhetStrings.js';
 import sceneryPhet from './sceneryPhet.js';
+import sceneryPhetStrings from './sceneryPhetStrings.js';
 
-const webglWarningBodyString = sceneryPhetStrings.webglWarning.body;
-const webglWarningTitleString = sceneryPhetStrings.webglWarning.title;
+class CanvasWarningNode extends HBox {
 
-/**
- * @constructor
- */
-function CanvasWarningNode() {
-  HBox.call( this, {
-    children: [
-      new FontAwesomeNode( 'warning_sign', {
-        fill: '#E87600', // "safety orange", according to Wikipedia
-        scale: 0.6
-      } ),
-      new VBox( {
-        children: [
-          new Text( webglWarningTitleString, {
-            font: new PhetFont( 14 ),
-            fill: '#ddd'
-          } ),
-          new Text( webglWarningBodyString, {
-            font: new PhetFont( 10 ),
-            fill: '#999'
-          } )
-        ],
-        spacing: 3,
-        align: 'left'
-      } )
-    ],
-    spacing: 12,
-    align: 'center',
-    cursor: 'pointer'
-  } );
+  constructor() {
 
-  this.mouseArea = this.touchArea = this.localBounds;
+    super( {
+      children: [
+        new FontAwesomeNode( 'warning_sign', {
+          fill: '#E87600', // "safety orange", according to Wikipedia
+          scale: 0.6
+        } ),
+        new VBox( {
+          children: [
+            new Text( sceneryPhetStrings.webglWarning.title, {
+              font: new PhetFont( 14 ),
+              fill: '#ddd'
+            } ),
+            new Text( sceneryPhetStrings.webglWarning.body, {
+              font: new PhetFont( 10 ),
+              fill: '#999'
+            } )
+          ],
+          spacing: 3,
+          align: 'left'
+        } )
+      ],
+      spacing: 12,
+      align: 'center',
+      cursor: 'pointer'
+    } );
 
-  this.addInputListener( {
-    up: function() {
-      openPopup( 'http://phet.colorado.edu/webgl-disabled-page?simLocale=' + phet.joist.sim.locale );
-    }
-  } );
+    this.mouseArea = this.touchArea = this.localBounds;
+
+    this.addInputListener( {
+      up: function() {
+        openPopup( 'http://phet.colorado.edu/webgl-disabled-page?simLocale=' + phet.joist.sim.locale );
+      }
+    } );
+  }
 }
 
 sceneryPhet.register( 'CanvasWarningNode', CanvasWarningNode );
-
-inherit( HBox, CanvasWarningNode );
 export default CanvasWarningNode;
