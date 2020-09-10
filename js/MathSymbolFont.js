a4b3e80f8fb646ba34736a3e58131c914e2d1d73
pixelzoom <cmalley@pixelzoom.com>
2020-09-10 16:16:32 -0600

    convert MathSymbolFont to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/MathSymbolFont.js
+++ js/MathSymbolFont.js
@@ -1,13 +1,12 @@
 // Copyright 2016-2020, University of Colorado Boulder
 
 /**
- * Font used for math symbols, e.g. 'x', 'y'.
+ * MathSymbolFont is the font used for math symbols (e.g. 'x', 'y') in PhET sims.
  * See https://github.com/phetsims/scenery/issues/545
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import inherit from '../../phet-core/js/inherit.js';
 import StringUtils from '../../phetcommon/js/util/StringUtils.js';
 import Font from '../../scenery/js/util/Font.js';
 import sceneryPhet from './sceneryPhet.js';
@@ -15,36 +14,30 @@ import sceneryPhet from './sceneryPhet.js';
 // constants
 const FAMILY = '"Times New Roman", Times, serif';
 
-/**
- * @param {Object|number|string} [options] number or string indicate the font size, otherwise same options as scenery.Font
- * @constructor
- */
-function MathSymbolFont( options ) {
+class MathSymbolFont extends Font {
 
-  assert && assert( arguments.length === 0 || arguments.length === 1, 'Too many arguments' );
+  /**
+   * @param {Object|number|string} [options] number or string indicate the font size, otherwise same options as scenery.Font
+   */
+  constructor( options ) {
 
-  options = options || {};
+    assert && assert( arguments.length <= 1, 'Too many arguments' );
 
-  // convenience constructor: new MathSymbolFont( {number|string} size )
-  if ( typeof options === 'number' || typeof options === 'string' ) {
-    options = { size: options };
+    options = options || {};
+
+    // convenience constructor: new MathSymbolFont( {number|string} size )
+    if ( typeof options === 'number' || typeof options === 'string' ) {
+      options = { size: options };
+    }
+
+    assert && assert( !options.family, 'MathSymbolFont sets family' );
+    options.family = FAMILY;
+
+    assert && assert( !options.style, 'MathSymbolFont sets style' );
+    options.style = 'italic';
+
+    super( options );
   }
-
-  assert && assert( !options.family, 'this type sets family' );
-  options.family = FAMILY;
-
-  assert && assert( !options.style, 'this type sets style' );
-  options.style = 'italic';
-
-  Font.call( this, options );
-}
-
-sceneryPhet.register( 'MathSymbolFont', MathSymbolFont );
-
-inherit( Font, MathSymbolFont, {}, {
-
-  // @public @static
-  FAMILY: FAMILY,
 
   /**
    * Converts a string to the markup needed to display that string with RichText,
@@ -52,14 +45,17 @@ inherit( Font, MathSymbolFont, {}, {
    * @param {string} text
    * @returns {string}
    * @public
-   * @static
    */
-  getRichTextMarkup: function( text ) {
+  static getRichTextMarkup( text ) {
     return StringUtils.fillIn( '<i><span style=\'font-family: {{face}};\'>{{text}}</span></i>', {
       face: FAMILY,
       text: text
     } );
   }
-} );
+}
 
+// @public
+MathSymbolFont.FAMILY = FAMILY;
+
+sceneryPhet.register( 'MathSymbolFont', MathSymbolFont );
 export default MathSymbolFont;
