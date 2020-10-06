a8223f57d91564e194729681199c7d72d50e812c
phet-dev <phetadmin@gmail.com>
2020-10-06 04:00:07 -0600

    update copyright dates from daily grunt work

--- js/keyboard/help/ComboBoxKeyboardHelpSection.js
+++ js/keyboard/help/ComboBoxKeyboardHelpSection.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2020, University of Colorado Boulder
+// Copyright 2020, University of Colorado Boulder
 
 /**
  * Help section for explaining how to use a keyboard to change a combo box.
