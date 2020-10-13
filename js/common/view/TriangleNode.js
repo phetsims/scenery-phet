03246dff802570f764ad9ba5fb29263b214f9cc7
Jonathan Olson <jonathan.olson@colorado.edu>
2020-10-13 12:43:57 -0600

    ES6'ing the sim

--- js/common/view/TriangleNode.js
+++ js/common/view/TriangleNode.js
@@ -8,7 +8,6 @@
 
 import Matrix3 from '../../../../dot/js/Matrix3.js';
 import Shape from '../../../../kite/js/Shape.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import merge from '../../../../phet-core/js/merge.js';
 import Path from '../../../../scenery/js/nodes/Path.js';
 import proportionPlayground from '../../proportionPlayground.js';
@@ -23,26 +22,24 @@ const LEFT_TRIANGLE_SHAPE = new Shape().moveTo( 0, 0 )
   .lineTo( 0, 0 );
 const RIGHT_TRIANGLE_SHAPE = LEFT_TRIANGLE_SHAPE.transformed( Matrix3.scaling( -1, 1 ) );
 
-/**
- * @constructor
- * @extends {Path}
- *
- * @param {Side} side - Side.LEFT or Side.RIGHT
- * @param {Object} [options]
- */
-function TriangleNode( side, options ) {
-  assert && assert( Side.isSide( side ), 'Side should be Side.LEFT or Side.RIGHT' );
+class TriangleNode extends Path {
+  /**
+   * @param {Side} side - Side.LEFT or Side.RIGHT
+   * @param {Object} [options]
+   */
+  constructor( side, options ) {
+    assert && assert( Side.isSide( side ), 'Side should be Side.LEFT or Side.RIGHT' );
 
-  // defaults
-  options = merge( {
-    stroke: 'black',
-    lineWidth: 1
-  }, options );
+    // defaults
+    options = merge( {
+      stroke: 'black',
+      lineWidth: 1
+    }, options );
 
-  Path.call( this, side === Side.LEFT ? LEFT_TRIANGLE_SHAPE : RIGHT_TRIANGLE_SHAPE, options );
+    super( side === Side.LEFT ? LEFT_TRIANGLE_SHAPE : RIGHT_TRIANGLE_SHAPE, options );
+  }
 }
 
 proportionPlayground.register( 'TriangleNode', TriangleNode );
 
-inherit( Path, TriangleNode );
 export default TriangleNode;
