8c6559412b906aa33f57b050a6180b47a49eb728
Jonathan Olson <jonathan.olson@colorado.edu>
2020-10-13 09:38:34 -0600

    ES6 ResetShape, see https://github.com/phetsims/tasks/issues/1044

--- js/ResetShape.js
+++ js/ResetShape.js
@@ -8,56 +8,55 @@
  */
 
 import Shape from '../../kite/js/Shape.js';
-import inherit from '../../phet-core/js/inherit.js';
 import sceneryPhet from './sceneryPhet.js';
 
-/**
- * @param {number} radius of the center of the reset arrow
- * @constructor
- */
-function ResetShape( radius ) {
+class ResetShape extends Shape {
+  /**
+   * @param {number} radius of the center of the reset arrow
+   */
+  constructor( radius ) {
 
-  Shape.call( this );
+    super();
 
-  // Adjust these parameters to tweak the appearance of the arrow.
-  const INNER_RADIUS = radius * 0.4;
-  const OUTER_RADIUS = radius * 0.625;
-  const HEAD_WIDTH = 2.25 * ( OUTER_RADIUS - INNER_RADIUS );
-  const START_ANGLE = -Math.PI * 0.35;
-  const END_TO_NECK_ANGULAR_SPAN = -2 * Math.PI * 0.85;
-  const ARROW_HEAD_ANGULAR_SPAN = -Math.PI * 0.18;
+    // Adjust these parameters to tweak the appearance of the arrow.
+    const INNER_RADIUS = radius * 0.4;
+    const OUTER_RADIUS = radius * 0.625;
+    const HEAD_WIDTH = 2.25 * ( OUTER_RADIUS - INNER_RADIUS );
+    const START_ANGLE = -Math.PI * 0.35;
+    const END_TO_NECK_ANGULAR_SPAN = -2 * Math.PI * 0.85;
+    const ARROW_HEAD_ANGULAR_SPAN = -Math.PI * 0.18;
 
-  // Create the curved arrow shape, starting at the inside of the non-pointed end.
-  // Inner edge of end.
-  this.moveTo( INNER_RADIUS * Math.cos( START_ANGLE ), INNER_RADIUS * Math.sin( START_ANGLE ) );
-  this.lineTo( OUTER_RADIUS * Math.cos( START_ANGLE ), OUTER_RADIUS * Math.sin( START_ANGLE ) );
-  const neckAngle = START_ANGLE + END_TO_NECK_ANGULAR_SPAN;
+    // Create the curved arrow shape, starting at the inside of the non-pointed end.
+    // Inner edge of end.
+    this.moveTo( INNER_RADIUS * Math.cos( START_ANGLE ), INNER_RADIUS * Math.sin( START_ANGLE ) );
+    this.lineTo( OUTER_RADIUS * Math.cos( START_ANGLE ), OUTER_RADIUS * Math.sin( START_ANGLE ) );
+    const neckAngle = START_ANGLE + END_TO_NECK_ANGULAR_SPAN;
 
-  // Outer curve.
-  this.arc( 0, 0, OUTER_RADIUS, START_ANGLE, neckAngle, true );
-  const HEAD_WIDTHExtrusion = ( HEAD_WIDTH - ( OUTER_RADIUS - INNER_RADIUS ) ) / 2;
-  this.lineTo(
-    ( OUTER_RADIUS + HEAD_WIDTHExtrusion ) * Math.cos( neckAngle ),
-    ( OUTER_RADIUS + HEAD_WIDTHExtrusion ) * Math.sin( neckAngle ) );
+    // Outer curve.
+    this.arc( 0, 0, OUTER_RADIUS, START_ANGLE, neckAngle, true );
+    const HEAD_WIDTHExtrusion = ( HEAD_WIDTH - ( OUTER_RADIUS - INNER_RADIUS ) ) / 2;
+    this.lineTo(
+      ( OUTER_RADIUS + HEAD_WIDTHExtrusion ) * Math.cos( neckAngle ),
+      ( OUTER_RADIUS + HEAD_WIDTHExtrusion ) * Math.sin( neckAngle ) );
 
-  // Tip of arrowhead.
-  const pointRadius = ( OUTER_RADIUS + INNER_RADIUS ) * 0.55; // Tweaked a little from center for better look.
-  this.lineTo(
-    pointRadius * Math.cos( neckAngle + ARROW_HEAD_ANGULAR_SPAN ),
-    pointRadius * Math.sin( neckAngle + ARROW_HEAD_ANGULAR_SPAN ) );
-  this.lineTo(
-    ( INNER_RADIUS - HEAD_WIDTHExtrusion ) * Math.cos( neckAngle ),
-    ( INNER_RADIUS - HEAD_WIDTHExtrusion ) * Math.sin( neckAngle ) );
-  this.lineTo(
-    INNER_RADIUS * Math.cos( neckAngle ),
-    INNER_RADIUS * Math.sin( neckAngle ) );
+    // Tip of arrowhead.
+    const pointRadius = ( OUTER_RADIUS + INNER_RADIUS ) * 0.55; // Tweaked a little from center for better look.
+    this.lineTo(
+      pointRadius * Math.cos( neckAngle + ARROW_HEAD_ANGULAR_SPAN ),
+      pointRadius * Math.sin( neckAngle + ARROW_HEAD_ANGULAR_SPAN ) );
+    this.lineTo(
+      ( INNER_RADIUS - HEAD_WIDTHExtrusion ) * Math.cos( neckAngle ),
+      ( INNER_RADIUS - HEAD_WIDTHExtrusion ) * Math.sin( neckAngle ) );
+    this.lineTo(
+      INNER_RADIUS * Math.cos( neckAngle ),
+      INNER_RADIUS * Math.sin( neckAngle ) );
 
-  // Inner curve.
-  this.arc( 0, 0, INNER_RADIUS, neckAngle, START_ANGLE );
-  this.close();
+    // Inner curve.
+    this.arc( 0, 0, INNER_RADIUS, neckAngle, START_ANGLE );
+    this.close();
+  }
 }
 
 sceneryPhet.register( 'ResetShape', ResetShape );
 
-inherit( Shape, ResetShape );
 export default ResetShape;
