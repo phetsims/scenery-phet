45e71634b2f2db76d87a70315a66cb08d00a8648
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 10:54:49 -0600

    convert PhetFont to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/PhetFont.js
+++ js/PhetFont.js
@@ -11,37 +11,36 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import Font from '../../scenery/js/util/Font.js';
 import sceneryPhet from './sceneryPhet.js';
 
-/**
- * @param {number|string|Object} [options] number or string indicate the font size, otherwise same options as scenery.Font
- * @constructor
- */
-function PhetFont( options ) {
+class PhetFont extends Font {
 
-  assert && assert( arguments.length === 0 || arguments.length === 1, 'Too many arguments' );
+  /**
+   * @param {number|string|Object} [options] number or string indicate the font size, otherwise same options as scenery.Font
+   */
+  constructor( options ) {
 
-  // convenience constructor: new PhetFont( {number|string} size )
-  if ( typeof options === 'number' || typeof options === 'string' ) {
-    options = { size: options };
+    assert && assert( arguments.length === 0 || arguments.length === 1, 'Too many arguments' );
+
+    // convenience constructor: new PhetFont( {number|string} size )
+    if ( typeof options === 'number' || typeof options === 'string' ) {
+      options = { size: options };
+    }
+
+    // PhET defaults
+    options = merge( {
+      family: 'Arial'
+    }, options );
+
+    // Guarantee a fallback family
+    assert && assert( options.family );
+    options.family = options.family + ', sans-serif';
+
+    super( options );
   }
-
-  // PhET defaults
-  options = merge( {
-    family: 'Arial'
-  }, options );
-
-  // Guarantee a fallback family
-  assert && assert( options.family );
-  options.family = options.family + ', sans-serif';
-
-  Font.call( this, options );
 }
 
 sceneryPhet.register( 'PhetFont', PhetFont );
-
-inherit( Font, PhetFont );
 export default PhetFont;
