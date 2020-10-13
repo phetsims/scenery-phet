b6658c2ef3e351d530238c7760be6915d91256ba
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 11:15:29 -0600

    convert to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/ShadowText.js
+++ js/ShadowText.js
@@ -6,42 +6,46 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import Node from '../../scenery/js/nodes/Node.js';
 import Text from '../../scenery/js/nodes/Text.js';
 import PhetFont from './PhetFont.js';
 import sceneryPhet from './sceneryPhet.js';
 
-/**
- * @param {string} text
- * @param {Object} [options]
- * @constructor
- */
-function ShadowText( text, options ) {
-  options = merge( {
-    font: new PhetFont( 24 ),
-    fill: 'lightGray',
-    stroke: null,
-    shadowXOffset: 3,
-    shadowYOffset: 1,
-    shadowFill: 'black'
-  }, options );
+class ShadowText extends Node {
 
-  options.children = [
-    new Text( text, {
-      font: options.font,
-      fill: options.shadowFill,
-      x: options.shadowXOffset,
-      y: options.shadowYOffset
-    } ), // shadow
-    new Text( text, { font: options.font, fill: options.fill, stroke: options.stroke } )
-  ];
+  /**
+   * @param {string} text
+   * @param {Object} [options]
+   */
+  constructor( text, options ) {
 
-  Node.call( this, options );
+    options = merge( {
+      font: new PhetFont( 24 ),
+      fill: 'lightGray',
+      stroke: null,
+      shadowXOffset: 3,
+      shadowYOffset: 1,
+      shadowFill: 'black'
+    }, options );
+
+    options.children = [
+
+      // background (shadow)
+      new Text( text, {
+        font: options.font,
+        fill: options.shadowFill,
+        x: options.shadowXOffset,
+        y: options.shadowYOffset
+      } ),
+
+      // foreground
+      new Text( text, { font: options.font, fill: options.fill, stroke: options.stroke } )
+    ];
+
+    super( options );
+  }
 }
 
 sceneryPhet.register( 'ShadowText', ShadowText );
-
-inherit( Node, ShadowText );
 export default ShadowText;
