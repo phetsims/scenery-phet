19da8ce4cf60936f2f54be27237141b35e202c12
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 17:07:27 -0600

    convert to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/WavelengthSlider.js
+++ js/WavelengthSlider.js
@@ -8,45 +8,42 @@
  */
 
 import Utils from '../../dot/js/Utils.js';
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import StringUtils from '../../phetcommon/js/util/StringUtils.js';
-import sceneryPhetStrings from './sceneryPhetStrings.js';
 import sceneryPhet from './sceneryPhet.js';
+import sceneryPhetStrings from './sceneryPhetStrings.js';
 import SpectrumSlider from './SpectrumSlider.js';
 import VisibleColor from './VisibleColor.js';
 
-const unitsNmString = sceneryPhetStrings.units_nm;
-const wavelengthSliderPattern0Wavelength1UnitsString = sceneryPhetStrings.WavelengthSlider.pattern_0wavelength_1units;
+class WavelengthSlider extends SpectrumSlider {
 
-/**
- * @param {Property.<number>} wavelengthProperty - wavelength, in nm
- * @param {Object} [options]
- * @constructor
- */
-function WavelengthSlider( wavelengthProperty, options ) {
+  /**
+   * @param {Property.<number>} wavelengthProperty - wavelength, in nm
+   * @param {Object} [options]
+   */
+  constructor( wavelengthProperty, options ) {
 
-  // options that are specific to this type
-  options = merge( {
+    // options that are specific to this type
+    options = merge( {
 
-    minWavelength: VisibleColor.MIN_WAVELENGTH,
-    maxWavelength: VisibleColor.MAX_WAVELENGTH,
-    valueToString: function( value ) {
-      return StringUtils.format( wavelengthSliderPattern0Wavelength1UnitsString, Utils.toFixed( value, 0 ), unitsNmString );
-    },
-    valueToColor: function( value ) {
-      return VisibleColor.wavelengthToColor( value );
-    }
-  }, options );
-  assert && assert( options.minValue === undefined, 'minValue is supplied by WavelengthSlider' );
-  assert && assert( options.maxValue === undefined, 'maxValue is supplied by WavelengthSlider' );
-  options.minValue = options.minWavelength;
-  options.maxValue = options.maxWavelength;
+      minWavelength: VisibleColor.MIN_WAVELENGTH,
+      maxWavelength: VisibleColor.MAX_WAVELENGTH,
+      valueToString: function( value ) {
+        return StringUtils.format( sceneryPhetStrings.WavelengthSlider.pattern_0wavelength_1units,
+          Utils.toFixed( value, 0 ), sceneryPhetStrings.units_nm );
+      },
+      valueToColor: function( value ) {
+        return VisibleColor.wavelengthToColor( value );
+      }
+    }, options );
+    assert && assert( options.minValue === undefined, 'minValue is supplied by WavelengthSlider' );
+    assert && assert( options.maxValue === undefined, 'maxValue is supplied by WavelengthSlider' );
+    options.minValue = options.minWavelength;
+    options.maxValue = options.maxWavelength;
 
-  SpectrumSlider.call( this, wavelengthProperty, options );
+    super( wavelengthProperty, options );
+  }
 }
 
 sceneryPhet.register( 'WavelengthSlider', WavelengthSlider );
-
-inherit( SpectrumSlider, WavelengthSlider );
 export default WavelengthSlider;
