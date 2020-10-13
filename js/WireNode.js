2f064e316673a9144593e350c854b6a12fba1bb3
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 11:24:26 -0600

    convert to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/WireNode.js
+++ js/WireNode.js
@@ -8,52 +8,50 @@
 
 import Property from '../../axon/js/Property.js';
 import Shape from '../../kite/js/Shape.js';
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import Path from '../../scenery/js/nodes/Path.js';
 import sceneryPhet from './sceneryPhet.js';
 
-/**
- * @param {Property.<Vector2>} position1Property - connects to one object, often this is a DerivedProperty
- * @param {Property.<Vector2>} normal1Property - defines the control point of the cubic curve, relative to the position1
- * @param {Property.<Vector2>} position2Property - connects to another object, often this is a DerivedProperty
- * @param {Property.<Vector2>} normal2Property - defines the control point of the cubic curve, relative to the position2
- * @param {Object} [options]
- * @constructor
- */
-function WireNode( position1Property, normal1Property, position2Property, normal2Property, options ) {
+class WireNode extends Path {
 
-  options = merge( {
-    stroke: 'black'
-  }, options );
+  /**
+   * @param {Property.<Vector2>} position1Property - connects to one object, often this is a DerivedProperty
+   * @param {Property.<Vector2>} normal1Property - defines the control point of the cubic curve, relative to the position1
+   * @param {Property.<Vector2>} position2Property - connects to another object, often this is a DerivedProperty
+   * @param {Property.<Vector2>} normal2Property - defines the control point of the cubic curve, relative to the position2
+   * @param {Object} [options]
+   */
+  constructor( position1Property, normal1Property, position2Property, normal2Property, options ) {
 
-  Path.call( this, null, options );
+    options = merge( {
+      stroke: 'black'
+    }, options );
 
-  // @private
-  this.multilink = Property.multilink( [
-    position1Property, normal1Property, position2Property, normal2Property
-  ], ( position1, normal1, position2, normal2 ) => {
-    this.shape = new Shape()
-      .moveToPoint( position1 )
-      .cubicCurveToPoint(
-        position1.plus( normal1 ),
-        position2.plus( normal2 ),
-        position2
-      );
-  } );
+    super( null, options );
+
+    // @private
+    this.multilink = Property.multilink( [
+      position1Property, normal1Property, position2Property, normal2Property
+    ], ( position1, normal1, position2, normal2 ) => {
+      this.shape = new Shape()
+        .moveToPoint( position1 )
+        .cubicCurveToPoint(
+          position1.plus( normal1 ),
+          position2.plus( normal2 ),
+          position2
+        );
+    } );
+  }
+
+  /**
+   * @public
+   * @override
+   */
+  dispose() {
+    this.multilink.dispose();
+    super.dispose();
+  }
 }
 
 sceneryPhet.register( 'WireNode', WireNode );
-
-inherit( Path, WireNode, {
-
-  /**
-   * Unlink listeners when disposed.
-   * @public
-   */
-  dispose: function() {
-    this.multilink.dispose();
-  }
-} );
-
 export default WireNode;
