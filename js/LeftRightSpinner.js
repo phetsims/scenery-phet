ef7f77ce170e3ed0852e72a8f80fdc2a820218f3
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 12:50:26 -0600

    convert to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/LeftRightSpinner.js
+++ js/LeftRightSpinner.js
@@ -10,83 +10,86 @@
 
 import Shape from '../../kite/js/Shape.js';
 import InstanceRegistry from '../../phet-core/js/documentation/InstanceRegistry.js';
-import inherit from '../../phet-core/js/inherit.js';
+import merge from '../../phet-core/js/merge.js';
 import HBox from '../../scenery/js/nodes/HBox.js';
 import Path from '../../scenery/js/nodes/Path.js';
 import RoundPushButton from '../../sun/js/buttons/RoundPushButton.js';
 import sceneryPhet from './sceneryPhet.js';
 
-/**
- *
- * @param {Property.<number>} valueProperty
- * @param {Property.<boolean>} leftEnabledProperty
- * @param {Property.<boolean>} rightEnabledProperty
- * @param {Object} [options]
- * @constructor
- */
-function LeftRightSpinner( valueProperty, leftEnabledProperty, rightEnabledProperty, options ) {
-  const shapeWidth = 26;
-  const leftShape = new Shape().moveTo( 0, 0 ).lineTo( -10, shapeWidth / 2 ).lineTo( 0, shapeWidth );
-  const rightShape = new Shape().moveTo( 0, 0 ).lineTo( 10, shapeWidth / 2 ).lineTo( 0, shapeWidth );
+class LeftRightSpinner extends HBox {
 
-  const leftIcon = new Path( leftShape, { lineWidth: 5, stroke: 'black', lineCap: 'round' } );
-  const rightIcon = new Path( rightShape, { lineWidth: 5, stroke: 'black', lineCap: 'round' } );
+  /**
+   * @param {Property.<number>} valueProperty
+   * @param {Property.<boolean>} leftEnabledProperty
+   * @param {Property.<boolean>} rightEnabledProperty
+   * @param {Object} [options]
+   */
+  constructor( valueProperty, leftEnabledProperty, rightEnabledProperty, options ) {
 
-  const radius = 20;
-  const leftButton = new RoundPushButton( {
-    content: leftIcon,
-    listener: function() {
-      valueProperty.set( valueProperty.get() - 1 );
-    },
-    baseColor: '#7fb539',
-    radius: radius,
-    touchAreaDilation: 10,
-    xContentOffset: -3
-  } );
-  const leftEnabledPropertyLinkAttribute = leftEnabledProperty.linkAttribute( leftButton, 'enabled' );
+    options = merge( {
+      spacing: 6
+    }, options );
 
-  const rightButton = new RoundPushButton( {
-    radius: radius,
-    listener: function() {
-      valueProperty.set( valueProperty.get() + 1 );
-    },
-    content: rightIcon,
-    touchAreaRadius: 24 * 1.3,
-    baseColor: '#7fb539',
-    xContentOffset: +3
-  } );
-  const rightEnabledPropertyLinkAttribute = rightEnabledProperty.linkAttribute( rightButton, 'enabled' );
+    const shapeWidth = 26;
+    const leftShape = new Shape().moveTo( 0, 0 ).lineTo( -10, shapeWidth / 2 ).lineTo( 0, shapeWidth );
+    const rightShape = new Shape().moveTo( 0, 0 ).lineTo( 10, shapeWidth / 2 ).lineTo( 0, shapeWidth );
 
-  HBox.call( this, { spacing: 6, children: [ leftButton, rightButton ] } );
+    const leftIcon = new Path( leftShape, { lineWidth: 5, stroke: 'black', lineCap: 'round' } );
+    const rightIcon = new Path( rightShape, { lineWidth: 5, stroke: 'black', lineCap: 'round' } );
 
-  this.mutate( options );
+    const radius = 20;
+    const leftButton = new RoundPushButton( {
+      content: leftIcon,
+      listener: function() {
+        valueProperty.set( valueProperty.get() - 1 );
+      },
+      baseColor: '#7fb539',
+      radius: radius,
+      touchAreaDilation: 10,
+      xContentOffset: -3
+    } );
+    const leftEnabledPropertyLinkAttribute = leftEnabledProperty.linkAttribute( leftButton, 'enabled' );
 
-  // @private
-  this.disposeLeftRightSpinner = function() {
-    if ( leftEnabledProperty.hasListener( leftEnabledPropertyLinkAttribute ) ) {
-      leftEnabledProperty.unlinkAttribute( leftEnabledPropertyLinkAttribute );
-    }
-    if ( rightEnabledProperty.hasListener( rightEnabledPropertyLinkAttribute ) ) {
-      rightEnabledProperty.unlinkAttribute( rightEnabledPropertyLinkAttribute );
-    }
-  };
+    const rightButton = new RoundPushButton( {
+      radius: radius,
+      listener: function() {
+        valueProperty.set( valueProperty.get() + 1 );
+      },
+      content: rightIcon,
+      touchAreaRadius: 24 * 1.3,
+      baseColor: '#7fb539',
+      xContentOffset: +3
+    } );
+    const rightEnabledPropertyLinkAttribute = rightEnabledProperty.linkAttribute( rightButton, 'enabled' );
 
-  // support for binder documentation, stripped out in builds and only runs when ?binder is specified
-  assert && phet.chipper.queryParameters.binder && InstanceRegistry.registerDataURL( 'scenery-phet', 'LeftRightSpinner', this );
+    assert && assert( !options.children, 'LeftRightSpinner sets children' );
+    options.children = [ leftButton, rightButton ];
+
+    super( options );
+
+    // @private
+    this.disposeLeftRightSpinner = function() {
+      if ( leftEnabledProperty.hasListener( leftEnabledPropertyLinkAttribute ) ) {
+        leftEnabledProperty.unlinkAttribute( leftEnabledPropertyLinkAttribute );
+      }
+      if ( rightEnabledProperty.hasListener( rightEnabledPropertyLinkAttribute ) ) {
+        rightEnabledProperty.unlinkAttribute( rightEnabledPropertyLinkAttribute );
+      }
+    };
+
+    // support for binder documentation, stripped out in builds and only runs when ?binder is specified
+    assert && phet.chipper.queryParameters.binder && InstanceRegistry.registerDataURL( 'scenery-phet', 'LeftRightSpinner', this );
+  }
+
+  /**
+   * @public
+   * @override
+   */
+  dispose() {
+    this.disposeLeftRightSpinner();
+    super.dispose();
+  }
 }
 
 sceneryPhet.register( 'LeftRightSpinner', LeftRightSpinner );
-
-inherit( HBox, LeftRightSpinner, {
-
-  /**
-   * Ensures that this node is subject to garbage collection
-   * @public
-   */
-  dispose: function() {
-    this.disposeLeftRightSpinner();
-    HBox.prototype.dispose.call( this );
-  }
-} );
-
 export default LeftRightSpinner;
