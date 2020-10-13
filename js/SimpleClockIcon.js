0de8739cb0d90a499cd61044c7a69c044bc2b024
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 11:17:36 -0600

    convert to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/SimpleClockIcon.js
+++ js/SimpleClockIcon.js
@@ -7,39 +7,46 @@
  * @author John Blanco
  */
 
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import Circle from '../../scenery/js/nodes/Circle.js';
 import Line from '../../scenery/js/nodes/Line.js';
 import Node from '../../scenery/js/nodes/Node.js';
 import sceneryPhet from './sceneryPhet.js';
 
-/**
- * @param {number} radius
- * @param {Object} [options]
- * @constructor
- */
-function SimpleClockIcon( radius, options ) {
+class SimpleClockIcon extends Node {
 
-  Node.call( this );
-  options = merge( { fill: 'white', stroke: 'black', lineWidth: 2 }, options );
-  this.addChild( new Circle( radius, options ) );
-  this.addChild( new Circle( radius * 0.15, { fill: options.stroke } ) );
-  const lineOptionsForClockHands = {
-    stroke: options.stroke,
-    lineWidth: options.lineWidth,
-    lineCap: 'round',
-    lineJoin: 'round'
-  };
-  // Hands at 4 o'clock
-  this.addChild( new Line( 0, 0, 0, -radius * 0.75, lineOptionsForClockHands ) );
-  this.addChild( new Line( 0, 0, radius * 0.45, radius * 0.3, lineOptionsForClockHands ) );
-  this.centerX = radius;
-  this.centerY = radius;
-  this.mutate( options );
+  /**
+   * @param {number} radius
+   * @param {Object} [options]
+   */
+  constructor( radius, options ) {
+
+    super();
+
+    options = merge( {
+      fill: 'white',
+      stroke: 'black',
+      lineWidth: 2
+    }, options );
+
+    this.addChild( new Circle( radius, options ) );
+    this.addChild( new Circle( radius * 0.15, { fill: options.stroke } ) );
+    const lineOptionsForClockHands = {
+      stroke: options.stroke,
+      lineWidth: options.lineWidth,
+      lineCap: 'round',
+      lineJoin: 'round'
+    };
+
+    // Hands at 4 o'clock
+    this.addChild( new Line( 0, 0, 0, -radius * 0.75, lineOptionsForClockHands ) );
+    this.addChild( new Line( 0, 0, radius * 0.45, radius * 0.3, lineOptionsForClockHands ) );
+    this.centerX = radius;
+    this.centerY = radius;
+
+    this.mutate( options );
+  }
 }
 
 sceneryPhet.register( 'SimpleClockIcon', SimpleClockIcon );
-
-inherit( Node, SimpleClockIcon );
 export default SimpleClockIcon;
