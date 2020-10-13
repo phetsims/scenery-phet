c2f4816f916af46b19843002585c064674fc26ad
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 11:45:41 -0600

    convert RulerNode and subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/RulerNode.js
+++ js/RulerNode.js
@@ -9,7 +9,6 @@
 
 import Shape from '../../kite/js/Shape.js';
 import InstanceRegistry from '../../phet-core/js/documentation/InstanceRegistry.js';
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import Node from '../../scenery/js/nodes/Node.js';
 import Path from '../../scenery/js/nodes/Path.js';
@@ -22,177 +21,177 @@ import sceneryPhet from './sceneryPhet.js';
 // constants
 const DEFAULT_FONT = new PhetFont( 18 );
 
-/**
- * @param {number} rulerWidth  distance between left-most and right-most tick, insets will be added to this
- * @param {number} rulerHeight
- * @param {number} majorTickWidth
- * @param {string[]} majorTickLabels
- * @param {string} units
- * @param {Object} [options]
- * @constructor
- */
-function RulerNode( rulerWidth, rulerHeight, majorTickWidth, majorTickLabels, units, options ) {
+class RulerNode extends Node {
 
-  // default options
-  options = merge( {
+  /**
+   * @param {number} rulerWidth  distance between left-most and right-most tick, insets will be added to this
+   * @param {number} rulerHeight
+   * @param {number} majorTickWidth
+   * @param {string[]} majorTickLabels
+   * @param {string} units
+   * @param {Object} [options]
+   */
+  constructor( rulerWidth, rulerHeight, majorTickWidth, majorTickLabels, units, options ) {
 
-    // body of the ruler
-    backgroundFill: 'rgb(236, 225, 113)',
-    backgroundStroke: 'black',
-    backgroundLineWidth: 1,
-    insetsWidth: 14, // space between the ends of the ruler and the first and last tick marks
+    // default options
+    options = merge( {
 
-    // major tick options
-    majorTickFont: DEFAULT_FONT,
-    majorTickHeight: ( 0.4 * rulerHeight ) / 2,
-    majorTickStroke: 'black',
-    majorTickLineWidth: 1,
+      // body of the ruler
+      backgroundFill: 'rgb(236, 225, 113)',
+      backgroundStroke: 'black',
+      backgroundLineWidth: 1,
+      insetsWidth: 14, // space between the ends of the ruler and the first and last tick marks
 
-    // minor tick options
-    minorTickFont: DEFAULT_FONT,
-    minorTickHeight: ( 0.2 * rulerHeight ) / 2,
-    minorTickStroke: 'black',
-    minorTickLineWidth: 1,
-    minorTicksPerMajorTick: 0,
+      // major tick options
+      majorTickFont: DEFAULT_FONT,
+      majorTickHeight: ( 0.4 * rulerHeight ) / 2,
+      majorTickStroke: 'black',
+      majorTickLineWidth: 1,
 
-    // units options
-    unitsFont: DEFAULT_FONT,
-    unitsMajorTickIndex: 0, // units will be place to the right of this major tick
-    unitsSpacing: 3, // horizontal space between the tick label and the units
+      // minor tick options
+      minorTickFont: DEFAULT_FONT,
+      minorTickHeight: ( 0.2 * rulerHeight ) / 2,
+      minorTickStroke: 'black',
+      minorTickLineWidth: 1,
+      minorTicksPerMajorTick: 0,
 
-    // appearance options
-    tickMarksOnTop: true,
-    tickMarksOnBottom: true,
+      // units options
+      unitsFont: DEFAULT_FONT,
+      unitsMajorTickIndex: 0, // units will be place to the right of this major tick
+      unitsSpacing: 3, // horizontal space between the tick label and the units
 
-    // phet-io
-    tandem: Tandem.REQUIRED
-  }, options );
+      // appearance options
+      tickMarksOnTop: true,
+      tickMarksOnBottom: true,
 
-  // things you're likely to mess up, add more as needed
-  assert && assert( Math.floor( rulerWidth / majorTickWidth ) + 1 === majorTickLabels.length ); // do we have enough major tick labels?
-  assert && assert( options.unitsMajorTickIndex < majorTickLabels.length );
-  assert && assert( options.majorTickHeight < rulerHeight / 2 );
-  assert && assert( options.minorTickHeight < rulerHeight / 2 );
+      // phet-io
+      tandem: Tandem.REQUIRED
+    }, options );
 
-  Node.call( this );
+    // things you're likely to mess up, add more as needed
+    assert && assert( Math.floor( rulerWidth / majorTickWidth ) + 1 === majorTickLabels.length ); // do we have enough major tick labels?
+    assert && assert( options.unitsMajorTickIndex < majorTickLabels.length );
+    assert && assert( options.majorTickHeight < rulerHeight / 2 );
+    assert && assert( options.minorTickHeight < rulerHeight / 2 );
 
-  // background
-  const backgroundNode = new Rectangle( 0, 0, rulerWidth + ( 2 * options.insetsWidth ), rulerHeight, {
-    fill: options.backgroundFill,
-    stroke: options.backgroundStroke,
-    lineWidth: options.backgroundLineWidth
-  } );
-  this.addChild( backgroundNode );
+    super();
 
-  // Lay out tick marks from left to right
-  const minorTickWidth = majorTickWidth / ( options.minorTicksPerMajorTick + 1 );
-  const numberOfTicks = Math.floor( rulerWidth / minorTickWidth ) + 1;
-  let x = options.insetsWidth;
-  let majorTickIndex = 0;
+    // background
+    const backgroundNode = new Rectangle( 0, 0, rulerWidth + ( 2 * options.insetsWidth ), rulerHeight, {
+      fill: options.backgroundFill,
+      stroke: options.backgroundStroke,
+      lineWidth: options.backgroundLineWidth
+    } );
+    this.addChild( backgroundNode );
 
-  // Minimize number of nodes by using one path for each type of tick line
-  const majorTickLinesShape = new Shape();
-  const minorTickLinesShape = new Shape();
+    // Lay out tick marks from left to right
+    const minorTickWidth = majorTickWidth / ( options.minorTicksPerMajorTick + 1 );
+    const numberOfTicks = Math.floor( rulerWidth / minorTickWidth ) + 1;
+    let x = options.insetsWidth;
+    let majorTickIndex = 0;
 
-  // Units label, which is positioned and (if necessary) scaled later
-  const unitsLabel = new Text( units, {
-    font: options.unitsFont,
-    pickable: false,
-    tandem: options.tandem.createTandem( 'unitsLabel' )
-  } );
-  let unitsLabelMaxWidth = Number.POSITIVE_INFINITY;
-  this.addChild( unitsLabel );
+    // Minimize number of nodes by using one path for each type of tick line
+    const majorTickLinesShape = new Shape();
+    const minorTickLinesShape = new Shape();
 
-  for ( let i = 0; i < numberOfTicks; i++ ) {
+    // Units label, which is positioned and (if necessary) scaled later
+    const unitsLabel = new Text( units, {
+      font: options.unitsFont,
+      pickable: false,
+      tandem: options.tandem.createTandem( 'unitsLabel' )
+    } );
+    let unitsLabelMaxWidth = Number.POSITIVE_INFINITY;
+    this.addChild( unitsLabel );
 
-    if ( i % ( options.minorTicksPerMajorTick + 1 ) === 0 ) {  // assumes that the first (leftmost) tick is a major tick
+    for ( let i = 0; i < numberOfTicks; i++ ) {
 
-      // Major tick
+      if ( i % ( options.minorTicksPerMajorTick + 1 ) === 0 ) {  // assumes that the first (leftmost) tick is a major tick
 
-      // Create the tick label regardless of whether we add it, since it's required to layout the units label
-      const majorTickLabel = majorTickLabels[ majorTickIndex ];
-      const majorTickLabelNode = new Text( majorTickLabel, {
-        font: options.majorTickFont,
-        centerX: x,
-        centerY: backgroundNode.centerY,
-        pickable: false
-      } );
+        // Major tick
 
-      // Only add a major tick at leftmost or rightmost end if the insetsWidth is nonzero
-      if ( options.insetsWidth !== 0 || ( i !== 0 && i !== numberOfTicks - 1 ) ) {
+        // Create the tick label regardless of whether we add it, since it's required to layout the units label
+        const majorTickLabel = majorTickLabels[ majorTickIndex ];
+        const majorTickLabelNode = new Text( majorTickLabel, {
+          font: options.majorTickFont,
+          centerX: x,
+          centerY: backgroundNode.centerY,
+          pickable: false
+        } );
 
-        // label, only added as a child if it's non-empty (and non-null)
-        if ( majorTickLabel ) {
-          this.addChild( majorTickLabelNode );
+        // Only add a major tick at leftmost or rightmost end if the insetsWidth is nonzero
+        if ( options.insetsWidth !== 0 || ( i !== 0 && i !== numberOfTicks - 1 ) ) {
+
+          // label, only added as a child if it's non-empty (and non-null)
+          if ( majorTickLabel ) {
+            this.addChild( majorTickLabelNode );
+          }
+
+          // line
+          if ( options.tickMarksOnTop ) {
+            majorTickLinesShape.moveTo( x, 0 ).lineTo( x, options.majorTickHeight );
+          }
+          if ( options.tickMarksOnBottom ) {
+            majorTickLinesShape.moveTo( x, rulerHeight - options.majorTickHeight ).lineTo( x, rulerHeight );
+          }
         }
 
-        // line
-        if ( options.tickMarksOnTop ) {
-          majorTickLinesShape.moveTo( x, 0 ).lineTo( x, options.majorTickHeight );
+        // Position the units label
+        if ( majorTickIndex === options.unitsMajorTickIndex ) {
+          unitsLabel.left = majorTickLabelNode.right + options.unitsSpacing;
+          unitsLabel.y = majorTickLabelNode.y;
         }
-        if ( options.tickMarksOnBottom ) {
-          majorTickLinesShape.moveTo( x, rulerHeight - options.majorTickHeight ).lineTo( x, rulerHeight );
+        else if ( majorTickIndex > options.unitsMajorTickIndex && unitsLabelMaxWidth === Number.POSITIVE_INFINITY && majorTickLabelNode.width > 0 ) {
+
+          // make sure the units label fits between the tick mark labels
+          unitsLabelMaxWidth = majorTickLabelNode.left - options.unitsSpacing - unitsLabel.left;
+          assert && assert( unitsLabelMaxWidth > 0, 'space for units label is negative or zero' );
+          unitsLabel.maxWidth = unitsLabelMaxWidth;
+        }
+
+        majorTickIndex++;
+      }
+      else {
+        // Minor tick
+        // Only add a minor tick at leftmost or rightmost end if the insetsWidth is nonzero
+        if ( options.insetsWidth !== 0 || ( i !== 0 && i !== numberOfTicks - 1 ) ) {
+          if ( options.tickMarksOnTop ) {
+            minorTickLinesShape.moveTo( x, 0 ).lineTo( x, options.minorTickHeight );
+          }
+          if ( options.tickMarksOnBottom ) {
+            minorTickLinesShape.moveTo( x, rulerHeight - options.minorTickHeight ).lineTo( x, rulerHeight );
+          }
         }
       }
-
-      // Position the units label
-      if ( majorTickIndex === options.unitsMajorTickIndex ) {
-        unitsLabel.left = majorTickLabelNode.right + options.unitsSpacing;
-        unitsLabel.y = majorTickLabelNode.y;
-      }
-      else if ( majorTickIndex > options.unitsMajorTickIndex && unitsLabelMaxWidth === Number.POSITIVE_INFINITY && majorTickLabelNode.width > 0 ) {
-
-        // make sure the units label fits between the tick mark labels
-        unitsLabelMaxWidth = majorTickLabelNode.left - options.unitsSpacing - unitsLabel.left;
-        assert && assert( unitsLabelMaxWidth > 0, 'space for units label is negative or zero' );
-        unitsLabel.maxWidth = unitsLabelMaxWidth;
-      }
-
-      majorTickIndex++;
+      x += minorTickWidth;
     }
-    else {
-      // Minor tick
-      // Only add a minor tick at leftmost or rightmost end if the insetsWidth is nonzero
-      if ( options.insetsWidth !== 0 || ( i !== 0 && i !== numberOfTicks - 1 ) ) {
-        if ( options.tickMarksOnTop ) {
-          minorTickLinesShape.moveTo( x, 0 ).lineTo( x, options.minorTickHeight );
-        }
-        if ( options.tickMarksOnBottom ) {
-          minorTickLinesShape.moveTo( x, rulerHeight - options.minorTickHeight ).lineTo( x, rulerHeight );
-        }
-      }
+
+    // Handle the case where the units label extends off the edge of the ruler.  This is kind of a corner case, but was
+    // seen when testing long strings on Pendulum Lab.
+    if ( unitsLabel.bounds.maxX > backgroundNode.bounds.maxX - options.unitsSpacing ) {
+      unitsLabelMaxWidth = ( backgroundNode.bounds.maxX - options.unitsSpacing ) - unitsLabel.x;
+      unitsLabel.scale( unitsLabelMaxWidth / unitsLabel.width );
     }
-    x += minorTickWidth;
+
+    // Major tick lines
+    this.addChild( new Path( majorTickLinesShape, {
+      stroke: options.majorTickStroke,
+      lineWidth: options.majorTickLineWidth,
+      pickable: false
+    } ) );
+
+    // Minor tick lines
+    this.addChild( new Path( minorTickLinesShape, {
+      stroke: options.minorTickStroke,
+      lineWidth: options.minorTickLineWidth,
+      pickable: false
+    } ) );
+
+    this.mutate( options );
+
+    // support for binder documentation, stripped out in builds and only runs when ?binder is specified
+    assert && phet.chipper.queryParameters.binder && InstanceRegistry.registerDataURL( 'scenery-phet', 'RulerNode', this );
   }
-
-  // Handle the case where the units label extends off the edge of the ruler.  This is kind of a corner case, but was
-  // seen when testing long strings on Pendulum Lab.
-  if ( unitsLabel.bounds.maxX > backgroundNode.bounds.maxX - options.unitsSpacing ) {
-    unitsLabelMaxWidth = ( backgroundNode.bounds.maxX - options.unitsSpacing ) - unitsLabel.x;
-    unitsLabel.scale( unitsLabelMaxWidth / unitsLabel.width );
-  }
-
-  // Major tick lines
-  this.addChild( new Path( majorTickLinesShape, {
-    stroke: options.majorTickStroke,
-    lineWidth: options.majorTickLineWidth,
-    pickable: false
-  } ) );
-
-  // Minor tick lines
-  this.addChild( new Path( minorTickLinesShape, {
-    stroke: options.minorTickStroke,
-    lineWidth: options.minorTickLineWidth,
-    pickable: false
-  } ) );
-
-  this.mutate( options );
-
-  // support for binder documentation, stripped out in builds and only runs when ?binder is specified
-  assert && phet.chipper.queryParameters.binder && InstanceRegistry.registerDataURL( 'scenery-phet', 'RulerNode', this );
 }
 
 sceneryPhet.register( 'RulerNode', RulerNode );
-
-inherit( Node, RulerNode );
 export default RulerNode;
