c308109a05be5329abf304ff50e1ca78bba093c8
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 10:53:31 -0600

    convert to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/IE11StencilWarningNode.js
+++ js/IE11StencilWarningNode.js
@@ -8,7 +8,6 @@
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import openPopup from '../../phet-core/js/openPopup.js';
 import HBox from '../../scenery/js/nodes/HBox.js';
@@ -16,52 +15,49 @@ import Text from '../../scenery/js/nodes/Text.js';
 import VBox from '../../scenery/js/nodes/VBox.js';
 import FontAwesomeNode from '../../sun/js/FontAwesomeNode.js';
 import PhetFont from './PhetFont.js';
-import sceneryPhetStrings from './sceneryPhetStrings.js';
 import sceneryPhet from './sceneryPhet.js';
+import sceneryPhetStrings from './sceneryPhetStrings.js';
 
 const webglWarningIe11StencilBodyString = sceneryPhetStrings.webglWarning.ie11StencilBody;
 const webglWarningTitleString = sceneryPhetStrings.webglWarning.title;
 
-/**
- * @constructor
- */
-function IE11StencilWarningNode() {
-  HBox.call( this, merge( {
-    children: [
-      new FontAwesomeNode( 'warning_sign', {
-        fill: '#E87600', // "safety orange", according to Wikipedia
-        scale: 0.6
-      } ),
-      new VBox( {
-        children: [
-          new Text( webglWarningTitleString, {
-            font: new PhetFont( 14 ),
-            fill: '#ddd'
-          } ),
-          new Text( webglWarningIe11StencilBodyString, {
-            font: new PhetFont( 10 ),
-            fill: '#999'
-          } )
-        ],
-        spacing: 3,
-        align: 'left'
-      } )
-    ],
-    spacing: 12,
-    align: 'center',
-    cursor: 'pointer'
-  } ) );
+class IE11StencilWarningNode extends HBox {
+  constructor() {
+    super( merge( {
+      children: [
+        new FontAwesomeNode( 'warning_sign', {
+          fill: '#E87600', // "safety orange", according to Wikipedia
+          scale: 0.6
+        } ),
+        new VBox( {
+          children: [
+            new Text( webglWarningTitleString, {
+              font: new PhetFont( 14 ),
+              fill: '#ddd'
+            } ),
+            new Text( webglWarningIe11StencilBodyString, {
+              font: new PhetFont( 10 ),
+              fill: '#999'
+            } )
+          ],
+          spacing: 3,
+          align: 'left'
+        } )
+      ],
+      spacing: 12,
+      align: 'center',
+      cursor: 'pointer'
+    } ) );
 
-  this.mouseArea = this.touchArea = this.localBounds;
+    this.mouseArea = this.touchArea = this.localBounds;
 
-  this.addInputListener( {
-    up: function() {
-      openPopup( 'http://windowsupdate.microsoft.com/' );
-    }
-  } );
+    this.addInputListener( {
+      up: function() {
+        openPopup( 'http://windowsupdate.microsoft.com/' );
+      }
+    } );
+  }
 }
 
 sceneryPhet.register( 'IE11StencilWarningNode', IE11StencilWarningNode );
-
-inherit( HBox, IE11StencilWarningNode );
 export default IE11StencilWarningNode;
