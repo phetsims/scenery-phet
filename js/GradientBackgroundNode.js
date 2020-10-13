6d32bb531c52d76f3eac32477e9732043c087ec8
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 12:46:09 -0600

    convert to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/GradientBackgroundNode.js
+++ js/GradientBackgroundNode.js
@@ -8,32 +8,30 @@
  * @author Sam Reid
  */
 
-import inherit from '../../phet-core/js/inherit.js';
 import Rectangle from '../../scenery/js/nodes/Rectangle.js';
 import LinearGradient from '../../scenery/js/util/LinearGradient.js';
 import sceneryPhet from './sceneryPhet.js';
 
-/**
- *
- * @param {number} x
- * @param {number} y
- * @param {number} width
- * @param {number} height
- * @param {string||Color} color1
- * @param {string||Color} color2
- * @param {number} y1
- * @param {number} y2
- * @constructor
- */
-function GradientBackgroundNode( x, y, width, height, color1, color2, y1, y2 ) {
-  const centerX = x + width / 2;
-  const gradient = new LinearGradient( centerX, y1, centerX, y2 );
-  gradient.addColorStop( 0, color1 );
-  gradient.addColorStop( 1, color2 );
-  Rectangle.call( this, x, y, width, height, 0, 0, { fill: gradient } );
+class GradientBackgroundNode extends Rectangle {
+
+  /**
+   * @param {number} x
+   * @param {number} y
+   * @param {number} width
+   * @param {number} height
+   * @param {string||Color} color1
+   * @param {string||Color} color2
+   * @param {number} y1
+   * @param {number} y2
+   */
+  constructor( x, y, width, height, color1, color2, y1, y2 ) {
+    const centerX = x + width / 2;
+    const gradient = new LinearGradient( centerX, y1, centerX, y2 );
+    gradient.addColorStop( 0, color1 );
+    gradient.addColorStop( 1, color2 );
+    super( x, y, width, height, 0, 0, { fill: gradient } );
+  }
 }
 
 sceneryPhet.register( 'GradientBackgroundNode', GradientBackgroundNode );
-
-inherit( Rectangle, GradientBackgroundNode );
 export default GradientBackgroundNode;
