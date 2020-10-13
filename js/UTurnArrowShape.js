f46a50c26ba961a23bc3570f43c4efc746b02b7d
Jonathan Olson <jonathan.olson@colorado.edu>
2020-10-13 09:39:39 -0600

    ES6 UTurnArrowShape, see https://github.com/phetsims/tasks/issues/1044

--- js/UTurnArrowShape.js
+++ js/UTurnArrowShape.js
@@ -7,44 +7,43 @@
  */
 
 import Shape from '../../kite/js/Shape.js';
-import inherit from '../../phet-core/js/inherit.js';
 import sceneryPhet from './sceneryPhet.js';
 
-/**
- * @param {number} size A size factor (it'll be a bit bigger)
- * @constructor
- */
-function UTurnArrowShape( size ) {
-  Shape.call( this );
+class UTurnArrowShape extends Shape {
+  /**
+   * @param {number} size A size factor (it'll be a bit bigger)
+   */
+  constructor( size ) {
+    super();
 
-  const strokeWidth = size * 0.3;
-  const strokeOffset = strokeWidth / 2;
-  const mainWidth = size * 0.6;
-  const mainHeight = size;
-  const headWidth = size * 0.5;
-  const headHeight = size * 0.75;
-  const halfHeadWidth = headWidth / 2;
-  const halfHeadHeight = headHeight / 2;
+    const strokeWidth = size * 0.3;
+    const strokeOffset = strokeWidth / 2;
+    const mainWidth = size * 0.6;
+    const mainHeight = size;
+    const headWidth = size * 0.5;
+    const headHeight = size * 0.75;
+    const halfHeadWidth = headWidth / 2;
+    const halfHeadHeight = headHeight / 2;
 
-  // starts adjacent to the arrowhead on the top, going clockwise
-  this.moveTo( halfHeadWidth, -strokeOffset );
-  this.lineTo( mainWidth, -strokeOffset );
-  // arc (mainWidth,-strokeOffset) => (mainWidth,mainHeight+strokeOffset)
-  this.arc( mainWidth, mainHeight / 2, mainHeight / 2 + strokeOffset, -Math.PI / 2, Math.PI / 2, false );
-  this.lineTo( 0, mainHeight + strokeOffset );
-  this.lineTo( 0, mainHeight - strokeOffset );
-  this.lineTo( mainWidth, mainHeight - strokeOffset );
-  // arc (mainWidth,mainHeight-strokeOffset) => (mainWidth,strokeOffset)
-  this.arc( mainWidth, mainHeight / 2, mainHeight / 2 - strokeOffset, Math.PI / 2, -Math.PI / 2, true );
-  this.lineTo( halfHeadWidth, strokeOffset );
-  // three lines of the arrow head
-  this.lineTo( halfHeadWidth, halfHeadHeight );
-  this.lineTo( -halfHeadWidth, 0 );
-  this.lineTo( halfHeadWidth, -halfHeadHeight );
-  this.close();
+    // starts adjacent to the arrowhead on the top, going clockwise
+    this.moveTo( halfHeadWidth, -strokeOffset );
+    this.lineTo( mainWidth, -strokeOffset );
+    // arc (mainWidth,-strokeOffset) => (mainWidth,mainHeight+strokeOffset)
+    this.arc( mainWidth, mainHeight / 2, mainHeight / 2 + strokeOffset, -Math.PI / 2, Math.PI / 2, false );
+    this.lineTo( 0, mainHeight + strokeOffset );
+    this.lineTo( 0, mainHeight - strokeOffset );
+    this.lineTo( mainWidth, mainHeight - strokeOffset );
+    // arc (mainWidth,mainHeight-strokeOffset) => (mainWidth,strokeOffset)
+    this.arc( mainWidth, mainHeight / 2, mainHeight / 2 - strokeOffset, Math.PI / 2, -Math.PI / 2, true );
+    this.lineTo( halfHeadWidth, strokeOffset );
+    // three lines of the arrow head
+    this.lineTo( halfHeadWidth, halfHeadHeight );
+    this.lineTo( -halfHeadWidth, 0 );
+    this.lineTo( halfHeadWidth, -halfHeadHeight );
+    this.close();
+  }
 }
 
 sceneryPhet.register( 'UTurnArrowShape', UTurnArrowShape );
 
-inherit( Shape, UTurnArrowShape );
 export default UTurnArrowShape;
