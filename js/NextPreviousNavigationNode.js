73a2be9bffa0610c16084f61106daf46019bd6e7
pixelzoom <cmalley@pixelzoom.com>
2020-10-14 16:43:52 -0600

    convert to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/NextPreviousNavigationNode.js
+++ js/NextPreviousNavigationNode.js
@@ -9,7 +9,6 @@
 
 import Property from '../../axon/js/Property.js';
 import Shape from '../../kite/js/Shape.js';
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import FireListener from '../../scenery/js/listeners/FireListener.js';
 import Node from '../../scenery/js/nodes/Node.js';
@@ -17,125 +16,125 @@ import Path from '../../scenery/js/nodes/Path.js';
 import Color from '../../scenery/js/util/Color.js';
 import sceneryPhet from './sceneryPhet.js';
 
-/**
- * @param {Node} centerNode
- * @param {Object} [selfOptions]  Valid options are:
- *                                arrowColor         - color for the arrow's fill
- *                                arrowStrokeColor   - color for the arrow's stroke
- *                                arrowWidth         - the width of the arrow, from its point to its side
- *                                arrowHeight        - the height of the arrow, from tip to tip
- *                                next               - a function to be called when the "next" arrow is pressed
- *                                previous           - a function to be called when the "previous" arrow is pressed
- *                                createTouchAreaShape - function( shape, isPrevious ) that returns the touch area for the specified arrow
- * @param {Object} [nodeOptions] Passed to the Node constructor
- */
-function NextPreviousNavigationNode( centerNode, selfOptions, nodeOptions ) {
-  const self = this;
+class NextPreviousNavigationNode extends Node {
 
-  // @public
-  this.hasNextProperty = new Property( false );
-  this.hasPreviousProperty = new Property( false );
+  /**
+   * @param {Node} centerNode
+   * @param {Object} [selfOptions]  Valid options are:
+   *                                arrowColor         - color for the arrow's fill
+   *                                arrowStrokeColor   - color for the arrow's stroke
+   *                                arrowWidth         - the width of the arrow, from its point to its side
+   *                                arrowHeight        - the height of the arrow, from tip to tip
+   *                                next               - a function to be called when the "next" arrow is pressed
+   *                                previous           - a function to be called when the "previous" arrow is pressed
+   *                                createTouchAreaShape - function( shape, isPrevious ) that returns the touch area for the specified arrow
+   * @param {Object} [nodeOptions] passed to the Node (super) constructor
+   */
+  constructor( centerNode, selfOptions, nodeOptions ) {
 
-  Node.call( this );
-
-  selfOptions = merge( {
-    arrowColor: Color.YELLOW,
-    arrowStrokeColor: Color.BLACK,
-    arrowWidth: 14,
-    arrowHeight: 18,
-    arrowPadding: 15,
-    next: null, // function() { ... }
-    previous: null, // function() { ... }
-    createTouchAreaShape: function( shape, isPrevious ) {
-      return null; // pass in function that returns a shape given the shape of the arrow
-    }
-  }, selfOptions );
-
-  const arrowWidth = selfOptions.arrowWidth;
-  const arrowHeight = selfOptions.arrowHeight;
-
-  /*---------------------------------------------------------------------------*
-   * previous
-   *----------------------------------------------------------------------------*/
-
-  // triangle pointing to the left
-  const previousShape = new Shape().moveTo( 0, arrowHeight / 2 )
-    .lineTo( arrowWidth, 0 )
-    .lineTo( arrowWidth, arrowHeight )
-    .close();
-
-  const previousKitNode = new Path( previousShape, {
-    fill: selfOptions.arrowColor,
-    stroke: selfOptions.arrowStrokeColor,
-    cursor: 'pointer', // TODO: buttonListener adds this maybe? https://github.com/phetsims/scenery-phet/issues/587
-    touchArea: selfOptions.createTouchAreaShape( previousShape, true )
-  } );
-  previousKitNode.addInputListener( new FireListener( {
-    fire: function() {
-      if ( self.hasPreviousProperty.value ) {
-        selfOptions.previous && selfOptions.previous();
+    selfOptions = merge( {
+      arrowColor: Color.YELLOW,
+      arrowStrokeColor: Color.BLACK,
+      arrowWidth: 14,
+      arrowHeight: 18,
+      arrowPadding: 15,
+      next: null, // function() { ... }
+      previous: null, // function() { ... }
+      createTouchAreaShape: function( shape, isPrevious ) {
+        return null; // pass in function that returns a shape given the shape of the arrow
       }
-    }
-  } ) );
-  this.hasPreviousProperty.link( function( available ) {
-    previousKitNode.visible = available;
-  } );
+    }, selfOptions );
 
-  this.addChild( previousKitNode );
+    super();
 
-  /*---------------------------------------------------------------------------*
-   * center
-   *----------------------------------------------------------------------------*/
+    // @public
+    this.hasNextProperty = new Property( false );
+    this.hasPreviousProperty = new Property( false );
 
-  this.addChild( centerNode );
+    const arrowWidth = selfOptions.arrowWidth;
+    const arrowHeight = selfOptions.arrowHeight;
 
-  /*---------------------------------------------------------------------------*
-   * next
-   *----------------------------------------------------------------------------*/
+    /*---------------------------------------------------------------------------*
+     * previous
+     *----------------------------------------------------------------------------*/
 
-  // triangle pointing to the right
-  const nextShape = new Shape().moveTo( arrowWidth, arrowHeight / 2 )
-    .lineTo( 0, 0 )
-    .lineTo( 0, arrowHeight )
-    .close();
+    // triangle pointing to the left
+    const previousShape = new Shape().moveTo( 0, arrowHeight / 2 )
+      .lineTo( arrowWidth, 0 )
+      .lineTo( arrowWidth, arrowHeight )
+      .close();
 
-  const nextKitNode = new Path( nextShape, {
-    fill: selfOptions.arrowColor,
-    stroke: selfOptions.arrowStrokeColor,
-    cursor: 'pointer', // TODO: buttonListener adds this maybe? https://github.com/phetsims/scenery-phet/issues/587
-    touchArea: selfOptions.createTouchAreaShape( nextShape, false )
-  } );
-  nextKitNode.addInputListener( new FireListener( {
-    fire: function() {
-      if ( self.hasNextProperty.value ) {
-        selfOptions.next && selfOptions.next();
+    const previousKitNode = new Path( previousShape, {
+      fill: selfOptions.arrowColor,
+      stroke: selfOptions.arrowStrokeColor,
+      cursor: 'pointer', // TODO: buttonListener adds this maybe? https://github.com/phetsims/scenery-phet/issues/587
+      touchArea: selfOptions.createTouchAreaShape( previousShape, true )
+    } );
+    previousKitNode.addInputListener( new FireListener( {
+      fire: () => {
+        if ( this.hasPreviousProperty.value ) {
+          selfOptions.previous && selfOptions.previous();
+        }
       }
-    }
-  } ) );
-  this.hasNextProperty.link( function( available ) {
-    nextKitNode.visible = available;
-  } );
+    } ) );
+    this.hasPreviousProperty.link( function( available ) {
+      previousKitNode.visible = available;
+    } );
 
-  this.addChild( nextKitNode );
+    this.addChild( previousKitNode );
 
-  /*---------------------------------------------------------------------------*
-   * positioning
-   *----------------------------------------------------------------------------*/
+    /*---------------------------------------------------------------------------*
+     * center
+     *----------------------------------------------------------------------------*/
 
-  const maxHeight = Math.max( arrowHeight, centerNode.height );
+    this.addChild( centerNode );
 
-  previousKitNode.centerY = maxHeight / 2;
-  centerNode.centerY = maxHeight / 2;
-  nextKitNode.centerY = maxHeight / 2;
+    /*---------------------------------------------------------------------------*
+     * next
+     *----------------------------------------------------------------------------*/
 
-  // previousKitNode.x = 0;
-  centerNode.x = arrowWidth + selfOptions.arrowPadding;
-  nextKitNode.x = centerNode.right + selfOptions.arrowPadding;
+    // triangle pointing to the right
+    const nextShape = new Shape().moveTo( arrowWidth, arrowHeight / 2 )
+      .lineTo( 0, 0 )
+      .lineTo( 0, arrowHeight )
+      .close();
 
-  this.mutate( nodeOptions );
+    const nextKitNode = new Path( nextShape, {
+      fill: selfOptions.arrowColor,
+      stroke: selfOptions.arrowStrokeColor,
+      cursor: 'pointer', // TODO: buttonListener adds this maybe? https://github.com/phetsims/scenery-phet/issues/587
+      touchArea: selfOptions.createTouchAreaShape( nextShape, false )
+    } );
+    nextKitNode.addInputListener( new FireListener( {
+      fire: () => {
+        if ( this.hasNextProperty.value ) {
+          selfOptions.next && selfOptions.next();
+        }
+      }
+    } ) );
+    this.hasNextProperty.link( function( available ) {
+      nextKitNode.visible = available;
+    } );
+
+    this.addChild( nextKitNode );
+
+    /*---------------------------------------------------------------------------*
+     * positioning
+     *----------------------------------------------------------------------------*/
+
+    const maxHeight = Math.max( arrowHeight, centerNode.height );
+
+    previousKitNode.centerY = maxHeight / 2;
+    centerNode.centerY = maxHeight / 2;
+    nextKitNode.centerY = maxHeight / 2;
+
+    // previousKitNode.x = 0;
+    centerNode.x = arrowWidth + selfOptions.arrowPadding;
+    nextKitNode.x = centerNode.right + selfOptions.arrowPadding;
+
+    this.mutate( nodeOptions );
+  }
 }
 
 sceneryPhet.register( 'NextPreviousNavigationNode', NextPreviousNavigationNode );
-
-inherit( Node, NextPreviousNavigationNode );
 export default NextPreviousNavigationNode;
