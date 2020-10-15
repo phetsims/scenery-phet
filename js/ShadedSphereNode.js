45d7c0eba18ec78157f3dc23c464cbf079c20001
pixelzoom <cmalley@pixelzoom.com>
2020-10-15 10:03:53 -0600

    convert ShadedSphereNode to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/ShadedSphereNode.js
+++ js/ShadedSphereNode.js
@@ -6,48 +6,47 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import Circle from '../../scenery/js/nodes/Circle.js';
 import RadialGradient from '../../scenery/js/util/RadialGradient.js';
 import sceneryPhet from './sceneryPhet.js';
 
-/**
- * @param {number} diameter
- * @param {Object} [options]
- * @constructor
- */
-function ShadedSphereNode( diameter, options ) {
+class ShadedSphereNode extends Circle {
 
-  options = merge( {
-    mainColor: 'gray',
-    highlightColor: 'white',
-    shadowColor: 'black',
-    highlightDiameter: 0.5 * diameter,
-    highlightXOffset: -0.4, // x-offset of the highlight from the center of the sphere, percentage of radius, [-1,1]
-    highlightYOffset: -0.4  // y-offset of the highlight from the center of the sphere, percentage of radius, [-1,1]
-  }, options );
+  /**
+   * @param {number} diameter
+   * @param {Object} [options]
+   */
+  constructor( diameter, options ) {
 
-  // validate option values
-  assert && assert( options.highlightDiameter < diameter,
-    'highlightDiameter must be < diameter: ' + options.highlightDiameter );
-  assert && assert( options.highlightXOffset >= -1 && options.highlightXOffset <= 1,
-    'highlightXOffset out of range: ' + options.highlightXOffset );
-  assert && assert( options.highlightYOffset >= -1 && options.highlightYOffset <= 1,
-    'highlightYOffset out of range: ' + options.highlightYOffset );
+    options = merge( {
+      mainColor: 'gray',
+      highlightColor: 'white',
+      shadowColor: 'black',
+      highlightDiameter: 0.5 * diameter,
+      highlightXOffset: -0.4, // x-offset of the highlight from the center of the sphere, percentage of radius, [-1,1]
+      highlightYOffset: -0.4  // y-offset of the highlight from the center of the sphere, percentage of radius, [-1,1]
+    }, options );
 
-  const radius = diameter / 2;
-  const highlightX = radius * options.highlightXOffset;
-  const highlightY = radius * options.highlightYOffset;
-  options.fill = new RadialGradient( highlightX, highlightY, 0, highlightX, highlightY, diameter )
-    .addColorStop( 0, options.highlightColor )
-    .addColorStop( options.highlightDiameter / diameter, options.mainColor )
-    .addColorStop( 1, options.shadowColor );
+    // validate option values
+    assert && assert( options.highlightDiameter < diameter,
+      'highlightDiameter must be < diameter: ' + options.highlightDiameter );
+    assert && assert( options.highlightXOffset >= -1 && options.highlightXOffset <= 1,
+      'highlightXOffset out of range: ' + options.highlightXOffset );
+    assert && assert( options.highlightYOffset >= -1 && options.highlightYOffset <= 1,
+      'highlightYOffset out of range: ' + options.highlightYOffset );
 
-  Circle.call( this, radius, options );
+    const radius = diameter / 2;
+    const highlightX = radius * options.highlightXOffset;
+    const highlightY = radius * options.highlightYOffset;
+    options.fill = new RadialGradient( highlightX, highlightY, 0, highlightX, highlightY, diameter )
+      .addColorStop( 0, options.highlightColor )
+      .addColorStop( options.highlightDiameter / diameter, options.mainColor )
+      .addColorStop( 1, options.shadowColor );
+
+    super( radius, options );
+  }
 }
 
 sceneryPhet.register( 'ShadedSphereNode', ShadedSphereNode );
-
-inherit( Circle, ShadedSphereNode );
 export default ShadedSphereNode;
