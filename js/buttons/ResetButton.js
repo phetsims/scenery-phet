eef75df8ac3cb87390f87bac1d93c8267ae37567
pixelzoom <cmalley@pixelzoom.com>
2020-10-23 14:22:41 -0600

    rename RoundButtonView to RoundButton, https://github.com/phetsims/sun/issues/643

--- js/buttons/ResetButton.js
+++ js/buttons/ResetButton.js
@@ -33,9 +33,9 @@ class ResetButton extends RoundPushButton {
       baseColor: 'white',
       arrowColor: 'black',
 
-      //TODO this should be handled by RoundButtonView.ThreeDAppearanceStrategy, see https://github.com/phetsims/sun/issues/236
-      // The icon doesn't look right when perfectly centered, account for that here,
-      // and see docs in RoundButtonView. The multiplier values were empirically determined.
+      //TODO this should be handled by RoundButton.ThreeDAppearanceStrategy, see https://github.com/phetsims/sun/issues/236
+      // The icon doesn't look right when perfectly centered, account for that here, and see docs in RoundButton.
+      // The multiplier values were empirically determined.
       xContentOffset: -0.03 * BUTTON_RADIUS,
       yContentOffset: -0.0125 * BUTTON_RADIUS,
 
