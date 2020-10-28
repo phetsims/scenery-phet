ac1099b031bb633a68432d6de9da732265824caa
phet-dev <phetadmin@gmail.com>
2020-10-28 04:03:53 -0600

    update copyright dates from daily grunt work

--- js/keyboard/NumberKeyNode.js
+++ js/keyboard/NumberKeyNode.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2020, University of Colorado Boulder
+// Copyright 2020, University of Colorado Boulder
 
 /**
  * NumberKeyNode looks like a keyboard key with a single letter. See LetterKeyNode for implementation details. This is
