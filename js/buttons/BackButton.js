7f206bc0c18c7677e2a2da311d8955a5c8444a85
phet-dev <phetadmin@gmail.com>
2021-06-02 03:39:02 -0600

    update copyright dates from daily grunt work

--- js/buttons/BackButton.js
+++ js/buttons/BackButton.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2020, University of Colorado Boulder
+// Copyright 2014-2021, University of Colorado Boulder
 
 /**
  * Button that is intended to indicate going backwards, much like the back button on a web browser.  It was originally
