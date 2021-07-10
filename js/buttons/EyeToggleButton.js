abc8a9dc5612df76c4e0a1560e71a7ae123f05e0
phet-dev <phetadmin@gmail.com>
2021-07-10 03:39:00 -0600

    update copyright dates from daily grunt work

--- js/buttons/EyeToggleButton.js
+++ js/buttons/EyeToggleButton.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, University of Colorado Boulder
+// Copyright 2016-2021, University of Colorado Boulder
 
 /**
  * Button that toggles between an open and closed eyeball, used to control the visibility of something.
