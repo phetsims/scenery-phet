9e539b5707ee24853514e988fec8cfae4e4a3063
phet-dev <phetadmin@gmail.com>
2021-10-14 03:20:25 -0600

    update copyright dates from daily grunt work

--- js/PlusNode.js
+++ js/PlusNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2020, University of Colorado Boulder
+// Copyright 2014-2021, University of Colorado Boulder
 
 /**
  * Plus sign, created using scenery.Path because scenery.Text("+") cannot be accurately centered.
