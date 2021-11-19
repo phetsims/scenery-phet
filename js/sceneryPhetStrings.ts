3609011050eadffb7ad080ef57afe56ff37d756f
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:02 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/sceneryPhetStrings.ts
@@ -0,0 +1,237 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import sceneryPhet from './sceneryPhet.js';
+
+type StringsType = {
+  'scenery-phet': {
+    'title': string
+  },
+  'WavelengthSlider': {
+    'pattern_0wavelength_1units': string
+  },
+  'frequencyUnitsPattern': string,
+  'stopwatchValueUnitsPattern': string,
+  'units_nm': string,
+  'shortCircuit': string,
+  'heat': string,
+  'cool': string,
+  'key': {
+    'tab': string,
+    'shift': string,
+    'alt': string,
+    'k': string,
+    'l': string,
+    'capsLock': string,
+    'enter': string,
+    'space': string,
+    'esc': string,
+    'fn': string,
+    'pageUp': string,
+    'pageDown': string,
+    'home': string,
+    'end': string,
+    'toGrabOrRelease': string
+  },
+  'webglWarning': {
+    'title': string,
+    'body': string,
+    'contextLossFailure': string,
+    'contextLossReload': string,
+    'ie11StencilBody': string
+  },
+  'keyboardHelpDialog': {
+    'sliderControls': string,
+    'adjustSlider': string,
+    'adjustInSmallerSteps': string,
+    'adjustInLargerSteps': string,
+    'jumpToMinimum': string,
+    'jumpToMaximum': string,
+    'adjust': string,
+    'slider': string,
+    'verbSliderPattern': string,
+    'verbInSmallerStepsPattern': string,
+    'verbInLargerStepsPattern': string,
+    'minimum': string,
+    'maximum': string,
+    'jumpToMinimumPattern': string,
+    'jumpToMaximumPattern': string,
+    'generalNavigation': string,
+    'basicActions': string,
+    'moveToNextItem': string,
+    'moveToPreviousItem': string,
+    'moveToNextItemOrGroup': string,
+    'moveToPreviousItemOrGroup': string,
+    'pressButtons': string,
+    'moveBetweenItemsInAGroup': string,
+    'exitADialog': string,
+    'toggleCheckboxes': string,
+    'or': string,
+    'hyphen': string,
+    'grabOrReleaseHeadingPattern': string,
+    'grabOrReleaseLabelPattern': string,
+    'comboBox': {
+      'chooseAThingPattern': string,
+      'popUpListPattern': string,
+      'moveThroughPattern': string,
+      'chooseNewPattern': string,
+      'closeWithoutChanging': string,
+      'options': string,
+      'option': string
+    }
+  },
+  'speed': {
+    'normal': string,
+    'slow': string,
+    'fast': string
+  },
+  'symbol': {
+    'ohms': string,
+    'resistivity': string
+  },
+  'comboBoxDisplay': {
+    'valueUnits': string
+  },
+  'wavelengthNMValuePattern': string,
+  'measuringTapeReadoutPattern': string,
+  'wavelength': string,
+  'rulerCapitalized': string,
+  'ruler': string,
+  'zero': string,
+  'one': string,
+  'two': string,
+  'three': string,
+  'four': string,
+  'five': string,
+  'six': string,
+  'seven': string,
+  'eight': string,
+  'nine': string,
+  'ten': string,
+  'a11y': {
+    'simSection': {
+      'screenSummary': {
+        'multiScreenIntro': string,
+        'singleScreenIntroPattern': string,
+        'keyboardShortcutsHint': string
+      },
+      'playArea': string,
+      'controlArea': string
+    },
+    'resetAll': {
+      'label': string,
+      'alert': string
+    },
+    'soundToggle': {
+      'label': string,
+      'alert': {
+        'simSoundOn': string,
+        'simSoundOff': string
+      }
+    },
+    'keyboardHelpDialog': {
+      'general': {
+        'tabDescription': string,
+        'shiftTabDescription': string,
+        'tabGroupDescription': string,
+        'shiftTabGroupDescription': string,
+        'pressButtonsDescription': string,
+        'groupNavigationDescription': string,
+        'exitDialogDescription': string,
+        'toggleCheckboxesDescription': string
+      },
+      'slider': {
+        'orKeysPattern': string,
+        'leftRightArrowKeys': string,
+        'upDownArrowKeys': string,
+        'defaultStepsDescriptionPattern': string,
+        'shiftLeftRightArrowKeys': string,
+        'shiftUpDownArrowKeys': string,
+        'smallerStepsDescriptionPattern': string,
+        'largerStepsDescriptionPattern': string,
+        'jumpToMinimumDescriptionPattern': string,
+        'jumpToMaximumDescriptionPattern': string
+      },
+      'grabOrReleaseDescriptionPattern': string,
+      'comboBox': {
+        'popUpListPatternDescription': string,
+        'moveThroughPatternDescription': string,
+        'chooseNewPatternDescription': string,
+        'closeWithoutChangingDescription': string
+      }
+    },
+    'playControlButton': {
+      'play': string,
+      'pause': string,
+      'stop': string
+    },
+    'stepButton': {
+      'stepForward': string,
+      'playingDescription': string,
+      'pausedDescription': string
+    },
+    'timeControlNode': {
+      'simSpeedDescription': string,
+      'label': string,
+      'simSpeeds': string,
+      'playPauseStepButtons': {
+        'playingDescription': string,
+        'playingWithSpeedDescription': string,
+        'pausedDescription': string,
+        'pausedWithSpeedDescription': string
+      }
+    },
+    'movementAlerter': {
+      'down': string,
+      'left': string,
+      'right': string,
+      'up': string,
+      'upAndToTheRight': string,
+      'upAndToTheLeft': string,
+      'downAndToTheRight': string,
+      'downAndToTheLeft': string,
+      'leftBorderAlert': string,
+      'rightBorderAlert': string,
+      'topBorderAlert': string,
+      'bottomBorderAlert': string
+    },
+    'grabDrag': {
+      'grabPattern': string,
+      'movable': string,
+      'button': string,
+      'defaultObjectToGrab': string,
+      'released': string,
+      'gestureHelpTextPattern': string,
+      'spaceToGrabOrRelease': string
+    },
+    'voicing': {
+      'simSection': {
+        'screenSummary': {
+          'singleScreenIntroPattern': string
+        }
+      },
+      'keyboardHelpDialog': {
+        'general': {
+          'descriptionPattern': string
+        }
+      },
+      'grabDragHintPattern': string,
+      'grabbedAlert': string,
+      'draggableAlert': string,
+      'resetAll': {
+        'contextResponse': string
+      }
+    },
+    'close': string
+  }
+};
+
+const sceneryPhetStrings = getStringModule( 'SCENERY_PHET' ) as StringsType;
+
+sceneryPhet.register( 'sceneryPhetStrings', sceneryPhetStrings );
+
+export default sceneryPhetStrings;
