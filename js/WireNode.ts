eeddd0791bc0154d4be0d5c86655a3a386a4b3a2
Jonathan Olson <jonathan.olson@colorado.edu>
2021-12-07 18:32:04 -0700

    Converting DerivedProperty/Multilink to properly parameterized TypeScript, and usage of IReadOnlyProperty. See https://github.com/phetsims/axon/issues/371

--- js/WireNode.ts
+++ js/WireNode.ts
@@ -6,22 +6,27 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
+import IReadOnlyProperty from '../../axon/js/IReadOnlyProperty.js';
+import Multilink from '../../axon/js/Multilink.js';
 import Property from '../../axon/js/Property.js';
+import Vector2 from '../../dot/js/Vector2.js';
 import Shape from '../../kite/js/Shape.js';
 import merge from '../../phet-core/js/merge.js';
-import { Path } from '../../scenery/js/imports.js';
+import { Path, PathOptions } from '../../scenery/js/imports.js';
 import sceneryPhet from './sceneryPhet.js';
 
 class WireNode extends Path {
 
+  private multilink: Multilink<[ Vector2, Vector2, Vector2, Vector2 ]>
+
   /**
-   * @param {Property.<Vector2>} position1Property - connects to one object, often this is a DerivedProperty
-   * @param {Property.<Vector2>} normal1Property - defines the control point of the cubic curve, relative to the position1
-   * @param {Property.<Vector2>} position2Property - connects to another object, often this is a DerivedProperty
-   * @param {Property.<Vector2>} normal2Property - defines the control point of the cubic curve, relative to the position2
-   * @param {Object} [options]
+   * @param position1Property - connects to one object, often this is a DerivedProperty
+   * @param normal1Property - defines the control point of the cubic curve, relative to the position1
+   * @param position2Property - connects to another object, often this is a DerivedProperty
+   * @param normal2Property - defines the control point of the cubic curve, relative to the position2
+   * @param [options]
    */
-  constructor( position1Property, normal1Property, position2Property, normal2Property, options ) {
+  constructor( position1Property: IReadOnlyProperty<Vector2>, normal1Property: IReadOnlyProperty<Vector2>, position2Property: IReadOnlyProperty<Vector2>, normal2Property: IReadOnlyProperty<Vector2>, options?: PathOptions ) {
 
     options = merge( {
       stroke: 'black'
@@ -43,10 +48,6 @@ class WireNode extends Path {
     } );
   }
 
-  /**
-   * @public
-   * @override
-   */
   dispose() {
     this.multilink.dispose();
     super.dispose();
