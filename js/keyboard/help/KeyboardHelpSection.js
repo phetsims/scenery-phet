05aa800805728a1756aafef0e75f08943f45dbd8
zepumph <michael.kauzmann@colorado.edu>
2021-12-13 14:32:11 -0700

    Voicing-related traits should not support options, but instead options should be set by mutate after initialization, https://github.com/phetsims/scenery/issues/1316

--- js/keyboard/help/KeyboardHelpSection.js
+++ js/keyboard/help/KeyboardHelpSection.js
@@ -18,12 +18,7 @@
 
 import merge from '../../../../phet-core/js/merge.js';
 import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
-import { ReadingBlock } from '../../../../scenery/js/imports.js';
-import { AlignGroup } from '../../../../scenery/js/imports.js';
-import { HBox } from '../../../../scenery/js/imports.js';
-import { RichText } from '../../../../scenery/js/imports.js';
-import { Text } from '../../../../scenery/js/imports.js';
-import { VBox } from '../../../../scenery/js/imports.js';
+import { AlignGroup, HBox, ReadingBlock, RichText, Text, VBox } from '../../../../scenery/js/imports.js';
 import PhetFont from '../../PhetFont.js';
 import sceneryPhet from '../../sceneryPhet.js';
 import sceneryPhetStrings from '../../sceneryPhetStrings.js';
@@ -146,9 +141,8 @@ class KeyboardHelpSection extends VBox {
     this.icons = icons;
 
     // voicing - initialize this section as a ReadingBlock
-    this.initializeReadingBlock( {
-      readingBlockContent: this.generateReadingBlockContent()
-    } );
+    this.initializeReadingBlock();
+    this.readingBlockContent = this.generateReadingBlockContent();
   }
 
   /**
