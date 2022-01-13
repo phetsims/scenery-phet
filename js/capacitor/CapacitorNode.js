28063ec2c627ad38b4c3cbed44ea98df74b4d63c
phet-dev <phetadmin@gmail.com>
2022-01-13 03:24:00 -0700

    update copyright dates from daily grunt work

--- js/capacitor/CapacitorNode.js
+++ js/capacitor/CapacitorNode.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2021, University of Colorado Boulder
+// Copyright 2019-2022, University of Colorado Boulder
 
 /**
  * Visual representation of a capacitor with a vacuum gap.
--- js/capacitor/PlateChargeNode.js
+++ js/capacitor/PlateChargeNode.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2021, University of Colorado Boulder
+// Copyright 2019-2022, University of Colorado Boulder
 
 /**
  * Base class for representation of plate charge.  Plate charge is represented
