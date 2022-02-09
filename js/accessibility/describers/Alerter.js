c7b2a6632a4838db2163720020ec196edbac469e
phet-dev <phetadmin@gmail.com>
2022-02-09 03:30:27 -0700

    update copyright dates from daily grunt work

--- js/accessibility/describers/Alerter.js
+++ js/accessibility/describers/Alerter.js
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * Generic base class responsible for interfacing between a Node to alert description.
--- js/accessibility/describers/MovementAlerter.js
+++ js/accessibility/describers/MovementAlerter.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2021, University of Colorado Boulder
+// Copyright 2018-2022, University of Colorado Boulder
 
 /**
  * A generic alerting type that will alert positional alerts based on a positionProperty and bounds (see
