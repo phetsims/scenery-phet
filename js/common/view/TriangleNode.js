68787ef22712e52df3bbe89ebbdf1e5326e349d8
zepumph <michael.kauzmann@colorado.edu>
2022-03-04 13:07:45 -0700

    import kite files from imports.ts, https://github.com/phetsims/kite/issues/96

--- js/common/view/TriangleNode.js
+++ js/common/view/TriangleNode.js
@@ -7,7 +7,7 @@
  */
 
 import Matrix3 from '../../../../dot/js/Matrix3.js';
-import Shape from '../../../../kite/js/Shape.js';
+import { Shape } from '../../../../kite/js/imports.js';
 import merge from '../../../../phet-core/js/merge.js';
 import { Path } from '../../../../scenery/js/imports.js';
 import proportionPlayground from '../../proportionPlayground.js';
