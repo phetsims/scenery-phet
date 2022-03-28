277f54ec29f503be2f4050ad99c4ca8b3882f158
pixelzoom <cmalley@pixelzoom.com>
2022-03-28 15:54:45 -0600

    convert to TS

--- js/StarShape.ts
+++ js/StarShape.ts
@@ -1,6 +1,5 @@
 // Copyright 2014-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * Star shape (full, 5-pointed)
  *
@@ -8,26 +7,37 @@
  */
 
 import { Shape } from '../../kite/js/imports.js';
-import merge from '../../phet-core/js/merge.js';
+import optionize from '../../phet-core/js/optionize.js';
 import sceneryPhet from './sceneryPhet.js';
 
-class StarShape extends Shape {
+type SelfOptions = {
+
+  // Distance from the center to the tip of a star limb
+  outerRadius: number;
+
+  // Distance from the center to the closest point on the exterior of the star.  Sets the "thickness" of the star limbs
+  innerRadius: number;
+
+  // Number of star points, must be an integer
+  numberStarPoints: number;
+};
+
+export type StarShapeOptions = SelfOptions;
+
+export default class StarShape extends Shape {
+
   /**
-   * @param {Object} [options]
+   * @param providedOptions
    */
-  constructor( options ) {
+  constructor( providedOptions?: StarShapeOptions ) {
 
-    options = merge( {
+    const options = optionize<StarShapeOptions, SelfOptions>( {
 
-      // Distance from the center to the tip of a star limb
+      // SelfOptions
       outerRadius: 15,
-
-      // Distance from the center to the closest point on the exterior of the star.  Sets the "thickness" of the star limbs
       innerRadius: 7.5,
-
-      // Number of star points, must be an integer
       numberStarPoints: 5
-    }, options );
+    }, providedOptions );
 
     super();
 
@@ -49,5 +59,3 @@ class StarShape extends Shape {
 }
 
 sceneryPhet.register( 'StarShape', StarShape );
-
-export default StarShape;
