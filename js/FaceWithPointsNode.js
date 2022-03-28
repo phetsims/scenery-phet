1de84a6bdc48ff53241f18e5858ffb03eb6b1e49
pixelzoom <cmalley@pixelzoom.com>
2022-03-28 15:08:10 -0600

    // @ts-nocheck

--- js/FaceWithPointsNode.js
+++ js/FaceWithPointsNode.js
@@ -1,5 +1,6 @@
 // Copyright 2014-2021, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * A face that either smiles or frowns.  When the face is smiling, it displays points awarded next to it.
  *
