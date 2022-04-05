1a4bffba543d42c914d97f5ad90e3a0e592897dc
pixelzoom <cmalley@pixelzoom.com>
2022-04-05 11:45:32 -0600

    convert to TS

--- js/WavelengthSpectrumNode.ts
+++ js/WavelengthSpectrumNode.ts
@@ -1,6 +1,5 @@
-// Copyright 2014-2020, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * WavelengthSpectrumNode displays a rectangle of the visible spectrum.
  *
@@ -8,35 +7,34 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
-import merge from '../../phet-core/js/merge.js';
+import optionize from '../../phet-core/js/optionize.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import sceneryPhet from './sceneryPhet.js';
-import SpectrumNode from './SpectrumNode.js';
+import SpectrumNode, { SpectrumNodeOptions } from './SpectrumNode.js';
 import VisibleColor from './VisibleColor.js';
 
-/**
- * Slider track that displays the visible spectrum of light.
- *
- * @param {Object} [options]
- * @constructor
- */
+type SelfOptions = {
+  minWavelength?: number;
+  maxWavelength?: number;
+};
+
+export type WavelengthSpectrumNodeOptions = SelfOptions & Omit<SpectrumNodeOptions, 'minValue' | 'maxValue'>;
+
 class WavelengthSpectrumNode extends SpectrumNode {
 
-  constructor( options ) {
+  constructor( providedOptions?: WavelengthSpectrumNodeOptions ) {
 
-    options = merge( {
-      valueToColor: value => VisibleColor.wavelengthToColor( value ),
+    const options = optionize<WavelengthSpectrumNodeOptions, SelfOptions, SpectrumNodeOptions>( {
+      valueToColor: ( value: number ) => VisibleColor.wavelengthToColor( value ),
       minWavelength: VisibleColor.MIN_WAVELENGTH,
       maxWavelength: VisibleColor.MAX_WAVELENGTH,
       tandem: Tandem.OPTIONAL
-    }, options );
+    }, providedOptions );
 
     // validation
     assert && assert( options.minWavelength < options.maxWavelength );
     assert && assert( options.minWavelength >= VisibleColor.MIN_WAVELENGTH && options.minWavelength <= VisibleColor.MAX_WAVELENGTH );
     assert && assert( options.maxWavelength >= VisibleColor.MIN_WAVELENGTH && options.maxWavelength <= VisibleColor.MAX_WAVELENGTH );
-    assert && assert( typeof options.minValue === 'undefined', 'minValue is supplied by WavelengthSlider' );
-    assert && assert( typeof options.maxValue === 'undefined', 'maxValue is supplied by WavelengthSlider' );
 
     options.minValue = options.minWavelength;
     options.maxValue = options.maxWavelength;
