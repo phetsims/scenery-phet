8f82199803a076aee1938f67a3d51b6890fcca0f
phet-dev <phetadmin@gmail.com>
2022-04-06 03:40:45 -0600

    update copyright dates from daily grunt work

--- js/SimpleClockIcon.ts
+++ js/SimpleClockIcon.ts
@@ -1,4 +1,4 @@
-// Copyright 2013-2021, University of Colorado Boulder
+// Copyright 2013-2022, University of Colorado Boulder
 
 /**
  * Scenery node that represents a simple, non-interactive clock.  It is
