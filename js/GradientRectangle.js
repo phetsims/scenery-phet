ffd37df641a2ecb866cd2798b4b36d38d46dec31
pixelzoom <cmalley@pixelzoom.com>
2022-04-11 13:25:23 -0600

    // @ts-nocheck

--- js/GradientRectangle.js
+++ js/GradientRectangle.js
@@ -1,5 +1,6 @@
 // Copyright 2019-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * Mostly like a normal Rectangle (Node), but instead of a hard transition from "in" to "out", it has a defined region
  * of gradients around the edges.
