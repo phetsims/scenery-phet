6be36396605c21aafe0a47211550b3fa3c0c2fb7
Sam Reid <reids@colorado.edu>
2022-05-07 09:35:56 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/sceneryPhetStrings.ts
+++ js/sceneryPhetStrings.ts
@@ -9,225 +9,225 @@ import sceneryPhet from './sceneryPhet.js';
 
 type StringsType = {
   'scenery-phet': {
-    'title': string
-  },
+    'title': string;
+  };
   'WavelengthSlider': {
-    'pattern_0wavelength_1units': string
-  },
-  'frequencyUnitsPattern': string,
-  'stopwatchValueUnitsPattern': string,
-  'units_nm': string,
-  'shortCircuit': string,
-  'heat': string,
-  'cool': string,
+    'pattern_0wavelength_1units': string;
+  };
+  'frequencyUnitsPattern': string;
+  'stopwatchValueUnitsPattern': string;
+  'units_nm': string;
+  'shortCircuit': string;
+  'heat': string;
+  'cool': string;
   'key': {
-    'tab': string,
-    'shift': string,
-    'alt': string,
-    'k': string,
-    'l': string,
-    'capsLock': string,
-    'enter': string,
-    'space': string,
-    'esc': string,
-    'fn': string,
-    'pageUp': string,
-    'pageDown': string,
-    'home': string,
-    'end': string,
-    'toGrabOrRelease': string
-  },
+    'tab': string;
+    'shift': string;
+    'alt': string;
+    'k': string;
+    'l': string;
+    'capsLock': string;
+    'enter': string;
+    'space': string;
+    'esc': string;
+    'fn': string;
+    'pageUp': string;
+    'pageDown': string;
+    'home': string;
+    'end': string;
+    'toGrabOrRelease': string;
+  };
   'webglWarning': {
-    'title': string,
-    'body': string,
-    'contextLossFailure': string,
-    'contextLossReload': string,
-    'ie11StencilBody': string
-  },
+    'title': string;
+    'body': string;
+    'contextLossFailure': string;
+    'contextLossReload': string;
+    'ie11StencilBody': string;
+  };
   'keyboardHelpDialog': {
-    'sliderControls': string,
-    'adjustSlider': string,
-    'adjustInSmallerSteps': string,
-    'adjustInLargerSteps': string,
-    'jumpToMinimum': string,
-    'jumpToMaximum': string,
-    'adjust': string,
-    'slider': string,
-    'verbSliderPattern': string,
-    'verbInSmallerStepsPattern': string,
-    'verbInLargerStepsPattern': string,
-    'minimum': string,
-    'maximum': string,
-    'jumpToMinimumPattern': string,
-    'jumpToMaximumPattern': string,
-    'generalNavigation': string,
-    'basicActions': string,
-    'moveToNextItem': string,
-    'moveToPreviousItem': string,
-    'moveToNextItemOrGroup': string,
-    'moveToPreviousItemOrGroup': string,
-    'pressButtons': string,
-    'moveBetweenItemsInAGroup': string,
-    'exitADialog': string,
-    'toggleCheckboxes': string,
-    'or': string,
-    'hyphen': string,
-    'grabOrReleaseHeadingPattern': string,
-    'grabOrReleaseLabelPattern': string,
+    'sliderControls': string;
+    'adjustSlider': string;
+    'adjustInSmallerSteps': string;
+    'adjustInLargerSteps': string;
+    'jumpToMinimum': string;
+    'jumpToMaximum': string;
+    'adjust': string;
+    'slider': string;
+    'verbSliderPattern': string;
+    'verbInSmallerStepsPattern': string;
+    'verbInLargerStepsPattern': string;
+    'minimum': string;
+    'maximum': string;
+    'jumpToMinimumPattern': string;
+    'jumpToMaximumPattern': string;
+    'generalNavigation': string;
+    'basicActions': string;
+    'moveToNextItem': string;
+    'moveToPreviousItem': string;
+    'moveToNextItemOrGroup': string;
+    'moveToPreviousItemOrGroup': string;
+    'pressButtons': string;
+    'moveBetweenItemsInAGroup': string;
+    'exitADialog': string;
+    'toggleCheckboxes': string;
+    'or': string;
+    'hyphen': string;
+    'grabOrReleaseHeadingPattern': string;
+    'grabOrReleaseLabelPattern': string;
     'comboBox': {
-      'chooseAThingPattern': string,
-      'headingString': string,
-      'popUpListPattern': string,
-      'moveThroughPattern': string,
-      'chooseNewPattern': string,
-      'closeWithoutChanging': string,
-      'options': string,
-      'option': string
+      'chooseAThingPattern': string;
+      'headingString': string;
+      'popUpListPattern': string;
+      'moveThroughPattern': string;
+      'chooseNewPattern': string;
+      'closeWithoutChanging': string;
+      'options': string;
+      'option': string;
     }
-  },
+  };
   'speed': {
-    'normal': string,
-    'slow': string,
-    'fast': string
-  },
+    'normal': string;
+    'slow': string;
+    'fast': string;
+  };
   'symbol': {
-    'ohms': string,
-    'resistivity': string
-  },
+    'ohms': string;
+    'resistivity': string;
+  };
   'comboBoxDisplay': {
-    'valueUnits': string
-  },
-  'wavelengthNMValuePattern': string,
-  'measuringTapeReadoutPattern': string,
-  'wavelength': string,
-  'rulerCapitalized': string,
-  'ruler': string,
-  'zero': string,
-  'one': string,
-  'two': string,
-  'three': string,
-  'four': string,
-  'five': string,
-  'six': string,
-  'seven': string,
-  'eight': string,
-  'nine': string,
-  'ten': string,
+    'valueUnits': string;
+  };
+  'wavelengthNMValuePattern': string;
+  'measuringTapeReadoutPattern': string;
+  'wavelength': string;
+  'rulerCapitalized': string;
+  'ruler': string;
+  'zero': string;
+  'one': string;
+  'two': string;
+  'three': string;
+  'four': string;
+  'five': string;
+  'six': string;
+  'seven': string;
+  'eight': string;
+  'nine': string;
+  'ten': string;
   'a11y': {
     'simSection': {
       'screenSummary': {
-        'multiScreenIntro': string,
-        'singleScreenIntroPattern': string,
-        'keyboardShortcutsHint': string
-      },
-      'playArea': string,
-      'controlArea': string
-    },
+        'multiScreenIntro': string;
+        'singleScreenIntroPattern': string;
+        'keyboardShortcutsHint': string;
+      };
+      'playArea': string;
+      'controlArea': string;
+    };
     'resetAll': {
-      'label': string,
-      'alert': string
-    },
+      'label': string;
+      'alert': string;
+    };
     'soundToggle': {
-      'label': string,
+      'label': string;
       'alert': {
-        'simSoundOn': string,
-        'simSoundOff': string
+        'simSoundOn': string;
+        'simSoundOff': string;
       }
-    },
+    };
     'keyboardHelpDialog': {
       'general': {
-        'tabDescription': string,
-        'shiftTabDescription': string,
-        'tabGroupDescription': string,
-        'shiftTabGroupDescription': string,
-        'pressButtonsDescription': string,
-        'groupNavigationDescription': string,
-        'exitDialogDescription': string,
-        'toggleCheckboxesDescription': string
-      },
+        'tabDescription': string;
+        'shiftTabDescription': string;
+        'tabGroupDescription': string;
+        'shiftTabGroupDescription': string;
+        'pressButtonsDescription': string;
+        'groupNavigationDescription': string;
+        'exitDialogDescription': string;
+        'toggleCheckboxesDescription': string;
+      };
       'slider': {
-        'orKeysPattern': string,
-        'leftRightArrowKeys': string,
-        'upDownArrowKeys': string,
-        'defaultStepsDescriptionPattern': string,
-        'shiftLeftRightArrowKeys': string,
-        'shiftUpDownArrowKeys': string,
-        'smallerStepsDescriptionPattern': string,
-        'largerStepsDescriptionPattern': string,
-        'jumpToMinimumDescriptionPattern': string,
-        'jumpToMaximumDescriptionPattern': string
-      },
-      'grabOrReleaseDescriptionPattern': string,
+        'orKeysPattern': string;
+        'leftRightArrowKeys': string;
+        'upDownArrowKeys': string;
+        'defaultStepsDescriptionPattern': string;
+        'shiftLeftRightArrowKeys': string;
+        'shiftUpDownArrowKeys': string;
+        'smallerStepsDescriptionPattern': string;
+        'largerStepsDescriptionPattern': string;
+        'jumpToMinimumDescriptionPattern': string;
+        'jumpToMaximumDescriptionPattern': string;
+      };
+      'grabOrReleaseDescriptionPattern': string;
       'comboBox': {
-        'popUpListPatternDescription': string,
-        'moveThroughPatternDescription': string,
-        'chooseNewPatternDescription': string,
-        'closeWithoutChangingDescription': string
+        'popUpListPatternDescription': string;
+        'moveThroughPatternDescription': string;
+        'chooseNewPatternDescription': string;
+        'closeWithoutChangingDescription': string;
       }
-    },
+    };
     'playControlButton': {
-      'play': string,
-      'pause': string,
-      'stop': string
-    },
+      'play': string;
+      'pause': string;
+      'stop': string;
+    };
     'stepButton': {
-      'stepForward': string,
-      'playingDescription': string,
-      'pausedDescription': string
-    },
+      'stepForward': string;
+      'playingDescription': string;
+      'pausedDescription': string;
+    };
     'timeControlNode': {
-      'simSpeedDescription': string,
-      'label': string,
-      'simSpeeds': string,
+      'simSpeedDescription': string;
+      'label': string;
+      'simSpeeds': string;
       'playPauseStepButtons': {
-        'playingDescription': string,
-        'playingWithSpeedDescription': string,
-        'pausedDescription': string,
-        'pausedWithSpeedDescription': string
+        'playingDescription': string;
+        'playingWithSpeedDescription': string;
+        'pausedDescription': string;
+        'pausedWithSpeedDescription': string;
       }
-    },
+    };
     'movementAlerter': {
-      'down': string,
-      'left': string,
-      'right': string,
-      'up': string,
-      'upAndToTheRight': string,
-      'upAndToTheLeft': string,
-      'downAndToTheRight': string,
-      'downAndToTheLeft': string,
-      'leftBorderAlert': string,
-      'rightBorderAlert': string,
-      'topBorderAlert': string,
-      'bottomBorderAlert': string
-    },
+      'down': string;
+      'left': string;
+      'right': string;
+      'up': string;
+      'upAndToTheRight': string;
+      'upAndToTheLeft': string;
+      'downAndToTheRight': string;
+      'downAndToTheLeft': string;
+      'leftBorderAlert': string;
+      'rightBorderAlert': string;
+      'topBorderAlert': string;
+      'bottomBorderAlert': string;
+    };
     'grabDrag': {
-      'grabPattern': string,
-      'movable': string,
-      'button': string,
-      'defaultObjectToGrab': string,
-      'released': string,
-      'gestureHelpTextPattern': string,
-      'spaceToGrabOrRelease': string
-    },
+      'grabPattern': string;
+      'movable': string;
+      'button': string;
+      'defaultObjectToGrab': string;
+      'released': string;
+      'gestureHelpTextPattern': string;
+      'spaceToGrabOrRelease': string;
+    };
     'voicing': {
       'simSection': {
         'screenSummary': {
-          'singleScreenIntroPattern': string
+          'singleScreenIntroPattern': string;
         }
-      },
+      };
       'keyboardHelpDialog': {
         'general': {
-          'descriptionPattern': string
+          'descriptionPattern': string;
         }
-      },
-      'grabDragHintPattern': string,
-      'grabbedAlert': string,
-      'draggableAlert': string,
+      };
+      'grabDragHintPattern': string;
+      'grabbedAlert': string;
+      'draggableAlert': string;
       'resetAll': {
-        'contextResponse': string
+        'contextResponse': string;
       }
-    },
-    'close': string
+    };
+    'close': string;
   }
 };
 
