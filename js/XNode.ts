e43343be995f52e2a15d2d97eaa559c80b9757fb
pixelzoom <cmalley@pixelzoom.com>
2022-06-02 14:59:50 -0600

    convert to .ts

--- js/XNode.ts
+++ js/XNode.ts
@@ -1,6 +1,5 @@
 // Copyright 2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * XNode is a specialized view for displaying a 'x'. It is used throughout the sim to indicate the center of mass
  * of a system of Balls. Generalized to appear as a icon as well.
@@ -10,29 +9,42 @@
  * @author Brandon Li
  * @author Alex Schor
  */
+
 import sceneryPhet from './sceneryPhet.js';
 import Dimension2 from '../../dot/js/Dimension2.js';
-import merge from '../../phet-core/js/merge.js';
-import PlusNode from './PlusNode.js';
-class XNode extends PlusNode {
-  /**
-   * @param {Object} [options]
-   */
-  constructor( options ) {
-    options = merge( {
-      legThickness: 6, // {number} - thickness of the legs of the 'x'
-      length: 22,      // {number} - the length of the diagonal of the 'x'.
-      // super-class options
+import PlusNode, { PlusNodeOptions } from './PlusNode.js';
+import optionize from '../../phet-core/js/optionize.js';
+import StrictOmit from '../../phet-core/js/types/StrictOmit.js';
+
+type SelfOptions = {
+
+  // thickness of the legs of the 'x'
+  legThickness?: number;
+
+  // the length of the diagonal of the 'x'.
+  length?: number;
+};
+
+export type XNodeOptions = SelfOptions & StrictOmit<PlusNodeOptions, 'size' | 'rotation'>;
+
+export default class XNode extends PlusNode {
+
+  constructor( providedOptions?: XNodeOptions ) {
+
+    const options = optionize<XNodeOptions, SelfOptions, PlusNodeOptions>()( {
+
+      // XNodeOptions
+      legThickness: 6,
+      length: 22,
+
+      // PlusNodeOptions
       lineWidth: 1.5
-    }, options );
-    //----------------------------------------------------------------------------------------
-    // Set super-class options that cannot be overridden.
-    assert && assert( !options.size, 'XNode sets size' );
-    assert && assert( !options.rotation, 'XNode sets rotation' );
+    }, providedOptions );
+
     options.size = new Dimension2( options.length, options.legThickness );
     options.rotation = Math.PI / 4;
+
     super( options );
   }
 }
 sceneryPhet.register( 'XNode', XNode );
-export default XNode;
