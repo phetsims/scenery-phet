35fb59b28a9ada782745f6ff3fa723506e41bb48
zepumph <michael.kauzmann@colorado.edu>
2022-06-06 17:10:12 -0600

    AnnouncingControlProperty specific for description and another for voicing, https://github.com/phetsims/utterance-queue/issues/82

--- js/accessibility/describers/Alerter.ts
+++ js/accessibility/describers/Alerter.ts
@@ -47,7 +47,7 @@ class Alerter {
   alert( alertable: IAlertable ): void {
     if ( this.alertToVoicing ) {
       assert && assert( alertable instanceof Utterance,
-        'If alerting to Voicing, the alertable needs to be an Utterance and have canAnnounceProperties' );
+        'If alerting to Voicing, the alertable needs to be an Utterance' );
       Voicing.alertUtterance( alertable as Utterance );
     }
 
