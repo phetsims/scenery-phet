f6defc44e6edbedabdc5bf1f43a38479a077f9e8
pixelzoom <cmalley@pixelzoom.com>
2022-06-20 17:25:38 -0600

    add TrashButton to scenery-phet, use in sims, https://github.com/phetsims/scenery-phet/issues/745

---
+++ js/buttons/TrashButton.ts
@@ -0,0 +1,40 @@
+// Copyright 2014-2022, University of Colorado Boulder
+
+/**
+ * TrashButton is a rectangular push button with PhET's standard 'trash can' icon.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+
+import optionize from '../../../phet-core/js/optionize.js';
+import EmptyObjectType from '../../../phet-core/js/types/EmptyObjectType.js';
+import StrictOmit from '../../../phet-core/js/types/StrictOmit.js';
+import { Path, PathOptions } from '../../../scenery/js/imports.js';
+import trashAltRegularShape from '../../../sherpa/js/fontawesome-5/trashAltRegularShape.js';
+import RectangularPushButton, { RectangularPushButtonOptions } from '../../../sun/js/buttons/RectangularPushButton.js';
+import sceneryPhet from '../sceneryPhet.js';
+
+type SelfOptions = {
+  iconOptions?: PathOptions;
+};
+
+export type TrashButtonOptions = SelfOptions & StrictOmit<RectangularPushButtonOptions, 'content'>;
+
+export default class TrashButton extends RectangularPushButton {
+
+  public constructor( providedOptions?: TrashButtonOptions ) {
+
+    const options = optionize<TrashButtonOptions, StrictOmit<SelfOptions, 'iconOptions'>, RectangularPushButtonOptions>()( {
+      // nothing currently customized
+    }, providedOptions );
+
+    options.content = new Path( trashAltRegularShape, optionize<PathOptions, EmptyObjectType, PathOptions>()( {
+      scale: 0.052,
+      fill: 'black'
+    }, options.iconOptions ) );
+
+    super( options );
+  }
+}
+
+sceneryPhet.register( 'TrashButton', TrashButton );
