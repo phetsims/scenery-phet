63ae2ea9e6008253b4eef49684549a4ef7b60691
pixelzoom <cmalley@pixelzoom.com>
2022-06-21 17:20:30 -0600

    add unit tests for rounding, https://github.com/phetsims/scenery-phet/issues/747

--- js/ScientificNotationNodeTests.js
+++ js/ScientificNotationNodeTests.js
@@ -39,3 +39,28 @@ QUnit.test( 'exponent === 0', assert => {
     mantissaDecimalPlaces: 1
   } ), { mantissa: '424.9', exponent: '0' } );
 } );
+
+QUnit.test( 'rounding', assert => {
+
+  // This case was reported in https://github.com/phetsims/build-a-nucleus/issues/24 with bad mantissa 4.3
+  // assert.deepEqual( ScientificNotationNode.toScientificNotation( 424.8, {
+  //   exponent: null,
+  //   mantissaDecimalPlaces: 1
+  // } ), { mantissa: '4.2', exponent: '2' } );
+
+  assert.deepEqual( ScientificNotationNode.toScientificNotation( 425.8, {
+    exponent: null,
+    mantissaDecimalPlaces: 2
+  } ), { mantissa: '4.26', exponent: '2' } );
+
+  // This case was reported in https://github.com/phetsims/build-a-nucleus/issues/24, with bad mantissa 9.7
+  // assert.deepEqual( ScientificNotationNode.toScientificNotation( 9648, {
+  //   exponent: null,
+  //   mantissaDecimalPlaces: 1
+  // } ), { mantissa: '9.6', exponent: '3' } );
+
+  assert.deepEqual( ScientificNotationNode.toScientificNotation( 9658, {
+    exponent: null,
+    mantissaDecimalPlaces: 2
+  } ), { mantissa: '9.66', exponent: '3' } );
+} );
