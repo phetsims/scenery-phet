d8e26570b6436a5af3a2c50e62ef525d6f9d2240
pixelzoom <cmalley@pixelzoom.com>
2022-06-21 17:07:24 -0600

    document unit tests, https://github.com/phetsims/scenery-phet/issues/747

--- js/ScientificNotationNodeTests.js
+++ js/ScientificNotationNodeTests.js
@@ -12,13 +12,13 @@ QUnit.module( 'ScientificNotationNode' );
 
 QUnit.test( 'value === 0', assert => {
 
-  // For zero value, the specified exponent should be used.
+  // For value === 0, the specified exponent should be used.
   assert.deepEqual( ScientificNotationNode.toScientificNotation( 0, {
     exponent: 2,
     mantissaDecimalPlaces: 1
   } ), { mantissa: '0.0', exponent: '2' } );
 
-  // For zero value, if no exponent is requested, exponent should be 0.
+  // For value === 0, if no exponent is requested, exponent should be 0.
   assert.deepEqual( ScientificNotationNode.toScientificNotation( 0, {
     exponent: null,
     mantissaDecimalPlaces: 1
@@ -27,13 +27,13 @@ QUnit.test( 'value === 0', assert => {
 
 QUnit.test( 'exponent === 0', assert => {
 
-  // For exponent zero, we use dot.Utils.toFixed to create the mantissa
+  // For exponent === 0, we use dot.Utils.toFixed to create the mantissa. This example adds zeros to decimal places.
   assert.deepEqual( ScientificNotationNode.toScientificNotation( 424.8, {
     exponent: 0,
     mantissaDecimalPlaces: 3
   } ), { mantissa: '424.800', exponent: '0' } );
 
-  // For exponent zero, we use dot.Utils.toFixed to create the mantissa
+  // For exponent === 0, we use dot.Utils.toFixed to create the mantissa. This example rounds decimal places.
   assert.deepEqual( ScientificNotationNode.toScientificNotation( 424.856, {
     exponent: 0,
     mantissaDecimalPlaces: 1
