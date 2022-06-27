bc281e9603f7f8d02e0ddce31b93c221562f6a9f
pixelzoom <cmalley@pixelzoom.com>
2022-06-27 17:25:38 -0600

    TODO https://github.com/phetsims/scenery-phet/issues/747

--- js/ScientificNotationNode.ts
+++ js/ScientificNotationNode.ts
@@ -198,7 +198,7 @@ export default class ScientificNotationNode extends Node {
     if ( options.exponent !== null ) {
 
       // M x 10^E, where E is options.exponent
-      //TODO https://github.com/phetsims/dot/issues/113 division here can result in floating-point error that affects rounding
+      //TODO https://github.com/phetsims/dot/issues/113 division here and in Utils.toFixed can result in floating-point error that affects rounding
       mantissa = Utils.toFixed( value / Math.pow( 10, options.exponent ), options.mantissaDecimalPlaces );
       exponent = options.exponent.toString();
     }
