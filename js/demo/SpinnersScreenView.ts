3dce22e67a35d54186a2cd9b383afb075942e4de
Marla Schulz <marlaa.schulz@gmail.com>
2022-06-28 16:46:25 -0600

    Move NumberPicker demo to sun, see: https://github.com/phetsims/sun/issues/436

--- js/demo/SpinnersScreenView.ts
+++ js/demo/SpinnersScreenView.ts
@@ -11,7 +11,6 @@
 
 import BooleanProperty from '../../../axon/js/BooleanProperty.js';
 import NumberProperty from '../../../axon/js/NumberProperty.js';
-import Property from '../../../axon/js/Property.js';
 import Bounds2 from '../../../dot/js/Bounds2.js';
 import Range from '../../../dot/js/Range.js';
 import optionize from '../../../phet-core/js/optionize.js';
@@ -22,7 +21,6 @@ import Checkbox from '../../../sun/js/Checkbox.js';
 import DemosScreenView, { DemosScreenViewOptions } from '../../../sun/js/demo/DemosScreenView.js';
 import Tandem from '../../../tandem/js/Tandem.js';
 import FineCoarseSpinner from '../FineCoarseSpinner.js';
-import NumberPicker from '../../../sun/js/NumberPicker.js';
 import PhetFont from '../PhetFont.js';
 import sceneryPhet from '../sceneryPhet.js';
 import sceneryPhetQueryParameters from '../sceneryPhetQueryParameters.js';
@@ -46,8 +44,7 @@ export default class SpinnersScreenView extends DemosScreenView {
        * {string} label - label in the combo box
        * {function(Bounds2): Node} createNode - creates the scene graph for the demo
        */
-      { label: 'FineCoarseSpinner', createNode: demoFineCoarseSpinner },
-      { label: 'NumberPicker', createNode: demoNumberPicker }
+      { label: 'FineCoarseSpinner', createNode: demoFineCoarseSpinner }
     ], options );
   }
 }
@@ -81,25 +78,6 @@ function demoFineCoarseSpinner( layoutBounds: Bounds2, providedOptions?: NodeOpt
   return new VBox( {
     spacing: 60,
     children: [ spinner, checkbox ],
-    center: layoutBounds.center
-  } );
-}
-
-// Creates a demo for NumberPicker
-function demoNumberPicker( layoutBounds: Bounds2 ) {
-
-  const enabledProperty = new BooleanProperty( true );
-
-  const numberPicker = new NumberPicker( new Property( 0 ), new Property( new Range( -10, 10 ) ), {
-    font: new PhetFont( 40 ),
-    enabledProperty: enabledProperty
-  } );
-
-  const enabledCheckbox = new Checkbox( enabledProperty, new Text( 'enabled', { font: new PhetFont( 20 ) } ) );
-
-  return new VBox( {
-    spacing: 40,
-    children: [ numberPicker, enabledCheckbox ],
     center: layoutBounds.center
   } );
 }
