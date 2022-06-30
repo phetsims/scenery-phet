485adacb2446e24ab28ea3dbc52b59ca08398960
pixelzoom <cmalley@pixelzoom.com>
2022-06-30 12:32:28 -0600

    convert ProtractorNode.ts to TypeScript, change the API to use options.visibleProperty and options.angle, fix client usages

--- js/ProtractorNode.ts
+++ js/ProtractorNode.ts
@@ -1,42 +1,47 @@
 // Copyright 2020-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
- * The protractor node is a circular device for measuring angles. In this sim it is used for measuring the angle of the
- * incident, reflected and refracted light.
+ * ProtractorNode is a device for measuring angles.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Chandrashekar Bemagoni (Actual Concepts)
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import NumberProperty from '../../axon/js/NumberProperty.js';
 import Property from '../../axon/js/Property.js';
+import Vector2 from '../../dot/js/Vector2.js';
 import { Shape } from '../../kite/js/imports.js';
-import merge from '../../phet-core/js/merge.js';
+import optionize from '../../phet-core/js/optionize.js';
+import StrictOmit from '../../phet-core/js/types/StrictOmit.js';
 import sceneryPhet from '../../scenery-phet/js/sceneryPhet.js';
 import protractor_png from '../../scenery-phet/mipmaps/protractor_png.js';
-import { DragListener } from '../../scenery/js/imports.js';
-import { Image } from '../../scenery/js/imports.js';
-import { Node } from '../../scenery/js/imports.js';
-import { Path } from '../../scenery/js/imports.js';
+import { DragListener, Image, Node, NodeOptions, Path } from '../../scenery/js/imports.js';
 
-class ProtractorNode extends Node {
+type SelfOptions = {
 
-  /**
-   * @param {Property.<boolean>} showProtractorProperty - controls the protractor visibility
-   * @param {Object} [options]
-   */
-  constructor( showProtractorProperty, options ) {
+  // whether the protractor is rotatable via user interaction
+  rotatable?: boolean;
 
-    options = merge( {
+  // the initial angle of the protractor, in radians
+  angle?: number;
+};
+export type ProtractorNodeOptions = SelfOptions & StrictOmit<NodeOptions, 'children'>;
+
+export default class ProtractorNode extends Node {
+
+  // angle of the protractor, in radians
+  public readonly angleProperty: Property<number>;
+
+  public constructor( providedOptions?: ProtractorNodeOptions ) {
+
+    const options = optionize<ProtractorNodeOptions, SelfOptions, NodeOptions>()( {
+      angle: 0,
       rotatable: false,
       cursor: 'pointer'
-    }, options );
-    assert && assert( !options.children, 'ProtractorNode sets children' );
+    }, providedOptions );
 
     super();
-
-    const showProtractorListener = showProtractorProperty.linkAttribute( this, 'visible' );
 
     // Image
     const protractor_pngNode = new Image( protractor_png, {
@@ -44,10 +49,9 @@ class ProtractorNode extends Node {
     } );
     this.addChild( protractor_pngNode );
 
-    if ( options.rotatable ) {
+    this.angleProperty = new NumberProperty( options.angle );
 
-      // @private
-      this.protractorAngleProperty = new Property( 0.0 );
+    if ( options.rotatable ) {
 
       // Use nicknames for width and height, to make the Shape code easier to understand.
       const w = protractor_pngNode.getWidth();
@@ -69,7 +73,7 @@ class ProtractorNode extends Node {
       this.addChild( outerRingPath );
 
       // Rotate the protractor when its outer ring is dragged.
-      let start;
+      let start: Vector2;
       outerRingPath.addInputListener( new DragListener( {
         start: event => {
           start = this.globalToParentPoint( event.pointer.point );
@@ -84,52 +88,30 @@ class ProtractorNode extends Node {
           const angle = Math.atan2( centerY - end.y, centerX - end.x );
 
           // rotate the protractor model
-          this.protractorAngleProperty.value += angle - startAngle;
+          this.angleProperty.value += angle - startAngle;
           start = end;
         }
       } ) );
 
       // Rotate to match the protractor angle
-      this.protractorAngleProperty.link( angle => {
+      this.angleProperty.link( angle => {
         this.rotateAround( this.center, angle - this.getRotation() );
       } );
     }
 
-    // @private
-    this.disposeProtractorNode = () => {
-      showProtractorProperty.unlinkAttribute( showProtractorListener );
-    };
-
     this.mutate( options );
   }
 
-  /**
-   * @public
-   * @override
-   */
-  dispose() {
-    this.disposeProtractorNode();
-    super.dispose();
-  }
-
-  /**
-   * @public
-   */
-  reset() {
-    this.protractorAngleProperty && this.protractorAngleProperty.reset();
+  public reset(): void {
+    this.angleProperty.reset();
   }
 
   /**
    * Creates an icon, to be used for toolboxes, checkboxes, etc.
-   * @param {Object} [options] - options to scenery.Node
-   * @returns {Node}
-   * @public
    */
-  static createIcon( options ) {
+  public static createIcon( options: NodeOptions ): Node {
     return new Image( protractor_png, options );
   }
 }
 
 sceneryPhet.register( 'ProtractorNode', ProtractorNode );
-
-export default ProtractorNode;
