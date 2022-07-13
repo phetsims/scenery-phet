5120e61614fce1cefc86dc9716ecc69807b33e32
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2022-07-13 17:50:29 -0600

    Convert ReturnButton.ts contents to typescript. See https://github.com/phetsims/fractions-common/issues/116.

--- js/buttons/ReturnButton.ts
+++ js/buttons/ReturnButton.ts
@@ -1,26 +1,37 @@
 // Copyright 2018-2022, University of Colorado Boulder
 
 /**
- * Button for group nodes that returns a piece from the group to the panel it came from.
+ * Button with a back arrow.
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ * @author Chris Klusendorf (PhET Interactive Simulations)
+ * @author Luisa Vargas
  */
 
-import { Shape } from '../../../kite/js/imports.ts';
-import merge from '../../../phet-core/js/merge.ts';
-import { Path } from '../../../scenery/js/imports.ts';
-import RectangularPushButton from '../../../sun/js/buttons/RectangularPushButton.ts';
-import FractionsCommonColors from '../../../fractions-common/js/common/view/FractionsCommonColors.js';
-import fractionsCommon from '../../../fractions-common/js/fractionsCommon.js';
+import EmptyObjectType from '../../../phet-core/js/types/EmptyObjectType.js';
+import optionize from '../../../phet-core/js/optionize.js';
+import RectangularPushButton, { RectangularPushButtonOptions } from '../../../sun/js/buttons/RectangularPushButton.js';
+import { Color, Path } from '../../../scenery/js/imports.js';
+import StrictOmit from '../../../phet-core/js/types/StrictOmit.js';
+import sceneryPhet from '../sceneryPhet.js';
+import { Shape } from '../../../kite/js/imports.js';
 
 const ICON_HEIGHT = 17;
 
+type SelfOptions = EmptyObjectType;
+type ReturnButtonSelfOptions = SelfOptions & StrictOmit<RectangularPushButtonOptions, 'content'>;
+
 class ReturnButton extends RectangularPushButton {
-  /**
-   * @param {function} listener
-   * @param {Object} [options]
-   */
-  constructor( listener, options ) {
+
+  public constructor( listener: () => void, providedOptions?: ReturnButtonSelfOptions ) {
+
+    const options = optionize<ReturnButtonSelfOptions, SelfOptions, RectangularPushButtonOptions>()( {
+      xMargin: 5,
+      yMargin: 5,
+      baseColor: Color.YELLOW,
+      listener: listener
+    }, providedOptions );
+
     const undoArrowShape = new Shape()
       .moveTo( 0, 0 )
       .lineTo( 0, ICON_HEIGHT )
@@ -30,20 +41,16 @@ class ReturnButton extends RectangularPushButton {
       .quadraticCurveTo( ICON_HEIGHT * 1.25, -ICON_HEIGHT * 0.5, ICON_HEIGHT * 0.3, ICON_HEIGHT * 0.3 )
       .close();
 
-    options = merge( {
-      content: new Path( undoArrowShape, {
-        fill: 'black',
-        scale: 0.7
-      } ),
-      xMargin: 5,
-      yMargin: 5,
-      baseColor: FractionsCommonColors.undoButtonProperty,
-      listener: listener
-    }, options );
+    const undoArrowPath = new Path( undoArrowShape, {
+      fill: 'black',
+      scale: 0.7
+    } );
+
+    options.content = undoArrowPath;
 
     super( options );
   }
 }
 
-fractionsCommon.register( 'ReturnButton', ReturnButton );
+sceneryPhet.register( 'ReturnButton', ReturnButton );
 export default ReturnButton;
