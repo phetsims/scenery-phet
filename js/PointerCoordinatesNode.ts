697aa013d83b6c5af9b7cbf328bfae5137444434
Sam Reid <reids@colorado.edu>
2022-07-20 11:31:25 -0600

    Disallow type arguments that are equal to the default, see https://github.com/phetsims/chipper/issues/1277

--- js/PointerCoordinatesNode.ts
+++ js/PointerCoordinatesNode.ts
@@ -11,7 +11,6 @@
 import Utils from '../../dot/js/Utils.js';
 import getGlobal from '../../phet-core/js/getGlobal.js';
 import optionize from '../../phet-core/js/optionize.js';
-import EmptyObjectType from '../../phet-core/js/types/EmptyObjectType.js';
 import ModelViewTransform2 from '../../phetcommon/js/view/ModelViewTransform2.js';
 import { Display, Font, IColor, Node, Rectangle, RichText, RichTextAlign, SceneryEvent } from '../../scenery/js/imports.js';
 import PhetFont from './PhetFont.js';
@@ -45,7 +44,7 @@ export default class PointerCoordinatesNode extends Node {
    */
   public constructor( modelViewTransform: ModelViewTransform2, providedOptions?: PointerCoordinatesNodeOptions ) {
 
-    const options = optionize<PointerCoordinatesNodeOptions, SelfOptions, EmptyObjectType>()( {
+    const options = optionize<PointerCoordinatesNodeOptions, SelfOptions>()( {
       display: getGlobal( 'phet.joist.display' ),
       pickable: false,
       font: DEFAULT_FONT,
