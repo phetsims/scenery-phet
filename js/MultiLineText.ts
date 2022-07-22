9a489f2627ffc110bd4309eacdc389f446ea21d6
pixelzoom <cmalley@pixelzoom.com>
2022-07-22 16:49:38 -0600

    convert to TS

--- js/MultiLineText.ts
+++ js/MultiLineText.ts
@@ -1,6 +1,5 @@
 // Copyright 2013-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * DO NOT USE IN NEW DEVELOPMENT - see deprecation notice below
  *
@@ -8,15 +7,21 @@
  * Lines are separated with the newline character '\n', which will be converted to '<br>'.
  * This was reimplemented as a subclass of RichText, see https://github.com/phetsims/scenery-phet/issues/392.
  *
+ * NOTE: While PhET does not typically convert deprecated code to TypeScript, this class is unlikely to go away,
+ * due to the cost of modifying translations. So it was converted to TypeScript on 7/22/2022.
+ *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
 import deprecationWarning from '../../phet-core/js/deprecationWarning.js';
-import merge from '../../phet-core/js/merge.js';
-import { RichText } from '../../scenery/js/imports.js';
+import optionize, { EmptySelfOptions } from '../../phet-core/js/optionize.js';
+import { RichText, RichTextOptions } from '../../scenery/js/imports.js';
 import PhetFont from './PhetFont.js';
 import sceneryPhet from './sceneryPhet.js';
+
+type SelfOptions = EmptySelfOptions;
+export type MultiLineTextOptions = SelfOptions & RichTextOptions;
 
 /**
  * @deprecated please use SCENERY/nodes/RichText
@@ -24,40 +29,35 @@ import sceneryPhet from './sceneryPhet.js';
 class MultiLineText extends RichText {
 
   /**
-   * @param {string} text - newlines will be replaced with '<br>'.
-   * @param {Object} [options]
+   * @param text - newlines will be replaced with '<br>'.
+   * @param [providedOptions]
    */
-  constructor( text, options ) {
+  public constructor( text: string, providedOptions: MultiLineTextOptions ) {
     assert && deprecationWarning( 'MultiLineText is deprecated, please use RichText instead' );
 
-    options = merge( {
+    const options = optionize<MultiLineTextOptions, SelfOptions, RichTextOptions>()( {
 
       // RichText options
       align: 'center',
       font: new PhetFont()
-    }, options );
+    }, providedOptions );
 
     super( replaceNewlines( text ), options );
   }
 
   /**
    * Sets the text, replacing newlines with '<br>'.
-   * @param {string} text
-   * @public
-   * @override
    */
-  setText( text ) {
-    super.setText( replaceNewlines( text ) );
+  public override setText( text: string | number ): this {
+    return super.setText( replaceNewlines( text ) );
   }
 }
 
 /**
  * Replaces newline characters with '<br>'.
- * @param {string} text
- * @returns {string}
  */
-function replaceNewlines( text ) {
-  return text.replace( /\n/g, '<br>' );
+function replaceNewlines( text: string | number ): string {
+  return text.toString().replace( /\n/g, '<br>' );
 }
 
 sceneryPhet.register( 'MultiLineText', MultiLineText );
