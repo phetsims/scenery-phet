cd59a1ea741f1e71c0e3b3551c3fe2198dc4f4de
Jesse <jesse.greenberg@colorado.edu>
2022-08-03 17:23:36 -0400

    implement options and get rid of anys for https://github.com/phetsims/greenhouse-effect/issues/194

--- js/keyboard/help/TimingControlsKeyboardHelpSection.ts
+++ js/keyboard/help/TimingControlsKeyboardHelpSection.ts
@@ -2,12 +2,13 @@
 
 /**
  * Help section that explains how to use a keyboard to toggle play/pause and timing controls.
+ *
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
-import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
+import optionize from '../../../../phet-core/js/optionize.js';
 import sceneryPhet from '../../sceneryPhet.js';
-import KeyboardHelpSection from './KeyboardHelpSection.js';
+import KeyboardHelpSection, { KeyboardHelpSectionOptions } from './KeyboardHelpSection.js';
 
 // strings for this are not translatable until they have been reviewed, see
 // https://github.com/phetsims/greenhouse-effect/issues/194
@@ -15,15 +16,24 @@ const timingControlsString = 'Timing Controls';
 const pauseOrPlayActionString = 'Pause or play action';
 const pauseOrPlayActionDescriptionString = 'Pause or play action with Alt key plus K';
 
+type SelfOptions = {
+
+  // The heading string for this section of keyboard help content
+  headingString?: string;
+
+  // Visible string that describes the action of pause/play from a key command. You may want sim-specific terminology
+  // for this command.
+  pauseOrPlayActionString?: string;
+
+  // String for the PDOM (screen readers) that describes the hotkeys for play/pause.
+  pauseOrPlayActionDescriptionString?: string;
+};
+type ParentOptions = KeyboardHelpSectionOptions;
+export type TimingControlsKeyboardHelpSectionOptions = SelfOptions & ParentOptions;
+
 class TimingControlsKeyboardHelpSection extends KeyboardHelpSection {
-
-  // TODO https://github.com/phetsims/scenery-phet/issues/762
-  // eslint-disable-next-line
-  public constructor( providedOptions?: any ) {
-
-    // TODO https://github.com/phetsims/scenery-phet/issues/762
-    // eslint-disable-next-line
-    const options = optionize<any, EmptySelfOptions, any>()( {
+  public constructor( providedOptions?: TimingControlsKeyboardHelpSectionOptions ) {
+    const options = optionize<TimingControlsKeyboardHelpSectionOptions, SelfOptions, ParentOptions>()( {
       headingString: timingControlsString,
       pauseOrPlayActionString: pauseOrPlayActionString,
       pauseOrPlayActionDescriptionString: pauseOrPlayActionDescriptionString
