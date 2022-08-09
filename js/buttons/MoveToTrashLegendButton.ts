2b767eaed6f1f531c19f1774685f2f811dd87792
phet-dev <phetadmin@gmail.com>
2022-08-09 05:33:49 -0600

    update copyright dates from daily grunt work

--- js/buttons/MoveToTrashLegendButton.ts
+++ js/buttons/MoveToTrashLegendButton.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2022, University of Colorado Boulder
+// Copyright 2022, University of Colorado Boulder
 
 /**
  * When a MoveToTrashLegendButton appears in a legend or as part of a bar chart label, typically it is smaller,
