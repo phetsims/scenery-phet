d3352dde6ff136615ac040fc9fada48a8f0509b0
Jonathan Olson <jonathan.olson@colorado.edu>
2022-09-13 10:53:13 -0600

    Made ShadedSphereNode update the gradient on radius size changes

--- js/ShadedSphereNode.ts
+++ js/ShadedSphereNode.ts
@@ -7,6 +7,7 @@
  */
 
 import optionize from '../../phet-core/js/optionize.js';
+import StrictOmit from '../../phet-core/js/types/StrictOmit.js';
 import { Circle, CircleOptions, TColor, RadialGradient } from '../../scenery/js/imports.js';
 import sceneryPhet from './sceneryPhet.js';
 
@@ -14,14 +15,16 @@ type SelfOptions = {
   mainColor?: TColor;
   highlightColor?: TColor;
   shadowColor?: TColor;
-  highlightDiameter?: number;
+  highlightDiameterRatio?: number;
   highlightXOffset?: number; // x-offset of the highlight from the center of the sphere, percentage of radius, [-1,1]
   highlightYOffset?: number; // y-offset of the highlight from the center of the sphere, percentage of radius, [-1,1]
 };
 
-export type ShadedSphereNodeOptions = SelfOptions & CircleOptions;
+export type ShadedSphereNodeOptions = SelfOptions & StrictOmit<CircleOptions, 'fill'>;
 
 export default class ShadedSphereNode extends Circle {
+
+  private readonly updateShadedSphereFill: () => void;
 
   public constructor( diameter: number, providedOptions?: ShadedSphereNodeOptions ) {
 
@@ -31,28 +34,40 @@ export default class ShadedSphereNode extends Circle {
       mainColor: 'gray',
       highlightColor: 'white',
       shadowColor: 'black',
-      highlightDiameter: 0.5 * diameter,
+      highlightDiameterRatio: 0.5,
       highlightXOffset: -0.4,
       highlightYOffset: -0.4
     }, providedOptions );
 
     // validate option values
-    assert && assert( options.highlightDiameter < diameter,
-      `highlightDiameter must be < diameter: ${options.highlightDiameter}` );
+    assert && assert( options.highlightDiameterRatio < 1,
+      `highlightDiameterRatio must be < diameter: ${options.highlightDiameterRatio}` );
     assert && assert( options.highlightXOffset >= -1 && options.highlightXOffset <= 1,
       `highlightXOffset out of range: ${options.highlightXOffset}` );
     assert && assert( options.highlightYOffset >= -1 && options.highlightYOffset <= 1,
       `highlightYOffset out of range: ${options.highlightYOffset}` );
 
-    const radius = diameter / 2;
-    const highlightX = radius * options.highlightXOffset;
-    const highlightY = radius * options.highlightYOffset;
-    options.fill = new RadialGradient( highlightX, highlightY, 0, highlightX, highlightY, diameter )
-      .addColorStop( 0, options.highlightColor )
-      .addColorStop( options.highlightDiameter / diameter, options.mainColor )
-      .addColorStop( 1, options.shadowColor );
+    super( diameter / 2, options );
 
-    super( radius, options );
+    this.updateShadedSphereFill = () => {
+      const radius = this.radius;
+      const highlightX = radius * options.highlightXOffset;
+      const highlightY = radius * options.highlightYOffset;
+      this.fill = new RadialGradient( highlightX, highlightY, 0, highlightX, highlightY, radius * 2 )
+        .addColorStop( 0, options.highlightColor )
+        .addColorStop( options.highlightDiameterRatio, options.mainColor )
+        .addColorStop( 1, options.shadowColor );
+    };
+
+    this.updateShadedSphereFill();
+  }
+
+  protected override invalidatePath(): void {
+    super.invalidatePath();
+
+    // Called during the super() call, so we may not be defined yet. We'll call this during the constructor
+    // manually.
+    this.updateShadedSphereFill && this.updateShadedSphereFill();
   }
 }
 
