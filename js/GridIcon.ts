43e81b257efab9fc241399b14eaee63e3efafce1
pixelzoom <cmalley@pixelzoom.com>
2022-10-16 08:42:04 -0400

    generalize GridIcon, https://github.com/phetsims/scenery-phet/issues/777

--- js/GridIcon.ts
+++ js/GridIcon.ts
@@ -1,7 +1,7 @@
 // Copyright 2022, University of Colorado Boulder
 
 /**
- * GridIcon is the icon for a 4x4 grid.
+ * GridIcon is the icon for an NxN grid of square cells.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
@@ -11,7 +11,8 @@ import { Path, PathOptions } from '../../scenery/js/imports.js';
 import { Shape } from '../../kite/js/imports.js';
 
 type SelfOptions = {
-  iconSize?: number;
+  size?: number; // dimensions of the icon, same for width and height
+  numberOfCells?: number; // number of cells in the grid, same for rows and columns
 };
 
 export type GridIconOptions = SelfOptions & PathOptions;
@@ -23,38 +24,33 @@ export default class GridIcon extends Path {
     const options = optionize<GridIconOptions, SelfOptions, PathOptions>()( {
 
       // SelfOptions
-      iconSize: 30,
+      size: 30,
+      numberOfCells: 4,
 
       // PathOptions
       stroke: 'rgb( 100, 100, 100 )',
       lineWidth: 1
     }, providedOptions );
 
-    const iconSize = options.iconSize;
+    assert && assert( options.size > 0, `invalid size: ${options.size}` );
+    assert && assert( Number.isInteger( options.numberOfCells ) && options.numberOfCells > 2,
+      `invalid numberOfCells: ${options.numberOfCells}` );
 
-    // The grid shape looks approximately like this:
-    //
-    //      |   |   |
-    //   ---|---|---|---
-    //      |   |   |
-    //   ---|---|---|---
-    //      |   |   |
-    //   ---|---|---|---
-    //      |   |   |
-    //
-    const shape = new Shape()
-      .moveTo( iconSize / 4, 0 )
-      .lineTo( iconSize / 4, iconSize )
-      .moveTo( iconSize / 2, 0 )
-      .lineTo( iconSize / 2, iconSize )
-      .moveTo( iconSize * 3 / 4, 0 )
-      .lineTo( iconSize * 3 / 4, iconSize )
-      .moveTo( 0, iconSize / 4 )
-      .lineTo( iconSize, iconSize / 4 )
-      .moveTo( 0, iconSize / 2 )
-      .lineTo( iconSize, iconSize / 2 )
-      .moveTo( 0, iconSize * 3 / 4 )
-      .lineTo( iconSize, iconSize * 3 / 4 );
+    const shape = new Shape();
+
+    // vertical lines
+    for ( let column = 1; column < options.numberOfCells; column++ ) {
+      const x = ( column / options.numberOfCells ) * options.size;
+      shape.moveTo( x, 0 );
+      shape.lineTo( x, options.size );
+    }
+
+    // horizontal lines
+    for ( let row = 1; row < options.numberOfCells; row++ ) {
+      const y = ( row / options.numberOfCells ) * options.size;
+      shape.moveTo( 0, y );
+      shape.lineTo( options.size, y );
+    }
 
     super( shape, options );
   }
