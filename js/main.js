4dcc5f577ab9e6056bcf35c3b077fb9a60a6813f
Jonathan Olson <jonathan.olson@colorado.edu>
2022-10-24 18:40:24 -0600

    Adding phet-lib, see https://github.com/phetsims/scenery/issues/1474

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Copyright 2022, University of Colorado Boulder
+
+import sceneryPhet from './sceneryPhet.js';
+import './ArrowNode.js';
+import './FaceNode.js';
+import './GradientRectangle.js';
+import './GridNode.js';
+import './NumberControl.js';
+import './NumberDisplay.js';
+import './PhetFont.js';
+import './ShadedRectangle.js';
+import './ShadedSphereNode.js';
+
+export default sceneryPhet;
