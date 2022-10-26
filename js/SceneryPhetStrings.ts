e696b762dac481c31ee45e21b16d18b638f29556
pixelzoom <cmalley@pixelzoom.com>
2022-10-25 18:10:36 -0600

    "supportsInteractiveDescription": true, grunt modulify, https://github.com/phetsims/scenery-phet/issues/780

--- js/SceneryPhetStrings.ts
+++ js/SceneryPhetStrings.ts
@@ -10,379 +10,222 @@ import sceneryPhet from './sceneryPhet.js';
 
 type StringsType = {
   'scenery-phet': {
-    'title': string;
     'titleStringProperty': LinkableProperty<string>;
   };
   'WavelengthSlider': {
-    'pattern_0wavelength_1units': string;
     'pattern_0wavelength_1unitsStringProperty': LinkableProperty<string>;
   };
-  'frequencyUnitsPattern': string;
   'frequencyUnitsPatternStringProperty': LinkableProperty<string>;
-  'stopwatchValueUnitsPattern': string;
   'stopwatchValueUnitsPatternStringProperty': LinkableProperty<string>;
-  'units_nm': string;
   'units_nmStringProperty': LinkableProperty<string>;
-  'shortCircuit': string;
   'shortCircuitStringProperty': LinkableProperty<string>;
-  'heat': string;
   'heatStringProperty': LinkableProperty<string>;
-  'cool': string;
   'coolStringProperty': LinkableProperty<string>;
   'key': {
-    'tab': string;
     'tabStringProperty': LinkableProperty<string>;
-    'shift': string;
     'shiftStringProperty': LinkableProperty<string>;
-    'alt': string;
     'altStringProperty': LinkableProperty<string>;
-    'k': string;
     'kStringProperty': LinkableProperty<string>;
-    'l': string;
     'lStringProperty': LinkableProperty<string>;
-    'capsLock': string;
     'capsLockStringProperty': LinkableProperty<string>;
-    'enter': string;
     'enterStringProperty': LinkableProperty<string>;
-    'space': string;
     'spaceStringProperty': LinkableProperty<string>;
-    'esc': string;
     'escStringProperty': LinkableProperty<string>;
-    'fn': string;
     'fnStringProperty': LinkableProperty<string>;
-    'pageUp': string;
     'pageUpStringProperty': LinkableProperty<string>;
-    'pageDown': string;
     'pageDownStringProperty': LinkableProperty<string>;
-    'home': string;
     'homeStringProperty': LinkableProperty<string>;
-    'end': string;
     'endStringProperty': LinkableProperty<string>;
-    'toGrabOrRelease': string;
     'toGrabOrReleaseStringProperty': LinkableProperty<string>;
   };
   'webglWarning': {
-    'title': string;
     'titleStringProperty': LinkableProperty<string>;
-    'body': string;
     'bodyStringProperty': LinkableProperty<string>;
-    'contextLossFailure': string;
     'contextLossFailureStringProperty': LinkableProperty<string>;
-    'contextLossReload': string;
     'contextLossReloadStringProperty': LinkableProperty<string>;
-    'ie11StencilBody': string;
     'ie11StencilBodyStringProperty': LinkableProperty<string>;
   };
   'keyboardHelpDialog': {
-    'sliderControls': string;
     'sliderControlsStringProperty': LinkableProperty<string>;
-    'adjustSlider': string;
     'adjustSliderStringProperty': LinkableProperty<string>;
-    'adjustInSmallerSteps': string;
     'adjustInSmallerStepsStringProperty': LinkableProperty<string>;
-    'adjustInLargerSteps': string;
     'adjustInLargerStepsStringProperty': LinkableProperty<string>;
-    'jumpToMinimum': string;
     'jumpToMinimumStringProperty': LinkableProperty<string>;
-    'jumpToMaximum': string;
     'jumpToMaximumStringProperty': LinkableProperty<string>;
-    'adjust': string;
     'adjustStringProperty': LinkableProperty<string>;
-    'slider': string;
     'sliderStringProperty': LinkableProperty<string>;
-    'verbSliderPattern': string;
     'verbSliderPatternStringProperty': LinkableProperty<string>;
-    'verbInSmallerStepsPattern': string;
     'verbInSmallerStepsPatternStringProperty': LinkableProperty<string>;
-    'verbInLargerStepsPattern': string;
     'verbInLargerStepsPatternStringProperty': LinkableProperty<string>;
-    'minimum': string;
     'minimumStringProperty': LinkableProperty<string>;
-    'maximum': string;
     'maximumStringProperty': LinkableProperty<string>;
-    'jumpToMinimumPattern': string;
     'jumpToMinimumPatternStringProperty': LinkableProperty<string>;
-    'jumpToMaximumPattern': string;
     'jumpToMaximumPatternStringProperty': LinkableProperty<string>;
-    'generalNavigation': string;
     'generalNavigationStringProperty': LinkableProperty<string>;
-    'basicActions': string;
     'basicActionsStringProperty': LinkableProperty<string>;
-    'moveToNextItem': string;
     'moveToNextItemStringProperty': LinkableProperty<string>;
-    'moveToPreviousItem': string;
     'moveToPreviousItemStringProperty': LinkableProperty<string>;
-    'moveToNextItemOrGroup': string;
     'moveToNextItemOrGroupStringProperty': LinkableProperty<string>;
-    'moveToPreviousItemOrGroup': string;
     'moveToPreviousItemOrGroupStringProperty': LinkableProperty<string>;
-    'pressButtons': string;
     'pressButtonsStringProperty': LinkableProperty<string>;
-    'moveBetweenItemsInAGroup': string;
     'moveBetweenItemsInAGroupStringProperty': LinkableProperty<string>;
-    'exitADialog': string;
     'exitADialogStringProperty': LinkableProperty<string>;
-    'toggleCheckboxes': string;
     'toggleCheckboxesStringProperty': LinkableProperty<string>;
-    'or': string;
     'orStringProperty': LinkableProperty<string>;
-    'hyphen': string;
     'hyphenStringProperty': LinkableProperty<string>;
-    'grabOrReleaseHeadingPattern': string;
     'grabOrReleaseHeadingPatternStringProperty': LinkableProperty<string>;
-    'grabOrReleaseLabelPattern': string;
     'grabOrReleaseLabelPatternStringProperty': LinkableProperty<string>;
     'comboBox': {
-      'chooseAThingPattern': string;
       'chooseAThingPatternStringProperty': LinkableProperty<string>;
-      'headingString': string;
       'headingStringStringProperty': LinkableProperty<string>;
-      'popUpListPattern': string;
       'popUpListPatternStringProperty': LinkableProperty<string>;
-      'moveThroughPattern': string;
       'moveThroughPatternStringProperty': LinkableProperty<string>;
-      'chooseNewPattern': string;
       'chooseNewPatternStringProperty': LinkableProperty<string>;
-      'closeWithoutChanging': string;
       'closeWithoutChangingStringProperty': LinkableProperty<string>;
-      'options': string;
       'optionsStringProperty': LinkableProperty<string>;
-      'option': string;
       'optionStringProperty': LinkableProperty<string>;
     }
   };
   'speed': {
-    'normal': string;
     'normalStringProperty': LinkableProperty<string>;
-    'slow': string;
     'slowStringProperty': LinkableProperty<string>;
-    'fast': string;
     'fastStringProperty': LinkableProperty<string>;
   };
   'symbol': {
-    'ohms': string;
     'ohmsStringProperty': LinkableProperty<string>;
-    'resistivity': string;
     'resistivityStringProperty': LinkableProperty<string>;
   };
   'comboBoxDisplay': {
-    'valueUnits': string;
     'valueUnitsStringProperty': LinkableProperty<string>;
   };
-  'wavelengthNMValuePattern': string;
   'wavelengthNMValuePatternStringProperty': LinkableProperty<string>;
-  'measuringTapeReadoutPattern': string;
   'measuringTapeReadoutPatternStringProperty': LinkableProperty<string>;
-  'wavelength': string;
   'wavelengthStringProperty': LinkableProperty<string>;
-  'rulerCapitalized': string;
   'rulerCapitalizedStringProperty': LinkableProperty<string>;
-  'ruler': string;
   'rulerStringProperty': LinkableProperty<string>;
-  'zero': string;
   'zeroStringProperty': LinkableProperty<string>;
-  'one': string;
   'oneStringProperty': LinkableProperty<string>;
-  'two': string;
   'twoStringProperty': LinkableProperty<string>;
-  'three': string;
   'threeStringProperty': LinkableProperty<string>;
-  'four': string;
   'fourStringProperty': LinkableProperty<string>;
-  'five': string;
   'fiveStringProperty': LinkableProperty<string>;
-  'six': string;
   'sixStringProperty': LinkableProperty<string>;
-  'seven': string;
   'sevenStringProperty': LinkableProperty<string>;
-  'eight': string;
   'eightStringProperty': LinkableProperty<string>;
-  'nine': string;
   'nineStringProperty': LinkableProperty<string>;
-  'ten': string;
   'tenStringProperty': LinkableProperty<string>;
   'a11y': {
     'simSection': {
       'screenSummary': {
-        'multiScreenIntro': string;
         'multiScreenIntroStringProperty': LinkableProperty<string>;
-        'singleScreenIntroPattern': string;
         'singleScreenIntroPatternStringProperty': LinkableProperty<string>;
-        'keyboardShortcutsHint': string;
         'keyboardShortcutsHintStringProperty': LinkableProperty<string>;
       };
-      'playArea': string;
       'playAreaStringProperty': LinkableProperty<string>;
-      'controlArea': string;
       'controlAreaStringProperty': LinkableProperty<string>;
     };
     'resetAll': {
-      'label': string;
       'labelStringProperty': LinkableProperty<string>;
-      'alert': string;
       'alertStringProperty': LinkableProperty<string>;
     };
     'soundToggle': {
-      'label': string;
       'labelStringProperty': LinkableProperty<string>;
       'alert': {
-        'simSoundOn': string;
         'simSoundOnStringProperty': LinkableProperty<string>;
-        'simSoundOff': string;
         'simSoundOffStringProperty': LinkableProperty<string>;
       }
     };
     'keyboardHelpDialog': {
       'general': {
-        'tabDescription': string;
         'tabDescriptionStringProperty': LinkableProperty<string>;
-        'shiftTabDescription': string;
         'shiftTabDescriptionStringProperty': LinkableProperty<string>;
-        'tabGroupDescription': string;
         'tabGroupDescriptionStringProperty': LinkableProperty<string>;
-        'shiftTabGroupDescription': string;
         'shiftTabGroupDescriptionStringProperty': LinkableProperty<string>;
-        'pressButtonsDescription': string;
         'pressButtonsDescriptionStringProperty': LinkableProperty<string>;
-        'groupNavigationDescription': string;
         'groupNavigationDescriptionStringProperty': LinkableProperty<string>;
-        'exitDialogDescription': string;
         'exitDialogDescriptionStringProperty': LinkableProperty<string>;
-        'toggleCheckboxesDescription': string;
         'toggleCheckboxesDescriptionStringProperty': LinkableProperty<string>;
       };
       'slider': {
-        'orKeysPattern': string;
         'orKeysPatternStringProperty': LinkableProperty<string>;
-        'leftRightArrowKeys': string;
         'leftRightArrowKeysStringProperty': LinkableProperty<string>;
-        'upDownArrowKeys': string;
         'upDownArrowKeysStringProperty': LinkableProperty<string>;
-        'defaultStepsDescriptionPattern': string;
         'defaultStepsDescriptionPatternStringProperty': LinkableProperty<string>;
-        'shiftLeftRightArrowKeys': string;
         'shiftLeftRightArrowKeysStringProperty': LinkableProperty<string>;
-        'shiftUpDownArrowKeys': string;
         'shiftUpDownArrowKeysStringProperty': LinkableProperty<string>;
-        'smallerStepsDescriptionPattern': string;
         'smallerStepsDescriptionPatternStringProperty': LinkableProperty<string>;
-        'largerStepsDescriptionPattern': string;
         'largerStepsDescriptionPatternStringProperty': LinkableProperty<string>;
-        'jumpToMinimumDescriptionPattern': string;
         'jumpToMinimumDescriptionPatternStringProperty': LinkableProperty<string>;
-        'jumpToMaximumDescriptionPattern': string;
         'jumpToMaximumDescriptionPatternStringProperty': LinkableProperty<string>;
       };
-      'grabOrReleaseDescriptionPattern': string;
       'grabOrReleaseDescriptionPatternStringProperty': LinkableProperty<string>;
       'comboBox': {
-        'popUpListPatternDescription': string;
         'popUpListPatternDescriptionStringProperty': LinkableProperty<string>;
-        'moveThroughPatternDescription': string;
         'moveThroughPatternDescriptionStringProperty': LinkableProperty<string>;
-        'chooseNewPatternDescription': string;
         'chooseNewPatternDescriptionStringProperty': LinkableProperty<string>;
-        'closeWithoutChangingDescription': string;
         'closeWithoutChangingDescriptionStringProperty': LinkableProperty<string>;
       }
     };
     'playControlButton': {
-      'play': string;
       'playStringProperty': LinkableProperty<string>;
-      'pause': string;
       'pauseStringProperty': LinkableProperty<string>;
-      'stop': string;
       'stopStringProperty': LinkableProperty<string>;
     };
     'stepButton': {
-      'stepForward': string;
       'stepForwardStringProperty': LinkableProperty<string>;
-      'playingDescription': string;
       'playingDescriptionStringProperty': LinkableProperty<string>;
-      'pausedDescription': string;
       'pausedDescriptionStringProperty': LinkableProperty<string>;
     };
     'timeControlNode': {
-      'simSpeedDescription': string;
       'simSpeedDescriptionStringProperty': LinkableProperty<string>;
-      'label': string;
       'labelStringProperty': LinkableProperty<string>;
-      'simSpeeds': string;
       'simSpeedsStringProperty': LinkableProperty<string>;
     };
     'playPauseStepButtonGroup': {
-      'playingDescription': string;
       'playingDescriptionStringProperty': LinkableProperty<string>;
-      'pausedDescription': string;
       'pausedDescriptionStringProperty': LinkableProperty<string>;
     };
     'movementAlerter': {
-      'down': string;
       'downStringProperty': LinkableProperty<string>;
-      'left': string;
       'leftStringProperty': LinkableProperty<string>;
-      'right': string;
       'rightStringProperty': LinkableProperty<string>;
-      'up': string;
       'upStringProperty': LinkableProperty<string>;
-      'upAndToTheRight': string;
       'upAndToTheRightStringProperty': LinkableProperty<string>;
-      'upAndToTheLeft': string;
       'upAndToTheLeftStringProperty': LinkableProperty<string>;
-      'downAndToTheRight': string;
       'downAndToTheRightStringProperty': LinkableProperty<string>;
-      'downAndToTheLeft': string;
       'downAndToTheLeftStringProperty': LinkableProperty<string>;
-      'leftBorderAlert': string;
       'leftBorderAlertStringProperty': LinkableProperty<string>;
-      'rightBorderAlert': string;
       'rightBorderAlertStringProperty': LinkableProperty<string>;
-      'topBorderAlert': string;
       'topBorderAlertStringProperty': LinkableProperty<string>;
-      'bottomBorderAlert': string;
       'bottomBorderAlertStringProperty': LinkableProperty<string>;
     };
     'grabDrag': {
-      'grabPattern': string;
       'grabPatternStringProperty': LinkableProperty<string>;
-      'movable': string;
       'movableStringProperty': LinkableProperty<string>;
-      'button': string;
       'buttonStringProperty': LinkableProperty<string>;
-      'defaultObjectToGrab': string;
       'defaultObjectToGrabStringProperty': LinkableProperty<string>;
-      'released': string;
       'releasedStringProperty': LinkableProperty<string>;
-      'gestureHelpTextPattern': string;
       'gestureHelpTextPatternStringProperty': LinkableProperty<string>;
-      'spaceToGrabOrRelease': string;
       'spaceToGrabOrReleaseStringProperty': LinkableProperty<string>;
     };
     'voicing': {
       'simSection': {
         'screenSummary': {
-          'singleScreenIntroPattern': string;
           'singleScreenIntroPatternStringProperty': LinkableProperty<string>;
         }
       };
       'keyboardHelpDialog': {
         'general': {
-          'descriptionPattern': string;
           'descriptionPatternStringProperty': LinkableProperty<string>;
         }
       };
-      'grabDragHintPattern': string;
       'grabDragHintPatternStringProperty': LinkableProperty<string>;
-      'grabbedAlert': string;
       'grabbedAlertStringProperty': LinkableProperty<string>;
-      'draggableAlert': string;
       'draggableAlertStringProperty': LinkableProperty<string>;
       'resetAll': {
-        'contextResponse': string;
         'contextResponseStringProperty': LinkableProperty<string>;
       }
     };
-    'close': string;
     'closeStringProperty': LinkableProperty<string>;
   }
 };
