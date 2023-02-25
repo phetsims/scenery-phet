93a9a032469662c6266a3fe5b2352039ac5ea7f0
phet-dev <phetadmin@gmail.com>
2023-02-25 06:14:55 -0700

    update copyright dates from daily grunt work

--- js/keyboard/LetterKeyNode.ts
+++ js/keyboard/LetterKeyNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2022, University of Colorado Boulder
+// Copyright 2017-2023, University of Colorado Boulder
 
 /**
  * LetterKeyNode is a TextKeyNode with layout and spacing defaults that look best for a single letter. A letter key is
