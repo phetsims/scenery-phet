75afe15b8bdffc71dcdac594178aa23955332ef4
phet-dev <phetadmin@gmail.com>
2023-03-02 05:03:43 -0700

    update copyright dates from daily grunt work

--- js/PointerCoordinatesNode.ts
+++ js/PointerCoordinatesNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2023, University of Colorado Boulder
 
 /**
  * Shows the model and view coordinates that correspond to the cursor position.
