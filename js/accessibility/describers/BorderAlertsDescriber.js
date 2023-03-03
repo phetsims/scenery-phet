0cc795be681a5b9209ab1ca91771f3e6ce742f28
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-03-03 15:29:40 -0700

    remove TODO, fixes https://github.com/phetsims/scenery-phet/issues/583

--- js/accessibility/describers/BorderAlertsDescriber.js
+++ js/accessibility/describers/BorderAlertsDescriber.js
@@ -94,7 +94,6 @@ class BorderAlertsDescriber {
   /**
    * Based on a position and the border bounds, if the position is touching the bounds, then alert that we are at border.
    * By passing in an optional key, you can prioritize that direction if you are at the corner.
-   * TODO see https://github.com/phetsims/scenery-phet/issues/583 don't alert perpendicular direction if you are sliding against it.
    * @private
    *
    * @param {Vector2} position
