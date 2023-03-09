050d2abb757d9fb08f9a2f251f0cf2c2f6c0a1a0
phet-dev <phetadmin@gmail.com>
2023-03-09 04:55:45 -0700

    update copyright dates from daily grunt work

--- js/ClapperboardButton.ts
+++ js/ClapperboardButton.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * A Node that produces a variety of loud outputs to support data synchronizing during a recording.
