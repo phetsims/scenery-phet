42fb33362c6e0e870acb6e14ba4ccdfeaa98d996
chrisklus <christopher.klusendorf@colorado.edu>
2023-04-10 12:41:02 -0600

    Add literal types for MathSymbols, see https://github.com/phetsims/number-play/issues/225

--- js/MathSymbols.ts
+++ js/MathSymbols.ts
@@ -10,6 +10,8 @@
  * Bad:   var expressionString = value1 + ' \u00d7 ' + value2;
  * Wrong: var expressionString = value1 + ' x ' + value2;
  *
+ * Note: These are all 'as const' to support strong typing. See NUMBER_SUITE_COMMON/SymbolType for an example.
+ *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
@@ -23,38 +25,38 @@ const MathSymbols = {
   //==================================================================================================================
 
   // binary operators
-  PLUS: '+',
-  MINUS: '\u2212', // longer than UNARY_MINUS (PhET-specific convention)
-  TIMES: '\u00d7',
-  DIVIDE: '\u00f7',
-  DOT: '\u22c5',
+  PLUS: '+' as const,
+  MINUS: '\u2212' as const, // longer than UNARY_MINUS (PhET-specific convention)
+  TIMES: '\u00d7' as const,
+  DIVIDE: '\u00f7' as const,
+  DOT: '\u22c5' as const,
 
   // unary operators
-  UNARY_PLUS: '+',
-  UNARY_MINUS: '-', // shorter than MINUS (PhET-specific convention)
+  UNARY_PLUS: '+' as const,
+  UNARY_MINUS: '-' as const, // shorter than MINUS (PhET-specific convention)
 
   // relational operators
-  EQUAL_TO: '=',
-  NOT_EQUAL_TO: '\u2260',
-  GREATER_THAN: '>',
-  LESS_THAN: '<',
-  LESS_THAN_OR_EQUAL: '\u2264',
-  GREATER_THAN_OR_EQUAL: '\u2265',
+  EQUAL_TO: '=' as const,
+  NOT_EQUAL_TO: '\u2260' as const,
+  GREATER_THAN: '>' as const,
+  LESS_THAN: '<' as const,
+  LESS_THAN_OR_EQUAL: '\u2264' as const,
+  GREATER_THAN_OR_EQUAL: '\u2265' as const,
 
   // other math symbols
-  PERCENT: '%',
-  INFINITY: '\u221E',
-  PI: '\u03c0',
-  PLUS_MINUS: '\u00B1',
+  PERCENT: '%' as const,
+  INFINITY: '\u221E' as const,
+  PI: '\u03c0' as const,
+  PLUS_MINUS: '\u00B1' as const,
 
   // Greek characters
-  THETA: '\u03b8',
+  THETA: '\u03b8' as const,
 
   // Used to symbolize 'no value', e.g. on a meter that is not reading anything.
   // See https://github.com/phetsims/scenery-phet/issues/431
-  NO_VALUE: '\u2014', // em dash
+  NO_VALUE: '\u2014' as const, // em dash
 
-  DEGREES: '\u00B0'
+  DEGREES: '\u00B0' as const
 };
 
 sceneryPhet.register( 'MathSymbols', MathSymbols );
