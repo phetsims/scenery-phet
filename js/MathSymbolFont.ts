4b4212f6c4e15cf0ef9321b2d2fd4c306b8af43b
phet-dev <phetadmin@gmail.com>
2023-06-06 03:34:28 -0600

    update copyright dates from daily grunt work

--- js/MathSymbolFont.ts
+++ js/MathSymbolFont.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2022, University of Colorado Boulder
+// Copyright 2016-2023, University of Colorado Boulder
 
 /**
  * MathSymbolFont is the font used for math symbols (e.g. 'x', 'y') in PhET sims.
