1ddb44ef47502bc9169b5d120bc364a76dc67aed
phet-dev <phetadmin@gmail.com>
2023-06-27 03:37:09 -0600

    update copyright dates from daily grunt work

--- js/OopsDialog.ts
+++ js/OopsDialog.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2023, University of Colorado Boulder
 
 /**
  * OopsDialog is displayed when some limitation of the simulation is encountered.
