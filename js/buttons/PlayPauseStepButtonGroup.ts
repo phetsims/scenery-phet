96d096c64d516dee8a47a2c9affbf34a4e0ad736
phet-dev <phetadmin@gmail.com>
2023-09-02 03:19:59 -0600

    update copyright dates from daily grunt work

--- js/buttons/PlayPauseStepButtonGroup.ts
+++ js/buttons/PlayPauseStepButtonGroup.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * PlayPauseStepButtonGroup has a play & pause button, optional step-forward button, and optional step-back button.
