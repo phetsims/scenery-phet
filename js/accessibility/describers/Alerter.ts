36f611bb7546745f08215ad9b09e9bba74e2b17f
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-09-11 12:14:05 -0600

    add EnabledComponent to Alerter, https://github.com/phetsims/scenery-phet/issues/819

--- js/accessibility/describers/Alerter.ts
+++ js/accessibility/describers/Alerter.ts
@@ -10,10 +10,11 @@ import { Node, Voicing } from '../../../../scenery/js/imports.js';
 import Utterance, { TAlertable } from '../../../../utterance-queue/js/Utterance.js';
 import UtteranceQueue from '../../../../utterance-queue/js/UtteranceQueue.js';
 import sceneryPhet from '../../sceneryPhet.js';
+import EnabledComponent, { EnabledComponentOptions } from '../../../../axon/js/EnabledComponent.js';
 
 type UtteranceQueueCallback = ( queue: UtteranceQueue ) => void;
 
-export type AlerterOptions = {
+type SelfOptions = {
 
   // When true, alerts will be sent to the voicingUtteranceQueue. This shutoff valve is similar to
   // descriptionAlertNode, but for voicing.
@@ -24,19 +25,21 @@ export type AlerterOptions = {
   // do not alert for description (same as alertToVoicing:false). NOTE: No description will alert without this option!
   descriptionAlertNode?: Node | null;
 };
+export type AlerterOptions = SelfOptions & EnabledComponentOptions;
 
-class Alerter {
+class Alerter extends EnabledComponent {
 
   public readonly alertToVoicing: boolean;
   public readonly descriptionAlertNode: Node | null;
 
   public constructor( providedOptions?: AlerterOptions ) {
 
-    const options = optionize<AlerterOptions>()( {
+    const options = optionize<AlerterOptions, SelfOptions, EnabledComponentOptions>()( {
       alertToVoicing: true,
       descriptionAlertNode: null
     }, providedOptions );
 
+    super( options );
     this.alertToVoicing = options.alertToVoicing;
     this.descriptionAlertNode = options.descriptionAlertNode;
   }
@@ -45,27 +48,30 @@ class Alerter {
    * Alert to both description and voicing utteranceQueues, depending on if both are supported by this instance
    */
   public alert( alertable: TAlertable ): void {
-    if ( this.alertToVoicing ) {
+    if ( this.enabled ) {
+      if ( this.alertToVoicing ) {
+        console.log( 'alerting to voicing' );
 
-      assert && assert( alertable instanceof Utterance, 'If alerting to Voicing, the alertable needs to be an Utterance' ); // eslint-disable-line no-simple-type-checking-assertions
-      Voicing.alertUtterance( alertable as Utterance );
+        assert && assert( alertable instanceof Utterance, 'If alerting to Voicing, the alertable needs to be an Utterance' ); // eslint-disable-line no-simple-type-checking-assertions
+        Voicing.alertUtterance( alertable as Utterance );
+      }
+
+      this.alertDescriptionUtterance( alertable );
     }
-
-    this.alertDescriptionUtterance( alertable );
   }
 
   /**
    * Forward to provided Node for UtteranceQueue alerting logic. See ParallelDOM.alertDescriptionUtterance() for details.
    */
   public alertDescriptionUtterance( alertable: TAlertable ): void {
-    this.descriptionAlertNode && this.descriptionAlertNode.alertDescriptionUtterance( alertable );
+    this.enabled && this.descriptionAlertNode && this.descriptionAlertNode.alertDescriptionUtterance( alertable );
   }
 
   /**
    * Forward to provided Node for UtteranceQueue alerting logic. See ParallelDOM.forEachUtteranceQueue() for details.
    */
   public forEachUtteranceQueue( utteranceQueueCallback: UtteranceQueueCallback ): void {
-    this.descriptionAlertNode && this.descriptionAlertNode.forEachUtteranceQueue( utteranceQueueCallback );
+    this.enabled && this.descriptionAlertNode && this.descriptionAlertNode.forEachUtteranceQueue( utteranceQueueCallback );
   }
 }
 
