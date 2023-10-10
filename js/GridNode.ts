ad6ff1de559fe4e5bcedb27ef878c2fe2c891d94
phet-dev <phetadmin@gmail.com>
2023-10-10 03:19:44 -0600

    update copyright dates from daily grunt work

--- js/GridNode.ts
+++ js/GridNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * When enabled, shows a grid across the play area that helps the user to make quantitative comparisons
