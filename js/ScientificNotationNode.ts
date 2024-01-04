7f15e7f9a325c4114c09300cb51cab165727348c
phet-dev <phetadmin@gmail.com>
2024-01-04 03:21:42 -0700

    update copyright dates from daily grunt work

--- js/ScientificNotationNode.ts
+++ js/ScientificNotationNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2023, University of Colorado Boulder
+// Copyright 2014-2024, University of Colorado Boulder
 
 /**
  * ScientificNotationNode displays a number in scientific notation, M x 10^E, where:
