6c671cf861dc2782f20129e257c4b426ea5f0b0c
phet-dev <phetadmin@gmail.com>
2024-01-10 03:18:34 -0700

    update copyright dates from daily grunt work

--- js/RichDragListener.ts
+++ js/RichDragListener.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 /**
  * RichDragListener extends DragListener to integrate the drag and drop sound clips.
  *
