0ba1d5b18c2c20d20ef5ccf292df9d04fb8535f8
pixelzoom <cmalley@pixelzoom.com>
2024-01-18 17:18:42 -0700

    delete unnecessary strictAxonDependencies: false, https://github.com/phetsims/axon/issues/441

--- js/MeasuringTapeNode.ts
+++ js/MeasuringTapeNode.ts
@@ -287,8 +287,7 @@ class MeasuringTapeNode extends Node {
         } );
       }, {
         tandem: options.phetioReadoutStringPropertyInstrumented ? options.tandem?.createTandem( 'readoutStringProperty' ) : Tandem.OPT_OUT,
-        phetioDocumentation: 'The text content of the readout on the measuring tape',
-        strictAxonDependencies: false //TODO https://github.com/phetsims/axon/issues/441
+        phetioDocumentation: 'The text content of the readout on the measuring tape'
       } );
 
     this.valueNode = new Text( readoutStringProperty, {
