d6d9bff503f5b432a42bdf5af67c53875103b072
phet-dev <phetadmin@gmail.com>
2024-02-02 03:26:24 -0700

    update copyright dates from daily grunt work

--- js/StringDisplay.ts
+++ js/StringDisplay.ts
@@ -1,4 +1,4 @@
-// Copyright 2023-2024, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 
 /**
  * StringDisplay displays the value of a string or TReadOnlyProperty<string> on a background. The background can
