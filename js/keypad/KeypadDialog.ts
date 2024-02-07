d12c87161acd7a7c02b2f4b685cdbee4bfe17d6e
phet-dev <phetadmin@gmail.com>
2024-02-07 03:26:12 -0700

    update copyright dates from daily grunt work

--- js/keypad/KeypadDialog.ts
+++ js/keypad/KeypadDialog.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * KeypadDialog is a Dialog sub-type that handles the creation and management of a Keypad.
