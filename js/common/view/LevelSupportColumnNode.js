50e41319d9110337fd4304caa1a38f70df08a766
Marla Schulz <marla.schulz@colorado.edu>
2024-02-12 11:52:51 -0700

    Change column style to hard stop gradients, see: https://github.com/phetsims/balancing-act/issues/144

--- js/common/view/LevelSupportColumnNode.js
+++ js/common/view/LevelSupportColumnNode.js
@@ -6,8 +6,9 @@
  * @author John Blanco
  */
 
-import { LinearGradient, Node, Path, Rectangle } from '../../../../scenery/js/imports.js';
+import { Node, Path, Rectangle } from '../../../../scenery/js/imports.js';
 import balancingAct from '../../balancingAct.js';
+import BASharedConstants from '../BASharedConstants.js';
 import ColumnState from '../model/ColumnState.js';
 
 class LevelSupportColumnNode extends Node {
@@ -22,7 +23,7 @@ class LevelSupportColumnNode extends Node {
 
     // Create and add the main body of the column.
     const transformedColumnShape = modelViewTransform.modelToViewShape( levelSupportColumn.shape );
-    const mainBodyGradient = new LinearGradient( transformedColumnShape.bounds.minX, 0, transformedColumnShape.bounds.maxX, 0 ).addColorStop( 0, 'rgb( 150, 150, 150 )' ).addColorStop( 0.25, 'rgb( 230, 230, 230 )' ).addColorStop( 0.65, 'rgb( 150, 150, 150 )' ).addColorStop( 1, 'rgb( 200, 200, 200 )' );
+    const mainBodyGradient = BASharedConstants.GET_COLUMN_BODY_GRADIENT( transformedColumnShape );
 
     const columnNode = new Path( transformedColumnShape,
       {
@@ -35,7 +36,8 @@ class LevelSupportColumnNode extends Node {
     // Create and add the column support.
     const supportWidth = transformedColumnShape.bounds.width * 1.3; // Empirically determined.
     const supportHeight = transformedColumnShape.bounds.height * 0.15; // Empirically determined.
-    const supportGradient = new LinearGradient( transformedColumnShape.bounds.centerX - supportWidth / 2, 0, transformedColumnShape.bounds.centerX + supportWidth / 2, 0 ).addColorStop( 0, 'rgb( 150, 150, 150 )' ).addColorStop( 0.25, 'rgb( 210, 210, 210 )' ).addColorStop( 0.65, 'rgb( 150, 150, 150 )' ).addColorStop( 1, 'rgb( 170, 170, 170 )' );
+    const supportGradient = BASharedConstants.GET_COLUMN_SUPPORT_GRADIENT( transformedColumnShape, supportWidth );
+
     const columnSupportNode = new Rectangle(
       transformedColumnShape.bounds.centerX - supportWidth / 2,
       transformedColumnShape.bounds.maxY - supportHeight,
