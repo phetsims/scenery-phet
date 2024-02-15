00ea50c6b4624dd183b59be4d908af1c8559d33c
phet-dev <phetadmin@gmail.com>
2024-02-15 03:24:58 -0700

    update copyright dates from daily grunt work

--- js/TriangleNode.ts
+++ js/TriangleNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * Draws an equilateral or isosceles triangle pointing up by default.
