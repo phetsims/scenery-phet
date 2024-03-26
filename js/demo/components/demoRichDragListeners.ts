894b2ed3b4086dbc22252a39f25b60bd15a155aa
AgustinVallejo <agustin.vallejo@udea.edu.co>
2024-03-26 17:03:03 -0500

    Adding text to the scenery-phet demo of Rich Drag Listener, see https://github.com/phetsims/scenery-phet/issues/849

--- js/demo/components/demoRichDragListeners.ts
+++ js/demo/components/demoRichDragListeners.ts
@@ -7,19 +7,28 @@
  * @author Agustín Vallejo (PhET Interactive Simulations)
  */
 
-import { Circle, Node, Rectangle } from '../../../../scenery/js/imports.js';
+import { Circle, Node, Rectangle, RichText } from '../../../../scenery/js/imports.js';
 import Bounds2 from '../../../../dot/js/Bounds2.js';
 import RichDragListener from '../../RichDragListener.js';
 import TinyProperty from '../../../../axon/js/TinyProperty.js';
 import RichKeyboardDragListener from '../../RichKeyboardDragListener.js';
+import PhetFont from '../../PhetFont.js';
 
 export default function demoRichDragListeners( layoutBounds: Bounds2 ): Node {
-  const RADIUS = 50;
+  const RADIUS = 75;
 
   const richDragListenerCircle = new Circle( RADIUS, {
     fill: 'red',
-    cursor: 'pointer'
+    cursor: 'pointer',
+    centerX: -RADIUS * 2
   } );
+  const innerCircleMessage = new RichText( 'Mouse-drag me!', {
+    font: new PhetFont( 15 ),
+    fill: 'white',
+    centerX: 0,
+    centerY: 0
+  } );
+  richDragListenerCircle.addChild( innerCircleMessage );
   const dragBoundsProperty = new TinyProperty( layoutBounds.shifted( layoutBounds.center.times( -1 ) ).eroded( RADIUS ) );
 
   richDragListenerCircle.addInputListener( new RichDragListener( {
@@ -28,11 +37,19 @@ export default function demoRichDragListeners( layoutBounds: Bounds2 ): Node {
     targetNode: richDragListenerCircle
   } ) );
 
-  const richKeyboardDragListenerRectangle = new Rectangle( 0, RADIUS * 2, RADIUS * 2, RADIUS, {
+  const richKeyboardDragListenerRectangle = new Rectangle( RADIUS * 2, -RADIUS / 2, RADIUS * 3, RADIUS, {
     fill: 'blue',
     tagName: 'div',
     focusable: true
   } );
+  const innerRectangleMessage = new RichText( 'Tab and keyboard-drag me!', {
+    font: new PhetFont( 15 ),
+    fill: 'white',
+    centerX: richKeyboardDragListenerRectangle.centerX,
+    centerY: richKeyboardDragListenerRectangle.centerY
+  } );
+  richKeyboardDragListenerRectangle.addChild( innerRectangleMessage );
+
   richKeyboardDragListenerRectangle.addInputListener( new RichKeyboardDragListener( {
     dragBoundsProperty: dragBoundsProperty,
     drag: delta => {
