e7b3d261a05931b29c474ee30889811b6bd4cacd
Jesse <jesse.greenberg@colorado.edu>
2024-04-17 17:08:41 -0400

    Use AccessibleNumberSpinner in FineCoarseSpinner, see https://github.com/phetsims/scenery-phet/issues/847

--- js/FineCoarseSpinner.ts
+++ js/FineCoarseSpinner.ts
@@ -17,38 +17,50 @@ import ArrowButton, { ArrowButtonOptions } from '../../sun/js/buttons/ArrowButto
 import Tandem from '../../tandem/js/Tandem.js';
 import NumberDisplay, { NumberDisplayOptions } from './NumberDisplay.js';
 import sceneryPhet from './sceneryPhet.js';
+import AccessibleNumberSpinner, { AccessibleNumberSpinnerOptions } from '../../sun/js/accessibility/AccessibleNumberSpinner.js';
 
 type SelfOptions = {
   deltaFine?: number; // amount to increment/decrement when the 'fine' tweakers are pressed
   deltaCoarse?: number; // amount to increment/decrement when the 'coarse' tweakers are pressed
   spacing?: number; // horizontal space between subcomponents
   numberDisplayOptions?: StrictOmit<NumberDisplayOptions, 'tandem'>;
-  arrowButtonOptions?: StrictOmit<ArrowButtonOptions, 'numberOfArrows' | 'tandem'>;
+  arrowButtonOptions?: StrictOmit<ArrowButtonOptions, 'numberOfArrows' | 'tandem' | 'focusable'>;
 };
 
-export type FineCoarseSpinnerOptions = SelfOptions & StrictOmit<NodeOptions, 'children'>;
+type ParentOptions = AccessibleNumberSpinnerOptions & NodeOptions;
 
-export default class FineCoarseSpinner extends Node {
+export type FineCoarseSpinnerOptions = SelfOptions & StrictOmit<ParentOptions, 'children' | 'valueProperty' | 'enabledRangeProperty' | 'keyboardStep' | 'shiftKeyboardStep' | 'pageKeyboardStep'>;
+
+export default class FineCoarseSpinner extends AccessibleNumberSpinner( Node, 0 ) {
 
   private readonly disposeFineCoarseSpinner: () => void;
 
   public constructor( numberProperty: NumberProperty, providedOptions?: FineCoarseSpinnerOptions ) {
 
     const options = optionize<FineCoarseSpinnerOptions,
-      StrictOmit<SelfOptions, 'numberDisplayOptions' | 'arrowButtonOptions'>, NodeOptions>()( {
+      StrictOmit<SelfOptions, 'numberDisplayOptions' | 'arrowButtonOptions'>, ParentOptions>()( {
 
       // SelfOptions
       deltaFine: 1,
       deltaCoarse: 10,
       spacing: 10,
 
+      // AccessibleNumberSpinnerOptions
+      valueProperty: numberProperty,
+      enabledRangeProperty: numberProperty.rangeProperty,
+
+      // Instead of changing the value with keyboard step options, the arrow buttons are synthetically
+      // pressed in response to keyboard input so that the buttons look pressed.
+      keyboardStep: 0,
+      shiftKeyboardStep: 0,
+      pageKeyboardStep: 0,
+
       // NodeOptions
       disabledOpacity: 0.5, // {number} opacity used to make the control look disabled
       tandem: Tandem.REQUIRED,
       tandemNameSuffix: 'Spinner',
       phetioFeatured: true,
-      phetioEnabledPropertyInstrumented: true,
-      groupFocusHighlight: true // see https://github.com/phetsims/scenery-phet/issues/794
+      phetioEnabledPropertyInstrumented: true
     }, providedOptions );
 
     const range = numberProperty.range;
@@ -58,6 +70,7 @@ export default class FineCoarseSpinner extends Node {
 
     // options for the 'fine' arrow buttons, which show 1 arrow
     const fineButtonOptions: ArrowButtonOptions = combineOptions<ArrowButtonOptions>( {
+      focusable: false,
       numberOfArrows: 1,
       arrowWidth: 12, // width of base
       arrowHeight: 14, // from tip to base
@@ -80,6 +93,7 @@ export default class FineCoarseSpinner extends Node {
 
     // options for the 'coarse' arrow buttons, which show 2 arrows
     const coarseButtonOptions = combineOptions<ArrowButtonOptions>( {}, fineButtonOptions, {
+      focusable: false,
       numberOfArrows: 2,
       arrowSpacing: -0.5 * fineButtonArrowHeight, // arrows overlap
 
@@ -144,6 +158,20 @@ export default class FineCoarseSpinner extends Node {
       incrementFineButton.enabled = incrementCoarseButton.enabled = ( value !== range.max );
     };
     numberProperty.link( numberPropertyListener ); // unlink required in dispose
+
+    // pdom - manually click arrow buttons from alt input events so that the buttons look pressed while the key is down
+    const increasedListener = ( isDown: boolean ) => {
+      if ( isDown ) {
+        this.shiftKeyDown ? incrementFineButton.pdomClick() : incrementCoarseButton.pdomClick();
+      }
+    };
+    const decreasedListener = ( isDown: boolean ) => {
+      if ( isDown ) {
+        this.shiftKeyDown ? decrementFineButton.pdomClick() : decrementCoarseButton.pdomClick();
+      }
+    };
+    this.pdomIncrementDownEmitter.addListener( increasedListener );
+    this.pdomDecrementDownEmitter.addListener( decreasedListener );
 
     this.disposeFineCoarseSpinner = () => {
 
