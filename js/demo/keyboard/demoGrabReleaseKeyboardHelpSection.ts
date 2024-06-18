3e38791045a08b08825bc9c6c17527c285400a85
Jesse <jesse.greenberg@colorado.edu>
2024-06-18 17:23:33 -0400

    minor formatting, see https://github.com/phetsims/mean-share-and-balance/issues/266

--- js/demo/keyboard/demoGrabReleaseKeyboardHelpSection.ts
+++ js/demo/keyboard/demoGrabReleaseKeyboardHelpSection.ts
@@ -1,19 +1,17 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
+
 /**
  * A demo of the Grab or Release keyboard help section.
  *
  * @author Marla Schulz (PhET Interactive Simulations)
- *
  */
 
 import Bounds2 from '../../../../dot/js/Bounds2.js';
-import { SunDemoOptions } from '../../../../sun/js/demo/DemosScreenView.js';
 import { Node } from '../../../../scenery/js/imports.js';
 import GrabReleaseKeyboardHelpSection from '../../keyboard/help/GrabReleaseKeyboardHelpSection.js';
 import StringProperty from '../../../../axon/js/StringProperty.js';
 
-
-export default function demoGrabReleaseKeyboardHelpSection( layoutBounds: Bounds2, providedOptions?: SunDemoOptions ): Node {
+export default function demoGrabReleaseKeyboardHelpSection( layoutBounds: Bounds2 ): Node {
   return new Node( {
     children: [
       new GrabReleaseKeyboardHelpSection( new StringProperty( 'Item' ), new StringProperty( 'item' ) )
