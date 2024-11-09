ba5c38ea8db5c66f2ea79f9659d7bc745758f373
phet-dev <phetadmin@gmail.com>
2024-11-09 03:34:27 -0700

    update copyright dates from daily grunt work

--- js/buttons/CameraButton.ts
+++ js/buttons/CameraButton.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * CameraButton is a push button with a camera icon.
--- js/buttons/ReturnButton.ts
+++ js/buttons/ReturnButton.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * ReturnButton is a push button for undoing a previous operation.
