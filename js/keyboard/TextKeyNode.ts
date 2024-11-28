5e46666f7d65d8f5e609a1b39acd2561c641b311
phet-dev <phetadmin@gmail.com>
2024-11-28 03:50:01 -0700

    update copyright dates from daily grunt work

--- js/keyboard/TextKeyNode.ts
+++ js/keyboard/TextKeyNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2023, University of Colorado Boulder
+// Copyright 2017-2024, University of Colorado Boulder
 
 /**
  * TextKeyNode is a KeyNode with a text icon. It has layout, spacing, and defaults for KeyNode that are suited for text.
