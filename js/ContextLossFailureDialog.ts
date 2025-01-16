0e9fa084e4dc2305dc43a7273bff4916779aea77
phet-dev <phetadmin@gmail.com>
2025-01-16 03:36:17 -0700

    update copyright dates from daily grunt work

--- js/ContextLossFailureDialog.ts
+++ js/ContextLossFailureDialog.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2024, University of Colorado Boulder
+// Copyright 2015-2025, University of Colorado Boulder
 
 /**
  * Failure message displayed when a WebGL context loss is experienced and we can't recover. Offers a button to reload
