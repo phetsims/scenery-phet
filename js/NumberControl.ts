5b3d5e3b1bc2fbf6e6c0d56ef56fea0c5faf6cd3
phet-dev <phetadmin@gmail.com>
2025-02-04 04:35:09 -0700

    update copyright dates from daily grunt work

--- js/NumberControl.ts
+++ js/NumberControl.ts
@@ -1,4 +1,4 @@
-// Copyright 2015-2024, University of Colorado Boulder
+// Copyright 2015-2025, University of Colorado Boulder
 
 /**
  * NumberControl is a control for changing a Property<number>, with flexible layout. It consists of a labeled value,
