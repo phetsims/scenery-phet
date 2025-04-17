9eaf46e029ec4c25e2906cd5f521bd9c0b63409c
phet-dev <phetadmin@gmail.com>
2025-04-17 04:18:09 -0600

    update copyright dates from daily grunt work

--- js/keypad/NumberAccumulator.ts
+++ js/keypad/NumberAccumulator.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2024, University of Colorado Boulder
+// Copyright 2017-2025, University of Colorado Boulder
 
 /**
  * A key accumulator that collects user input for integer and floating point values, intended for use in conjunction
