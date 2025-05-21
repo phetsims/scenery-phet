de568cbbbf4f54caec0ece3d6b588b4f8e94cabc
phet-dev <phetadmin@gmail.com>
2025-05-21 03:30:47 -0600

    update copyright dates from daily grunt work

--- js/accessibility/grab-drag/GrabDragModel.ts
+++ js/accessibility/grab-drag/GrabDragModel.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2025, University of Colorado Boulder
 
 /**
  * The model of the grab drag interaction. This has the current interaction state, and the cueing state.
--- js/accessibility/grab-drag/GrabDragUsageTracker.ts
+++ js/accessibility/grab-drag/GrabDragUsageTracker.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2025, University of Colorado Boulder
 
 /**
  * The model of the grab drag cueing logic. This is a separate model in part so that different interaction instances
