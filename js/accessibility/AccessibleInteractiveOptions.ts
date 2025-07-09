c34cccf98f323f833d3951244b2c2f13140c6a31
Jesse <jesse.greenberg@colorado.edu>
2025-07-09 12:22:25 -0400

    AccessibleDdraggableOptions extends new AccessibleInteractiveOptions, see https://github.com/phetsims/membrane-transport/issues/269

---
+++ js/accessibility/AccessibleInteractiveOptions.ts
@@ -0,0 +1,34 @@
+// Copyright 2025, University of Colorado Boulder
+
+/**
+ * Options that you should apply to a Scenery Node that implements a custom interaction with the keyboard
+ * and for accessibility. Applies ParallelDOM options that make the Node focusable, ensures
+ * that the accessible name is read correctly, and forces the screen reader to send all keyboard events.
+ *
+ * @author Jesse Greenberg (PhET Interactive Simulations)
+ */
+
+import { ParallelDOMOptions, PDOMValueType } from '../../../scenery/js/accessibility/pdom/ParallelDOM.js';
+import Node from '../../../scenery/js/nodes/Node.js';
+
+const AccessibleInteractiveOptions: ParallelDOMOptions = {
+  tagName: 'div',
+  focusable: true,
+
+  // Application role tells the screen reader to provide access to arrow keys and switch to a 'forms' mode.
+  ariaRole: 'application',
+
+  // Use aria-label to enforce the accessible name. The application role can prevent the inner content from
+  // being used as the accessible name on some platforms.
+  accessibleNameBehavior: ( node: Node, options: ParallelDOMOptions, accessibleName: PDOMValueType ): ParallelDOMOptions => {
+    options.ariaLabel = accessibleName;
+    options.innerContent = accessibleName;
+
+    return options;
+  },
+
+  // So that you can drag and drop this component on screen readers on mobile devices.
+  positionInPDOM: true
+} as const;
+
+export default AccessibleInteractiveOptions;
--- js/accessibility/grab-drag/AccessibleDraggableOptions.ts
+++ js/accessibility/grab-drag/AccessibleDraggableOptions.ts
@@ -6,34 +6,22 @@
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
-import { ParallelDOMOptions, PDOMValueType } from '../../../../scenery/js/accessibility/pdom/ParallelDOM.js';
-import Node from '../../../../scenery/js/nodes/Node.js';
+import { combineOptions } from '../../../../phet-core/js/optionize.js';
+import { ParallelDOMOptions } from '../../../../scenery/js/accessibility/pdom/ParallelDOM.js';
 import SceneryPhetStrings from '../../SceneryPhetStrings.js';
+import AccessibleInteractiveOptions from '../AccessibleInteractiveOptions.js';
 
 // constants
 const movableStringProperty = SceneryPhetStrings.a11y.grabDrag.movableStringProperty;
 
-const AccessibleDraggableOptions: ParallelDOMOptions = {
-  tagName: 'div',
-  focusable: true,
+const AccessibleDraggableOptions: ParallelDOMOptions = combineOptions<ParallelDOMOptions>(
+  {},
+  AccessibleInteractiveOptions,
+  {
 
-  // Application role tells the screen reader to provide access to arrow keys and switch to a 'forms' mode.
-  ariaRole: 'application',
-
-  // Use aria-label to enforce the accessible name. The application role can prevent the inner content from
-  // being used as the accessible name on some platforms.
-  accessibleNameBehavior: ( node: Node, options: ParallelDOMOptions, accessibleName: PDOMValueType ): ParallelDOMOptions => {
-    options.ariaLabel = accessibleName;
-    options.innerContent = accessibleName;
-
-    return options;
-  },
-
-  // So that you can drag and drop this component on screen readers on mobile devices.
-  positionInPDOM: true,
-
-  // The screen reader will call this a 'movable', which is more intuitive for the user.
-  accessibleRoleDescription: movableStringProperty
-} as const;
+    // The screen reader will call this a 'movable', which is more intuitive for the user.
+    accessibleRoleDescription: movableStringProperty
+  }
+);
 
 export default AccessibleDraggableOptions;
