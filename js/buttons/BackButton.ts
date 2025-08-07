9ae4d79d92ebb8dfef1a339232387702edf66e7e
jbphet <jpblanco@yahoo.com>
2025-08-07 15:46:52 -0600

    use new shared "go back" sound, see https://github.com/phetsims/tambo/issues/199

--- js/buttons/BackButton.ts
+++ js/buttons/BackButton.ts
@@ -13,10 +13,8 @@ import optionize from '../../../phet-core/js/optionize.js';
 import StrictOmit from '../../../phet-core/js/types/StrictOmit.js';
 import Path from '../../../scenery/js/nodes/Path.js';
 import RectangularPushButton, { RectangularPushButtonOptions } from '../../../sun/js/buttons/RectangularPushButton.js';
-import SoundClip from '../../../tambo/js/sound-generators/SoundClip.js';
-import soundManager from '../../../tambo/js/soundManager.js';
+import sharedSoundPlayers from '../../../tambo/js/sharedSoundPlayers.js';
 import TSoundPlayer from '../../../tambo/js/TSoundPlayer.js';
-import goBack_mp3 from '../../sounds/goBack_mp3.js';
 import ArrowShape from '../ArrowShape.js';
 import PhetColorScheme from '../PhetColorScheme.js';
 import sceneryPhet from '../sceneryPhet.js';
@@ -34,20 +32,14 @@ export default class BackButton extends RectangularPushButton {
     const options = optionize<BackButtonOptions, StrictOmit<SelfOptions, 'soundPlayer'>, RectangularPushButtonOptions>()( {
 
       // Default margin values were set up to make this button match the size of the refresh button, since these
-      // buttons often appear together.  See see https://github.com/phetsims/scenery-phet/issues/44.
+      // buttons often appear together.  See https://github.com/phetsims/scenery-phet/issues/44.
       xMargin: 8,
       yMargin: 10.9,
 
-      baseColor: PhetColorScheme.BUTTON_YELLOW
+      baseColor: PhetColorScheme.BUTTON_YELLOW,
+      soundPlayer: sharedSoundPlayers.get( 'goBack' )
 
     }, providedOptions );
-
-    // Create and add the default sound generator if none was provided.
-    if ( !options.soundPlayer ) {
-      const goBackSoundClip = new SoundClip( goBack_mp3, { initialOutputLevel: 0.35 } );
-      soundManager.addSoundGenerator( goBackSoundClip, { categoryName: 'user-interface' } );
-      options.soundPlayer = goBackSoundClip;
-    }
 
     const arrowShape = new ArrowShape( 0, 0, -28.5, 0, {
       tailWidth: 8,
