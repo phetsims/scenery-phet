2d4c67406f1139a2e8ab2be7ed0e06463d596b7b
phet-dev <phetadmin@gmail.com>
2026-01-09 03:55:46 -0700

    update copyright dates from daily grunt work

--- js/SceneryPhetFluent.ts
+++ js/SceneryPhetFluent.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 // AUTOMATICALLY GENERATED – DO NOT EDIT.
 // Generated from scenery-phet-strings_en.yaml
 
