46f9c4da83614b90750167cf714a2c5b60ec91b3
phet-dev <phetadmin@gmail.com>
2026-02-06 04:09:41 -0700

    update copyright dates from daily grunt work

--- js/TimeControlNode.ts
+++ js/TimeControlNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2025, University of Colorado Boulder
+// Copyright 2018-2026, University of Colorado Boulder
 
 /**
  * TimeControlNode provides a UI for controlling time.  It includes a play/pause button, step-forward button,
--- js/buttons/StepBackwardButton.ts
+++ js/buttons/StepBackwardButton.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2024, University of Colorado Boulder
+// Copyright 2014-2026, University of Colorado Boulder
 
 /**
  * Step backward button.
