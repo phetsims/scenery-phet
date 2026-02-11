de83d1a935a27b3bfcfb8244c6cd78f612b3d823
Jesse <jesse.greenberg@colorado.edu>
2026-02-11 12:10:01 -0500

    update demo, see https://github.com/phetsims/scenery-phet/issues/963

--- js/demo/keyboard/demoKeyboardHelpSection.ts
+++ js/demo/keyboard/demoKeyboardHelpSection.ts
@@ -6,19 +6,41 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import Property from '../../../../axon/js/Property.js';
 import Bounds2 from '../../../../dot/js/Bounds2.js';
+import HotkeyData from '../../../../scenery/js/input/HotkeyData.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
-import KeyboardHelpIconFactory from '../../keyboard/help/KeyboardHelpIconFactory.js';
 import KeyboardHelpSection from '../../keyboard/help/KeyboardHelpSection.js';
 import KeyboardHelpSectionRow from '../../keyboard/help/KeyboardHelpSectionRow.js';
 import TextKeyNode from '../../keyboard/TextKeyNode.js';
+import sceneryPhet from '../../sceneryPhet.js';
 
 export default function demoKeyboardHelpSection( layoutBounds: Bounds2 ): Node {
 
+  const labelWithArrowKeysRowIcon = KeyboardHelpSectionRow.fromHotkeyData( new HotkeyData( {
+    keys: [ 'arrowUp', 'arrowLeft', 'arrowDown', 'arrowRight' ],
+    keyboardHelpDialogLabelStringProperty: new Property( 'Do one thing' ),
+    repoName: sceneryPhet.name
+  } ) );
+
+  const labelWithUpDownArrowKeysRowIcon = KeyboardHelpSectionRow.fromHotkeyData( new HotkeyData( {
+    keys: [ 'arrowUp', 'arrowDown' ],
+    keyboardHelpDialogLabelStringProperty: new Property( 'Do another thing' ),
+    repoName: sceneryPhet.name
+  } ) );
+
+  const labelWithLeftRightArrowKeysRowIcon = KeyboardHelpSectionRow.fromHotkeyData( new HotkeyData( {
+    keys: [ 'arrowLeft', 'arrowRight' ],
+    keyboardHelpDialogLabelStringProperty: new Property( 'Do some other thing' ),
+    repoName: sceneryPhet.name
+  } ) );
+
+  // Unusual example that does not use keys or HotkeyData, but you can create an icon with any content you want.
   const labelWithIcon = KeyboardHelpSectionRow.labelWithIcon( 'Label With Icon:', new TextKeyNode( 'Hi' ), {
     labelInnerContent: 'Label With Icon Hi'
   } );
 
+  // Unusual example that does not use keys or HotkeyData, but you can create a list of icons if you want.
   const labelWithIconList = KeyboardHelpSectionRow.labelWithIconList( 'Label With Icon List:', [
     new TextKeyNode( 'Hi' ),
     new TextKeyNode( 'Hello' ),
@@ -27,27 +49,12 @@ export default function demoKeyboardHelpSection( layoutBounds: Bounds2 ): Node {
     labelInnerContent: 'Label with icon list of hi, hello, Ahoy Manatee.'
   } );
 
-  const labelWithArrowKeysRowIcon = KeyboardHelpSectionRow.labelWithIcon( 'Label with arrows:',
-    KeyboardHelpIconFactory.arrowKeysRowIcon(), {
-      labelInnerContent: 'Label with arrows, up, left, down, right'
-    } );
-
-  const labelWithUpDownArrowKeysRowIcon = KeyboardHelpSectionRow.labelWithIcon( 'Label with up down arrows:',
-    KeyboardHelpIconFactory.upDownArrowKeysRowIcon(), {
-      labelInnerContent: 'Label with up down arrows'
-    } );
-
-  const labelWithLeftRightArrowKeysRowIcon = KeyboardHelpSectionRow.labelWithIcon( 'Label with left right arrows:',
-    KeyboardHelpIconFactory.leftRightArrowKeysRowIcon(), {
-      labelInnerContent: 'Label with left right arrows'
-    } );
-
   const content = [
-    labelWithIcon,
-    labelWithIconList,
     labelWithArrowKeysRowIcon,
     labelWithUpDownArrowKeysRowIcon,
-    labelWithLeftRightArrowKeysRowIcon
+    labelWithLeftRightArrowKeysRowIcon,
+    labelWithIcon,
+    labelWithIconList
   ];
 
   return new KeyboardHelpSection( 'Custom Help Content', content, {
