32c7c81e03cb8b1af7fdc5440cb4f0936e2477ae
phet-dev <phetadmin@gmail.com>
2026-02-12 03:53:27 -0700

    update copyright dates from daily grunt work

--- js/accessibility/grab-drag/GrabDragInteraction.ts
+++ js/accessibility/grab-drag/GrabDragInteraction.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2025, University of Colorado Boulder
+// Copyright 2018-2026, University of Colorado Boulder
 
 /**
  * The main interaction for grabbing and dragging an object through the PDOM and assistive technology. It works by
