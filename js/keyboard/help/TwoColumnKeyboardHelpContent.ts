c2048ae186fea2c61802cf159e7c5929e382d67c
phet-dev <phetadmin@gmail.com>
2026-06-26 03:02:04 -0600

    sync-to-poly: Update copyright dates from daily grunt work
    Mono-SHA: 1566e72f76a8f8d5a260938ec1247b46ca1aed31

--- js/keyboard/help/TwoColumnKeyboardHelpContent.ts
+++ js/keyboard/help/TwoColumnKeyboardHelpContent.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2025, University of Colorado Boulder
+// Copyright 2019-2026, University of Colorado Boulder
 
 /**
  * TwoColumnKeyboardHelpContentOptions handles layout of KeyboardHelpSections in 2 columns.
